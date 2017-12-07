d41fa7d624683bc6c7f7d0101412c477151b3641
Jeff Gulbronson <jgulbronson@squareup.com>
2017-12-07 08:53:29 -0500

    Rename ENV to ENVIRONMENT to keep naming consistent

--- misk/src/main/kotlin/misk/environment/EnvironmentModule.kt
+++ misk/src/main/kotlin/misk/environment/EnvironmentModule.kt
@@ -11,15 +11,18 @@ class EnvironmentModule(val environment: Environment) : AbstractModule() {
     }
 
     companion object {
+        private val environmentKey = "ENVIRONMENT"
+
         @JvmStatic
         fun fromEnvironmentVariable(): EnvironmentModule {
-            val environmentName = System.getenv("ENV")
+            val environmentName = System.getenv(environmentKey)
             val environment = if (environmentName != null) {
-                Environment.valueOf(environmentName.toUpperCase())
+                Environment.valueOf(environmentName)
             } else {
-                logger.warn("No ENV variable found, running in DEVELOPMENT")
+                logger.warn { "No environment variable with key $environmentKey found, running in DEVELOPMENT" }
                 Environment.DEVELOPMENT
             }
+            logger.info { "Running with environment ${environment.name}" }
             return EnvironmentModule(environment)
         }
     }
