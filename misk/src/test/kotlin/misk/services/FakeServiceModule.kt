e300dbddae87dff81d443c0c95cb4fc9e08dbd07
Jeff Gulbronson <jgulbronson@squareup.com>
2017-12-08 13:31:03 -0500

    Rename newMultibinder extension function to make sense

--- misk/src/test/kotlin/misk/services/FakeServiceModule.kt
+++ misk/src/test/kotlin/misk/services/FakeServiceModule.kt
@@ -2,12 +2,11 @@ package misk.services
 
 import com.google.common.util.concurrent.Service
 import com.google.inject.AbstractModule
-import misk.inject.asSingleton
-import misk.inject.newMultibinder
+import misk.inject.addMultibinderBinding
 import misk.inject.to
 
 class FakeServiceModule : AbstractModule() {
   override fun configure() {
-    binder().newMultibinder<Service>().to<FakeService>()
+    binder().addMultibinderBinding<Service>().to<FakeService>()
   }
 }
