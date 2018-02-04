5eb3f3f49cd4f253a401bc2dffe638b9ba14f3a6
Matthew Mihic <mmihic@squareup.com>
2018-02-04 09:27:55 -0500

    Expose Guice extensions

--- misk/src/main/kotlin/misk/inject/Guice.kt
+++ misk/src/main/kotlin/misk/inject/Guice.kt
@@ -18,58 +18,64 @@ import kotlin.reflect.KClass
 import kotlin.reflect.KType
 import kotlin.reflect.jvm.javaType
 
-internal inline fun <reified T : Any> LinkedBindingBuilder<in T>.to() = to(T::class.java)
-
-internal inline fun <reified T : Any, reified A : Annotation> Binder.addMultibinderBindingWithAnnotation() =
-        addMultibinderBinding<T>(A::class.java)
+inline fun <reified T : Any> LinkedBindingBuilder<in T>.to() = to(T::class.java)
 
 @Suppress("UNCHECKED_CAST")
-internal inline fun <reified T : Any> Binder.addMultibinderBinding(
+inline fun <reified T : Any> Binder.newMultibinder(
         annotation: Class<out Annotation>? = null
-): LinkedBindingBuilder<T> {
+): Multibinder<T> {
     val typeLiteral =
             parameterizedType<List<*>>(subtypeOf<T>()).typeLiteral() as TypeLiteral<List<T>>
     bind(typeLiteral).toProvider(parameterizedType<ListProvider<*>>(
             T::class.java).typeLiteral() as TypeLiteral<Provider<List<T>>>)
 
     return when (annotation) {
-        null -> Multibinder.newSetBinder(this, T::class.java).addBinding()
-        else -> Multibinder.newSetBinder(this, T::class.java, annotation).addBinding()
+        null -> Multibinder.newSetBinder(this, T::class.java)
+        else -> Multibinder.newSetBinder(this, T::class.java, annotation)
     }
 }
+
+inline fun <reified T : Any, reified A : Annotation> Binder.addMultibinderBindingWithAnnotation() =
+        addMultibinderBinding<T>(A::class.java)
+
+@Suppress("UNCHECKED_CAST")
+inline fun <reified T : Any> Binder.addMultibinderBinding(
+        annotation: Class<out Annotation>? = null
+): LinkedBindingBuilder<T> = newMultibinder<T>(annotation).addBinding()
 
 fun ScopedBindingBuilder.asSingleton() {
     `in`(Singleton::class.java)
 }
 
-internal class ListProvider<T> : Provider<List<T>> {
-    @Inject lateinit var set: MutableSet<T>
+class ListProvider<T> : Provider<List<T>> {
+    @Inject
+    lateinit var set: MutableSet<T>
 
     override fun get(): List<T> = ArrayList(set)
 }
 
-internal inline fun <reified T : Any> subtypeOf(): WildcardType {
+inline fun <reified T : Any> subtypeOf(): WildcardType {
     return Types.subtypeOf(T::class.java)
 }
 
-internal inline fun <reified T : Any> parameterizedType(vararg typeParameters: Type)
+inline fun <reified T : Any> parameterizedType(vararg typeParameters: Type)
         : ParameterizedType {
     return Types.newParameterizedType(T::class.java, *typeParameters)
 }
 
-internal fun ParameterizedType.typeLiteral() = TypeLiteral.get(this)
+fun ParameterizedType.typeLiteral() = TypeLiteral.get(this)
 
-internal fun KType.typeLiteral(): TypeLiteral<*> {
+fun KType.typeLiteral(): TypeLiteral<*> {
     return TypeLiteral.get(javaType)
 }
 
-internal inline fun <reified T : Any> Injector.getInstance(): T {
+inline fun <reified T : Any> Injector.getInstance(): T {
     return getInstance(T::class.java)
 }
 
-internal inline fun <reified T : Any> keyOf(): Key<T> = Key.get(T::class.java)
-internal inline fun <reified T : Any> keyOf(a: Annotation): Key<T> = Key.get(T::class.java, a)
-internal inline fun <reified T : Any, A : Annotation> keyOf(a: KClass<A>): Key<T> =
+inline fun <reified T : Any> keyOf(): Key<T> = Key.get(T::class.java)
+inline fun <reified T : Any> keyOf(a: Annotation): Key<T> = Key.get(T::class.java, a)
+inline fun <reified T : Any, A : Annotation> keyOf(a: KClass<A>): Key<T> =
         Key.get(T::class.java, a.java)
 
 fun uninject(target: Any) {
