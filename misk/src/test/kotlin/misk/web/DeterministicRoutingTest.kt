7d53518d4ff95a4f43a9b0985438c5af955534e4
Matthew Mihic <mmihic@squareup.com>
2018-02-05 08:45:31 -0500

    Rename ActionTest to MiskTest
    Most tests using this annotation are testing things other than actions

--- misk/src/test/kotlin/misk/web/DeterministicRoutingTest.kt
+++ misk/src/test/kotlin/misk/web/DeterministicRoutingTest.kt
@@ -3,8 +3,8 @@ package misk.web
 import com.google.inject.util.Modules
 import misk.MiskModule
 import misk.inject.KAbstractModule
-import misk.testing.ActionTest
-import misk.testing.ActionTestModule
+import misk.testing.MiskTest
+import misk.testing.MiskTestModule
 import misk.testing.TestWebModule
 import misk.web.actions.WebAction
 import misk.web.jetty.JettyService
@@ -16,9 +16,9 @@ import org.junit.jupiter.api.Test
 import java.util.Collections.shuffle
 import javax.inject.Inject
 
-@ActionTest(startService = true)
+@MiskTest(startService = true)
 internal class DeterministicRoutingTest {
-    @ActionTestModule
+    @MiskTestModule
     val module = Modules.combine(
             MiskModule(),
             WebModule(),
