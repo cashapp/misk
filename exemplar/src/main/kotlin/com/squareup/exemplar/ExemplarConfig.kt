bd06fd357036fe7f1f305751f252a9f70ade3ebd
Matthew Mihic <mmihic@squareup.com>
2018-02-05 21:31:47 -0500

    update exemplar config

--- exemplar/src/main/kotlin/com/squareup/exemplar/ExemplarConfig.kt
+++ exemplar/src/main/kotlin/com/squareup/exemplar/ExemplarConfig.kt
@@ -3,4 +3,4 @@ package com.squareup.exemplar
 import misk.config.Config
 import misk.web.WebConfig
 
-data class ExemplarConfig(val web_config: WebConfig) : Config
+data class ExemplarConfig(val web: WebConfig) : Config
