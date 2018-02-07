b3a9e6b233a738db5d30813f8a47d552098e8a66
Matthew Mihic <mmihic@squareup.com>
2018-02-07 12:21:15 -0500

    Google Cloud Datastore and Cloud Storage support
    Adds modules for configuration access to Google Cloud Datastore and
    Google Cloud Storage, as well as fakes / emulation support for writing
    tests against those services.
    
    Datastore testing is done against the local datastore emulator running
    in memory-only mode. This ensures tests will experience as close as
    possible real datastore semantics. Machines running these tests will
    need to have the emulator installed (see `gcloud beta emulators`) but
    the emulator itself will be automatically started and terminated by the
    tests.
    
    Cloud storage does not have an emulator, so tests will instead run
    against a fake implementation of the storage RPC layer which keeps
    everything in memory. As the cloud storage API and semantics are
    substantially simpler than datastore, this is a reasonable compromise.

---
+++ misk/testing/src/main/kotlin/misk/cloud/gcp/datastore/FakeDatastoreModule.kt
@@ -0,0 +1,52 @@
+package misk.cloud.gcp.datastore
+
+import com.google.cloud.datastore.Datastore
+import com.google.cloud.datastore.testing.LocalDatastoreHelper
+import com.google.common.util.concurrent.AbstractIdleService
+import com.google.common.util.concurrent.Service
+import com.google.inject.Provides
+import com.google.inject.Singleton
+import misk.inject.KAbstractModule
+import misk.inject.addMultibinderBinding
+import misk.inject.to
+import javax.inject.Inject
+
+/** Installs a version of the [Datastore] that works off an in-memory local store */
+class FakeDatastoreModule : KAbstractModule() {
+    override fun configure() {
+        binder().addMultibinderBinding<Service>().to<FakeDatastoreService>()
+    }
+
+    @Provides
+    @Singleton
+    fun provideDatastoreHelper(): LocalDatastoreHelper = datastoreHelper.value
+
+    @Provides
+    @Singleton
+    fun provideDatastore(datastoreHelper: LocalDatastoreHelper): Datastore =
+            datastoreHelper.options.service
+
+    @Singleton
+    class FakeDatastoreService @Inject constructor(
+            private val datastoreHelper: LocalDatastoreHelper
+    ) : AbstractIdleService() {
+
+        override fun startUp() {
+            // Reset on every restart / test run
+            datastoreHelper.reset()
+        }
+
+        override fun shutDown() {}
+    }
+
+    // NB(mmihic): We use a VM-wide singleton for the datastore because starting the datastore
+    // service is expensive, and we don't want to do it for every test.
+    companion object {
+        private val datastoreHelper = lazy {
+            // Start as soon as we create it
+            val helper = LocalDatastoreHelper.create(1.0)
+            helper.start()
+            helper
+        }
+    }
+}
