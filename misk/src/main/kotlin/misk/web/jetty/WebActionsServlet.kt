fbba4b25dbaf435bdd2941f5ce4061567603b254
Tristan Ohlson <tohlson@squareup.com>
2018-02-07 15:31:33 -0500

    Expand upon routing rules for MediaRanges
    Prior to this change if we had two routes with the same path
    the one returned was arbitrary, even if one was the clear choice
    in regards to content type. We now attempt to choose the route which
    matches the desired content type.
    
    We also attempt to route based, in some cases, on whether or not
    the specified charset matches our route.
    
    Along with this comes two small fixes:
      - Allow multiple media ranges to be specified in the accept header
      - Parse the URL only once per request

--- misk/src/main/kotlin/misk/web/jetty/WebActionsServlet.kt
+++ misk/src/main/kotlin/misk/web/jetty/WebActionsServlet.kt
@@ -6,7 +6,10 @@ import misk.scope.ActionScope
 import misk.web.BoundAction
 import misk.web.Request
 import misk.web.actions.WebAction
-import misk.web.asRequest
+import okhttp3.Headers
+import okhttp3.HttpUrl
+import okio.Okio
+import org.eclipse.jetty.http.HttpMethod
 import javax.inject.Inject
 import javax.inject.Singleton
 import javax.servlet.http.HttpServlet
@@ -29,17 +32,37 @@ internal class WebActionsServlet @Inject constructor(
   }
 
   private fun handleCall(request: HttpServletRequest, response: HttpServletResponse) {
+    val asRequest = request.asRequest()
     val seedData = mapOf(
         keyOf<HttpServletRequest>() to request,
-        keyOf<Request>() to request.asRequest())
+        keyOf<Request>() to asRequest)
 
     scope.enter(seedData).use {
-      val candidateActions = boundActions.mapNotNull { it.match(request) }
+      val candidateActions = boundActions.mapNotNull { it.match(request, asRequest.url) }
       val bestAction = candidateActions.sorted().firstOrNull()
-      if (bestAction != null) {
-        bestAction.handle(request, response)
-        logger.debug("Request handled by WebActionServlet")
-      }
+      bestAction?.handle(asRequest, response)
+      logger.debug {"Request handled by WebActionServlet" }
     }
   }
+}
+
+internal fun HttpServletRequest.asRequest(): Request {
+  val urlString = requestURL.toString() +
+      if (queryString != null) "?" + queryString
+      else ""
+
+  val headersBuilder = Headers.Builder()
+  val headerNames = headerNames
+  for (headerName in headerNames) {
+    val headerValues = getHeaders(headerName)
+    for (headerValue in headerValues) {
+      headersBuilder.add(headerName, headerValue)
+    }
+  }
+  return Request(
+      HttpUrl.parse(urlString)!!,
+      HttpMethod.valueOf(method),
+      headersBuilder.build(),
+      Okio.buffer(Okio.source(inputStream))
+  )
 }
