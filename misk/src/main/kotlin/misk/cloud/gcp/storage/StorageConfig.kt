ae29fc7f7777401c9a36e9e0bdc267008ad2747a
Matthew Mihic <mmihic@squareup.com>
2018-02-08 18:17:38 -0500

    Disk backed storage service for local dev
    Adds a storage service implementation backed by local disk, suitable for
    use in development mode where you want data preserved across service
    restarts. Works around the lack of a GCS emulator in the gcloud tools.
    Supports multiple services pointing to the same local storage directory,
    effectively emulating sharing GCS buckets across services.
    
    Also adds more tests for the storage interfaces.

--- misk/src/main/kotlin/misk/cloud/gcp/storage/StorageConfig.kt
+++ misk/src/main/kotlin/misk/cloud/gcp/storage/StorageConfig.kt
@@ -5,5 +5,10 @@ import misk.config.Config
 
 /** Configuration for talking to Google Cloud Storage */
 data class StorageConfig(
+    val use_local_storage: Boolean = false,
+    val local_storage: LocalStorageConfig? = null,
     val transport : TransportConfig = TransportConfig()
 ) : Config
+
+/** Configuration for local (emulated) storage */
+data class LocalStorageConfig(val data_dir: String)
