cdb284d3d3e80a35e16c2155aa5bdae9341fdbbb
Nate Broyles <nb@squareup.com>
2018-02-15 16:58:59 -0500

    Add metrics backend for SignalFx

---
+++ misk/src/main/kotlin/misk/metrics/backends/signalfx/SignalFxBackendModule.kt
@@ -0,0 +1,32 @@
+package misk.metrics.backends.signalfx
+
+import com.codahale.metrics.MetricRegistry
+import com.google.common.util.concurrent.Service
+import com.google.inject.Provides
+import com.signalfx.codahale.reporter.SignalFxReporter
+import com.signalfx.metrics.auth.StaticAuthToken
+import misk.config.AppName
+import misk.inject.KAbstractModule
+import misk.inject.addMultibinderBinding
+import misk.inject.to
+import javax.inject.Singleton
+
+class SignalFxBackendModule : KAbstractModule(){
+    override fun configure() {
+        binder().addMultibinderBinding<Service>().to<SignalFxReporterService>()
+    }
+
+    @Provides
+    @Singleton
+    fun signalFxReporter(
+            @AppName appName: String,
+            config: SignalFxBackendConfig,
+            metricRegistry: MetricRegistry
+    ): SignalFxReporter {
+        return SignalFxReporter.Builder(
+                metricRegistry,
+                StaticAuthToken(config.access_token),
+                appName
+        ).build()
+    }
+}
--- misk/src/test/kotlin/misk/config/TestConfig.kt
+++ misk/src/test/kotlin/misk/config/TestConfig.kt
@@ -1,11 +1,14 @@
 package misk.config
 
 import misk.web.WebConfig
+import java.time.Duration
 
 data class TestConfig(
     val web: WebConfig,
     val consumer_a: ConsumerConfig,
-    val consumer_b: ConsumerConfig
+    val consumer_b: ConsumerConfig,
+    val duration: DurationConfig
 ) : Config
 
 data class ConsumerConfig(val min_items: Int = 0, val max_items: Int) : Config
+data class DurationConfig(val interval: Duration) : Config
