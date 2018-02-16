16f94dac848a0d492861763f41e605ba36df5e23
Ryan Hall <rhall@squareup.com>
2018-02-16 15:37:57 -0800

    Allow binding an ExceptionMapper by an Exception type
    This adds deterministic ExceptionMapper resolution, instead of relying
    on the binding order of ExceptionMappers.
    
    If an Exception is thrown without an explicit bound ExceptionMapper, the
    superclasses of the Exception are explored for possible bound
    ExceptionMappers. The closest bound superclass wins.
    
    e.g If an application adds an explicit binding for NotFoundException,
    that explicit Mapper is preferred over the base ActionExceptionMapper.
    However, if no binding for BadRequestException exists, the base
    ActionExceptionMapper is used instead.
    
    Since an Exception always resolves to the same ExceptionMapper for the
    life of a VM, it is safe to cache the resolution result.

--- misk/src/main/kotlin/misk/web/exceptions/ExceptionMapperModule.kt
+++ misk/src/main/kotlin/misk/web/exceptions/ExceptionMapperModule.kt
@@ -1,20 +1,46 @@
 package misk.web.exceptions
 
 import com.google.inject.TypeLiteral
-import com.google.inject.multibindings.Multibinder
+import com.google.inject.multibindings.MapBinder
 import misk.inject.KAbstractModule
 import kotlin.reflect.KClass
 
-class ExceptionMapperModule<T : ExceptionMapper<*>>(
-        private val kclass: KClass<T>
+/**
+ * Binds a [Throwable] to an [ExceptionMapper].
+ *
+ * When an Exception occurs dispatching an Action, the bound ExceptionMapper is called to handle
+ * the Exception. If there is not an explicit binding for the thrown Exception, the mapper for the
+ * closest superclass is used.
+ *
+ * Given the example code
+ *
+ * ```
+ * install(ExceptionMapperModule.create<ActionException, ActionExceptionMapper>())
+ * install(ExceptionMapperModule.create<MyActionException, MyActionExceptionMapper>())
+ *
+ * class MyActionException : ActionException {}
+ * class MyOtherActionException : ActionException {}
+ * ```
+ *
+ * MyActionException maps to the specific MyActionExceptionMapper and MyOtherActionException
+ * maps to the ActionExceptionMapper since uses the binding of the closest bound superclass.
+ */
+class ExceptionMapperModule<M : ExceptionMapper<T>, in T : Throwable>(
+    private val exceptionClass: KClass<T>,
+    private val mapperClass: KClass<M>
 ) : KAbstractModule() {
     override fun configure() {
-        Multibinder.newSetBinder(binder(), exceptionMapperTypeLiteral).addBinding().to(kclass.java)
+      MapBinder.newMapBinder(binder(), exceptionTypeLiteral, exceptionMapperTypeLiteral)
+          .addBinding(exceptionClass)
+          .to(mapperClass.java)
     }
 
     companion object {
-        inline fun <reified T : ExceptionMapper<*>> create() = ExceptionMapperModule(T::class)
+      inline fun <reified T : Throwable, reified M : ExceptionMapper<T>> create() = ExceptionMapperModule(
+          T::class, M::class
+      )
 
-        private val exceptionMapperTypeLiteral = object : TypeLiteral<ExceptionMapper<*>>() {}
+      private val exceptionMapperTypeLiteral = object : TypeLiteral<ExceptionMapper<*>>() {}
+      private val exceptionTypeLiteral = object : TypeLiteral<KClass<*>>() {}
     }
 }
