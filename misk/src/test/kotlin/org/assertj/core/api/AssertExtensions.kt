058d0a25af11b008faa4c6da0830b56aa93efe1c
Matthew Mihic <mmihic@squareup.com>
2018-02-17 15:02:23 -0500

    Fix wildcard based content routing
    Fixed content based routing when the requests or actions have wildcards
    in their accepted or emitted media ranges. The previous logic had
    several issues:
    
    1. We weren't considering the action's content type when ordering, so if
    there were an action accepting text/* and another accepting text/plain,
    and a request arrived with content-type text/plain, we'd randomly select
    between those two actions. We now deterministically prefer the action
    with the most specific matching content type (thus a request with text/plain
    would match the action accepting text/plain, and a request text/yaml
    would match the action accepting text/*)
    
    2. We were ordering actions based on the Accepts header supplied by the
    client rather than the media type defined on the action, so if there
    were an action that could emit text/plain and another that could emit
    */* and a request arrived with Accepts: text/*, we'd randonly select
    between those two actions. We now deteministically prefer the action
    with the most specific matching response content type (thus a request
    accepting text/* would prefer the action emitteing text/plain over the
    action emitting */*).
    
    As part of this, also cleaned up toString() methods on the various
    matchers to make debugging easier.

--- misk/src/test/kotlin/org/assertj/core/api/AssertExtensions.kt
+++ misk/src/test/kotlin/org/assertj/core/api/AssertExtensions.kt
@@ -1,5 +1,6 @@
 package org.assertj.core.api
 
+import org.assertj.core.api.Assertions.assertThat
 import org.assertj.core.data.MapEntry
 
 fun <KEY, VALUE> MapAssert<KEY, VALUE>.containsExactly(
@@ -19,4 +20,38 @@ fun <ACTUAL : CharSequence> AbstractCharSequenceAssert<*, ACTUAL>.isEqualToAsJso
     return this
 }
 
+class OrderingAssert<in A : Comparable<A>>(private val actual: A) {
+    infix fun comesBefore(other: A) {
+        assertThat(actual).isLessThan(other)
+        assertThat(other).isGreaterThan(actual)
+    }
 
+    infix fun comesAfter(other: A) {
+        assertThat(actual).isGreaterThan(other)
+        assertThat(other).isLessThan(actual)
+    }
+
+    infix fun eq(other: A) {
+        assertThat(actual).isEqualByComparingTo(other)
+    }
+}
+
+fun <A : Comparable<A>> assertOrdering(vararg values: A) {
+    values.forEachIndexed { index, value ->
+        assertThat(value).isEqualByComparingTo(value)
+
+        val before = if (index == 0) listOf() else values.take(index - 1)
+        before.forEach {
+            assertThat(value).isGreaterThan(it)
+            assertThat(it).isLessThan(value)
+        }
+
+        val after = if (index == values.size - 1) listOf() else values.drop(index + 1)
+        after.forEach {
+            assertThat(value).isLessThan(it)
+            assertThat(it).isGreaterThan(value)
+        }
+    }
+}
+
+fun <A : Comparable<A>> assertOrderOf(value: A) = OrderingAssert(value)
