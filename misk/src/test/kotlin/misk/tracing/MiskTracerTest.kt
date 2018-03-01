5be262883f1ae8dda4ad4466360c76249bb5cd36
Nate Broyles <nb@squareup.com>
2018-03-01 14:16:33 -0500

    Tag errors in ad-hoc tracer

--- misk/src/test/kotlin/misk/tracing/MiskTracerTest.kt
+++ misk/src/test/kotlin/misk/tracing/MiskTracerTest.kt
@@ -2,10 +2,16 @@ package misk.tracing
 
 import io.opentracing.Tracer
 import io.opentracing.mock.MockTracer
+import io.opentracing.tag.Tags
+import misk.exceptions.ActionException
+import misk.exceptions.BadRequestException
+import misk.exceptions.StatusCode
 import misk.testing.MiskTest
 import misk.testing.MiskTestModule
 import misk.testing.MockTracingBackendModule
 import org.assertj.core.api.Assertions.assertThat
+import org.assertj.core.api.Assertions.assertThatThrownBy
+import org.junit.jupiter.api.Assertions.assertThrows
 import org.junit.jupiter.api.Test
 import javax.inject.Inject
 
@@ -26,5 +32,19 @@ class MiskTracerTest {
     assertThat(mockTracer.finishedSpans().size).isEqualTo(1)
   }
 
+  @Test
+  fun tagTracingFailures() {
+    val mockTracer = tracer as MockTracer
+
+    assertThat(mockTracer.finishedSpans().size).isEqualTo(0)
+    assertThrows(ActionException::class.java, { miskTracer.trace("failedTrace", ::failedTrace) })
+    assertThat(mockTracer.finishedSpans().size).isEqualTo(1)
+    assertThat(mockTracer.finishedSpans().get(0).tags().get(Tags.ERROR.key)).isEqualTo(true)
+  }
+
   fun traceMe(){}
+
+  fun failedTrace() {
+    throw ActionException(StatusCode.BAD_REQUEST, "sadness")
+  }
 }
