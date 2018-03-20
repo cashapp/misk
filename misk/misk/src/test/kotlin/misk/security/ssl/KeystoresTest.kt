7c3d33610b827c014e040273236b1f9d94c9a09f
Aaron Young <young@squareup.com>
2018-03-20 18:36:30 -0400

    Support loading keystores from combined private key and certificate chain PEM files

--- misk/misk/src/test/kotlin/misk/security/ssl/KeystoresTest.kt
+++ misk/misk/src/test/kotlin/misk/security/ssl/KeystoresTest.kt
@@ -1,6 +1,5 @@
 package misk.security.ssl
 
-import misk.resources.ResourceLoader
 import misk.security.ssl.Keystores.TYPE_JCEKS
 import misk.security.ssl.Keystores.TYPE_PEM
 import org.assertj.core.api.Assertions.assertThat
@@ -8,10 +7,25 @@ import org.junit.jupiter.api.Test
 import java.security.KeyStore
 
 internal class KeystoresTest {
+  val clientComboPemPath = "src/test/resources/ssl/client_cert_key_combo.pem"
+  val clientTrustPemPath = "src/test/resources/ssl/client_cert.pem"
+  val serverKeystoreJceksPath = "src/test/resources/ssl/server_keystore.jceks"
+
   @Test
-  fun loadFromPEM() {
-    val input = ResourceLoader.open("ssl/client_cert.pem")!!.inputStream()
-    val keystore = Keystores.load(input, TYPE_PEM, null)
+  fun loadKeystoreFromPEM() {
+    val keystore = Keystores.loadCertStore(clientComboPemPath, TYPE_PEM, "password")!!.keyStore
+    assertThat(keystore.aliasesOfType<KeyStore.PrivateKeyEntry>()).containsExactly("key")
+    assertThat(keystore.getPrivateKey("password".toCharArray())).isNotNull()
+
+    assertThat((keystore.getX509Certificate()).issuerX500Principal.name)
+        .isEqualTo("CN=misk-client,OU=Client,O=Misk,L=San Francisco,ST=CA,C=US")
+    assertThat(keystore.getX509Certificate().subjectAlternativeNames.toList()[0][1])
+        .isEqualTo("127.0.0.1")
+  }
+
+  @Test
+  fun loadTrustFromPEM() {
+    val keystore = Keystores.loadTrustStore(clientTrustPemPath, TYPE_PEM, "serverpassword")!!.keyStore
     assertThat(keystore.aliases().toList()).containsExactly("0")
     assertThat((keystore.getX509Certificate()).issuerX500Principal.name)
         .isEqualTo("CN=misk-client,OU=Client,O=Misk,L=San Francisco,ST=CA,C=US")
@@ -21,8 +35,7 @@ internal class KeystoresTest {
 
   @Test
   fun loadFromJCEKS() {
-    val input = ResourceLoader.open("ssl/server_keystore.jceks")!!.inputStream()
-    val keystore = Keystores.load(input, TYPE_JCEKS, "serverpassword")
+    val keystore = Keystores.loadTrustStore(serverKeystoreJceksPath, TYPE_JCEKS, "serverpassword")!!.keyStore
     assertThat(keystore.aliasesOfType<KeyStore.PrivateKeyEntry>()).containsExactly("1")
     assertThat(keystore.getPrivateKey("serverpassword".toCharArray())).isNotNull()
 
