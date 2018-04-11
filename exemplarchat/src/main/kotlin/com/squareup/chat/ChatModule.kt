fd873a2bb61f80b3a73e00b55cc4f36069ffeecb
Tristan Ohlson <tohlson@squareup.com>
2018-04-11 16:57:05 -0400

    Create a cluster wide admin healthcheck page

--- exemplarchat/src/main/kotlin/com/squareup/chat/ChatModule.kt
+++ exemplarchat/src/main/kotlin/com/squareup/chat/ChatModule.kt
@@ -1,9 +1,13 @@
 package com.squareup.chat
 
-import com.squareup.chat.actions.ChatPage
+import com.squareup.chat.actions.ChatPageAction
 import com.squareup.chat.actions.ChatWebSocketAction
+import com.squareup.chat.actions.ToggleManualHealthCheckAction
+import com.squareup.chat.healthchecks.ManualHealthCheck
 import misk.NetworkInterceptor
 import misk.eventrouter.RealEventRouterModule
+import misk.healthchecks.ClusterWideHealthModule
+import misk.healthchecks.HealthCheck
 import misk.inject.KAbstractModule
 import misk.inject.addMultibinderBinding
 import misk.inject.to
@@ -14,10 +18,13 @@ import misk.web.actions.DefaultActionsModule
 
 class ChatModule : KAbstractModule() {
   override fun configure() {
-    install(WebActionModule.create<ChatPage>())
+    install(WebActionModule.create<ChatPageAction>())
     install(WebActionModule.create<ChatWebSocketAction>())
+    install(WebActionModule.create<ToggleManualHealthCheckAction>())
     install(DefaultActionsModule())
     install(RealEventRouterModule())
+    install(ClusterWideHealthModule())
+    binder().addMultibinderBinding<HealthCheck>().to<ManualHealthCheck>()
     binder().addMultibinderBinding<StaticResourceMapper.Entry>()
         .toInstance(StaticResourceMapper.Entry("/", "web/exemplarchat", "exemplarchat/web/build"))
   }
