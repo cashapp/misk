35c756dad9b774e8f689b1caac8dc27e89c12c78
Joey Muia <jmuia@squareup.com>
2018-05-24 14:57:23 -0400

    Add a JPAEntityModule for binding entities for a DataSource
    JPA entities can be bound for a DataSource with the JPAEntityModule.
    Multiple modules can contribute entities for a particular DataSource
    using its qualifying annotation (or lack thereof).
    
    Higher-level abstractions such as Hibernate can use the entities bound
    for a particular DataSource as part of its relational mapping.
    
    The annotation `@JvmName("-createKClass")` prevents declaration clashes
    on the JVM due to type erasure. Declarations such as `fun create(entities: Set<KClass<*>>)`
    and `fun create(entities: Set<Class<*>>)` will have the same signature.
    
    The "-" prefix in the above annotation also "hides" the method from Java developers
    as Java does not allow the hyphen even though the JVM does. Discussion here:
    https://discuss.kotlinlang.org/t/more-characters-allowed-for-identifiers-than-grammar-specifies-what-is-supported/2359/2

---
+++ misk/src/test/kotlin/misk/jdbc/JPAEntityModuleTest.kt
@@ -0,0 +1,51 @@
+package misk.jdbc
+
+import com.google.inject.Guice
+import misk.inject.getSetOf
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.Test
+import javax.inject.Qualifier
+import kotlin.reflect.KClass
+
+internal class JPAEntityModuleTest {
+
+  @Test fun multipleDataSources() {
+    val dinosaurs = setOf(Triceratops::class, Stegosaurus::class)
+    val shapes = setOf(Square::class, Circle::class)
+
+    val injector = Guice.createInjector(
+      JPAEntityModule.create(dinosaurs),
+      JPAEntityModule.create(annotatedBy = Shapes::class, entities = shapes)
+    )
+
+    assertEntities(injector.getSetOf(JPAEntity::class), dinosaurs)
+    assertEntities(injector.getSetOf(JPAEntity::class, Shapes::class), shapes)
+  }
+
+  @Test fun multipleModulesSameDataSource() {
+    val injector = Guice.createInjector(
+      JPAEntityModule.create(setOf(Triceratops::class)),
+      JPAEntityModule.create(setOf(Stegosaurus::class)),
+      JPAEntityModule.create(annotatedBy = Shapes::class, entities = setOf(Square::class)),
+      JPAEntityModule.create(annotatedBy = Shapes::class, entities = setOf(Circle::class))
+    )
+
+    val dinosaurs = setOf(Triceratops::class, Stegosaurus::class)
+    val shapes = setOf(Square::class, Circle::class)
+
+    assertEntities(injector.getSetOf(JPAEntity::class), dinosaurs)
+    assertEntities(injector.getSetOf(JPAEntity::class, Shapes::class), shapes)
+  }
+
+  fun assertEntities(actual: Set<JPAEntity>, expected: Set<KClass<*>>) =
+    assertThat(actual.map { it.entity }).containsExactlyElementsOf(expected.map { it.java })
+}
+
+@Qualifier
+annotation class Shapes
+
+class Square
+class Circle
+
+class Triceratops
+class Stegosaurus
