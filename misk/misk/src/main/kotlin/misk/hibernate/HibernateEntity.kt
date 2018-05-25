2c9d5f3c2561102c875f6688a63500851789373a
Jesse Wilson <jwilson@squareup.com>
2018-05-25 14:19:09 -0400

    Use JPA entity types in HibernateModule.
    This renames JPAEntityModule to HibernateEntityModule.

---
+++ misk/misk/src/main/kotlin/misk/hibernate/HibernateEntity.kt
@@ -0,0 +1,9 @@
+package misk.hibernate
+
+import kotlin.reflect.KClass
+
+/**
+ * [HibernateEntity] is a wrapper class that allows for unqualified binding of JPA entity classes without
+ * collision.
+ */
+internal data class HibernateEntity(val entity: KClass<*>)
