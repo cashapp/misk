81c691274ed807f9477e9882ebc17977c310240a
Sye van der Veen <svanderveen@squareup.com>
2018-06-01 15:52:30 -0400

    Log the reasons why liveness/readiness checks fail

--- misk/src/main/kotlin/misk/web/actions/LivenessCheckAction.kt
+++ misk/src/main/kotlin/misk/web/actions/LivenessCheckAction.kt
@@ -2,12 +2,16 @@ package misk.web.actions
 
 import com.google.common.util.concurrent.Service
 import com.google.common.util.concurrent.Service.State
+import misk.logging.getLogger
 import misk.web.Get
 import misk.web.Response
 import misk.web.ResponseContentType
 import misk.web.mediatype.MediaTypes
 import javax.inject.Inject
 import javax.inject.Singleton
+
+private val logger = getLogger<LivenessCheckAction>()
+private val failedServiceStates = listOf(State.FAILED, State.TERMINATED)
 
 @Singleton
 class LivenessCheckAction @Inject internal constructor(
@@ -17,11 +21,17 @@ class LivenessCheckAction @Inject internal constructor(
   @Get("/_liveness")
   @ResponseContentType(MediaTypes.TEXT_PLAIN_UTF8)
   fun livenessCheck(): Response<String> {
-    val isAlive = services.all {
-      it.state() != State.FAILED && it.state() != State.TERMINATED
+    val failedServices = services.filter {
+      failedServiceStates.contains(it.state())
     }
 
-    // TODO(jgulbronson) - Should return an empty body
-    return Response("", statusCode = if (isAlive) 200 else 503)
+    if (failedServices.isEmpty()) {
+      return Response("", statusCode = 200)
+    }
+
+    for (service in failedServices) {
+      logger.info("Service failed: $service")
+    }
+    return Response("", statusCode = 503)
   }
 }
--- misk/src/main/kotlin/misk/web/actions/ReadinessCheckAction.kt
+++ misk/src/main/kotlin/misk/web/actions/ReadinessCheckAction.kt
@@ -2,12 +2,15 @@ package misk.web.actions
 
 import com.google.common.util.concurrent.Service
 import misk.healthchecks.HealthCheck
+import misk.logging.getLogger
 import misk.web.Get
 import misk.web.Response
 import misk.web.ResponseContentType
 import misk.web.mediatype.MediaTypes
 import javax.inject.Inject
 import javax.inject.Singleton
+
+private val logger = getLogger<ReadinessCheckAction>()
 
 @Singleton
 class ReadinessCheckAction @Inject internal constructor(
@@ -18,13 +21,21 @@ class ReadinessCheckAction @Inject internal constructor(
   @Get("/_readiness")
   @ResponseContentType(MediaTypes.APPLICATION_JSON)
   fun readinessCheck(): Response<String> {
-    val isReady = services.all { it.isRunning }
-        && healthChecks.all { it.status().isHealthy }
+    val servicesNotRunning = services.filter { !it.isRunning }
+    val failedHealthChecks = healthChecks
+        .map { it.status() }
+        .filter { !it.isHealthy }
 
-    // TODO(jgulbronson) - Should return an empty body
-    return Response(
-        "",
-        statusCode = if (isReady) 200 else 503
-    )
+    if (servicesNotRunning.isEmpty() && failedHealthChecks.isEmpty()) {
+      return Response("", statusCode = 200)
+    }
+
+    for (service in servicesNotRunning) {
+      logger.info("Service not running: $service")
+    }
+    for (healthCheck in failedHealthChecks) {
+      logger.info("Failed health check: ${healthCheck.messages}")
+    }
+    return Response("", statusCode = 503)
   }
 }
