dd8afd799cc735875a4f20c32b316e50c8fedc68
Jesse Wilson <jwilson@squareup.com>
2018-06-14 01:31:08 -0400

    LogCollector for testing
    This requires Logback as the SLF4J backend. That's what we're using
    elsewhere (ie. CloudwatchLogAppender) and having multiple is bad news.

---
+++ misk/testing/src/test/kotlin/misk/logging/LogCollectorTest.kt
@@ -0,0 +1,74 @@
+package misk.logging
+
+import ch.qos.logback.classic.Level
+import com.google.inject.util.Modules
+import misk.MiskModule
+import misk.testing.MiskTest
+import misk.testing.MiskTestModule
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+import javax.inject.Inject
+
+@MiskTest(startService = true)
+class LogCollectorTest {
+  @MiskTestModule
+  val module = Modules.combine(
+      MiskModule(),
+      LogCollectorModule()
+  )
+
+  @Inject lateinit var logCollector: LogCollector
+
+  @Test
+  fun happyPath() {
+    val logger = getLogger<LogCollectorTest>()
+
+    logger.info("this is a log message!")
+    assertThat(logCollector.takeMessages()).containsExactly("this is a log message!")
+
+    logger.info("another log message")
+    logger.info("and a third!")
+    assertThat(logCollector.takeMessages()).containsExactly("another log message", "and a third!")
+  }
+
+  @Test
+  fun filterByLevel() {
+    val logger = getLogger<LogCollectorTest>()
+
+    logger.debug("this is DEBUG.")
+    logger.info("this is INFO.")
+    logger.warn("this is WARN!")
+    assertThat(logCollector.takeMessages(minLevel = Level.INFO))
+        .containsExactly("this is INFO.", "this is WARN!")
+  }
+
+  @Test
+  fun filterByLogger() {
+    val testLogger = getLogger<LogCollectorTest>()
+    val realLogger = getLogger<RealLogCollector>()
+
+    testLogger.info("this is from the test logger")
+    realLogger.info("this is from the real logger")
+    assertThat(logCollector.takeMessages(loggerClass = RealLogCollector::class))
+        .containsExactly("this is from the real logger")
+  }
+
+  @Test
+  fun filterByPattern() {
+    val logger = getLogger<LogCollectorTest>()
+
+    logger.info("this matches the pattern")
+    logger.info("this does not match the pattern")
+    assertThat(logCollector.takeMessages(pattern = Regex("m[a-z]tch[a-z]s")))
+        .containsExactly("this matches the pattern")
+  }
+
+  @Test
+  fun takeConsumesUnmatched() {
+    val logger = getLogger<LogCollectorTest>()
+
+    logger.info("this is a log message!")
+    assertThat(logCollector.takeMessages(minLevel = Level.ERROR)).isEmpty()
+    assertThat(logCollector.takeMessages()).isEmpty()
+  }
+}
