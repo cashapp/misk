5ee1656101140d8b5b0b59d20e82b4cd7edf46db
Andrew Paradi <account+github@adrw.ch>
2018-06-22 13:50:00 -0400

    PR Fixes

--- misk/src/main/kotlin/misk/config/ConfigWebModule.kt
+++ misk/src/main/kotlin/misk/config/ConfigWebModule.kt
@@ -3,7 +3,6 @@ package misk.config
 import misk.inject.KAbstractModule
 import misk.web.WebActionModule
 
-//    @TODO(jwilson swankjesse) https://github.com/square/misk/issues/272 hacky fix
 class ConfigWebModule : KAbstractModule() {
   override fun configure() {
     install(WebActionModule.create<ConfigAdminAction>())
