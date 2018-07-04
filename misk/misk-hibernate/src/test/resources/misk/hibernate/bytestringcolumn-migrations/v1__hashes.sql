996b37d3e61c9e2ca84f074846d14d838e34cf97
Joey Muia <jmuia@squareup.com>
2018-07-04 16:48:24 -0400

    Remove bigint(20) display width hint.
    (M) indicates the maximum display width for integer types.
    Display width is unrelated to the range of values a type can contain.
    In practice, without the ZEROFILL option, the hint is meaningless.

--- misk/misk-hibernate/src/test/resources/misk/hibernate/bytestringcolumn-migrations/v1__hashes.sql
+++ misk/misk-hibernate/src/test/resources/misk/hibernate/bytestringcolumn-migrations/v1__hashes.sql
@@ -1,5 +1,5 @@
 CREATE TABLE text_hashes(
-  id bigint(20) NOT NULL PRIMARY KEY AUTO_INCREMENT,
+  id bigint NOT NULL PRIMARY KEY AUTO_INCREMENT,
   text varchar(255) NOT NULL,
   hash varbinary(32) NOT NULL,
   UNIQUE KEY `unq_hash` (`hash`)
