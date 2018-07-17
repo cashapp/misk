cf82c5a6bb69e98fbd9eb6601e0dcde5764426f6
Jesse Wilson <jwilson@squareup.com>
2018-07-17 17:57:37 -0400

    Rewrite ResourceLoader to have pluggable backends.

--- misk/src/main/kotlin/misk/resources/ResourceLoaderModule.kt
+++ misk/src/main/kotlin/misk/resources/ResourceLoaderModule.kt
@@ -1,9 +1,13 @@
 package misk.resources
 
+import com.google.inject.multibindings.MapBinder
 import misk.inject.KAbstractModule
 
 class ResourceLoaderModule : KAbstractModule() {
   override fun configure() {
-    bind<ResourceLoader>().toInstance(ClasspathResourceLoader)
+    val mapBinder = MapBinder.newMapBinder(
+        binder(), String::class.java, ResourceLoader.Backend::class.java)
+    mapBinder.addBinding("/resources").toInstance(ClasspathResourceLoaderBackend)
+    mapBinder.addBinding("/memory").to<MemoryResourceLoaderBackend>()
   }
 }
