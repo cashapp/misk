f363749dfd856bf434a874aa3e8a15aaab5264c7
Jesse Wilson <jwilson@squareup.com>
2018-07-18 19:03:44 -0400

    Add a /filesystem backend to ResourceLoader

---
+++ misk/misk/src/main/kotlin/misk/resources/FilesystemLoaderBackend.kt
@@ -0,0 +1,26 @@
+package misk.resources
+
+import okio.BufferedSource
+import okio.Okio
+import java.io.File
+import java.io.FileNotFoundException
+import javax.inject.Singleton
+
+/**
+ * Read-only resources that are fetched from the local filesystem using absolute paths.
+ *
+ * This is mounted at `/filesystem`.
+ */
+@Singleton
+object FilesystemLoaderBackend : ResourceLoader.Backend() {
+  override fun open(path: String): BufferedSource? {
+    val file = File(path)
+    try {
+      return Okio.buffer(Okio.source(file))
+    } catch (e: FileNotFoundException) {
+      return null
+    }
+  }
+
+  override fun exists(path: String) = File(path).exists()
+}
