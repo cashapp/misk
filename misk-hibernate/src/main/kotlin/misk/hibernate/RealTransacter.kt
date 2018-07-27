4b4014914e42396ec7817831cea1c0c3c5be5257
Jon Tirsen <jontirsen@squareup.com>
2018-07-27 13:09:41 +0200

    Re-enable SHOW VITESS_TARGET for shard targeting

--- misk-hibernate/src/main/kotlin/misk/hibernate/RealTransacter.kt
+++ misk-hibernate/src/main/kotlin/misk/hibernate/RealTransacter.kt
@@ -168,12 +168,9 @@ internal class RealTransacter private constructor(
         return useConnection { connection ->
           // This broke on master Vitess so we temporarily disable it.
           val previousTarget =
-              if (true) "" else {
-                // TODO re-enable this when it's working again: https://github.com/vitessio/vitess/pull/3962
-                connection.createStatement().use { statement ->
-                  statement.executeQuery("SHOW vitess_target").uniqueResult { it.getString(1) }!!
-                }
-              }
+            connection.createStatement().use { statement ->
+              statement.executeQuery("SHOW VITESS_TARGET").uniqueResult { it.getString(1) }!!
+            }
           connection.createStatement().use { statement ->
             statement.execute("USE `$shard`")
           }
