1a339d92f1b8d43be82a1cd8930db01ecda3a651
Matthew Mihic <mmihic@squareup.com>
2018-07-30 18:06:14 -0400

    Convert to using Prometheus metrics client
    For native labeling and aggregatable histogram support

--- misk/misk-signalfx/src/main/kotlin/misk/metrics/backends/signalfx/SignalFxBackendConfig.kt
+++
@@ -1,9 +0,0 @@
-package misk.metrics.backends.signalfx
-
-import misk.config.Config
-import java.time.Duration
-
-data class SignalFxBackendConfig(
-  val access_token: String,
-  val interval: Duration
-) : Config
--- misk/misk/src/main/kotlin/misk/client/ClientMetricsInterceptor.kt
+++ misk/misk/src/main/kotlin/misk/client/ClientMetricsInterceptor.kt
@@ -1,31 +1,36 @@
 package misk.client
 
+import io.prometheus.client.Histogram
 import misk.metrics.Metrics
-import misk.metrics.MetricsScope
+import misk.time.timed
 import okhttp3.Response
-import java.util.concurrent.Callable
 import javax.inject.Inject
 import javax.inject.Singleton
 
 class ClientMetricsInterceptor internal constructor(
-  private val scope: MetricsScope
+  private val actionName: String,
+  private val requestDuration: Histogram
 ) : ClientNetworkInterceptor {
+
   override fun intercept(chain: ClientNetworkChain): Response {
-    scope.counter("requests").inc()
-    val result = scope.timer("timing").time(Callable { chain.proceed(chain.request) })
-    if (result != null) {
-      scope.counter("responses.${result.code() / 100}xx").inc()
-      scope.counter("responses.${result.code()}").inc()
-    }
+    val (elapsedTime, result) = timed { chain.proceed(chain.request) }
+    val elapsedMillis = elapsedTime.toMillis().toDouble()
+
+    requestDuration.labels(actionName, "all").observe(elapsedMillis)
+    requestDuration.labels(actionName, "${result.code() / 100}xx").observe(elapsedMillis)
+    requestDuration.labels(actionName, "${result.code()}").observe(elapsedMillis)
 
     return result
   }
 
   @Singleton
-  class Factory @Inject internal constructor(
-    private val m: Metrics
-  ) : ClientNetworkInterceptor.Factory {
+  class Factory @Inject internal constructor(m: Metrics) : ClientNetworkInterceptor.Factory {
+    internal val requestDuration = m.histogram(
+        name = "client_http_request_latency_ms",
+        help = "count and duration in ms of outgoing client requests",
+        labelNames = listOf("action", "code"))
+
     override fun create(action: ClientAction) =
-        ClientMetricsInterceptor(m.scope("clients.http.${action.name}"))
+        ClientMetricsInterceptor(action.name, requestDuration)
   }
 }
