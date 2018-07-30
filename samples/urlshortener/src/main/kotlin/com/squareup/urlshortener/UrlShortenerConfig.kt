a69d0b4e93e928587fe6541a1fe19e9ed9c0d3b0
Matthew Mihic <mmihic@squareup.com>
2018-07-30 18:06:53 -0400

    Use prometheus metrics in samples

--- samples/urlshortener/src/main/kotlin/com/squareup/urlshortener/UrlShortenerConfig.kt
+++ samples/urlshortener/src/main/kotlin/com/squareup/urlshortener/UrlShortenerConfig.kt
@@ -3,11 +3,13 @@ package com.squareup.urlshortener
 import misk.client.HttpClientsConfig
 import misk.config.Config
 import misk.hibernate.DataSourceClusterConfig
+import misk.metrics.backends.prometheus.PrometheusConfig
 import misk.web.WebConfig
 
 data class UrlShortenerConfig(
   val web: WebConfig,
   val data_source_cluster: DataSourceClusterConfig,
   val endpoint: EndpointConfig,
-  val http_clients: HttpClientsConfig
+  val http_clients: HttpClientsConfig,
+  val prometheus: PrometheusConfig
 ) : Config
