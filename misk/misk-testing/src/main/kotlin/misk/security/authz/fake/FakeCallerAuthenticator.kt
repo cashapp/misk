3d0fd4578493d0e68247fe41fa0755b7e8d4fc77
Matthew Mihic <mmihic@squareup.com>
2018-08-03 11:47:13 -0400

    Add caller identity as a label in request metrics

--- misk/misk-testing/src/main/kotlin/misk/security/authz/fake/FakeCallerAuthenticator.kt
+++ misk/misk-testing/src/main/kotlin/misk/security/authz/fake/FakeCallerAuthenticator.kt
@@ -1,22 +1,17 @@
-package misk.web.actions
+package misk.security.authz.fake
 
 import misk.MiskCaller
 import misk.scope.ActionScoped
 import misk.security.authz.MiskCallerAuthenticator
-import misk.web.Request
 import javax.inject.Inject
 import javax.inject.Singleton
-
-internal const val SERVICE_HEADER = "X-Forwarded-Service"
-internal const val USER_HEADER = "X-Forwarded-User"
-internal const val ROLES_HEADER = "X-Forwarded-Roles"
 
 /**
  * A caller authenticator that blindly trusts HTTP headers. Unsafe for production use.
  */
 @Singleton
-internal class FakeCallerAuthenticator : MiskCallerAuthenticator {
-  @Inject lateinit var currentRequest: ActionScoped<Request>
+class FakeCallerAuthenticator : MiskCallerAuthenticator {
+  @Inject lateinit var currentRequest: ActionScoped<misk.web.Request>
 
   override fun getAuthenticatedCaller(): MiskCaller? {
     val request = currentRequest.get()
@@ -26,4 +21,11 @@ internal class FakeCallerAuthenticator : MiskCallerAuthenticator {
     if (user == null && service == null) return null
     return MiskCaller(service, user, roles)
   }
+
+  companion object {
+    const val SERVICE_HEADER = "X-Forwarded-Service"
+    const val USER_HEADER = "X-Forwarded-User"
+    const val ROLES_HEADER = "X-Forwarded-Roles"
+  }
 }
+
