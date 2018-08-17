32b349f6f429774e3ad2e4f72f278b7088f252ba
Andrew Paradi <account+github@adrw.ch>
2018-08-17 14:07:44 -0400

    Still some runtime errors, excess ITEM and LOADER related sagas removed from config sagas

--- misk/web/tabs/config/src/reducers/index.ts
+++ misk/web/tabs/config/src/reducers/index.ts
@@ -3,8 +3,6 @@ import { RouterState } from "connected-react-router"
 import { fromJS, List } from "immutable"
 import { combineReducers } from "redux"
 import ConfigReducer from "./ConfigReducer"
-import ItemReducer from "./ItemReducer"
-import LoadTabReducer from "./LoadTabReducer"
 
 export const defaultInitialState = fromJS({
   data: List([]),
@@ -15,14 +13,10 @@ export const defaultInitialState = fromJS({
 
 const rootReducer = combineReducers({
   config: ConfigReducer,
-  item: ItemReducer,
-  loader: LoadTabReducer
 })
 
 export interface IState {
   config: any
-  item: any
-  loader: any
   router: RouterState
 }
 
--- misk/web/tabs/config/src/sagas/index.ts
+++ misk/web/tabs/config/src/sagas/index.ts
@@ -1,12 +1,8 @@
 import { all, fork } from "redux-saga/effects"
 import watchConfigSagas from "./configSaga"
-import watchItemSagas from "./itemSaga"
-import watchLoaderSagas from "./loaderSaga"
 
 export default function * rootSaga () {
   yield all([
-    fork(watchConfigSagas),
-    fork(watchItemSagas),
-    fork(watchLoaderSagas)
+    fork(watchConfigSagas)
   ])
 }
