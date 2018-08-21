2ac7ac8dbc7f68655954bec7e92966b9fe1e4f6b
Jesse Wilson <jwilson@squareup.com>
2018-08-21 10:48:30 -0400

    Cleanup an unnecessary @Singleton on FakeDatstoreModule

--- misk/misk-gcp/src/main/kotlin/misk/cloud/gcp/GoogleCloudModule.kt
+++ misk/misk-gcp/src/main/kotlin/misk/cloud/gcp/GoogleCloudModule.kt
@@ -81,7 +81,7 @@ class GoogleCloudModule : KAbstractModule() {
 }
 
 /** Logs cloud configuration on startup */
-@javax.inject.Singleton
+@Singleton
 private class GoogleCloud @Inject internal constructor(
   private val datastore: Datastore,
   private val storage: Storage
