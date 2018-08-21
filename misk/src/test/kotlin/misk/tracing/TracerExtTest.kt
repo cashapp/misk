b140287e6a7fc49a73d7d0e8038d7dc43ff0eaa6
Matthew Mihic <mmihic@squareup.com>
2018-08-21 15:13:43 -0400

    Replace MiskTracer with extensions...
    ...and make sure it works with nested spans

--- misk/src/test/kotlin/misk/tracing/TracerExtTest.kt
+++ misk/src/test/kotlin/misk/tracing/TracerExtTest.kt
@@ -1,7 +1,7 @@
 package misk.tracing
 
-import io.opentracing.Span
 import io.opentracing.Tracer
+import io.opentracing.mock.MockSpan
 import io.opentracing.mock.MockTracer
 import io.opentracing.tag.Tags
 import misk.exceptions.ActionException
@@ -15,19 +15,18 @@ import javax.inject.Inject
 import kotlin.test.assertFailsWith
 
 @MiskTest
-class MiskTracerTest {
+class TracerExtTest {
   @MiskTestModule
   val module = MockTracingBackendModule()
 
   @Inject private lateinit var tracer: Tracer
-  @Inject private lateinit var miskTracer: MiskTracer
 
   @Test
   fun traceTracedMethod() {
     val mockTracer = tracer as MockTracer
 
     assertThat(mockTracer.finishedSpans().size).isEqualTo(0)
-    val spanUsed = miskTracer.trace("traceMe", ::traceMe)
+    val spanUsed = tracer.traceWithSpan("traceMe") { span -> span }
     assertThat(mockTracer.finishedSpans().size).isEqualTo(1)
     assertThat(spanUsed).isEqualTo(mockTracer.finishedSpans().first())
   }
@@ -37,16 +36,30 @@ class MiskTracerTest {
     val mockTracer = tracer as MockTracer
 
     assertThat(mockTracer.finishedSpans().size).isEqualTo(0)
-    assertFailsWith<ActionException> { miskTracer.trace("failedTrace", ::failedTrace) }
+    assertFailsWith<ActionException> {
+      tracer.trace("failedTrace") {
+        throw ActionException(StatusCode.BAD_REQUEST, "sadness")
+      }
+    }
     assertThat(mockTracer.finishedSpans().size).isEqualTo(1)
     assertThat(mockTracer.finishedSpans().get(0).tags().get(Tags.ERROR.key)).isEqualTo(true)
   }
 
-  fun traceMe(span: Span) : Span {
-    return span
-  }
+  @Test
+  fun nestedTracing() {
+    val mockTracer = tracer as MockTracer
 
-  fun failedTrace(@Suppress("UNUSED_PARAMETER") span: Span) {
-    throw ActionException(StatusCode.BAD_REQUEST, "sadness")
+    assertThat(mockTracer.finishedSpans().size).isEqualTo(0)
+    val (parentSpan, childSpan) = tracer.traceWithSpan("parent") { span1 ->
+      span1 to tracer.traceWithSpan("child") { span2 -> span2 }
+    }
+    assertThat(mockTracer.finishedSpans().size).isEqualTo(2)
+    assertThat(mockTracer.finishedSpans()[0]).isEqualTo(childSpan)
+    assertThat(mockTracer.finishedSpans()[1]).isEqualTo(parentSpan)
+
+    val parentContext = parentSpan.context() as MockSpan.MockContext
+    val childContext = childSpan.context() as MockSpan.MockContext
+    assertThat(childContext.traceId()).isEqualTo(parentContext.traceId())
+    assertThat((childSpan as MockSpan).parentId()).isEqualTo(parentContext.spanId())
   }
 }
