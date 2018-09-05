a33a3d164f20dd0b63a5e27ea2e01b18a317f65c
Andrew Paradi <account+github@adrw.ch>
2018-09-05 16:44:03 -0400

    Change how optional additional Webpack config fields are passed into builder in @misk/dev 0.0.21

--- misk/web/tabs/loader/webpack.config.js
+++ misk/web/tabs/loader/webpack.config.js
@@ -3,5 +3,8 @@ const path = require('path')
 const miskTabWebpack = require(path.join(process.cwd(), "package.json")).miskTabWebpack
 module.exports = MiskWebpackConfigBase(process.env.NODE_ENV, {
   "dirname": __dirname,
-  miskTabWebpack,
+  miskTabWebpack
+},
+{
+  // optional: any other Webpack config fields to be merged with the Misk Webpack Base Config
 })
