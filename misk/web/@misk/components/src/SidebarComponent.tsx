f7888b6d6a8931d56c5ebf146c54da4fff9abfef
Andrew Paradi <account+github@adrw.ch>
2018-09-10 17:09:21 -0400

    UI: Menu Button * Part 1 of building dynamic slide down from top menu for tabs * Styled, responsive, menu button * Changed endpoint `/api/admintab/all` -> `/api/admintabs` for less prone-ness to spelling errors

--- misk/web/@misk/components/src/SidebarComponent.tsx
+++ misk/web/@misk/components/src/SidebarComponent.tsx
@@ -3,7 +3,7 @@ import { IMiskAdminTabs } from "@misk/common"
 import * as React from "react"
 import styled from "styled-components" 
 
-interface INavSidebarProps {
+interface ISidebarProps {
   adminTabs: IMiskAdminTabs
 }
 
@@ -15,7 +15,7 @@ const buildMenuItems = (adminTabs: IMiskAdminTabs) => (
   Object.entries(adminTabs).map(([key, tab]) => <MenuItem key={key} href={tab.url_path_prefix} className="" icon={tab.icon} text={tab.name}/>)
 )
 
-export const NavSidebarComponent = (props: INavSidebarProps) => (
+export const SidebarComponent = (props: ISidebarProps) => (
   <Sidebar>
     <Menu>
       {buildMenuItems(props.adminTabs)}
