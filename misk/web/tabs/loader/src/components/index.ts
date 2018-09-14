64ab44949ee31175adcf518277c5b130e91e1639
Andrew Paradi <account+github@adrw.ch>
2018-09-14 12:35:57 -0400

    AdminTabAction Refactor * Each tab `src/actions/util.ts` moved to `@misk/common/actions.ts` * AdminTabAction endpoint changed so returns simple array of IMiskAdminTab * `@misk/components/{Sidebar|Topbar}Component` updated for new input of `IMiskAdmintab[]` * New sorting and categorizing functions to be done client side for `MiskMenu` * Saga flows for tabs simplified, unused functions removed * Increased padding on `MiskMenuButton` * `@misk/common@0.0.36` * `@misk/components@0.0.36`

--- misk/web/tabs/loader/src/components/index.ts
+++ misk/web/tabs/loader/src/components/index.ts
@@ -1,4 +1,2 @@
-export * from "./AdminTabDebugComponent"
-export * from "./HomeComponent"
 export * from "./MountingDivComponent"
 export * from "./ScriptComponent"
--- misk/web/tabs/loader/src/sagas/loaderSaga.tsx
+++ misk/web/tabs/loader/src/sagas/loaderSaga.tsx
@@ -6,25 +6,15 @@
 // import { push } from react-router-redux and then
 // yield put(push('/next-page'))
 
-import { IMiskAdminTab } from "@misk/common"
+import { IMiskAction, IMiskAdminTab } from "@misk/common"
 import axios from "axios"
 import { all, call, put, takeEvery, takeLatest } from "redux-saga/effects"
 
 import {
-  dispatchLoader, IAction, IActionType, LOADER
+  dispatchLoader, IActionType, LOADER
 } from "../actions"
 
-function * handleCacheTabEntries (action: IAction<IActionType, { MiskBinder: any}>) {
-  const { MiskBinder } = action.payload
-    yield put(dispatchLoader.success({ adminTabComponents: { blah: "blah"} , staleTabCache: false }))
-    if (MiskBinder) {
-      yield put(dispatchLoader.success({ adminTabComponents: MiskBinder.TabEntry, staleTabCache: false }))
-    } else {
-      yield put(dispatchLoader.failure({ staleTabCache: true }))
-    }
-}
-
-function * handleGetAllTabs (action: IAction<IActionType, { url: string}>) {
+function * handleGetAllTabs (action: IMiskAction<IActionType, { url: string}>) {
   const { url } = action.payload
   try {
     const { data } = yield call(axios.get, url)
@@ -35,37 +25,7 @@ function * handleGetAllTabs (action: IAction<IActionType, { url: string}>) {
   }
 }
 
-function * handleGetAllAndTabs (action: IAction<IActionType, { url: string}>) {
-  const { url } = action.payload
-  let adminTabs: any = {}
-  let adminTabCategories: any = {}
-  try {
-    yield put(dispatchLoader.getAllTabs(url))
-    const { data } = yield call(axios.get, url)
-    adminTabs = data.adminTabs
-    adminTabCategories = data.adminTabCategories
-    yield put(dispatchLoader.success({ adminTabs, adminTabCategories }))
-    if (Object.entries(adminTabs).length === 0) {
-      yield put(dispatchLoader.success({ adminTabComponents: { } }))
-    }
-  } catch (e) {
-    yield put(dispatchLoader.failure({ error: { ...e } }))
-  }
-
-  for (const key in adminTabs) {
-    if (adminTabs.hasOwnProperty(key)) {
-      const tab = adminTabs[key]
-      try {
-        const { data } = yield call(axios.get, `/_tab/${tab.slug}/tab_${tab.slug}.js`)
-        yield put(dispatchLoader.success({ adminTabComponents: { [tab.slug]: data } }))
-      } catch (e) {
-        yield put(dispatchLoader.failure({ error: { ...e } }))
-      }
-    }
-  }
-}
-
-function * handleGetOneComponent (action: IAction<IActionType, { tab: IMiskAdminTab }>) {
+function * handleGetOneComponent (action: IMiskAction<IActionType, { tab: IMiskAdminTab }>) {
   const { tab } = action.payload
   const url = `/_tab/${tab.slug}/tab_${tab.slug}.js`
   try {
@@ -76,19 +36,10 @@ function * handleGetOneComponent (action: IAction<IActionType, { tab: IMiskAdmin
   }
 }
 
-function * handleRegisterComponent (action: IAction<IActionType, { name: string, Component: any }>) {
-  const { name } = action.payload
-  const { Component } = action.payload
-  yield put(dispatchLoader.success({ adminTabComponents: { [name]: Component } }))
-}
-
 function * watchLoaderSagas () {
   yield all([
-    takeLatest(LOADER.CACHE_TAB_ENTRIES, handleCacheTabEntries),
     takeEvery(LOADER.GET_ONE_COMPONENT, handleGetOneComponent),
     takeLatest(LOADER.GET_ALL_TABS, handleGetAllTabs),
-    takeLatest(LOADER.GET_ALL_COMPONENTS_AND_TABS, handleGetAllAndTabs),
-    takeEvery(LOADER.REGISTER_COMPONENT, handleRegisterComponent),
   ])
 }
 
