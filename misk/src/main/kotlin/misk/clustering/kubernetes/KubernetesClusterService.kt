51c7ce3b28966ff7b133f7e1da08bbda77b806f4
Matthew Mihic <mmihic@squareup.com>
2018-09-18 11:41:51 -0400

    Expose cluster as a consistent snapshot

--- misk/src/main/kotlin/misk/clustering/kubernetes/KubernetesClusterService.kt
+++ misk/src/main/kotlin/misk/clustering/kubernetes/KubernetesClusterService.kt
@@ -1,12 +1,11 @@
 package misk.clustering.kubernetes
 
-import com.google.common.util.concurrent.AbstractIdleService
+import com.google.common.util.concurrent.AbstractExecutionThreadService
 import com.google.inject.Key
 import io.kubernetes.client.models.V1Pod
 import io.kubernetes.client.util.Watch
 import misk.DependentService
 import misk.clustering.Cluster
-import misk.clustering.ClusterHashRing
 import misk.clustering.ClusterWatch
 import misk.logging.getLogger
 import org.eclipse.jetty.util.BlockingArrayQueue
@@ -14,7 +13,6 @@ import java.util.concurrent.atomic.AtomicBoolean
 import java.util.concurrent.atomic.AtomicReference
 import javax.inject.Inject
 import javax.inject.Singleton
-import kotlin.concurrent.thread
 
 /**
  * [KubernetesClusterService] implements the [Cluster] interface on top of the kubernetes APIs.
@@ -24,29 +22,52 @@ import kotlin.concurrent.thread
 @Singleton
 internal class KubernetesClusterService @Inject internal constructor(
   config: KubernetesConfig
-) : AbstractIdleService(), Cluster, DependentService {
-  private val peersRef = AtomicReference<Set<Cluster.Member>>(setOf())
-  private val hashRingRef = AtomicReference<ClusterHashRing>(ClusterHashRing(setOf()))
+) : AbstractExecutionThreadService(), Cluster, DependentService {
+
+  private val self: Cluster.Member = Cluster.Member(config.my_pod_name, config.my_pod_ip)
+  private val snapshotRef = AtomicReference<Cluster.Snapshot>(Cluster.Snapshot(
+      self = self,
+      selfReady = false,
+      readyMembers = setOf()
+  ))
   private val running = AtomicBoolean(false)
   private val events = BlockingArrayQueue<Event>()
 
-  override val hashRing: ClusterHashRing get() = hashRingRef.get()
-  override val peers: Set<Cluster.Member> get() = peersRef.get()
-  override val self: Cluster.Member = Cluster.Member(config.my_pod_name, config.my_pod_ip)
-
+  override val snapshot: Cluster.Snapshot get() = snapshotRef.get()
   override val consumedKeys: Set<Key<*>> = setOf()
   override val producedKeys: Set<Key<*>> = setOf(
       Key.get(Cluster::class.java),
       Key.get(KubernetesClusterService::class.java)
   )
 
+  /**
+   * Runs the internal event loop that handles requests to add watches or cluster changes. We use
+   * a single threaded event loop to ensure that watches get consistent diffs for the cluster
+   * without being racy or requiring locks that span application code (which might result in
+   * deadlocks). When a watch is registered, it is provided with the cluster membership as it
+   * is known at that time, and then receives diffs as they arrive.
+   */
+  override fun run() {
+    val watches = mutableSetOf<ClusterWatch>()
+
+    while (running.get()) {
+      val event = events.take()
+      when (event.type) {
+        Event.Type.CLUSTER_CHANGE -> handleClusterChange(event.watchResponse!!, watches)
+        Event.Type.NEW_WATCH -> handleNewWatch(event.newWatch!!, watches)
+        Event.Type.SYNC_POINT -> try {
+          event.syncPointCallback!!()
+        } catch (th: Throwable) {
+          log.error(th) { "error triggering syncpoint callback" }
+        }
+        Event.Type.SHUTDOWN -> running.set(false)
+      }
+    }
+  }
+
   override fun startUp() {
     log.info { "starting k8s cluster manager" }
     running.set(true)
-
-    thread(name = "k8s-cluster-event-loop") {
-      runEventLoop()
-    }
   }
 
   override fun shutDown() {
@@ -66,42 +87,13 @@ internal class KubernetesClusterService @Inject internal constructor(
     events.offer(Event(callback))
   }
 
-  /**
-   * Runs the internal event loop that handles requests to add watches or cluster changes. We use
-   * a single threaded event loop to ensure that watches get consistent diffs for the cluster
-   * without being racy or requiring locks that span application code (which might result in
-   * deadlocks). When a watch is registered, it is provided with the cluster membership as it
-   * is known at that time, and then receives diffs as they arrive.
-   */
-  private fun runEventLoop() {
-    val watches = mutableSetOf<ClusterWatch>()
-
-    while (running.get()) {
-      val event = events.take()
-      when (event.type) {
-        Event.Type.CLUSTER_CHANGE -> handleClusterChange(event.watchResponse!!, watches)
-        Event.Type.NEW_WATCH -> handleNewWatch(event.newWatch!!, peersRef.get(), watches)
-        Event.Type.SYNC_POINT -> try {
-          event.syncPointCallback!!()
-        } catch (th: Throwable) {
-          log.error(th) { "error triggering syncpoint callback" }
-        }
-        Event.Type.SHUTDOWN -> running.set(false)
-      }
-    }
-  }
-
   /** Handles a change to a cluster, updating the current peer set and triggering all watches */
   private fun handleClusterChange(response: Watch.Response<V1Pod>, watches: Set<ClusterWatch>) {
-    val newPeers = mutableMapOf<String, Cluster.Member>()
-    peersRef.get()?.forEach { newPeers[it.name] = it }
-
-    val changes = applyChange(newPeers, response.type, response.`object`)
+    val currentSnapshot = snapshotRef.get()
+    val changes = applyChange(currentSnapshot, response.type, response.`object`)
     if (!changes.hasDiffs) return
 
-    peersRef.set(changes.current)
-    hashRingRef.set(ClusterHashRing(changes.current))
-
+    snapshotRef.set(changes.snapshot)
     watches.forEach {
       try {
         it(changes)
@@ -116,13 +108,11 @@ internal class KubernetesClusterService @Inject internal constructor(
    * the current set of peers
    */
   private fun handleNewWatch(
-    newWatch: ClusterWatch,
-    peers: Set<Cluster.Member>,
-    watches: MutableSet<ClusterWatch>
+    newWatch: ClusterWatch, watches: MutableSet<ClusterWatch>
   ) {
     watches.add(newWatch)
     try {
-      newWatch(Cluster.Changes(peers))
+      newWatch(Cluster.Changes(snapshot))
     } catch (th: Throwable) {
       log.error(th) { "error triggering watch during registration" }
     }
@@ -130,37 +120,19 @@ internal class KubernetesClusterService @Inject internal constructor(
 
   /** Applies an incoming change to a set of cluster members */
   private fun applyChange(
-    peers: MutableMap<String, Cluster.Member>,
+    currentSnapshot: Cluster.Snapshot,
     changeType: String,
     pod: V1Pod
   ): Cluster.Changes {
+    val changeTracker = ClusterChangeTracker(currentSnapshot)
     val member = Cluster.Member(pod.metadata.name, pod.status.podIP ?: "")
 
-    // Don't bother to include ourselves in the peer list
-    if (member.name == self.name) return Cluster.Changes(peers.values.toSet())
-
     return when (changeType) {
-      CHANGE_TYPE_ADDED, CHANGE_TYPE_MODIFIED -> {
-        val isReady = pod.status.containerStatuses?.all { it.isReady } ?: false
-        if (isReady && member.ipAddress.isNotBlank()) {
-          log.info { "added peer ${member.name}" }
-          peers[member.name] = member
-          Cluster.Changes(peers.values.toSet(), added = setOf(member))
-        } else if (peers.containsKey(member.name)) {
-          log.info { "peer ${member.name} no longer ready" }
-          peers.remove(member.name)
-          Cluster.Changes(peers.values.toSet(), removed = setOf(member))
-        } else Cluster.Changes(peers.values.toSet())
-      }
-
-      CHANGE_TYPE_DELETED -> {
-        if (peers.containsKey(member.name)) {
-          log.info { "deleted peer ${member.name}" }
-          peers.remove(member.name)
-          Cluster.Changes(peers.values.toSet(), removed = setOf(member))
-        } else Cluster.Changes(peers.values.toSet())
-      }
-      else -> Cluster.Changes(peers.values.toSet())
+      CHANGE_TYPE_ADDED, CHANGE_TYPE_MODIFIED ->
+        if (pod.isReady) changeTracker.memberReady(member)
+        else changeTracker.memberNotReady(member)
+      CHANGE_TYPE_DELETED -> changeTracker.memberNotReady(member)
+      else -> Cluster.Changes(currentSnapshot)
     }
   }
 
@@ -208,3 +180,41 @@ internal class KubernetesClusterService @Inject internal constructor(
     const val CHANGE_TYPE_ADDED = "ADDED"
   }
 }
+
+private val V1Pod.isReady: Boolean
+  get() {
+    if (status.containerStatuses == null) return false
+    if (status.containerStatuses.any { !it.isReady }) return false
+    return status.podIP?.isNotEmpty() ?: false
+  }
+
+private class ClusterChangeTracker(private val snapshot: Cluster.Snapshot) {
+  private val readyMembers = snapshot.readyMembers.map { it.name to it }.toMap()
+
+  fun memberReady(member: Cluster.Member): Cluster.Changes {
+    val existingReadyMember = readyMembers[member.name]
+    if (existingReadyMember != null && existingReadyMember.ipAddress == member.ipAddress) {
+      // We already know about this as a ready member, so just return the current snapshot
+      return Cluster.Changes(snapshot)
+    }
+
+    // Either the member wasn't ready or the IP address changed, so add it as a ready member
+    return Cluster.Changes(snapshot = Cluster.Snapshot(
+        readyMembers = (readyMembers - member.name).values.toSet() + member,
+        self = snapshot.self,
+        selfReady = if (member.name == snapshot.self.name) true else snapshot.selfReady
+    ), added = setOf(member))
+  }
+
+  fun memberNotReady(member: Cluster.Member): Cluster.Changes {
+    // If we're not currently tracking this member as ready, just return the current snapshot
+    if (!readyMembers.containsKey(member.name)) return Cluster.Changes(snapshot)
+
+    return Cluster.Changes(snapshot = Cluster.Snapshot(
+        readyMembers = (readyMembers - member.name).values.toSet(),
+        self = snapshot.self,
+        selfReady = if (member.name == snapshot.self.name) false else snapshot.selfReady
+    ), removed = setOf(member))
+  }
+}
+
