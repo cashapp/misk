0cb2b6ae21709c68fb0a22199de79f49fa13ba2b
Matthew Mihic <mmihic@squareup.com>
2018-09-26 15:55:10 -0400

    Event interfaces

---
+++ misk/misk-events/src/main/kotlin/misk/events/SpooledProducer.kt
@@ -0,0 +1,17 @@
+package misk.events
+
+import misk.hibernate.Gid
+import misk.hibernate.Session
+
+/**
+ * A [SpooledProducer] is a producer that writes events to a local spool stored within a
+ * service's database. [SpooledProducer]s can be used to coordinate event publishing with
+ * local database transactions. Events published to the pool are done within the application's
+ * local database transaction; a rollback of the database transaction will also rollback
+ * any events published to the spool. Events are asynchronously forwarded from the spool
+ * to the event stream, and are done so through a [Producer] transaction.
+ */
+interface SpooledProducer {
+  fun publish(session: Session, groupRootId: Gid<*, *>, topic: Topic, vararg event: Event)
+  fun publish(session: Session, topic: Topic, vararg event: Event)
+}
