6895151adc6116bfb4f4c582bee11894fe8a88ad
Daniel Weisberg <dweisberg@squareup.com>
2018-10-01 12:07:06 -0700

    Implemented VeneurDigest and MergingDigest

---
+++ misk/src/main/kotlin/misk/digester/VeneurDigest.kt
@@ -0,0 +1,71 @@
+package misk.digester
+
+import com.squareup.digester.protos.service.DigestData
+import com.squareup.digester.protos.tdigest.MergingDigestData
+import okio.ByteString.Companion.toByteString
+
+/**
+ * Wraps an adapted t-digest implementation from Stripe's Veneur project
+ */
+class VeneurDigest {
+
+  private val mergingDigest: MergingDigest
+  private var count: Long = 0
+  private var sum: Double = 0.0
+
+  /** Creates a TDigest backed by a VeneurDigest, using a default compression level */
+  constructor() {
+    mergingDigest = MergingDigest(50.0)
+  }
+
+  /**
+   * Creates a VeneurDigest from a DigestData proto
+   * The DigestData proto must have veneur_digest set correctly
+   */
+  constructor (digestData: DigestData) {
+    val mergingDigestData: MergingDigestData =
+        MergingDigestData.ADAPTER.decode(digestData.veneur_digest)
+
+    mergingDigest = MergingDigest(mergingDigestData)
+    count = digestData.count
+    sum = digestData.sum
+  }
+
+  /** Adds a new observation to the t-digest */
+  fun add(value: Double) {
+    mergingDigest.add(value, 1.0)
+    count += 1
+    sum += value
+  }
+
+  /** Returns the mergingDigest instance */
+  fun mergingDigest(): MergingDigest {
+    return mergingDigest
+  }
+
+  /** Returns the count of the number of observations recorded within the t-digest */
+  fun count(): Long {
+    return count
+  }
+
+  /** Returns the sum of all values added into the digest, or NaN if no values have been added */
+  fun sum(): Double {
+    if (count > 0) {
+      return sum
+    }
+    return Double.NaN
+  }
+
+  /** Merges this t-digest into another t-digest */
+  fun mergeInto(other: VeneurDigest) {
+    other.mergingDigest.mergeFrom(mergingDigest)
+    other.count += count
+    other.sum += sum
+  }
+
+  /** Returns a representation fo the t-digest that can be later be reconstituted into an instance of the same type */
+  fun proto(): DigestData {
+    val encode: ByteArray = MergingDigestData.ADAPTER.encode(mergingDigest.data())
+    return DigestData(count, sum, encode.toByteString())
+  }
+}
---
+++ misk/src/test/kotlin/misk/digester/VeneurDigestTest.kt
@@ -0,0 +1,101 @@
+package misk.digester
+
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+
+class VeneurDigestTest {
+
+  @Test
+  fun testVeneurDigest() {
+    val digest = VeneurDigest()
+    assertThat(digest.mergingDigest().quantile(0.5)).isEqualTo(Double.NaN)
+    assertThat(digest.sum()).isEqualTo(Double.NaN)
+    assertThat(digest.count()).isEqualTo(0)
+
+    digest.add(10.0)
+    digest.add(20.0)
+    assertThat(digest.mergingDigest().quantile(0.5)).isEqualTo(15.0)
+    assertThat(digest.count()).isEqualTo(2)
+    assertThat(digest.sum()).isEqualTo(30.0)
+  }
+
+  private data class VeneurDigestMergeTestClass(
+    val sourceVals: Array<Double> = emptyArray(),
+    val destVals: Array<Double> = emptyArray(),
+    val expectedMedian: Double = Double.NaN,
+    val expectedSum: Double = Double.NaN
+  )
+
+  @Test
+  fun testVeneurDigest_MergeInto() {
+    val testCases: Array<VeneurDigestMergeTestClass> = arrayOf(
+        VeneurDigestMergeTestClass(),
+        VeneurDigestMergeTestClass(
+            emptyArray(),
+            arrayOf(30.0, 40.0),
+            35.0,
+            70.0
+        ),
+        VeneurDigestMergeTestClass(
+            arrayOf(10.0, 20.0),
+            emptyArray(),
+            15.0,
+            30.0
+        ),
+        VeneurDigestMergeTestClass(
+            arrayOf(10.0, 20.0),
+            arrayOf(30.0, 40.0),
+            25.0,
+            100.0
+        )
+    )
+
+    for (tc in testCases) {
+      val src = VeneurDigest()
+      val dest = VeneurDigest()
+
+      for (v in tc.sourceVals) {
+        src.add(v)
+      }
+      for (v in tc.destVals) {
+        dest.add(v)
+      }
+
+      val srcMedian = src.mergingDigest().quantile(0.5)
+      val srcSum = src.sum()
+      val srcCount = src.count()
+
+      src.mergeInto(dest)
+
+      // Check that src is unchanged
+      assertThat(srcMedian).isEqualTo(src.mergingDigest().quantile(0.5))
+      assertThat(srcSum).isEqualTo(src.sum())
+      assertThat(srcCount).isEqualTo(src.count())
+
+      // Check dest
+      assertThat(tc.expectedMedian).isEqualTo(dest.mergingDigest().quantile(0.5))
+      assertThat(tc.expectedSum).isEqualTo(dest.sum())
+      assertThat((tc.sourceVals.size + tc.destVals.size).toLong()).isEqualTo(dest.count())
+    }
+  }
+
+  @Test
+  fun testVeneurDigest_Proto() {
+    val digest = VeneurDigest()
+    digest.add(10.0)
+    digest.add(20.0)
+    digest.add(30.0)
+    digest.add(40.0)
+
+    val proto = digest.proto()
+
+    val deserialized = VeneurDigest(proto)
+
+    assertThat(deserialized.sum()).isEqualTo(digest.sum())
+    assertThat(deserialized.count()).isEqualTo(digest.count())
+    assertThat(deserialized.mergingDigest().quantile(0.1)).isEqualTo(
+        digest.mergingDigest().quantile(0.1))
+    assertThat(deserialized.mergingDigest().quantile(0.99)).isEqualTo(
+        digest.mergingDigest().quantile(0.99))
+  }
+}
