cb78514831b00b11b65aacd2ffa6d7a3dba6b94b
Andrew Paradi <account+github@adrw.ch>
2018-10-23 10:23:58 -0400

    Web Documentation

--- misk/web/tabs/example/src/ducks/example.ts
+++ misk/web/tabs/example/src/ducks/example.ts
@@ -220,5 +220,5 @@ export default function ExampleReducer (state = initialState, action: IAction<st
  */
 export interface IExampleState {
   [key: string]: any
-  toJS: () => any
+  toJS?: () => any
 }
