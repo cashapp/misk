3f2025f680c2acc496c20d38b7e1c2dba2995071
Andrew Paradi <account+github@adrw.ch>
2018-11-02 15:03:12 -0400

    Simplify Web Resource Multibinding (#611)
    * [CLOSES #560]
    * Simplified binding for each tab looks like the following:
    
      ```Kotlin
      // Tab API Endpoints
      multibind<WebActionEntry>().toInstance(WebActionEntry<ConfigAdminAction>())
      // Show tab in menu
      multibind<DashboardTab, AdminDashboardTab>().toInstance(DashboardTab(
            name = "Config",
            slug = "config",
            url_path_prefix = "/_admin/config/",
            category = "Container Admin"
            ))
      // Wire up tab resources (static and web proxy dev-server)
      install(WebTabResourceModule(
            environment = environment,
            slug = "config",
            web_proxy_url = "http://localhost:3200/"
            ))
    
      ```

--- misk/src/main/kotlin/misk/web/metadata/AdminDashboardModule.kt
+++ misk/src/main/kotlin/misk/web/metadata/AdminDashboardModule.kt
@@ -1,5 +1,6 @@
 package misk.web.metadata
 
+import misk.config.ConfigAdminAction
 import misk.environment.Environment
 import misk.inject.KAbstractModule
 import misk.web.DashboardTab
@@ -8,6 +9,7 @@ import misk.web.actions.AdminDashboardTab
 import misk.web.actions.AdminDashboardTabAction
 import misk.web.actions.ServiceMetadataAction
 import misk.web.actions.WebActionEntry
+import misk.web.actions.WebActionMetadataAction
 import misk.web.interceptors.WideOpenDevelopmentInterceptorFactory
 import misk.web.proxy.WebProxyAction
 import misk.web.proxy.WebProxyEntry
@@ -16,79 +18,86 @@ import misk.web.resources.StaticResourceEntry
 
 /**
  * Installs default Admin Dashboard that runs at passed in url_path_prefix
- * Each Misk included tab in dashboard is installed with the respective tab module (ie. ConfigMetadataModule)
+ * Each Misk included tab in dashboard is installed with the respective:
+ *  - Multibindings for API endpoints
+ *  - Multibindings for Dashboard Tab registration
+ *  - Dashboard Tab Module that configures location of the tab compiled web code (classpath and web proxy)
  * Non-Misk tabs can be added by binding AdminDashboardTab entries which will be displayed in the dashboard menu
  */
 class AdminDashboardModule(val environment: Environment) : KAbstractModule() {
   override fun configure() {
-    // Loader Endpoints
-    multibind<WebActionEntry>().toInstance(WebActionEntry<AdminDashboardTabAction>())
-    multibind<WebActionEntry>().toInstance(WebActionEntry<ServiceMetadataAction>())
-
     // Adds open CORS headers in development to allow through API calls from webpack servers
     multibind<NetworkInterceptor.Factory>().to<WideOpenDevelopmentInterceptorFactory>()
 
-    // Tab Modules
-    install(ConfigMetadataModule(environment))
-    install(WebActionMetadataModule(environment))
+    // Loader
+    multibind<WebActionEntry>().toInstance(WebActionEntry<AdminDashboardTabAction>())
+    multibind<WebActionEntry>().toInstance(WebActionEntry<ServiceMetadataAction>())
+    install(WebTabResourceModule(
+        environment = environment,
+        slug = "loader",
+        web_proxy_url = "http://localhost:3100/"
+    ))
+    install(WebTabResourceModule(
+        environment = environment,
+        slug = "loader",
+        web_proxy_url = "http://localhost:3100/",
+        url_path_prefix = "/_admin/",
+        resourcePath = "classpath:/web/_tab/loader/"
+    ))
 
-    //  AdminDashboardTab Bindings
+    // @misk packages
+    install(WebTabResourceModule(
+        environment = environment,
+        slug = "@misk",
+        web_proxy_url = "http://localhost:9100/",
+        url_path_prefix = "/@misk/",
+        resourcePath = "classpath:/web/@misk/"
+    ))
+
+    // Config
+    multibind<WebActionEntry>().toInstance(WebActionEntry<ConfigAdminAction>())
+    multibind<DashboardTab, AdminDashboardTab>().toInstance(DashboardTab(
+        name = "Config",
+        slug = "config",
+        url_path_prefix = "/_admin/config/",
+        category = "Container Admin"
+    ))
+    install(WebTabResourceModule(
+        environment = environment,
+        slug = "config",
+        web_proxy_url = "http://localhost:3200/"
+    ))
+
+    // Web Actions
+    multibind<WebActionEntry>().toInstance(WebActionEntry<WebActionMetadataAction>())
+    multibind<DashboardTab, AdminDashboardTab>().toInstance(DashboardTab(
+        name = "Web Actions",
+        slug = "webactions",
+        url_path_prefix = "/_admin/webactions/"
+    ))
+    install(WebTabResourceModule(
+        environment = environment,
+        slug = "webactions",
+        web_proxy_url = "http://localhost:3201/"
+    ))
+
+    // Example
     multibind<DashboardTab, AdminDashboardTab>().toInstance(DashboardTab(
         name = "Example",
         slug = "example",
         url_path_prefix = "/_admin/example/",
         category = "Misk Development"
     ))
+    install(WebTabResourceModule(
+        environment = environment,
+        slug = "example",
+        web_proxy_url = "http://localhost:3199/"
+    ))
 
-    multibind<StaticResourceEntry>()
-        .toInstance(StaticResourceEntry(url_path_prefix = "/_admin/",
-            resourcePath = "classpath:/web/_tab/loader/"))
-    multibind<StaticResourceEntry>()
-        .toInstance(StaticResourceEntry(url_path_prefix = "/_tab/loader/",
-            resourcePath = "classpath:/web/_tab/loader/"))
-    multibind<StaticResourceEntry>()
-        .toInstance(StaticResourceEntry(url_path_prefix = "/_tab/example/",
-            resourcePath = "classpath:/web/_tab/example/"))
-    multibind<StaticResourceEntry>()
-        .toInstance(StaticResourceEntry(url_path_prefix = "/@misk/",
-            resourcePath = "classpath:/web/@misk/"))
 
-    // Environment Dependent WebProxyAction or StaticResourceAction bindings
-    if (environment == Environment.DEVELOPMENT) {
-      multibind<WebActionEntry>().toInstance(
-          WebActionEntry<WebProxyAction>(url_path_prefix = "/_admin/"))
-      multibind<WebActionEntry>().toInstance(
-          WebActionEntry<WebProxyAction>(url_path_prefix = "/_tab/loader/"))
-      multibind<WebActionEntry>().toInstance(
-          WebActionEntry<WebProxyAction>(url_path_prefix = "/_tab/example/"))
-      multibind<WebActionEntry>().toInstance(
-          WebActionEntry<WebProxyAction>(url_path_prefix = "/@misk/"))
-
-      multibind<WebProxyEntry>().toInstance(
-          WebProxyEntry(url_path_prefix = "/_admin/",
-              web_proxy_url = "http://localhost:3100/"))
-      multibind<WebProxyEntry>().toInstance(
-          WebProxyEntry(url_path_prefix = "/_tab/loader/",
-              web_proxy_url = "http://localhost:3100/"))
-      multibind<WebProxyEntry>().toInstance(
-          WebProxyEntry(url_path_prefix = "/_tab/example/",
-              web_proxy_url = "http://localhost:3199/"))
-      multibind<WebProxyEntry>().toInstance(
-          WebProxyEntry(url_path_prefix = "/@misk/", web_proxy_url = "http://localhost:9100/"))
-
-    } else {
-      multibind<WebActionEntry>().toInstance(
-          WebActionEntry<StaticResourceAction>(url_path_prefix = "/_admin/"))
-      multibind<WebActionEntry>().toInstance(
-          WebActionEntry<StaticResourceAction>(url_path_prefix = "/_tab/loader/"))
-      multibind<WebActionEntry>().toInstance(
-          WebActionEntry<StaticResourceAction>(url_path_prefix = "/_tab/example/"))
-      multibind<WebActionEntry>().toInstance(
-          WebActionEntry<StaticResourceAction>(url_path_prefix = "/@misk/"))
-    }
 
     // True for testing Misk Menu with populated tabs and categories, tabs are not functional
-    if (true) {
+    if (environment == Environment.DEVELOPMENT || environment == Environment.TESTING) {
       multibind<DashboardTab, AdminDashboardTab>().toInstance(DashboardTab(
           name = "gRPC client",
           slug = "a",
