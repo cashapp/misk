ae748d5dbea18e936fe4a03e9461c9830567db08
Matthew Mihic <mmihic@squareup.com>
2018-11-04 22:27:25 -0500

    Cleanup AWS related bindings
    Introduce AwsRegion and AwsAccountId types, and AwsEnvironmentModule to
    pull the values for these from well-known environment variables. We
    intentionally do _not_ consult the AWS Metadata Service, so that this
    service can be locked down to prevent credential hijacking attacks.
    
    Eliminate cloudwatch logging - this is no longer needed.

--- misk/misk/src/main/kotlin/misk/environment/EnvironmentModule.kt
+++ misk/misk/src/main/kotlin/misk/environment/EnvironmentModule.kt
@@ -2,16 +2,9 @@ package misk.environment
 
 import misk.inject.KAbstractModule
 
-/**
- * Binds [Environment] and [InstanceMetadata] to make both available to services and actions
- */
-class EnvironmentModule(
-  private val environment: Environment,
-  private val instanceMetadata: InstanceMetadata? = null
-) : KAbstractModule() {
+/** Binds [Environment] to make it available to services and actions */
+class EnvironmentModule(private val environment: Environment) : KAbstractModule() {
   override fun configure() {
-    val instanceMetadata = instanceMetadata ?: InstanceMetadata.fromEnvironmentVariables()
-    bind<InstanceMetadata>().toInstance(instanceMetadata)
     bind<Environment>().toInstance(environment)
   }
 }
