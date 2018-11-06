8aea874120a562c7712a2aa4b0a4e33bdf9fc740
Daniel Weisberg <dweisberg@squareup.com>
2018-11-06 14:29:05 -0800

    Added additional test

--- misk-metrics-digester/src/test/kotlin/misk/metrics/digester/RegistryTest.kt
+++ misk-metrics-digester/src/test/kotlin/misk/metrics/digester/RegistryTest.kt
@@ -6,11 +6,22 @@ import org.junit.jupiter.api.Test
 class RegistryTest {
 
   @Test
-  fun registryDigest() {
-    val registry = TDigestHistogramRegistry(
-        fun() = SlidingWindowDigest(
-            Windower(windowSecs = 30, stagger = 3), // this does not matter
-            fun() = FakeDigest()))
+  fun registyDigest() {
+    val registry = histogramRegistryFactory()
+
+    val histogram = registry.newHistogram("name", "help", listOf(), mapOf(0.1 to 0.1, 0.2 to 0.2))
+    val metric = histogram.labels()
+    metric.observe(1.23)
+    metric.observe(4.56)
+
+    val firstWindow = histogram.getMetric().digest.windows[0].digest as FakeDigest
+    assertThat(firstWindow.addedValues).isEqualTo(listOf(1.23, 4.56))
+    assertThat(histogram.count()).isEqualTo(3)
+  }
+
+  @Test
+  fun registryDigestWithLabels() {
+    val registry = histogramRegistryFactory()
 
     val histogram = registry.newHistogram("name", "help", listOf(), mapOf(0.1 to 0.1, 0.2 to 0.2))
     val metric = histogram.labels("test")
@@ -32,5 +43,12 @@ class RegistryTest {
     // Check that a new metric gets created for the new key
     assertThat(histogram.getMetric("another_test", "another_test_2")).isNotNull()
     assertThat(histogram.getMetric("another_test_2", "another_test")).isNotNull()
+  }
+
+  fun histogramRegistryFactory(): TDigestHistogramRegistry<FakeDigest> {
+    return TDigestHistogramRegistry(
+        fun() = SlidingWindowDigest(
+            Windower(windowSecs = 30, stagger = 3), // this does not matter
+            fun() = FakeDigest()))
   }
 }
