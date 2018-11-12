f2d88006aa3b6e48df4510610077bcdef3115772
Andrew (Paradi) Alexander <account+github@adrw.ch>
2018-11-12 11:22:26 -0500

    CSS + Parallel Docker Web (#627)
    * All Blueprint CSS now included
    * CSS is minified to output `styles.css` instead of `styles.js` for 16% size savings
    * [CLOSES #569] `gradle/web.gradle` now by default spins up parallel docker instances, tags output, and also sends to web/logs directory
    * Improved Docker documentation which includes which `@misk/` packages shipped in which docker image
    * `@misk/common` and `@misk/components` prepare tasks updated to use Gradle Docker builds when publishing to NPM instead of relying on local builds
    * `misk-web@0.0.5`
    * [CLOSES #631] Replace sortBy and groupBy with lodash equivalents

--- docker/build-tag-push.sh
+++ docker/build-tag-push.sh
@@ -8,9 +8,15 @@ version=$2
 
 echo "[BUILD] ${org}${name}:${version}"
 docker build --no-cache -t "${org}${name}:${version}" ./${name}
+
 echo "[TAG] ${org}${name}:latest"
 docker tag "${org}${name}:${version}" "${org}${name}:latest"
+
 echo "[PUSH] ${org}${name}:${version}"
 docker push "${org}${name}:${version}"
+
 echo "[PUSH] ${org}${name}:latest"
 docker push "${org}${name}:latest"
+
+echo "[INSPECT] ${org}${name}:${version} shipped with following @misk/ NPM packages"
+docker run -it --rm ${org}${name}:${version} yarn cache list --pattern @misk
