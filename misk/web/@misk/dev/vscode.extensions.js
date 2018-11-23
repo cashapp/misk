3f16a13d3f0d4951a5b04655128497708cf28d7a
Andrew (Paradi) Alexander <account+github@adrw.ch>
2018-11-23 11:12:17 -0500

    Migrate to Misk-Web (#658)
    * Move `@misk/` packages and web related Docker/Gradle code to [Misk-Web](https://github.com/square/misk-web)
    * [Migration Instructions](https://github.com/square/misk-web)

--- misk/web/@misk/dev/vscode.extensions.js
+++
@@ -1,9 +0,0 @@
-module.exports = () => {
-  return [
-    "EditorConfig.EditorConfig",
-    "eg2.tslint",
-    "esbenp.prettier-vscode",
-    "giladgray.theme-blueprint",
-    "timonwong.shellcheck"
-  ]
-}
