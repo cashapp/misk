bafa6936bd45cfe131eae9274161874b2f4d1c0f
Mario Acosta <maacosta@squareup.com>
2018-11-26 16:59:49 -0500

    Load secrets as referenced in config files
    as per https://github.com/square/misk/issues/660

---
+++ misk/misk/src/test/kotlin/misk/config/SecretTestConfigs.kt
@@ -0,0 +1,33 @@
+package misk.config
+
+data class SuperSecretConfig(
+  val string_value: String,
+  val secret_information: Secret<SecretInformationConfig>,
+  val consumer_a: Secret<SecretInformationConfig>,
+  val consumer_b: Secret<SecretInformationConfig>,
+  val secret_api_key: Secret<String>,
+  val secret_number: Secret<Int>,
+  val secrets_list: Secret<List<SecretInformationConfig>>,
+  val secrets_list_map: Secret<List<Map<String, Int>>>,
+  val nested_secret: Secret<NestedSecretConfig>,
+  val secret_information_wrapper: SecretInformationWrapperConfig
+) : Config
+
+data class NestedSecretConfig(val nested_nested: SecretInformationWrapperConfig) : Config
+
+data class SecretInformationWrapperConfig(val secret_information: Secret<SecretInformationConfig>) :
+    Config
+
+data class SecretInformationConfig(
+  val answer_to_universe: String,
+  val limit: Int
+) : Config
+
+// These are examples of how NOT to define secrets.
+data class SelfSecretConfigRoot(
+  override var value: SelfSecretConfigRoot
+) : Secret<SelfSecretConfigRoot>, Config
+
+data class SecretConfigRoot(
+  override var value: SecretInformationConfig
+) : Secret<SecretInformationConfig>, Config
