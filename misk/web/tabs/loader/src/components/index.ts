4ac49e422f7e9b7f4a9bfe2c909ff39550118650
Andrew (Paradi) Alexander <account+github@adrw.ch>
2018-11-26 13:53:19 -0500

    Bump Misk-Web Dependencies (#666)
    * Update Loader tab to use new `<TabLoaderComponent/>`
    
    * Only run compile dep.miskWeb in SQCI environments [#668] [square/misk-web#29]

--- misk/web/tabs/loader/src/components/index.ts
+++
@@ -1,2 +0,0 @@
-export * from "./MountingDivComponent"
-export * from "./ScriptComponent"
--- misk/web/tabs/loader/src/index.tsx
+++ misk/web/tabs/loader/src/index.tsx
@@ -1,7 +1,6 @@
 import { createApp, createIndex } from "@misk/components"
 import * as Ducks from "./ducks"
 import routes from "./routes"
-export * from "./components"
 export * from "./containers"
 
 createIndex("loader", createApp(routes), Ducks)
