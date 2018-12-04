11a0215465ce2a78e9d61adaf9960a629cedbe39
Hamdan Javeed <hamdan@squareup.com>
2018-12-04 11:10:33 -0800

    Address comments

--- misk-redis/src/main/kotlin/misk/redis/Redis.kt
+++ misk-redis/src/main/kotlin/misk/redis/Redis.kt
@@ -1,19 +1,50 @@
 package misk.redis
 
+import java.time.Duration
+
 /** A Redis client. */
 interface Redis {
-  /** Deletes a single key. */
+  /**
+   * Deletes a single key.
+   *
+   * @param key the key to delete
+   * @return 0 if the key was not deleted, 1 if the key was deleted
+   */
   fun del(key: String): Long
 
-  /** Deletes a number of keys. */
+  /**
+   * Deletes multiple keys.
+   *
+   * @param keys the keys to delete
+   * @return 0 if none of the keys were deleted, otherwise a positive integer
+   *         representing the number of keys that were deleted
+   */
   fun del(vararg keys: String): Long
 
-  /** Retrieves a key. */
+  /**
+   * Retrieves a key.
+   *
+   * @param key the key to retrieve
+   * @return a string value if the key was found, null if the key was not found
+   */
   fun get(key: String): String?
 
-  /** Sets a key. */
+  /**
+   * Sets a value for a key.
+   *
+   * @param key the key to set
+   * @param value the value to set
+   * @return the value that was set
+   */
   fun set(key: String, value: String): String
 
-  /** Sets a key with an expiration date. */
-  fun setex(key: String, seconds: Int, value: String): String
+  /**
+   * Sets a key with an expiration date.
+   *
+   * @param key the key to set
+   * @param expiryDuration the amount of time before the key expires
+   * @param value the value to set
+   * @return the value that was set
+   */
+  fun setex(key: String, expiryDuration: Duration, value: String): String
 }
