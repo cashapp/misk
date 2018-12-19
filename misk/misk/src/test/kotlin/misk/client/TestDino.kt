db31fe9ebf9ebee14a3e89aa61b1eafa1e53cf5e
Ryan Hall <rhall@squareup.com>
2018-12-19 13:15:57 -0800

    Allow installing an okhttp EventListener.Factory
    misk apps can provide a binding for EventListener.Factory if they wish
    to instrument their okhttp calls

---
+++ misk/misk/src/test/kotlin/misk/client/TestDino.kt
@@ -0,0 +1,46 @@
+package misk.client
+
+import com.google.inject.Provides
+import helpers.protos.Dinosaur
+import misk.inject.KAbstractModule
+import misk.web.Post
+import misk.web.RequestBody
+import misk.web.RequestContentType
+import misk.web.ResponseContentType
+import misk.web.actions.WebAction
+import misk.web.jetty.JettyService
+import misk.web.mediatype.MediaTypes
+import retrofit2.Call
+import retrofit2.http.Body
+import retrofit2.http.POST
+import javax.inject.Singleton
+
+internal interface ReturnADinosaur {
+  @POST("/cooldinos")
+  fun getDinosaur(@Body request: Dinosaur): Call<Dinosaur>
+}
+
+internal class ReturnADinosaurAction : WebAction {
+  @Post("/cooldinos")
+  @RequestContentType(MediaTypes.APPLICATION_JSON)
+  @ResponseContentType(MediaTypes.APPLICATION_JSON)
+  fun getDinosaur(@RequestBody request: Dinosaur): Dinosaur = request.newBuilder()
+      .name("super${request.name}")
+      .build()
+}
+
+internal class DinoClientModule(private val jetty : JettyService) : KAbstractModule() {
+  override fun configure() {
+    install(TypedHttpClientModule.create<ReturnADinosaur>("dinosaur"))
+    super.configure()
+  }
+
+  @Provides
+  @Singleton
+  fun provideHttpClientConfig(): HttpClientsConfig {
+    return HttpClientsConfig(
+        endpoints = mapOf(
+            "dinosaur" to HttpClientEndpointConfig(jetty.httpServerUrl.toString())
+        ))
+  }
+}
