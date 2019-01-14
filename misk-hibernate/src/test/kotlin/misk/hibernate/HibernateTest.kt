a5fa25d9b5d1cc949edb0e948ab2202cfd066ec7
Yunchi Luo <yunchiluo@squareup.com>
2019-01-14 16:35:53 -0500

    Hibernate.field can find inherited properties

---
+++ misk-hibernate/src/test/kotlin/misk/hibernate/HibernateTest.kt
@@ -0,0 +1,32 @@
+package misk.hibernate
+
+import org.hibernate.mapping.Property
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
+
+internal class HibernateTest {
+  @Test
+  fun inheritedFieldTest() {
+    val parentProperty = Property()
+    parentProperty.name = "parentVal"
+    val childProperty = Property()
+    childProperty.name = "childVal"
+
+    assertEquals("parentVal", field(Parent::class.java, parentProperty).name)
+    assertEquals("parentVal", field(Child::class.java, parentProperty).name)
+    assertEquals("childVal", field(Child::class.java, childProperty).name)
+
+    assertThrows<IllegalStateException> {
+      field(Parent::class.java, childProperty)
+    }
+  }
+
+  open class Parent {
+    lateinit var parentVal: String
+  }
+
+  class Child: Parent() {
+    lateinit var childVal: String
+  }
+}
