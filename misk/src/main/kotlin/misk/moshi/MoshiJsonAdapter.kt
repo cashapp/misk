f3675749e3656f6aaac40a846fb60958587ea0e8
Michael <MichaelCPell@gmail.com>
2019-01-29 17:01:58 -0800

    Updates MoshiJsonAdapter to be available externally
    I think we need to make the `MoshiJsonAdapter` annotation public, so that I can inject my custom Factory.

--- misk/src/main/kotlin/misk/moshi/MoshiJsonAdapter.kt
+++ misk/src/main/kotlin/misk/moshi/MoshiJsonAdapter.kt
@@ -5,4 +5,4 @@ import javax.inject.Qualifier
 @Qualifier
 @Retention(AnnotationRetention.RUNTIME)
 @Target(AnnotationTarget.FIELD, AnnotationTarget.FUNCTION, AnnotationTarget.VALUE_PARAMETER)
-internal annotation class MoshiJsonAdapter
+annotation class MoshiJsonAdapter
