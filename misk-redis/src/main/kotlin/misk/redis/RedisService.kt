a63d2d6d92e84969f7f3981e65927ad5a7aa37a2
Hamdan <hamdan@hamdanjaveed.com>
2019-02-07 09:46:34 -0800

    Modify RedisConfig and use JedisPool (#756)
    RedisConfig updated to include a writer and reader endpoint. We also use JedisPool
    instead of single Jedis instances to ensure thread safety.

--- misk-redis/src/main/kotlin/misk/redis/RedisService.kt
+++ misk-redis/src/main/kotlin/misk/redis/RedisService.kt
@@ -5,9 +5,7 @@ import javax.inject.Inject
 import javax.inject.Provider
 import javax.inject.Singleton
 
-/**
- * Controls the connection lifecycle for Redis.
- */
+/** Controls the connection lifecycle for Redis. */
 @Singleton
 internal class RedisService @Inject internal constructor(
   private val redisProvider: Provider<Redis>
