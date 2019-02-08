b790d9265866ed052554eb99a543f55a18a0c854
Alec Holmes <alec@squareup.com>
2019-02-07 16:25:09 -0800

    Add tag support to TracerExt

--- misk/misk/src/main/kotlin/misk/tracing/TracerExt.kt
+++ misk/misk/src/main/kotlin/misk/tracing/TracerExt.kt
@@ -4,12 +4,19 @@ import io.opentracing.Span
 import io.opentracing.Tracer
 import io.opentracing.tag.Tags
 
-/** [trace] traces the given function with the specific span name */
-fun <T : Any?> Tracer.trace(spanName: String, f: () -> T): T = traceWithSpan(spanName) { _ -> f() }
+/** [trace] traces the given function with the specific span name and optional tags */
+fun <T : Any?> Tracer.trace(spanName: String, tags: Map<String, String> = mapOf(), f: () -> T): T =
+    traceWithSpan(spanName, tags) { f() }
 
 /** [trace] traces the given function, passing the span into the function */
-fun <T : Any?> Tracer.traceWithSpan(spanName: String, f: (Span) -> T): T {
+fun <T : Any?> Tracer.traceWithSpan(
+  spanName: String,
+  tags: Map<String, String> = mapOf(),
+  f: (Span) -> T
+): T {
   val spanBuilder = buildSpan(spanName)
+  tags.forEach { k, v -> spanBuilder.withTag(k, v) }
+
   activeSpan()?.let { spanBuilder.asChildOf(it) }
 
   val scope = spanBuilder.startActive(true)
