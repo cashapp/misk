dab6b828a7f0a3ac180fbd65bd4ba47a00a280d8
Hamdan Javeed <hamdan@squareup.com>
2019-02-12 00:42:38 -0800

    Add functions to get/set values as ByteArrays in misk-redis.
    These functions can be used to store arbitrary arrays of bytes in Redis. A service may
    want to do this to store protos for example.

--- misk-redis/src/main/kotlin/misk/redis/RealRedis.kt
+++ misk-redis/src/main/kotlin/misk/redis/RealRedis.kt
@@ -1,42 +1,54 @@
 package misk.redis
 
+import okio.ByteString
+import okio.ByteString.Companion.toByteString
 import redis.clients.jedis.JedisPool
 import java.time.Duration
 
 /** For each command, a Jedis instance is retrieved from the pool and returned once the command has been issued. */
 class RealRedis(private val jedisPool: JedisPool) : Redis {
+  // Delete key
   override fun del(key: String): Boolean {
     jedisPool.resource.use { jedis ->
       return (jedis.del(key) == 1L)
     }
   }
 
+  // Delete multiple keys
   override fun del(vararg keys: String): Int {
     jedisPool.resource.use { jedis ->
       return jedis.del(*keys).toInt()
     }
   }
 
-  override fun get(key: String): String? {
+  // Get a ByteString value
+  override fun get(key: String): ByteString? {
     jedisPool.resource.use { jedis ->
-      return jedis.get(key)
+      return jedis.get(key.toByteArray(charset)).toByteString()
     }
   }
 
-  override fun set(key: String, value: String): String {
+  // Set a ByteArray value
+  override fun set(key: String, value: ByteString) {
     jedisPool.resource.use { jedis ->
-      return jedis.set(key, value)
+      jedis.set(key.toByteArray(charset), value.toByteArray())
     }
   }
 
-  override fun set(key: String, expiryDuration: Duration, value: String): String {
+  // Set a ByteArray value with an expiration
+  override fun set(key: String, expiryDuration: Duration, value: ByteString) {
     jedisPool.resource.use { jedis ->
-      return jedis.setex(key, expiryDuration.seconds.toInt(), value)
+      jedis.setex(key.toByteArray(charset), expiryDuration.seconds.toInt(), value.toByteArray())
     }
   }
 
   /** Closes the connection to Redis. */
   fun close() {
     return jedisPool.close()
+  }
+
+  companion object {
+    /** The charset used to convert String keys to ByteArrays for Jedis commands. */
+    private val charset = Charsets.UTF_8
   }
 }
