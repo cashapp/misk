da1403602a105ce24a6c0e4276608e53ce4f67a7
Wesley Kim <wesleyjkim@gmail.com>
2019-03-05 23:26:19 -0500

    Include allowed services/roles in WebActionMetadata API (#819)

---
+++ misk/src/test/kotlin/misk/web/actions/TestWebActionModule.kt
@@ -0,0 +1,60 @@
+package misk.web.actions
+
+import misk.MiskCaller
+import misk.inject.KAbstractModule
+import misk.scope.ActionScoped
+import misk.security.authz.AccessAnnotationEntry
+import misk.security.authz.AccessControlModule
+import misk.security.authz.FakeCallerAuthenticator
+import misk.security.authz.MiskCallerAuthenticator
+import misk.web.Get
+import misk.web.ResponseContentType
+import misk.web.WebTestingModule
+import misk.web.mediatype.MediaTypes
+import misk.web.toResponseBody
+import javax.inject.Inject
+
+// Common module for web action-related tests to use to use that bind up some sample web actions
+class TestWebActionModule : KAbstractModule() {
+  override fun configure() {
+    install(WebTestingModule())
+    install(AccessControlModule())
+
+    multibind<WebActionEntry>().toInstance(WebActionEntry<CustomServiceAccessAction>())
+    multibind<WebActionEntry>().toInstance(WebActionEntry<CustomRoleAccessAction>())
+
+    multibind<AccessAnnotationEntry>().toInstance(
+        AccessAnnotationEntry<CustomServiceAccess>(services = listOf("payments")))
+    multibind<AccessAnnotationEntry>().toInstance(
+        AccessAnnotationEntry<CustomRoleAccess>(roles = listOf("admin")))
+    multibind<MiskCallerAuthenticator>().to<FakeCallerAuthenticator>()
+  }
+
+  class CustomServiceAccessAction : WebAction {
+    @Inject
+    lateinit var scopedCaller: ActionScoped<MiskCaller?>
+
+    @Get("/custom_service_access")
+    @ResponseContentType(MediaTypes.TEXT_PLAIN_UTF8)
+    @CustomServiceAccess
+    fun get() = "${scopedCaller.get()} authorized as custom service".toResponseBody()
+  }
+
+  @Retention(AnnotationRetention.RUNTIME)
+  @Target(AnnotationTarget.FUNCTION)
+  annotation class CustomServiceAccess
+
+  class CustomRoleAccessAction : WebAction {
+    @Inject
+    lateinit var scopedCaller: ActionScoped<MiskCaller?>
+
+    @Get("/custom_role_access")
+    @ResponseContentType(MediaTypes.TEXT_PLAIN_UTF8)
+    @CustomRoleAccess
+    fun get() = "${scopedCaller.get()} authorized as custom role".toResponseBody()
+  }
+
+  @Retention(AnnotationRetention.RUNTIME)
+  @Target(AnnotationTarget.FUNCTION)
+  annotation class CustomRoleAccess
+}
