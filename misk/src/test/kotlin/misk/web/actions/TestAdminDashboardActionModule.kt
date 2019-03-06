46bc6cd7ac05ab61d10cbc19cbc8676677a26152
Yunchi Luo <mightyguava@gmail.com>
2019-03-06 16:04:14 -0500

    ConfigAdminAction reads the full loaded Config (#821)

--- misk/src/test/kotlin/misk/web/actions/TestAdminDashboardActionModule.kt
+++ misk/src/test/kotlin/misk/web/actions/TestAdminDashboardActionModule.kt
@@ -1,6 +1,7 @@
 package misk.web.actions
 
 import misk.config.AppName
+import misk.config.Config
 import misk.environment.Environment
 import misk.inject.KAbstractModule
 import misk.security.authz.AccessAnnotationEntry
@@ -14,7 +15,10 @@ class TestAdminDashboardActionModule : KAbstractModule() {
     install(AdminDashboardModule(Environment.TESTING))
     multibind<AccessAnnotationEntry>()
         .toInstance(AccessAnnotationEntry<AdminDashboardAccess>(roles = listOf("engineers")))
+    bind<Config>().toInstance(TestAdminDashboardConfig())
     // TODO(wesley): Remove requirement for AppName to bind AdminDashboard APIs
     bind<String>().annotatedWith<AppName>().toInstance("testApp")
   }
 }
+
+class TestAdminDashboardConfig : Config
