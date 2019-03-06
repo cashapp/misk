80351c28c1ed021ad0cf002f65f558f4652868c0
Yunchi Luo <yunchiluo@squareup.com>
2019-03-06 13:59:44 -0500

    remove some redundant docs

--- misk/src/main/kotlin/misk/web/WebActionModule.kt
+++ misk/src/main/kotlin/misk/web/WebActionModule.kt
@@ -16,16 +16,8 @@ class WebActionModule<A : WebAction> private constructor(
   }
 
   companion object {
-    /**
-     * Registers a web action.
-     * @param actionClass: The web action to register.
-     */
     inline fun <reified A : WebAction> create(): WebActionModule<A> = create(A::class)
 
-    /**
-     * Registers a web action.
-     * @param actionClass: The web action to register.
-     */
     @JvmStatic
     fun <A : WebAction> create(actionClass: Class<A>): WebActionModule<A> {
       return create(actionClass.kotlin)
@@ -33,29 +25,14 @@ class WebActionModule<A : WebAction> private constructor(
 
     /**
      * Registers a web action.
+     * @param actionClass: The web action to register.
      */
     fun <A : WebAction> create(actionClass: KClass<A>): WebActionModule<A> {
       return WebActionModule(actionClass, "/")
     }
-
-    /**
-     * Registers a web action with a path prefix.
-     * @param url_path_prefix: defaults to "/". If not empty, must match pattern requirements:
-     *   - must begin with "/"
-     *   - any number of non-whitespace characters (including additional path segments or "/")
-     *   - must terminate with a non-"/" because rest of path will start with "/"
-     */
     inline fun <reified A : WebAction> createWithPrefix(url_path_prefix: String): WebActionModule<A> =
         createWithPrefix(A::class, url_path_prefix)
 
-    /**
-     * Registers a web action with a path prefix.
-     * @param actionClass: The web action to register.
-     * @param url_path_prefix: Defaults to "/". If not empty, must match pattern requirements:
-     *   - must begin with "/"
-     *   - any number of non-whitespace characters (including additional path segments or "/")
-     *   - must terminate with a non-"/" because rest of path will start with "/"
-     */
     @JvmStatic
     fun <A : WebAction> createWithPrefix(
       actionClass: Class<A>,
