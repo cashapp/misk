2f5b2a39dd36133bab16fd36e154b80511c8f3b2
Wesley Kim <wesleyjkim@gmail.com>
2019-03-07 12:13:32 -0800

    Introduce TestAdminDashboardModule that test environments can actually install (#828)

--- misk/src/test/kotlin/misk/web/actions/TestAdminDashboardActionModule.kt
+++ misk/src/test/kotlin/misk/web/actions/TestAdminDashboardActionModule.kt
@@ -4,17 +4,13 @@ import misk.config.AppName
 import misk.config.Config
 import misk.environment.Environment
 import misk.inject.KAbstractModule
-import misk.security.authz.AccessAnnotationEntry
-import misk.web.metadata.AdminDashboardAccess
-import misk.web.metadata.AdminDashboardModule
+import misk.web.metadata.AdminDashboardTestingModule
 
 // Common test module used to be able to test admin dashboard WebActions
 class TestAdminDashboardActionModule : KAbstractModule() {
   override fun configure() {
     install(TestWebActionModule())
-    install(AdminDashboardModule(Environment.TESTING))
-    multibind<AccessAnnotationEntry>()
-        .toInstance(AccessAnnotationEntry<AdminDashboardAccess>(roles = listOf("engineers")))
+    install(AdminDashboardTestingModule(Environment.TESTING))
     bind<Config>().toInstance(TestAdminDashboardConfig())
     // TODO(wesley): Remove requirement for AppName to bind AdminDashboard APIs
     bind<String>().annotatedWith<AppName>().toInstance("testApp")
