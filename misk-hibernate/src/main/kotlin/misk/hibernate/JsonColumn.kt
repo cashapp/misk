409c31e8dbef00ca19dc62e1c446dbd06dd9223a
Sye van der Veen <svanderveen@squareup.com>
2019-03-08 10:28:44 -0500

    JsonColumn: Document how to support custom types

--- misk-hibernate/src/main/kotlin/misk/hibernate/JsonColumn.kt
+++ misk-hibernate/src/main/kotlin/misk/hibernate/JsonColumn.kt
@@ -1,8 +1,40 @@
 package misk.hibernate
 
 /**
- * Put this on a column field to get Hibernate to persist it as JSON using Moshi. It will use the
- * injector's configured Moshi instance. Use [misk.moshi.MoshiModule] to customize.
+ * Put this on a column field to get Hibernate to persist it as JSON using Moshi and the injector's
+ * configured JSON adapters.
+ *
+ * By default, the configured adapters support Kotlin's basic types, as well as [List]s, [Set]s, and
+ * [Map]s of supported types. Hibernate will raise an exception during initialization if there are
+ * no adapters for the field type. You can add support for custom types by calling
+ * [misk.inject.KAbstractModule.install] with [misk.moshi.MoshiAdapterModule] as part of your module
+ * initialization:
+ *
+ * ```
+ * install(MoshiAdapterModule(MyCustomClass.JsonAdapter))
+ * ```
+ *
+ * The configured adapters do _not_ support [Any]. To get around this, define a custom type for
+ * the field:
+ *
+ * ```
+ * class MyCustomClass(
+ *    private val map: Map<String, Any?>
+ * ) : Map<String, Any?> by map {
+ *   object JsonAdapter {
+ *     private val moshiAdapter =
+ *         Moshi.Builder().build().adapter(MyCustomClass::class.java)!!
+ *
+ *     @FromJson
+ *     fun fromJson(reader: JsonReader): MyCustomClass? =
+ *         moshiAdapter.fromJson(reader)
+ *
+ *     @ToJson
+ *     fun toJson(writer: JsonWriter, value: MyCustomClass?): Unit =
+ *         moshiAdapter.toJson(writer, value)
+ *   }
+ * }
+ * ```
  *
  * It is an error to put this annotation on a mutable field. We don't defensively copy these when
  * we read them out of the database.
