c7f85ebdcb1c61b99e76edee555340661c947972
Wesley Kim <wesleyjkim@gmail.com>
2019-03-14 06:11:30 -0700

    New ClassNameFormatter that handles formatting a valid class name (#851)

---
+++ misk/misk/src/main/kotlin/misk/web/formatter/ClassNameFormatter.kt
@@ -0,0 +1,13 @@
+package misk.web.formatter
+
+import javax.inject.Inject
+import kotlin.reflect.KClass
+
+class ClassNameFormatter @Inject constructor() {
+  fun <T : Any> format(kclass: KClass<T>): String {
+    return when (kclass.qualifiedName) {
+      null -> kclass.toString().split("class ").last()
+      else -> kclass.qualifiedName.toString()
+    }
+  }
+}
