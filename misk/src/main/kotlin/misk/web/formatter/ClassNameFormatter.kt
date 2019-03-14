a08a4aefe6b5ec028aa78f3a3adb3991a0893d3d
Wesley Kim <wesleyjkim@gmail.com>
2019-03-14 12:56:38 -0700

    Format application/network interceptor names for WebActionMetadata (#853)

--- misk/src/main/kotlin/misk/web/formatter/ClassNameFormatter.kt
+++ misk/src/main/kotlin/misk/web/formatter/ClassNameFormatter.kt
@@ -4,10 +4,12 @@ import javax.inject.Inject
 import kotlin.reflect.KClass
 
 class ClassNameFormatter @Inject constructor() {
-  fun <T : Any> format(kclass: KClass<T>): String {
-    return when (kclass.qualifiedName) {
-      null -> kclass.toString().split("class ").last()
-      else -> kclass.qualifiedName.toString()
+  companion object {
+    fun <T : Any> format(kclass: KClass<T>): String {
+      return when (kclass.qualifiedName) {
+        null -> kclass.toString().split("class ").last()
+        else -> kclass.qualifiedName.toString()
+      }
     }
   }
 }
