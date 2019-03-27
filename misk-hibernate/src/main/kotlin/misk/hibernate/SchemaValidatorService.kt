dd05f3349bd64611612358dae943cfab0970fd2f
Ryan Hall <rhall@squareup.com>
2019-03-27 08:36:46 -0700

    Block consumers of SchemaMigratorService until vitess starts
    StartVitessService takes care of running migrations in tests, however,
    it does not produce the SchemaMigratorService key that other services
    rely on, e.g SchemaValidatorService. This introduces a flake where the
    validator might run before the migrator.
    
    This required splitting the SchemaMigratorService into 2 services, one
    that performs migrations in tests and one that validates migrations have
    been run in prod envs.

--- misk-hibernate/src/main/kotlin/misk/hibernate/SchemaValidatorService.kt
+++ misk-hibernate/src/main/kotlin/misk/hibernate/SchemaValidatorService.kt
@@ -13,14 +13,10 @@ import kotlin.reflect.KClass
 internal class SchemaValidatorService internal constructor(
   qualifier: KClass<out Annotation>,
   private val sessionFactoryServiceProvider: Provider<SessionFactoryService>,
-  private val transacterProvider: Provider<Transacter>,
-  private val config: DataSourceConfig
+  private val transacterProvider: Provider<Transacter>
 ) : AbstractIdleService(), DependentService {
 
-  override val consumedKeys = setOf<Key<*>>(
-      SchemaMigratorService::class.toKey(qualifier),
-      SessionFactoryService::class.toKey(qualifier)
-  )
+  override val consumedKeys = setOf<Key<*>>(SchemaMigratorService::class.toKey(qualifier))
   override val producedKeys = setOf<Key<*>>(SchemaValidatorService::class.toKey(qualifier))
 
   override fun startUp() {
