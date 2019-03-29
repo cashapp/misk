94223307e89a5e22ecb5e91687024abc1596dee7
Andrew (Paradi) Alexander <account+github@adrw.ch>
2019-03-29 14:27:17 -0400

    Web Actions Tab: Include type in placeholder (#881)

--- misk/misk/web/tabs/web-actions/src/components/RequestFormComponent.tsx
+++ misk/misk/web/tabs/web-actions/src/components/RequestFormComponent.tsx
@@ -4,6 +4,7 @@ import {
   ControlGroup,
   FormGroup,
   InputGroup,
+  Intent,
   TextArea,
   Tooltip,
   Intent
@@ -148,6 +149,7 @@ const RequestFormField = (props: IFieldProps & IState & IDispatchProps) => {
               props.simpleFormInput,
               `${tag}::${nestPath}${id}::Data`
             )}
+            placeholder={type}
             value={simpleSelect(
               props.simpleForm,
               `${tag}::${nestPath}${id}::Data`,
@@ -169,6 +171,7 @@ const RequestFormField = (props: IFieldProps & IState & IDispatchProps) => {
               props.simpleFormInput,
               `${tag}::${nestPath}${id}::Data`
             )}
+            placeholder={`${type}\nDrag bottom right corner of text area input to expand.`}
             value={simpleSelect(
               props.simpleForm,
               `${tag}::${nestPath}${id}::Data`,
