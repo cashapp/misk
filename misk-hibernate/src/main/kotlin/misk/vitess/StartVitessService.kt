f030fae38977d047cfe52f1bc9db0fb7e190906f
Jon Tirsen <jontirsen@squareup.com>
2019-04-05 08:15:24 +1100

    Extend transaction timeout for debugging

--- misk-hibernate/src/main/kotlin/misk/vitess/StartVitessService.kt
+++ misk-hibernate/src/main/kotlin/misk/vitess/StartVitessService.kt
@@ -42,7 +42,7 @@ import kotlin.concurrent.thread
 import kotlin.reflect.KClass
 import kotlin.streams.toList
 
-const val VITESS_VERSION = "sha256:3cef0e042dee2312e5e0d80c56a6bf1581b52063ad0441551241f6785b110c38"
+const val VITESS_IMAGE = "vitess/base@sha256:ad6d22aafa73c9bb64cebf6dffe5f82df7cfcded00cf801fd4e64d0f46dbab43"
 const val CONTAINER_NAME_PREFIX = "misk-vitess-testing"
 
 class Keyspace(val sharded: Boolean) {
@@ -177,6 +177,9 @@ class DockerVitessCluster(
         "-mysql_bind_host=0.0.0.0",
         "-data_dir=/vt/vtdataroot",
         "-schema_dir=schema",
+        // Increase the transaction timeout so you can have a breakpoint
+        // inside a transaction without it timing out
+        "-queryserver-config-transaction-timeout=${Duration.ofHours(24).toMillis()}",
         "-extra_my_cnf=/vt/src/vitess.io/vitess/config/mycnf/rbr.cnf",
         "-keyspaces=$keyspacesArg",
         "-num_shards=$shardCounts"
@@ -196,7 +199,7 @@ class DockerVitessCluster(
     } else {
       StartVitessService.logger.info(
           "Starting Vitess cluster with command: ${cmd.joinToString(" ")}")
-      containerId = docker.createContainerCmd("vitess/base@$VITESS_VERSION")
+      containerId = docker.createContainerCmd(VITESS_IMAGE)
           .withCmd(cmd.toList())
           .withVolumes(schemaVolume)
           .withBinds(Bind(cluster.schemaDir.toAbsolutePath().toString(), schemaVolume))
@@ -352,9 +355,10 @@ class StartVitessService(
   init {
     // We need to do this outside of the service start up because this takes a really long time
     // the first time you do it and can cause service manager to time out.
-    if (shouldRunVitess() && imagePulled.compareAndSet(false, true) &&
-        runCommand("docker pull vitess/base@$VITESS_VERSION") != 0) {
-      logger.warn("Failed to pull Vitess docker image. Proceeding regardless.")
+    if (shouldRunVitess() && imagePulled.compareAndSet(false, true)) {
+      if (runCommand("docker pull $VITESS_IMAGE") != 0) {
+        logger.warn("Failed to pull Vitess docker image. Proceeding regardless.")
+      }
     }
   }
 
@@ -422,8 +426,7 @@ class StartVitessService(
     fun startVitessDaemon(
       /** The same qualifier passed into [HibernateModule], used to uniquely name the container */
       qualifier: KClass<out Annotation>,
-      /** Config for the Vitess clu
-       * ster */
+      /** Config for the Vitess cluster */
       config: DataSourceConfig
     ) {
       val docker: DockerClient = DockerClientBuilder.getInstance()
