77e0490ebf8fc93efa951c33c61c97b25b59be31
Sye van der Veen <svanderveen@squareup.com>
2019-04-04 08:33:58 -0400

    VitessScaleSafetyChecks: Support multiple independent transactions at the same time

--- misk-hibernate/src/test/kotlin/misk/hibernate/ScaleSafetyTest.kt
+++ misk-hibernate/src/test/kotlin/misk/hibernate/ScaleSafetyTest.kt
@@ -9,6 +9,8 @@ import misk.jdbc.TableScanException
 import misk.jdbc.uniqueLong
 import misk.testing.MiskTest
 import misk.testing.MiskTestModule
+import org.hibernate.SessionFactory
+import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertThrows
 import java.time.LocalDate
@@ -22,6 +24,7 @@ class ScaleSafetyTest {
   @MiskTestModule
   val module = MoviesTestModule()
 
+  @Inject @Movies lateinit var sessionFactory: SessionFactory
   @Inject @Movies lateinit var transacter: Transacter
   @Inject lateinit var queryFactory: Query.Factory
 
@@ -29,8 +32,7 @@ class ScaleSafetyTest {
   fun crossShardTransactions() {
     val jg = transacter.save(
         DbActor("Jeff Goldblum", LocalDate.of(1952, 10, 22)))
-    val cf = transacter.save(
-        DbActor("Carrie Fisher", null))
+    val cf = transacter.save(DbActor("Carrie Fisher"))
 
     val jp = transacter.save(
         DbMovie("Jurassic Park", LocalDate.of(1993, 6, 9)))
@@ -62,8 +64,7 @@ class ScaleSafetyTest {
   fun transactionsSpanningEntityGroupsInTheSameShard() {
     val jg = transacter.save(
         DbActor("Jeff Goldblum", LocalDate.of(1952, 10, 22)))
-    val cf = transacter.save(
-        DbActor("Carrie Fisher", null))
+    val cf = transacter.save(DbActor("Carrie Fisher"))
 
     val jp = transacter.save(
         DbMovie("Jurassic Park", LocalDate.of(1993, 6, 9)))
@@ -85,6 +86,66 @@ class ScaleSafetyTest {
         swEntity.release_date = LocalDate.now()
       }
     }
+  }
+
+  @Test
+  fun concurrentIndependentTransactions() {
+    // misk.hibernate.Transacter.transaction prevents nested transactions, and with good reason.
+    // However, in some very specific, Misk-internal cases, org.hibernate.SessionFactory can be used
+    // to spin up a new, independent transaction while another is ongoing.
+    val jg = transacter.save(
+        DbActor("Jeff Goldblum", LocalDate.of(1952, 10, 22)))
+    val cf = transacter.save(DbActor("Carrie Fisher"))
+    val ld = transacter.save(DbActor("Laura Dern"))
+    val sn = transacter.save(DbActor("Sam Neill"))
+
+    val jp = transacter.save(
+        DbMovie("Jurassic Park", LocalDate.of(1993, 6, 9)))
+    val sw = transacter.save(
+        DbMovie("Star Wars: The Last Jedi", LocalDate.of(2017, 12, 15)))
+
+    transacter.transaction { session ->
+      session.save(DbCharacter("Dr. Ian Malcolm", session.load(jp), session.load(jg)))
+
+      // The Hibernate session can operate on a different entity group. (This would normally be
+      // done in some sort of library call, where the Misk session is intentionally omitted.)
+      hibernateTransaction { hSession ->
+        hSession.save(DbCharacter("Leia Organa",
+            hSession.load(DbMovie::class.java, sw), hSession.load(DbActor::class.java, cf)))
+      }
+    }
+    assertEquals(setOf("Dr. Ian Malcolm"), movieCharacterNames(jp))
+    assertEquals(setOf("Leia Organa"), movieCharacterNames(sw))
+
+    // Regardless, one Hibernate session still cannot operate on two entity groups.
+    assertThrows<CowriteException> {
+      hibernateTransaction { hSession ->
+        hSession.save(DbCharacter("Dr. Ellie Sattler",
+            hSession.load(DbMovie::class.java, jp), hSession.load(DbActor::class.java, ld)))
+        hSession.save(DbCharacter("Vice Admiral Holdo",
+            hSession.load(DbMovie::class.java, sw), hSession.load(DbActor::class.java, ld)))
+      }
+    }
+    assertEquals(setOf("Dr. Ian Malcolm"), movieCharacterNames(jp))
+    assertEquals(setOf("Leia Organa"), movieCharacterNames(sw))
+
+    // Further, we should still detect cowrites in a Misk session even if a Hibernate session
+    // is used in the middle of it, and that Hibernate session is completely independent of any
+    // other transaction.
+    assertThrows<CowriteException> {
+      transacter.transaction { session ->
+        session.save(DbCharacter("Dr. Ellie Sattler", session.load(jp), session.load(ld)))
+
+        hibernateTransaction { hSession ->
+          hSession.save(DbCharacter("Dr. Alan Grant",
+              hSession.load(DbMovie::class.java, jp), hSession.load(DbActor::class.java, sn)))
+        }
+
+        session.save(DbCharacter("Vice Admiral Holdo", session.load(sw), session.load(ld)))
+      }
+    }
+    assertEquals(setOf("Dr. Ian Malcolm", "Dr. Alan Grant"), movieCharacterNames(jp))
+    assertEquals(setOf("Leia Organa"), movieCharacterNames(sw))
   }
 
   @Test
@@ -139,6 +200,27 @@ class ScaleSafetyTest {
       }
     }
   }
+
+  private fun movieCharacterNames(sw: Id<DbMovie>) = transacter.transaction { session ->
+    queryFactory.newQuery<CharacterQuery>().movieId(sw).list(session).map { it.name }.toSet()
+  }
+
+  private fun <R> hibernateTransaction(block: (hSession: org.hibernate.Session) -> R): R =
+      sessionFactory.openSession().use { hSession ->
+        val transaction = hSession.beginTransaction()
+        try {
+          val result = block(hSession)
+          transaction.commit()
+          return result
+        } catch (e: Throwable) {
+          try {
+            if (transaction.isActive) transaction.rollback()
+          } catch (suppressed: Exception) {
+            e.addSuppressed(suppressed)
+          }
+          throw e
+        }
+      }
 }
 
 private fun <T : DbEntity<T>> Transacter.save(entity: T): Id<T> = transaction { it.save(entity) }
