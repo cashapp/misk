cd12f540d4f327502d4a22b6f4f012e4dc3865c5
Yoav Amit <yoav.amit@gmail.com>
2019-04-24 11:29:41 -0400

    Added support for using more Tink primitives (Mac) (#927)
    * Removed the Tink interface wrapper
    
    And added support for using more Tink primitives (Mac)
    
    * added extension functions
    
    This makes it easier to deal with ByteStrings and make sure we don't
    leave any extra copies of plaintext data in memory.
    
    * split key manager to 2 classes - for aead and for mac
    
    * updated readme file

--- misk-crypto/src/main/kotlin/misk/crypto/KeyManager.kt
+++ misk-crypto/src/main/kotlin/misk/crypto/KeyManager.kt
@@ -1,21 +1,70 @@
 package misk.crypto
 
+import com.google.crypto.tink.Aead
+import com.google.crypto.tink.Mac
+import com.google.inject.ConfigurationException
 import com.google.inject.Inject
+import com.google.inject.Injector
+import com.google.inject.Key
+import com.google.inject.name.Names
 import javax.inject.Singleton
 
 /**
- * Singleton class used as a map of key name to its corresponding [Cipher] implementation.
+ * Holds a map of every [Aead] key name to its primitive listed in the configuration for this app.
+ * Users may use this object to obtain an [Aead] dynamically:
+ * ```
+ * val myKey: Aead? = aeadKeyManager["myKey"]
+ * ```
  */
 @Singleton
-class KeyManager @Inject constructor() {
+class AeadKeyManager @Inject internal constructor(
+  private val injector: Injector
+) {
+  private val aeads: HashMap<String, Aead> = LinkedHashMap()
 
-  private var keys: HashMap<String, Cipher> = LinkedHashMap()
-
-  internal operator fun set(name: String, cipher: Cipher) {
-    keys[name] = cipher
+  internal operator fun set(name: String, aead: Aead) {
+    aeads[name] = aead
   }
 
-  operator fun get(name: String): Cipher? {
-    return keys[name]
+  operator fun get(name: String): Aead? {
+    var aead = aeads[name]
+    if (aead == null) {
+      try {
+        aead = injector.getInstance(Key.get(Aead::class.java, Names.named(name)))
+        this[name] = aead
+      } catch (e: ConfigurationException) {
+      }
+    }
+    return aead
+  }
+}
+
+/**
+ * Holds a map of every [Mac] key name to its primitive listed in the configuration for this app.
+ * Users may use htis objecy to obtain a [Mac] object dynamically:
+ * ```
+ * val hmac: Mac? = macKeyManager["myHmac"]
+ * ```
+ */
+@Singleton
+class MacKeyManager @Inject internal constructor(
+  private val injector: Injector
+) {
+  private val macs: HashMap<String, Mac> = LinkedHashMap()
+
+  internal operator fun set(name: String, mac: Mac) {
+    macs[name] = mac
+  }
+
+  operator fun get(name: String): Mac? {
+    var mac = macs[name]
+    if (mac == null) {
+      try {
+        mac = injector.getInstance(Key.get(Mac::class.java, Names.named(name)))
+        this[name] = mac
+      } catch (e: ConfigurationException) {
+      }
+    }
+    return mac
   }
 }
