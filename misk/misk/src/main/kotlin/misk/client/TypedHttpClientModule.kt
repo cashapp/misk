cd504e2cf05331164d4175233bc7d11cc9ec35ab
Alec Holmes <alecholmes310@gmail.com>
2019-05-14 11:17:25 -0700

    Add clients for calling other members in an app's cluster (#973)

--- misk/misk/src/main/kotlin/misk/client/TypedHttpClientModule.kt
+++ misk/misk/src/main/kotlin/misk/client/TypedHttpClientModule.kt
@@ -4,8 +4,10 @@ import com.google.inject.Inject
 import com.google.inject.Key
 import com.google.inject.Provider
 import com.google.inject.name.Names
+import com.google.inject.util.Types
 import com.squareup.moshi.Moshi
 import io.opentracing.Tracer
+import misk.clustering.Cluster
 import misk.inject.KAbstractModule
 import okhttp3.EventListener
 import okhttp3.OkHttpClient
@@ -46,58 +48,124 @@ class TypedHttpClientModule<T : Any>(
   }
 
   private class TypedClientProvider<T : Any>(
-    private val kclass: KClass<T>,
+    kclass: KClass<T>,
     private val name: String,
     private val httpClientProvider: Provider<OkHttpClient>
-  ) : Provider<T> {
-    @Inject
-    private lateinit var httpClientsConfig: HttpClientsConfig
+  ) : TypedClientFactory<T>(kclass, name), Provider<T> {
 
-    // Use Providers for the interceptors so Guice can properly detect cycles when apps inject
-    // an HTTP Client in an Interceptor.
-    // https://gist.github.com/ryanhall07/e3eac6d2d47b72a4c37bce87219d7ced
-    @Inject
-    private lateinit var clientNetworkInterceptorFactories: Provider<List<ClientNetworkInterceptor.Factory>>
-
-    @Inject
-    private lateinit var clientApplicationInterceptorFactories: Provider<List<ClientApplicationInterceptor.Factory>>
-
-    @Inject
-    private lateinit var moshi: Moshi
-
-    @Inject(optional = true)
-    private val tracer: Tracer? = null
-
-    @Inject(optional = true)
-    private val eventListenerFactory: EventListener.Factory? = null
-
-    @Inject
-    private lateinit var httpClientConfigUrlProvider: HttpClientConfigUrlProvider
+    @Inject private lateinit var httpClientsConfig: HttpClientsConfig
+    @Inject private lateinit var httpClientConfigUrlProvider: HttpClientConfigUrlProvider
 
     override fun get(): T {
-      val okhttp = httpClientProvider.get()
+      val client = httpClientProvider.get()
       val endpointConfig = httpClientsConfig[name]
-      val retrofit = Retrofit.Builder()
-          .baseUrl(httpClientConfigUrlProvider.getUrl(endpointConfig))
-          .build()
+      val baseUrl = httpClientConfigUrlProvider.getUrl(endpointConfig)
 
-      val invocationHandler = ClientInvocationHandler(
-          kclass,
-          name,
-          retrofit,
-          okhttp,
-          clientNetworkInterceptorFactories,
-          clientApplicationInterceptorFactories,
-          eventListenerFactory,
-          tracer,
-          moshi)
-
-      @Suppress("UNCHECKED_CAST")
-      return Proxy.newProxyInstance(
-          ClassLoader.getSystemClassLoader(),
-          arrayOf(kclass.java),
-          invocationHandler
-      ) as T
+      return typedClient(client, baseUrl)
     }
+  }
+}
+
+/**
+ * Factory for creating typed clients that call other members of a cluster.
+ */
+interface TypedPeerClientFactory<T> {
+  fun client(peer: Cluster.Member): T
+}
+
+/**
+ * Creates a retrofit-backed typed client factory given an API interface and an HTTP configuration.
+ *
+ * The factory returned typed clients that can be used to call other members of the cluster.
+ */
+class TypedPeerHttpClientModule<T : Any>(
+  private val kclass: KClass<T>,
+  private val name: String
+) : KAbstractModule() {
+
+  override fun configure() {
+    // Initialize empty sets for our multibindings.
+    newMultibinder<ClientNetworkInterceptor.Factory>()
+    newMultibinder<ClientApplicationInterceptor.Factory>()
+
+    @Suppress("UNCHECKED_CAST")
+    val key = Key.get(
+        Types.newParameterizedType(TypedPeerClientFactory::class.java,
+            kclass.java)) as Key<TypedPeerClientFactory<T>>
+
+    bind(key).toProvider(PeerTypedClientProvider(kclass, name))
+  }
+
+  companion object {
+    inline fun <reified T : Any> create(name: String): TypedPeerHttpClientModule<T> {
+      return TypedPeerHttpClientModule(T::class, name)
+    }
+  }
+
+  private class PeerTypedClientProvider<T : Any>(
+    kclass: KClass<T>,
+    name: String
+  ) : TypedClientFactory<T>(kclass, name), Provider<TypedPeerClientFactory<T>> {
+
+    @Inject private lateinit var peerClientFactory: PeerClientFactory
+
+    override fun get(): TypedPeerClientFactory<T> {
+      return object : TypedPeerClientFactory<T> {
+        override fun client(peer: Cluster.Member): T {
+          return typedClient(peerClientFactory.client(peer), peerClientFactory.baseUrl(peer))
+        }
+      }
+    }
+  }
+}
+
+private abstract class TypedClientFactory<T : Any>(
+  private val kclass: KClass<T>,
+  private val name: String
+) {
+
+  @Inject
+  private lateinit var httpClientsConfig: HttpClientsConfig
+
+  // Use Providers for the interceptors so Guice can properly detect cycles when apps inject
+  // an HTTP Client in an Interceptor.
+  // https://gist.github.com/ryanhall07/e3eac6d2d47b72a4c37bce87219d7ced
+  @Inject
+  private lateinit var clientNetworkInterceptorFactories: Provider<List<ClientNetworkInterceptor.Factory>>
+
+  @Inject
+  private lateinit var clientApplicationInterceptorFactories: Provider<List<ClientApplicationInterceptor.Factory>>
+
+  @Inject
+  private lateinit var moshi: Moshi
+
+  @Inject(optional = true)
+  private val tracer: Tracer? = null
+
+  @Inject(optional = true)
+  private val eventListenerFactory: EventListener.Factory? = null
+
+  fun typedClient(client: OkHttpClient, baseUrl: String): T {
+    val retrofit = Retrofit.Builder()
+        .baseUrl(baseUrl)
+        .build()
+
+    val invocationHandler = ClientInvocationHandler(
+        kclass,
+        name,
+        retrofit,
+        client,
+        clientNetworkInterceptorFactories,
+        clientApplicationInterceptorFactories,
+        eventListenerFactory,
+        tracer,
+        moshi)
+
+    @Suppress("UNCHECKED_CAST")
+    return Proxy.newProxyInstance(
+        ClassLoader.getSystemClassLoader(),
+        arrayOf(kclass.java),
+        invocationHandler
+    ) as T
   }
 }
