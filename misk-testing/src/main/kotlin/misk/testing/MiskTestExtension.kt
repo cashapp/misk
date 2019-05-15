73074168f0dad6485bb66f64e22215029a02a60e
Jonathan Estrella <jayestrella@squareup.com>
2019-05-15 13:58:19 -0400

    Bump hibernate-core

--- misk-testing/src/main/kotlin/misk/testing/MiskTestExtension.kt
+++ misk-testing/src/main/kotlin/misk/testing/MiskTestExtension.kt
@@ -80,7 +80,7 @@ internal class MiskTestExtension : BeforeEachCallback, AfterEachCallback {
       if (context.startService()) {
         try {
           serviceManager.startAsync().awaitHealthy(60, TimeUnit.SECONDS)
-        } catch (e: IllegalStateException) {
+        } catch (e: IllegalStateException) { log.error(e) { "Error" }
           // Unearth the root cause so we can see it in logs easily.
           log.error("Startup failure -- ${e.cause?.message}")
           throw e
