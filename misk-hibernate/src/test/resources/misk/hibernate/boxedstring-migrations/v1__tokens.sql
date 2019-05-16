5fb62cce684a5b57c70b0c6b140618c3dcbd7944
Hamdan Javeed <hamdan@squareup.com>
2019-05-16 11:02:33 -0700

    Make UserType function parameters nullable in BoxedStringType

--- misk-hibernate/src/test/resources/misk/hibernate/boxedstring-migrations/v1__tokens.sql
+++ misk-hibernate/src/test/resources/misk/hibernate/boxedstring-migrations/v1__tokens.sql
@@ -2,5 +2,6 @@ CREATE TABLE text_tokens(
   id bigint NOT NULL PRIMARY KEY AUTO_INCREMENT,
   text varchar(255) NOT NULL,
   token varchar(32) NOT NULL COLLATE utf8_bin,
+  optional_token varchar(32) NULL DEFAULT NULL COLLATE utf8_bin,
   UNIQUE KEY `unq_token` (`token`)
 );
