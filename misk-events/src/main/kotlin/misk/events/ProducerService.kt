28a41f539bd179d8e7f5f8618ef222b4f535cb1e
Keefer Rourke <keefer@squareup.com>
2019-05-29 10:54:57 -0400

    added marker interface for producer services

---
+++ misk-events/src/main/kotlin/misk/events/ProducerService.kt
@@ -0,0 +1,6 @@
+package misk.events
+
+import com.google.common.util.concurrent.Service
+
+/** Marker interface for services that are [Producer]s. **/
+interface ProducerService : Service
--- misk/src/main/kotlin/misk/clustering/ClusterService.kt
+++ misk/src/main/kotlin/misk/clustering/ClusterService.kt
@@ -2,5 +2,5 @@ package misk.clustering
 
 import com.google.common.util.concurrent.Service
 
-/** Marker interface for the toBeEnhanced that produces a [Cluster]. */
+/** Marker interface for the service that produces a [Cluster]. */
 interface ClusterService : Service
