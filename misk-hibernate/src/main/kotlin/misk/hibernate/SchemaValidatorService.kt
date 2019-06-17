59226b7684c3b634e18e94b7899909ceddba799e
Jesse Wilson <jwilson@squareup.com>
2019-06-17 15:20:13 -0400

    Expose what was validated in SchemaValidator

--- misk-hibernate/src/main/kotlin/misk/hibernate/SchemaValidatorService.kt
+++ misk-hibernate/src/main/kotlin/misk/hibernate/SchemaValidatorService.kt
@@ -1,29 +1,37 @@
 package misk.hibernate
 
 import com.google.common.util.concurrent.AbstractIdleService
-import misk.jdbc.DataSourceConfig
+import com.google.common.util.concurrent.Service
+import misk.healthchecks.HealthCheck
+import misk.healthchecks.HealthStatus
 import java.util.Collections
 import javax.inject.Provider
-import javax.inject.Singleton
 import kotlin.reflect.KClass
 
-@Singleton
 internal class SchemaValidatorService internal constructor(
   private val qualifier: KClass<out Annotation>,
   private val sessionFactoryServiceProvider: Provider<SessionFactoryService>,
-  private val transacterProvider: Provider<Transacter>,
-  private val config: DataSourceConfig
-) : AbstractIdleService() {
+  private val transacterProvider: Provider<Transacter>
+) : AbstractIdleService(), HealthCheck {
+  private lateinit var report: ValidationReport
+
   override fun startUp() {
-    synchronized(this) {
-      if (validated.contains(qualifier)) {
-        return
-      }
+    report = reports.computeIfAbsent(qualifier) {
       val validator = SchemaValidator()
       val sessionFactoryService = sessionFactoryServiceProvider.get()
       validator.validate(transacterProvider.get(), sessionFactoryService.hibernateMetadata)
-      validated.add(qualifier)
     }
+  }
+
+  override fun status(): HealthStatus {
+    val state = state()
+    if (state != Service.State.RUNNING) {
+      return HealthStatus.unhealthy("SchemaValidatorService: ${qualifier.simpleName} is $state")
+    }
+
+    return HealthStatus.healthy("SchemaValidatorService: ${qualifier.simpleName} is valid: " +
+        "schemas=${report.schemas} tables=${report.tables} columnCount=${report.columns.size} " +
+        "columns=${report.columns}")
   }
 
   override fun shutDown() {
@@ -31,6 +39,7 @@ internal class SchemaValidatorService internal constructor(
 
   companion object {
     /** Make sure we only validate each database once. It can be quite slow sometimes. */
-    private val validated = Collections.synchronizedSet(HashSet<KClass<out Annotation>>())
+    private val reports: MutableMap<KClass<out Annotation>, ValidationReport> =
+        Collections.synchronizedMap(mutableMapOf<KClass<out Annotation>, ValidationReport>())
   }
 }
