805cebf8dd82baf9dc4adc12408be9c30fba7d4c
Ryan Hall <rhall@squareup.com>
2019-06-25 16:42:07 -0700

    Add capabilities to @Authenticated
    This is the first part of https://github.com/cashapp/misk/issues/1078 to
    add a new capabilities field to @Authenticated that will eventually
    replace the roles field.
    
    I did not Deprecate the roles field, because many repos have -WError set
    which will fail the build if there is a Deprecated Warning.

--- misk/src/main/kotlin/misk/web/WebTab.kt
+++ misk/src/main/kotlin/misk/web/WebTab.kt
@@ -5,16 +5,16 @@ import misk.MiskCaller
 abstract class WebTab(
   slug: String,
   url_path_prefix: String,
-  // roles, services permissions control visibility of tab to misk web application user
+  // capabilities, services permissions control visibility of tab to misk web application user
   // it does not deal with any other permissions such as static resource access or otherwise
-  val roles: Set<String> = setOf(),
+  val capabilities: Set<String> = setOf(),
   val services: Set<String> = setOf()
 ) : ValidWebEntry(slug = slug, url_path_prefix = url_path_prefix) {
   fun isAuthenticated(caller: MiskCaller?): Boolean {
     return when {
-      roles.isEmpty() && services.isEmpty() -> true       // no role/service requirement => unauthenticated requests allowed (including when caller is null)
+      capabilities.isEmpty() && services.isEmpty() -> true       // no capabilities/service requirement => unauthenticated requests allowed (including when caller is null)
       caller == null -> false                             // role/service requirement present but caller null => assume authentication broken
-      roles.any { caller.roles.contains(it) } -> true     // matching role
+      capabilities.any { caller.allCapabilities.contains(it) } -> true     // matching role
       services.any { caller.service == it } -> true       // matching service
       else -> false
     }
@@ -26,6 +26,8 @@ class DashboardTab(
   url_path_prefix: String,
   val name: String,
   val category: String = "Container Admin",
+    // TODO(rhall): Deprecate this https://github.com/cashapp/misk/issues/1078
   roles: Set<String> = setOf(),
+  capabilities: Set<String> = setOf(),
   services: Set<String> = setOf()
-) : WebTab(slug = slug, url_path_prefix = url_path_prefix, roles = roles, services = services)
+) : WebTab(slug = slug, url_path_prefix = url_path_prefix, capabilities = roles + capabilities, services = services)
