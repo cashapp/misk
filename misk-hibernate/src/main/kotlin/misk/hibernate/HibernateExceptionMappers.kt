439521aa6e6b6f426796bf0f41f8f256368676d9
Jon Tirsen <jontirsen@squareup.com>
2019-07-02 10:29:25 +0200

    Send 429 when over capacity
    When Vitess returns the GRPC error code RESOURCE_EXHAUSTED that means
    we are temporarily over capacity. If we send back the error code 429 at
    that point then clients can take appropriate action and we also avoid
    paging oncall since otherwise we would be returning a 500 error.

--- misk-hibernate/src/main/kotlin/misk/hibernate/HibernateExceptionMappers.kt
+++ misk-hibernate/src/main/kotlin/misk/hibernate/HibernateExceptionMappers.kt
@@ -2,6 +2,7 @@ package misk.hibernate
 
 import com.google.common.base.Throwables
 import misk.exceptions.ConflictException
+import misk.exceptions.StatusCode
 import misk.web.Response
 import misk.web.ResponseBody
 import misk.web.exceptions.ExceptionMapper
@@ -10,7 +11,9 @@ import misk.web.toResponseBody
 import okhttp3.Headers
 import org.hibernate.StaleObjectStateException
 import org.hibernate.exception.ConstraintViolationException
+import org.hibernate.exception.GenericJDBCException
 import org.slf4j.event.Level
+import java.sql.SQLException
 import java.sql.SQLIntegrityConstraintViolationException
 import javax.inject.Inject
 import javax.persistence.OptimisticLockException
@@ -18,13 +21,13 @@ import javax.persistence.OptimisticLockException
 internal class RetryTransactionExceptionMapper @Inject internal constructor() : ExceptionMapper<RetryTransactionException> {
 
   override fun toResponse(th: RetryTransactionException): Response<ResponseBody> =
-    ConflictExceptionResponder.toResponse()
+      ConflictExceptionResponder.toResponse()
 
   override fun canHandle(th: Throwable): Boolean {
     val rootCause = Throwables.getRootCause(th)
 
     return rootCause is SQLIntegrityConstraintViolationException ||
-      rootCause is StaleObjectStateException
+        rootCause is StaleObjectStateException
   }
 
   override fun loggingLevel(th: RetryTransactionException) = Level.WARN
@@ -32,7 +35,7 @@ internal class RetryTransactionExceptionMapper @Inject internal constructor() : 
 
 internal class ConstraintViolationExceptionMapper @Inject internal constructor() : ExceptionMapper<ConstraintViolationException> {
   override fun toResponse(th: ConstraintViolationException): Response<ResponseBody> =
-    ConflictExceptionResponder.toResponse()
+      ConflictExceptionResponder.toResponse()
 
   override fun canHandle(th: Throwable): Boolean = th is ConstraintViolationException
 
@@ -41,16 +44,37 @@ internal class ConstraintViolationExceptionMapper @Inject internal constructor()
 
 internal class OptimisticLockExceptionMapper @Inject internal constructor() : ExceptionMapper<OptimisticLockException> {
   override fun toResponse(th: OptimisticLockException): Response<ResponseBody> =
-    ConflictExceptionResponder.toResponse()
+      ConflictExceptionResponder.toResponse()
 
   override fun canHandle(th: Throwable): Boolean = th is OptimisticLockException
 
   override fun loggingLevel(th: OptimisticLockException) = Level.WARN
 }
 
+internal class ResourceExhaustedExceptionMapper @Inject internal constructor() : ExceptionMapper<GenericJDBCException> {
+  private val HEADERS: Headers =
+      Headers.of(listOf("Content-Type" to MediaTypes.TEXT_PLAIN_UTF8).toMap())
+
+  override fun toResponse(th: GenericJDBCException): Response<ResponseBody> =
+      Response(StatusCode.TOO_MANY_REQUESTS.name.toResponseBody(), HEADERS,
+          statusCode = StatusCode.TOO_MANY_REQUESTS.code)
+
+  override fun canHandle(th: Throwable): Boolean =
+      th is GenericJDBCException &&
+          th.cause is SQLException &&
+          // This GRPC error code is sent from Vitess when we can't get a connection from a pool or
+          // if we trigger concurrency limits. This means we are over capacity and need to reject
+          // requests until we get within capacity again.
+          // Unfortunately the GRPC error code isn't being returned in a structured way so we have
+          // to look for this string in the error message.
+          th.cause?.message?.contains("Code: RESOURCE_EXHAUSTED") == true
+
+  override fun loggingLevel(th: GenericJDBCException) = Level.WARN
+}
+
 internal object ConflictExceptionResponder {
   private val HEADERS: Headers =
-    Headers.of(listOf("Content-Type" to MediaTypes.TEXT_PLAIN_UTF8).toMap())
+      Headers.of(listOf("Content-Type" to MediaTypes.TEXT_PLAIN_UTF8).toMap())
   private val CONFLICT_EXCEPTION = ConflictException()
 
   fun toResponse(): Response<ResponseBody> {
