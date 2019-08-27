c1cbd48cc057ab973eb4de726c7e0f04532ce9bf
Michael Gershunovsky <mgersh@squareup.com>
2019-08-26 23:21:45 -0400

    Create misk-slack to send messages over slack webhooks

---
+++ misk/misk-slack/src/main/kotlin/misk/slack/SlackClient.kt
@@ -0,0 +1,19 @@
+package misk.slack
+
+/** Dummy client that does nothing if SlackModule is not installed .*/
+open class SlackClient {
+  /**
+   * Post a message as the specified bot username and icon emoji in the channel.
+   * If no channel is provided, the default channel configured by the service is used.
+   * If the service has not configured a slack module, this method is a no-op.
+   * Does not throw on IO exceptions.
+   */
+  open fun postMessage(
+    username: String,
+    iconEmoji: String,
+    message: String,
+    channel: String? = null
+  ): SlackWebhookResponse? {
+    return null
+  }
+}
