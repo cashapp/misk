90f17f544e7237684f67ec88da8d948c6be562a8
Andres Taylor <antaylor@squareup.com>
2019-09-12 05:12:19 +0200

    Renamed file and reformated code
    Signed-off-by: Andres Taylor <antaylor@squareup.com>

--- misk-hibernate-testing/src/test/kotlin/misk/jdbc/SpanInjectorTest.kt
+++ misk-hibernate-testing/src/test/kotlin/misk/jdbc/SpanInjectorTest.kt
@@ -7,7 +7,7 @@ import io.opentracing.mock.MockTracer
 import org.assertj.core.api.Assertions.assertThat
 import javax.sql.DataSource
 
-class JaegerSpanInjectorTest {
+class SpanInjectorTest {
   @Test
   fun parseQueryPlans() {
     val tracer = MockTracer()
@@ -17,7 +17,7 @@ class JaegerSpanInjectorTest {
     val contextString = span.context().toString()
 
     val config = DataSourceConfig(DataSourceType.VITESS)
-    val injector = JaegerSpanInjector(tracer, config)
+    val injector = SpanInjector(tracer, config)
     val query = "SELECT * FROM table"
     val transformInfo = TransformInfo(null, null, query, false, 0)
     val result = injector.transformQuery(transformInfo)
@@ -30,21 +30,19 @@ class JaegerSpanInjectorTest {
   fun testNotDecorateIfItsVitess() {
     val tracer = MockTracer()
     val config = DataSourceConfig(DataSourceType.VITESS)
-    val injector = JaegerSpanInjector(tracer, config)
+    val injector = SpanInjector(tracer, config)
     val ds = Mockito.mock(DataSource::class.java)
 
-    assertThat(injector.decorate(ds)).
-        isSameAs(ds)
+    assertThat(injector.decorate(ds)).isSameAs(ds)
   }
 
   @Test
   fun testDecoratesIfItsVitessMysql() {
     val tracer = MockTracer()
     val config = DataSourceConfig(DataSourceType.VITESS_MYSQL)
-    val injector = JaegerSpanInjector(tracer, config)
+    val injector = SpanInjector(tracer, config)
     val ds = Mockito.mock(DataSource::class.java)
 
-    assertThat(injector.decorate(ds)).
-        isNotSameAs(ds)
+    assertThat(injector.decorate(ds)).isNotSameAs(ds)
   }
 }
