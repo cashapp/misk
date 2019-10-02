e73ebdbcdb07b1eb3f36cce91508c00c0171d9ab
Yunchi Luo <yunchiluo@squareup.com>
2019-10-02 10:41:24 -0400

    LaunchDarkly modules (#1212)
    * LaunchDarkly modules
    
    * fixes for comments

---
+++ misk-feature-testing/src/main/kotlin/misk/feature/testing/FakeFeatureFlags.kt
@@ -0,0 +1,65 @@
+package misk.feature.testing
+
+import com.google.common.util.concurrent.AbstractIdleService
+import misk.feature.Attributes
+import misk.feature.Feature
+import misk.feature.FeatureFlags
+import misk.feature.FeatureService
+import java.util.concurrent.ConcurrentHashMap
+import javax.inject.Inject
+import javax.inject.Singleton
+
+/**
+ * In-memory test implementation of [FeatureFlags] that allows flags to be overridden.
+ */
+@Singleton
+class FakeFeatureFlags @Inject constructor() : AbstractIdleService(),
+    FeatureFlags,
+    FeatureService {
+  override fun startUp() {}
+  override fun shutDown() {}
+
+  private val overrides = ConcurrentHashMap<Feature, Any>()
+
+  override fun getBoolean(feature: Feature, token: String, attributes: Attributes): Boolean {
+    return overrides.getOrDefault(feature, false) as Boolean
+  }
+
+  override fun getInt(feature: Feature, token: String, attributes: Attributes): Int =
+      overrides[feature] as? Int ?: throw IllegalArgumentException(
+          "Int flag $feature must be overridden with override() before use")
+
+  override fun getString(feature: Feature, token: String, attributes: Attributes): String =
+      overrides[feature] as? String ?: throw IllegalArgumentException(
+          "String flag $feature must be overridden with override() before use")
+
+  override fun <T : Enum<T>> getEnum(
+    feature: Feature,
+    token: String,
+    clazz: Class<T>,
+    attributes: Attributes
+  ): T {
+    @Suppress("unchecked_cast")
+    return overrides.getOrDefault(feature, clazz.enumConstants[0]) as T
+  }
+
+  fun override(feature: Feature, value: Boolean) {
+    overrides[feature] = value
+  }
+
+  fun override(feature: Feature, value: Int) {
+    overrides[feature] = value
+  }
+
+  fun override(feature: Feature, value: String) {
+    overrides[feature] = value
+  }
+
+  fun override(feature: Feature, value: Enum<*>) {
+    overrides[feature] = value
+  }
+
+  fun reset() {
+    overrides.clear()
+  }
+}
---
+++ misk-feature-testing/src/main/kotlin/misk/feature/testing/FakeFeatureFlagsModule.kt
@@ -0,0 +1,41 @@
+package misk.feature.testing
+
+import misk.feature.FeatureFlags
+import misk.feature.FeatureService
+import misk.ServiceModule
+import misk.inject.KAbstractModule
+import misk.inject.toKey
+import kotlin.reflect.KClass
+
+/**
+ * Binds a [FakeFeatureFlags] that allows tests to override values.
+ */
+class FakeFeatureFlagsModule(
+  private val qualifier: KClass<out Annotation>? = null
+) : KAbstractModule() {
+  private val testFeatureFlags = FakeFeatureFlags()
+
+  override fun configure() {
+    val key = FakeFeatureFlags::class.toKey(qualifier)
+    bind(key).toInstance(testFeatureFlags)
+    bind<FeatureFlags>().to(key)
+    bind<FeatureService>().to(key)
+    install(ServiceModule(FeatureService::class.toKey(qualifier)))
+  }
+
+  /**
+   * Add overrides for the feature flags. Allows flags to be overridden at module instantiation
+   * instead of within individual test classes.
+   *
+   * Usage:
+   * ```
+   * install(FakeFeatureFlagsModule().withOverrides { flags ->
+   *   flags.overrideBool(Feature("foo"), true)
+   * })
+   * ```
+   */
+  fun withOverrides(lambda: (FakeFeatureFlags.() -> Unit)): FakeFeatureFlagsModule {
+    lambda(testFeatureFlags)
+    return this
+  }
+}
