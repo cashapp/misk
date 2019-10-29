adf05e820a87deebedc6a7cf7aab8c648ed0e834
Yunchi Luo <mightyguava@gmail.com>
2019-10-29 13:19:08 -0400

    tracing: do not extract span from headers if already done (#1254)
    * tracing: do not extract span from headers if already done
    
    * change tracinginterceptor op name to match convention

--- misk/misk/src/main/kotlin/misk/web/interceptors/TracingInterceptor.kt
+++ misk/misk/src/main/kotlin/misk/web/interceptors/TracingInterceptor.kt
@@ -2,6 +2,7 @@ package misk.web.interceptors
 
 import com.google.inject.Inject
 import com.google.inject.Singleton
+import io.opentracing.Span
 import io.opentracing.SpanContext
 import io.opentracing.Tracer
 import io.opentracing.propagation.Format
@@ -30,25 +31,35 @@ internal class TracingInterceptor internal constructor(private val tracer: Trace
   }
 
   override fun intercept(chain: NetworkChain) {
-    val parentContext: SpanContext? = try {
-      tracer.extract(Format.Builtin.HTTP_HEADERS,
-          TextMultimapExtractAdapter(chain.httpCall.requestHeaders.toMultimap()))
-    } catch (e: Exception) {
-      logger.warn("Failure attempting to extract span context. Existing context, if any," +
-          " will be ignored in creation of span", e)
-      null
-    }
-
-    val scopeBuilder = tracer.buildSpan(chain.webAction.javaClass.name)
+    val scopeBuilder = tracer.buildSpan("http.action")
         .withTag(Tags.HTTP_METHOD.key, chain.httpCall.dispatchMechanism.method.toString())
         .withTag(Tags.HTTP_URL.key, chain.httpCall.url.toString())
         .withTag(Tags.SPAN_KIND.key, SPAN_KIND_SERVER)
 
-    if (parentContext != null) {
-      scopeBuilder.asChildOf(parentContext)
+    val parentSpan: Span? = tracer.activeSpan()
+    if (parentSpan != null) {
+      // Certain tracing implementations (Datadog) do their own header extraction. Skip our custom
+      // one if that happened.
+      scopeBuilder.asChildOf(parentSpan)
+    } else {
+      val parentContext: SpanContext? = try {
+        tracer.extract(Format.Builtin.HTTP_HEADERS,
+            TextMultimapExtractAdapter(chain.httpCall.requestHeaders.toMultimap()))
+      } catch (e: Exception) {
+        logger.warn("Failure attempting to extract span context. Existing context, if any," +
+            " will be ignored in creation of span", e)
+        null
+      }
+
+      if (parentContext != null) {
+        scopeBuilder.asChildOf(parentContext)
+      }
     }
 
     val scope = scopeBuilder.startActive(true)
+    // This is a datadog convention. Must be set after span is created because otherwise it would
+    // be overwritten by the method/url
+    scope.span().setTag("resource.name", chain.webAction.javaClass.name)
     scope.use {
       try {
         chain.proceed(chain.httpCall)
