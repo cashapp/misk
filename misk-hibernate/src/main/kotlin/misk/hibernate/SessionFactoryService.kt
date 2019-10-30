6ce7b5ae2c9d4761c1f604646e0efb3787bbbc09
Zach Cross <zcross@squareup.com>
2019-10-30 13:03:14 -0400

    Add DataSourceConfig field for hibernate.jdbc.batch_size (unset by default)

--- misk-hibernate/src/main/kotlin/misk/hibernate/SessionFactoryService.kt
+++ misk-hibernate/src/main/kotlin/misk/hibernate/SessionFactoryService.kt
@@ -89,6 +89,12 @@ internal class SessionFactoryService(
       applySetting(AvailableSettings.USE_GET_GENERATED_KEYS, "true")
       applySetting(AvailableSettings.USE_NEW_ID_GENERATOR_MAPPINGS, "false")
       applySetting(AvailableSettings.JDBC_TIME_ZONE, "UTC")
+      if (config.jdbc_statement_batch_size != null) {
+        require(config.jdbc_statement_batch_size > 0) {
+          "Invalid jdbc_statement_batch_size: must be > 0."
+        }
+        applySetting(AvailableSettings.STATEMENT_BATCH_SIZE, config.jdbc_statement_batch_size)
+      }
     }
 
     val registry = registryBuilder.build()
