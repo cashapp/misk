9f3841110b4416c449b592f4ca1e29f52a5364bc
Jon Tirsen <jontirsen@squareup.com>
2019-12-06 10:26:11 +1100

    Re add method for backwards compatibility

--- misk-hibernate/src/main/kotlin/misk/vitess/StartVitessService.kt
+++ misk-hibernate/src/main/kotlin/misk/vitess/StartVitessService.kt
@@ -5,14 +5,16 @@ import kotlin.reflect.KClass
 
 class StartVitessService {
   companion object {
-    @Deprecated("Renamed", replaceWith = ReplaceWith("DockerVitessCluster.startVitessDaemon(qualifier,config)"))
+    @Deprecated("Moved",
+        replaceWith = ReplaceWith(
+            "misk.vitess.DockerVitessCluster.startVitessDaemon(qualifier, config)"))
     fun startVitessDaemon(
       /** The same qualifier passed into [HibernateModule], used to uniquely name the container */
       qualifier: KClass<out Annotation>,
       /** Config for the Vitess cluster */
       config: DataSourceConfig
     ) {
-      startVitessDaemon(qualifier, config)
+      DockerVitessCluster.startVitessDaemon(qualifier, config)
     }
   }
 }
