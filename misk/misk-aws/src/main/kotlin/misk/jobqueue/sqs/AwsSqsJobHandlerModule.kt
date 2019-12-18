d562d8ef991b57cbd9e99e005a32c688eb21d3c0
Francisco Rojas <frojas@squareup.com>
2019-12-18 12:17:15 -0800

    Revert "Support multiple prioritized queues"
    This reverts commit 80c40f655afb9a7c097497b125833f1ff05ee259.

--- misk/misk-aws/src/main/kotlin/misk/jobqueue/sqs/AwsSqsJobHandlerModule.kt
+++ misk/misk-aws/src/main/kotlin/misk/jobqueue/sqs/AwsSqsJobHandlerModule.kt
@@ -4,53 +4,43 @@ import com.google.common.util.concurrent.AbstractIdleService
 import misk.ServiceModule
 import misk.inject.KAbstractModule
 import misk.jobqueue.JobHandler
-import misk.jobqueue.QueuesKey
 import misk.jobqueue.QueueName
 import javax.inject.Inject
 import javax.inject.Singleton
 import kotlin.reflect.KClass
 
 /**
- * Install this module to register a handler for one or more SQS queues.
- *
- * @param queueNames a list of names for SQS queues. If more than one queue name is provided, these
- * will be treated as a list of prioritized queues (in priority order, first queue is top priority).
- *
- * See [misk.jobqueue.JobConsumer] for details
+ * Install this module to register a handler for an SQS queue.
  */
 class AwsSqsJobHandlerModule<T : JobHandler> private constructor(
-  private val queueNames: List<QueueName>,
+  private val queueName: QueueName,
   private val handler: KClass<T>
 ) : KAbstractModule() {
   override fun configure() {
-    newMapBinder(QueuesKey::class, JobHandler::class).addBinding(QueuesKey(queueNames)).to(handler.java)
+    newMapBinder<QueueName, JobHandler>().addBinding(queueName).to(handler.java)
     install(ServiceModule<AwsSqsJobHandlerSubscriptionService>())
   }
 
   companion object {
     inline fun <reified T : JobHandler> create(queueName: QueueName):
-        AwsSqsJobHandlerModule<T> = create(listOf(queueName), T::class)
-
-    inline fun <reified T : JobHandler> createPrioritized(vararg queueNames: QueueName):
-        AwsSqsJobHandlerModule<T> = create(queueNames.asList(), T::class)
+        AwsSqsJobHandlerModule<T> = create(queueName, T::class)
 
     @JvmStatic
     fun <T : JobHandler> create(
       queueName: QueueName,
       handlerClass: Class<T>
     ): AwsSqsJobHandlerModule<T> {
-      return create(listOf(queueName), handlerClass.kotlin)
+      return create(queueName, handlerClass.kotlin)
     }
 
     /**
      * Returns a module that registers a handler for an SQS queue.
      */
-    @JvmStatic
     fun <T : JobHandler> create(
-      queueNames: List<QueueName>,
+      queueName: QueueName,
       handlerClass: KClass<T>
     ): AwsSqsJobHandlerModule<T> {
-      return AwsSqsJobHandlerModule(queueNames, handlerClass)
+      return AwsSqsJobHandlerModule(queueName, handlerClass)
     }
   }
 }
@@ -58,10 +48,10 @@ class AwsSqsJobHandlerModule<T : JobHandler> private constructor(
 @Singleton
 internal class AwsSqsJobHandlerSubscriptionService @Inject constructor(
   private val consumer: SqsJobConsumer,
-  private val consumerMapping: Map<QueuesKey, JobHandler>
+  private val consumerMapping: Map<QueueName, JobHandler>
 ) : AbstractIdleService() {
   override fun startUp() {
-    consumerMapping.forEach { consumer.subscribe(it.key.names, it.value) }
+    consumerMapping.forEach { consumer.subscribe(it.key, it.value) }
   }
 
   override fun shutDown() {}
