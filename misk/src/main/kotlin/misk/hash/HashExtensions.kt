85006d764396defaf1df05ee2d3edbda2cb85460
Jay Estrella <jjestrel@users.noreply.github.com>
2019-12-20 09:38:05 -0500

    Remove misk.hash package since it's unused. Part of #1330 for breaking up misk module (#1339)

--- misk/src/main/kotlin/misk/hash/HashExtensions.kt
+++
@@ -1,7 +0,0 @@
-package misk.hash
-
-import com.google.common.hash.HashCode
-import okio.ByteString
-
-/** @return the [HashCode] as a [ByteString] */
-fun HashCode.asByteString(): ByteString = ByteString.of(*asBytes())
