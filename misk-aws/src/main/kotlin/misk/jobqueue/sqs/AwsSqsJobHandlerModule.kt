2da12f3908eda26917042ae926d2c91e3669e23d
Tony Lu <dlu@squareup.com>
2020-01-30 10:31:15 -0800

    Add option to install retry queue

--- misk-aws/src/main/kotlin/misk/jobqueue/sqs/AwsSqsJobHandlerModule.kt
+++ misk-aws/src/main/kotlin/misk/jobqueue/sqs/AwsSqsJobHandlerModule.kt
@@ -11,38 +11,49 @@ import kotlin.reflect.KClass
 
 /**
  * Install this module to register a handler for an SQS queue,
- * along with its corresponding retry queue
+ * and if specified, registers its corresponding retry queue.
  */
 class AwsSqsJobHandlerModule<T : JobHandler> private constructor(
   private val queueName: QueueName,
-  private val handler: KClass<T>
+  private val handler: KClass<T>,
+  private val installRetryQueue: Boolean
 ) : KAbstractModule() {
   override fun configure() {
     newMapBinder<QueueName, JobHandler>().addBinding(queueName).to(handler.java)
-    newMapBinder<QueueName, JobHandler>().addBinding(queueName.retryQueue).to(handler.java)
+
+    if (installRetryQueue) {
+      newMapBinder<QueueName, JobHandler>().addBinding(queueName.retryQueue).to(handler.java)
+    }
+
     install(ServiceModule<AwsSqsJobHandlerSubscriptionService>())
   }
 
   companion object {
-    inline fun <reified T : JobHandler> create(queueName: QueueName):
-        AwsSqsJobHandlerModule<T> = create(queueName, T::class)
+    @JvmOverloads
+    inline fun <reified T : JobHandler> create(
+      queueName: QueueName,
+      installRetryQueue: Boolean = true
+    ): AwsSqsJobHandlerModule<T> = create(queueName, T::class, installRetryQueue)
 
-    @JvmStatic
+    @JvmStatic @JvmOverloads
     fun <T : JobHandler> create(
       queueName: QueueName,
-      handlerClass: Class<T>
+      handlerClass: Class<T>,
+      installRetryQueue: Boolean = true
     ): AwsSqsJobHandlerModule<T> {
-      return create(queueName, handlerClass.kotlin)
+      return create(queueName, handlerClass.kotlin, installRetryQueue)
     }
 
     /**
      * Returns a module that registers a handler for an SQS queue.
      */
+    @JvmOverloads
     fun <T : JobHandler> create(
       queueName: QueueName,
-      handlerClass: KClass<T>
+      handlerClass: KClass<T>,
+      installRetryQueue: Boolean = true
     ): AwsSqsJobHandlerModule<T> {
-      return AwsSqsJobHandlerModule(queueName, handlerClass)
+      return AwsSqsJobHandlerModule(queueName, handlerClass, installRetryQueue)
     }
   }
 }
