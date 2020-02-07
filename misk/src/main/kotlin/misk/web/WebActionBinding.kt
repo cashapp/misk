e7bbe25cc8014f9942d5ffc6a94806d176a685c4
Bruno de Carvalho <bruno@squareup.com>
2020-02-06 16:07:05 -0800

    Disallow @RequestBody for actions annotated with @Delete
    Follow up to #1369

--- misk/src/main/kotlin/misk/web/WebActionBinding.kt
+++ misk/src/main/kotlin/misk/web/WebActionBinding.kt
@@ -106,8 +106,9 @@ internal class WebActionBinding @Inject constructor(
 
     override fun claimRequestBody() {
       check(requestBody == null) { "already claimed by $requestBody" }
-      check(action.dispatchMechanism != DispatchMechanism.GET) {
-        "cannot claim request body of GET"
+      check(action.dispatchMechanism != DispatchMechanism.GET
+          && action.dispatchMechanism != DispatchMechanism.DELETE) {
+        "cannot claim request body of ${action.dispatchMechanism.name}"
       }
       requestBody = Placeholder
     }
