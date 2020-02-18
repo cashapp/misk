e72b6818c411cf44e14b1df7c75ad5266a9da444
Andrew Bakes <abakes@squareup.com>
2020-02-18 16:04:22 -0500

    added dynamo files from peddle

---
+++ misk/misk-aws/src/main/kotlin/misk/dynamodb/RealDynamoDbModule.kt
@@ -0,0 +1,28 @@
+package misk.dynamodb
+
+import com.amazonaws.auth.AWSCredentialsProvider
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDB
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder
+import com.google.inject.Provides
+import misk.cloud.aws.AwsRegion
+import misk.inject.KAbstractModule
+import javax.inject.Singleton
+
+internal class RealDynamoDbModule : KAbstractModule() {
+  override fun configure() {
+    requireBinding<AWSCredentialsProvider>()
+    requireBinding<AwsRegion>()
+  }
+
+  @Provides @Singleton
+  fun providesAmazonDynamoDB(
+    awsRegion: AwsRegion,
+    awsCredentialsProvider: AWSCredentialsProvider
+  ): AmazonDynamoDB {
+    return AmazonDynamoDBClientBuilder
+        .standard()
+        .withRegion(awsRegion.name)
+        .withCredentials(awsCredentialsProvider)
+        .build()
+  }
+}
