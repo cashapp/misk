949bac8e5849d919cd8ff2863bd691cbe9741d9b
Andrew Bakes <abakes@squareup.com>
2020-02-20 09:16:06 -0500

    PR feedback

--- misk-aws/src/main/kotlin/misk/dynamodb/DyEntity.kt
+++ misk-aws/src/main/kotlin/misk/dynamodb/DyEntity.kt
@@ -1,9 +1,14 @@
 package misk.dynamodb
 
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBTable
 import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBVersionAttribute
 
+/*
+ * This version field is used for optimistic locking.
+ */
+@DynamoDBTable(tableName = "")
 abstract class DyEntity {
 
-  @DynamoDBVersionAttribute
+  @DynamoDBVersionAttribute(attributeName = "version")
   var version: Long = 0
 }
