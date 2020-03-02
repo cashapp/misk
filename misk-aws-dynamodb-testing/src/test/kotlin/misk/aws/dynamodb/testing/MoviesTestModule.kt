785a695f9c5e85f8660bf3a98cf960257f63affe
Zhixuan Lai <zhxnlai@gmail.com>
2020-03-02 11:50:25 -0800

    Add DockerDynamoDb

---
+++ misk-aws-dynamodb-testing/src/test/kotlin/misk/aws/dynamodb/testing/MoviesTestModule.kt
@@ -0,0 +1,16 @@
+package misk.aws.dynamodb.testing
+
+import misk.MiskTestingServiceModule
+import misk.environment.Environment
+import misk.environment.EnvironmentModule
+import misk.inject.KAbstractModule
+
+class MoviesTestModule: KAbstractModule() {
+
+  override fun configure() {
+    install(MiskTestingServiceModule())
+    install(EnvironmentModule(Environment.TESTING))
+
+    install(DockerDynamoDbModule(listOf(DyMovie::class, DyCharacter::class)))
+  }
+}
