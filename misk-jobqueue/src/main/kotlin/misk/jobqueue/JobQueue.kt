cace38902dc4744bbed6cd50ae76656c23dfc260
Bruno de Carvalho <bruno@squareup.com>
2020-03-05 15:21:06 -0800

    Add idempotence key and jobqueue internal metadata attributes
    This changes brings internal and misk jobqueue libraries closer in
    feature-parity by offering the option to specify an application assigned
    `idempotence_key` (as opposed to the system assigned `id`, only available after
    a job is enqueued), which can be used by `JobHandler` implementations to detect
    duplicate jobs.
    
    Internal attributes required for jobqueue-related functionality are stored in a
    single message attribute (JSON-encoded `Map<String, String>`), leaving a
    maximum of 8 arbitrary attributes for applications (the other reserved
    attribute is for trace id; while not always present, it makes sense to assume
    it'll exist and just consistently limit app keys to 8).
    
    This change does not introduce dependency on metadata or any of its attributes,
    meaning new jobs will begin having these properties as services are deployed.
    
    The two metadata attributes added with this change are:
    
    * Idempotence key: our internal jobqueue libs have always provided idempotence
    key support, which some services heavily rely on to avoid duplicate work.
    
    * Origin queue: when a job is transferred from main or retry queue to
    dead-letter queue, it does not contain any metadata about its origins.
    As we prepare to roll out a(n optional) more functional solution for
    dead-lettered job management, which relies on a single, global dead-letter
    queue, it's necessary to record the main queue where a job came from (so that
    it's known where it should be sent to, in case of reprocessing).
    
    Support for global DLQ (vs per-queue DLQ) will be introduced in a follow-up PR.

--- misk-jobqueue/src/main/kotlin/misk/jobqueue/JobQueue.kt
+++ misk-jobqueue/src/main/kotlin/misk/jobqueue/JobQueue.kt
@@ -1,6 +1,7 @@
 package misk.jobqueue
 
 import java.time.Duration
+import java.util.UUID
 
 /**
  * A [JobQueue] enqueues jobs for asynchronous execution, possibly in another process. Jobs
@@ -12,16 +13,22 @@ interface JobQueue {
   /**
    * Enqueue a job onto the given queue, along with a set of job attributes.
    *
-   * @param queueName the name of the queue on which to place the job
+   * @param queueName The name of the queue on which to place the job.
    * @param body The body of the job; can be any arbitrary string - it is up to the enqueuer and
-   * consumer to agree on the format of the body
+   * consumer to agree on the format of the body.
+   * @param idempotenceKey Client-assigned unique key, useful for application code to detect duplicate work.
+   * Implementations of both [JobQueue] and [JobConsumer] are expected to _not_ perform any filtering based on this
+   * value, as it carries meaning only for application code (i.e. any logic around this property should take place in
+   * [JobHandler]s). Defaults to a randomly generated UUID when not explicitly set.
    * @param deliveryDelay If specified, the job will only become visible to the consumer after
    * the provided duration. Used for jobs that should delay processing for a period of time.
-   * @param attributes Arbitrary contextual attributes associated with the job
+   * @param attributes Arbitrary contextual attributes associated with the job. Implementations may limit the number of
+   * attributes per message.
    */
   fun enqueue(
     queueName: QueueName,
     body: String,
+    idempotenceKey: String = UUID.randomUUID().toString(),
     deliveryDelay: Duration? = null,
     attributes: Map<String, String> = mapOf()
   )
