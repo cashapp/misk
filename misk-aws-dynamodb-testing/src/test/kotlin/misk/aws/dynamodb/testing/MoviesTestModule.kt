473a5e660cc4b6887d29987d4e748e84e476afb6
Andrew Bakes <12255910+abakes@users.noreply.github.com>
2020-03-23 15:20:17 -0400

    added ability to customize table creation (#1446)
    * added ability to customize table creation
    
    * configure table rename
    
    * feedback
    
    Co-authored-by: Andrew Bakes <abakes@squareup.com>

--- misk-aws-dynamodb-testing/src/test/kotlin/misk/aws/dynamodb/testing/MoviesTestModule.kt
+++ misk-aws-dynamodb-testing/src/test/kotlin/misk/aws/dynamodb/testing/MoviesTestModule.kt
@@ -1,5 +1,9 @@
 package misk.aws.dynamodb.testing
 
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBMapper
+import com.amazonaws.services.dynamodbv2.model.BillingMode
+import com.amazonaws.services.dynamodbv2.model.CreateTableRequest
+import com.amazonaws.services.dynamodbv2.model.ProvisionedThroughput
 import misk.MiskTestingServiceModule
 import misk.environment.Environment
 import misk.environment.EnvironmentModule
@@ -9,8 +13,12 @@ class MoviesTestModule: KAbstractModule() {
 
   override fun configure() {
     install(MiskTestingServiceModule())
-    install(EnvironmentModule(Environment.TESTING))
 
-    install(DockerDynamoDbModule(DyMovie::class, DyCharacter::class))
+    install(DockerDynamoDbModule(
+        DynamoDbTable(DyMovie::class),
+        DynamoDbTable(DyCharacter::class) {
+          it.withBillingMode(BillingMode.PAY_PER_REQUEST)
+        }
+    ))
   }
 }
