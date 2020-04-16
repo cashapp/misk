759868dec323167d47a4cb108c8654a04e4211a5
Bruno de Carvalho <bruno@squareup.com>
2020-04-15 18:56:14 -0700

    Swap global DLQ AWS SQS config for a DLQ provider
    At SQ Cash all our queues have their redrive policy sending dead-lettered jobs
    to a global DLQ. To avoid forcing this approach on non-sqcash misk clients,
    `QueueResolver` previously relied on an optional config param that would
    override the DLQ for all queues with a global DLQ. However, this approach was
    leading to lots of repetition when updating all Cash services.
    
    This change swaps that config property with a more generic dead-letter queue
    provider,which receives a `QueueName` and returns the matching dead-letter
    queue's `QueueName`, allowing applications to use an existing impl (e.g.
    "<main_queue>_dlq" or static global DLQ) or build their own logic (e.g. read
    from config file, map different queues to different DLQs, mix & match, etc.)
    
    The default implementation preserves status quo — adds a "_dlq" suffix to the
    main queue's name — but allows apps to bind a different provider.
    
    h/t to rhall@ for the suggestion.

---
+++ misk-aws/src/test/kotlin/misk/jobqueue/sqs/StaticDeadLetterQueueProviderTest.kt
@@ -0,0 +1,17 @@
+package misk.jobqueue.sqs
+
+import misk.jobqueue.QueueName
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+
+internal class StaticDeadLetterQueueProviderTest {
+  @Test fun `always returns the same DLQ name`() {
+    val staticDlqName = "constant"
+    val provider = StaticDeadLetterQueueProvider(staticDlqName)
+
+    assertThat(provider.deadLetterQueueFor(QueueName("queue_1")))
+        .isEqualTo(QueueName(staticDlqName))
+    assertThat(provider.deadLetterQueueFor(QueueName("queue_2")))
+        .isEqualTo(QueueName(staticDlqName))
+  }
+}
