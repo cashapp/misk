739ffc89c00f71b76dd358c0532995b4062168f9
Isabel Fan <isabel@squareup.com>
2020-05-26 13:58:41 -0400

    Remove misk-jaeger

--- misk/misk-jaeger/src/main/kotlin/misk/tracing/backends/jaeger/JaegerBackendConfig.kt
+++
@@ -1,8 +0,0 @@
-package misk.tracing.backends.jaeger
-
-import misk.config.Config
-
-data class JaegerBackendConfig(
-  val sampler: JaegerSamplerConfig?,
-  val reporter: JaegerReporterConfig?
-) : Config
