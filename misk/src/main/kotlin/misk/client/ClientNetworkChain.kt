676f4b5d802a0710dfda6381078500fac8866cf5
Ryan Hall <rhall@squareup.com>
2020-05-28 19:39:00 -0700

    Add with*Timeout methods to ClientNetworkChain (#1519)
    This will support a future NetworkInterceptor that updates the outgoing
    timeouts based on a propagated deadline.
    
    Currently prototyping this in another project and will move over once I
    prove it works (including a test).

--- misk/src/main/kotlin/misk/client/ClientNetworkChain.kt
+++ misk/src/main/kotlin/misk/client/ClientNetworkChain.kt
@@ -2,9 +2,25 @@ package misk.client
 
 import okhttp3.Request
 import okhttp3.Response
+import java.util.concurrent.TimeUnit
 
 interface ClientNetworkChain {
   val action: ClientAction
   val request: Request
   fun proceed(request: Request): Response
+
+  /**
+   * Override the connect timeout.
+   */
+  fun withConnectTimeout(timeout: Int, unit: TimeUnit): ClientNetworkChain
+
+  /**
+   * Override the read timeout.
+   */
+  fun withReadTimeout(timeout: Int, unit: TimeUnit): ClientNetworkChain
+
+  /**
+   * Override the write timeout.
+   */
+  fun withWriteTimeout(timeout: Int, unit: TimeUnit): ClientNetworkChain
 }
