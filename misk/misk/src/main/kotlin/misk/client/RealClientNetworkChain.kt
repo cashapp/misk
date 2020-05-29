85db9219bce84573b0c8bf4070edeb3cbdd533ba
Ryan Hall <rhall@squareup.com>
2020-05-29 16:36:38 -0700

    Revert "Add with*Timeout methods to ClientNetworkChain (#1519)"
    This reverts commit 34fd84d7baa1c3c835a4a8ded5a9f5b7521b3755.

--- misk/misk/src/main/kotlin/misk/client/RealClientNetworkChain.kt
+++ misk/misk/src/main/kotlin/misk/client/RealClientNetworkChain.kt
@@ -2,24 +2,11 @@ package misk.client
 
 import okhttp3.Request
 import okhttp3.Response
-import java.util.concurrent.TimeUnit
 
 internal class RealClientNetworkChain(
   private val okhttpChain: okhttp3.Interceptor.Chain,
   override val action: ClientAction
 ) : ClientNetworkChain {
-  override val request: Request = okhttpChain.request()
+  override val request: okhttp3.Request = okhttpChain.request()
   override fun proceed(request: Request): Response = okhttpChain.proceed(request)
-
-  override fun withConnectTimeout(timeout: Int, unit: TimeUnit): ClientNetworkChain {
-    return RealClientNetworkChain(okhttpChain.withConnectTimeout(timeout, unit), action)
-  }
-
-  override fun withReadTimeout(timeout: Int, unit: TimeUnit): ClientNetworkChain {
-    return RealClientNetworkChain(okhttpChain.withReadTimeout(timeout, unit), action)
-  }
-
-  override fun withWriteTimeout(timeout: Int, unit: TimeUnit): ClientNetworkChain {
-    return RealClientNetworkChain(okhttpChain.withWriteTimeout(timeout, unit), action)
-  }
 }
