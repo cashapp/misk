b141b5b7b5c2681a9388fe2a1579c7ab423da9a9
Jesse Wilson <jwilson@squareup.com>
2020-08-10 18:47:14 -0400

    Log fewer errors when shedding requests

--- misk/src/main/kotlin/misk/web/interceptors/ConcurrencyLimitsInterceptor.kt
+++ misk/src/main/kotlin/misk/web/interceptors/ConcurrencyLimitsInterceptor.kt
@@ -5,10 +5,13 @@ import com.netflix.concurrency.limits.limiter.SimpleLimiter
 import misk.Action
 import misk.exceptions.StatusCode
 import misk.logging.getLogger
+import misk.logging.log
 import misk.web.AvailableWhenDegraded
 import misk.web.NetworkChain
 import misk.web.NetworkInterceptor
+import org.slf4j.event.Level
 import java.time.Clock
+import java.util.concurrent.TimeUnit
 import javax.inject.Inject
 import javax.inject.Singleton
 import kotlin.reflect.full.findAnnotation
@@ -23,14 +26,21 @@ import kotlin.reflect.full.findAnnotation
  */
 internal class ConcurrencyLimitsInterceptor internal constructor(
   private val actionName: String,
-  private val limiter: Limiter<String>
+  private val limiter: Limiter<String>,
+  private val clock: Clock
 ) : NetworkInterceptor {
+  /**
+   * When this fails, it fails a lot. Log at most one error per minute per node and let the
+   * developers use metrics to see what the failure rate is.
+   */
+  private val durationBetweenErrorsMs = TimeUnit.MINUTES.toMillis(1)
+  private var lastErrorLoggedAtMs = -1L
 
   override fun intercept(chain: NetworkChain) {
     val listener: Limiter.Listener? = limiter.acquire(actionName).orElse(null)
 
     if (listener == null) {
-      logger.error { "concurrency limits interceptor shedding $actionName" }
+      logShedRequest()
       chain.httpCall.statusCode = StatusCode.SERVICE_UNAVAILABLE.code
       chain.httpCall.takeResponseBody()?.use { sink ->
         sink.writeUtf8("service unavailable")
@@ -58,6 +68,17 @@ internal class ConcurrencyLimitsInterceptor internal constructor(
     }
   }
 
+  private fun logShedRequest() {
+    val nowMs = clock.millis()
+    val durationSinceLastErrorMs = nowMs - lastErrorLoggedAtMs
+    var level = Level.INFO
+    if (lastErrorLoggedAtMs == -1L || durationSinceLastErrorMs >= durationBetweenErrorsMs) {
+      lastErrorLoggedAtMs = nowMs
+      level = Level.ERROR
+    }
+    logger.log(level = level) { "concurrency limits interceptor shedding $actionName" }
+  }
+
   @Singleton
   class Factory @Inject constructor(val clock: Clock) : NetworkInterceptor.Factory {
     override fun create(action: Action): NetworkInterceptor? {
@@ -67,7 +88,7 @@ internal class ConcurrencyLimitsInterceptor internal constructor(
           .clock { clock.millis() }
           .build<String>()
 
-      return ConcurrencyLimitsInterceptor(action.name, limiter)
+      return ConcurrencyLimitsInterceptor(action.name, limiter, clock)
     }
   }
 
