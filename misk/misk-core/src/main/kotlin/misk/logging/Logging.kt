29f799dcc5e2af24701fddeea2d22de8ecdcc6d9
Yunchi Luo <mightyguava@gmail.com>
2020-08-18 16:13:56 -0400

    document and simplify SampledLogger construction (#1591)

--- misk/misk-core/src/main/kotlin/misk/logging/Logging.kt
+++ misk/misk-core/src/main/kotlin/misk/logging/Logging.kt
@@ -12,6 +12,18 @@ inline fun <reified T> getLogger(): KLogger {
   return KotlinLogging.logger(T::class.qualifiedName!!)
 }
 
+/**
+ * Returns a logger that samples logs. This logger MUST be instantiated statically,
+ * in a companion object or as a Singleton.
+ *
+ * To get a rate limited logger:
+ *
+ *   val logger = getLogger<MyClass>().sampled(RateLimitingSampler(RATE_PER_SECOND))
+ *
+ * To get a probabilistic sampler
+ *
+ *   val logger = getLogger<MyClass>().sampled(PercentSampler(PERCENTAGE_TO_ALLOW))
+ */
 fun KLogger.sampled(sampler: Sampler): KLogger {
   return SampledLogger(this, sampler)
 }
