29d68ff0c55ef35df6f597e383342dd569268643
tso <tohlson@squareup.com>
2020-09-29 15:33:39 -0400

    Adds PGP support via BouncyCastle (#1612)

--- misk/misk-crypto/src/test/kotlin/misk/crypto/CryptoTestModule.kt
+++ misk/misk-crypto/src/test/kotlin/misk/crypto/CryptoTestModule.kt
@@ -1,17 +1,17 @@
 package misk.crypto
 
 import com.google.crypto.tink.KmsClient
-import com.google.inject.Provides
-import com.google.inject.Singleton
-import misk.inject.KAbstractModule
-import misk.logging.LogCollectorModule
 import com.google.crypto.tink.aead.AeadConfig
 import com.google.crypto.tink.daead.DeterministicAeadConfig
 import com.google.crypto.tink.hybrid.HybridConfig
 import com.google.crypto.tink.mac.MacConfig
 import com.google.crypto.tink.signature.SignatureConfig
 import com.google.crypto.tink.streamingaead.StreamingAeadConfig
+import com.google.inject.Provides
+import com.google.inject.Singleton
 import misk.MiskTestingServiceModule
+import misk.inject.KAbstractModule
+import misk.logging.LogCollectorModule
 
 /**
  * Test module that binds [FakeKmsClient] to be used as the [KmsClient]
