260a8dfd0482148bf895fe9b7216a037826245ed
Andrew (Paradi) Alexander <adrw@squareup.com>
2020-12-17 16:35:00 -0500

    Database Tab: Add maxRows config and audit logging (#1703)
    This adds configurability of max rows (ie. sql limit) for all queries
    and adds audit logging to all queries that include the caller, the
    select paths, and the query they ran for audit purposes.

--- misk/misk-hibernate/src/main/kotlin/misk/hibernate/actions/HibernateDatabaseQueryDynamicAction.kt
+++ misk/misk-hibernate/src/main/kotlin/misk/hibernate/actions/HibernateDatabaseQueryDynamicAction.kt
@@ -13,6 +13,7 @@ import misk.hibernate.actions.HibernateDatabaseQueryWebActionModule.Companion.ch
 import misk.hibernate.actions.HibernateDatabaseQueryWebActionModule.Companion.findDatabaseQueryMetadata
 import misk.hibernate.actions.HibernateDatabaseQueryWebActionModule.Companion.getTransacterForDatabaseQueryAction
 import misk.hibernate.actions.HibernateDatabaseQueryWebActionModule.Companion.validateSelectPathsOrDefault
+import misk.logging.getLogger
 import misk.scope.ActionScoped
 import misk.web.Post
 import misk.web.RequestBody
@@ -33,25 +34,25 @@ internal class HibernateDatabaseQueryDynamicAction @Inject constructor(
   @JvmSuppressWildcards private val callerProvider: ActionScoped<MiskCaller?>,
   private val databaseQueryMetadata: List<DatabaseQueryMetadata>,
   private val injector: Injector,
-  private val queryConfig: ReflectionQuery.QueryLimitsConfig
+  private val queryLimitsConfig: ReflectionQuery.QueryLimitsConfig
 ) : WebAction {
 
   @Post(HIBERNATE_QUERY_DYNAMIC_WEBACTION_PATH)
   @RequestContentType(MediaTypes.APPLICATION_JSON)
   @ResponseContentType(MediaTypes.APPLICATION_JSON)
-  @LogRequestResponse
   @AdminDashboardAccess
   fun query(@RequestBody request: Request): Response {
     val caller = callerProvider.get()!!
     val queryClass = request.queryClass
 
     checkQueryMatchesAction(queryClass, true)
+    logger.info("Query sent from dashboard [principal=$caller][dbEntity=${request.entityClass}] ${request.query}")
 
     val metadata = findDatabaseQueryMetadata(databaseQueryMetadata, queryClass)
     val transacter = getTransacterForDatabaseQueryAction(injector, metadata)
 
     val results = if (caller.isAllowed(metadata.allowedCapabilities, metadata.allowedServices)) {
-      runDynamicQuery(transacter, request, metadata)
+      runDynamicQuery(transacter, caller.principal, request, metadata)
     } else {
       throw UnauthorizedException("Unauthorized to query [dbEntity=${metadata.entityClass}]")
     }
@@ -61,6 +62,7 @@ internal class HibernateDatabaseQueryDynamicAction @Inject constructor(
 
   private fun runDynamicQuery(
     transacter: Transacter,
+    principal: String,
     request: Request,
     metadata: DatabaseQueryMetadata
   ) = transacter.transaction { session ->
@@ -68,7 +70,7 @@ internal class HibernateDatabaseQueryDynamicAction @Inject constructor(
       ?: throw BadRequestException(
         "[dbEntity=${metadata.entityClass}] is not an installed HibernateEntity"
       )
-    val (selectPaths, rows) = runDynamicQuery(session, dbEntity, request)
+    val (selectPaths, rows) = runDynamicQuery(session, principal, dbEntity, request)
     rows.map { row ->
       // TODO (adrw) sort the map based on DbEntity order
       // TODO (adrw) Mirror this over to the static path
@@ -78,17 +80,22 @@ internal class HibernateDatabaseQueryDynamicAction @Inject constructor(
 
   private fun runDynamicQuery(
     session: Session,
+    principal: String,
     dbEntity: KClass<out DbEntity<*>>,
     request: Request,
   ): Pair<List<String>, List<List<Any?>>> {
-    val configuredQuery =
-      ReflectionQuery.Factory(queryConfig).dynamicQuery(dbEntity).configureDynamic(request)
+    val maxRows = request.query.queryConfig?.maxRows ?: queryLimitsConfig.maxMaxRows
+    val configuredQuery = ReflectionQuery.Factory(queryLimitsConfig)
+      .dynamicQuery(dbEntity)
+      .configureDynamic(request, maxRows)
     val selectPaths = validateSelectPathsOrDefault(dbEntity, request.query.select?.paths)
+    logger.info("Query sent from dashboard [principal=$principal][dbEntity=${request.entityClass}][selectPaths=$selectPaths] ${request.query}")
     val rows = configuredQuery.dynamicList(session, selectPaths)
     return Pair(selectPaths, rows)
   }
 
-  private fun Query<out DbEntity<*>>.configureDynamic(request: Request) = apply {
+  private fun Query<out DbEntity<*>>.configureDynamic(request: Request, rowLimit: Int) = apply {
+    maxRows = rowLimit
     request.query.constraints?.forEach { (path, operator, value) ->
       if (path == null) throw BadRequestException("Constraint path must be non-null")
       if (operator == null) throw BadRequestException("Constraint operator must be non-null")
@@ -99,12 +106,13 @@ internal class HibernateDatabaseQueryDynamicAction @Inject constructor(
       if (ascending == null) throw BadRequestException("Order ascending must be non-null")
       dynamicAddOrder(path = path, asc = ascending)
     }
+
   }
 
   data class Request(
     val entityClass: String,
     val queryClass: String,
-    val query: HibernateDatabaseQueryMetadataFactory.Companion.DynamicQuery
+    val query: HibernateDatabaseQueryMetadataFactory.Companion.DynamicQuery,
   )
 
   data class Response(
@@ -112,6 +120,8 @@ internal class HibernateDatabaseQueryDynamicAction @Inject constructor(
   )
 
   companion object {
+    private val logger = getLogger<HibernateDatabaseQueryDynamicAction>()
+
     const val HIBERNATE_QUERY_DYNAMIC_WEBACTION_PATH = "/api/database/query/hibernate/dynamic"
   }
 }
