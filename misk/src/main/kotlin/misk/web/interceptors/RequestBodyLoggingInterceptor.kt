f159975de384b551e9bc4bba4dfb4093237d7f03
Ricardo Mariano <rmariano@squareup.com>
2021-02-02 16:16:20 +1100

    Filter headers being captured in request/response logging interceptor (#1750)

--- misk/src/main/kotlin/misk/web/interceptors/RequestBodyLoggingInterceptor.kt
+++ misk/src/main/kotlin/misk/web/interceptors/RequestBodyLoggingInterceptor.kt
@@ -6,6 +6,7 @@ import misk.Chain
 import misk.MiskCaller
 import misk.logging.getLogger
 import misk.scope.ActionScoped
+import okhttp3.Headers
 import javax.inject.Inject
 import javax.inject.Singleton
 import kotlin.reflect.full.findAnnotation
@@ -56,13 +57,32 @@ class RequestBodyLoggingInterceptor @Inject internal constructor(
 
     try {
       val result = chain.proceed(chain.args)
-      bodyCapture.set(RequestResponseBody(chain.args, result))
+      // Only log some request headers.
+      val args = chain.args.map { if (it is Headers) HeadersCapture(it) else it }
+      bodyCapture.set(RequestResponseBody(args, result))
       return result
     } catch (t: Throwable) {
       logger.info { "${action.name} principal=$principal failed" }
       throw t
     }
   }
+}
+
+internal data class HeadersCapture(
+  val headers: Map<String, List<String>>
+) {
+  constructor(okHttpHeaders: Headers) : this(
+    okHttpHeaders.toMultimap()
+      .filter { (key, _) ->
+        key.toLowerCase() in listOf(
+          "accept",
+          "accept-encoding",
+          "connection",
+          "content-type",
+          "content-length",
+        )
+      }
+  )
 }
 
 internal class RequestResponseCapture @Inject constructor() {
