53f46722f74a47bebcfb29f2c4d3952c35382a63
Yoav Amit <yoav@squareup.com>
2021-02-05 15:58:02 -0500

    Use static keys in tests

--- misk-crypto/src/main/kotlin/misk/crypto/FakeExternalKeyManager.kt
+++ misk-crypto/src/main/kotlin/misk/crypto/FakeExternalKeyManager.kt
@@ -1,18 +1,7 @@
 package misk.crypto
 
-import com.google.crypto.tink.CleartextKeysetHandle
-import com.google.crypto.tink.JsonKeysetWriter
-import com.google.crypto.tink.KeysetHandle
-import com.google.crypto.tink.aead.AeadKeyTemplates
-import com.google.crypto.tink.daead.DeterministicAeadKeyTemplates
-import com.google.crypto.tink.hybrid.HybridKeyTemplates
-import com.google.crypto.tink.mac.MacKeyTemplates
-import com.google.crypto.tink.signature.SignatureKeyTemplates
-import com.google.crypto.tink.streamingaead.StreamingAeadKeyTemplates
 import misk.config.MiskConfig
-import java.io.ByteArrayOutputStream
 import java.lang.UnsupportedOperationException
-import java.nio.charset.Charset
 
 class FakeExternalKeyManager : ExternalKeyManager {
   private var returnedKeysets: MutableMap<KeyAlias, Key> = mutableMapOf()
@@ -23,31 +12,42 @@ class FakeExternalKeyManager : ExternalKeyManager {
   // Mock remote keys
   constructor(aliases: Map<KeyAlias, KeyType>) {
     aliases.forEach { (alias, type) ->
-      val template = when (type) {
-        KeyType.AEAD -> AeadKeyTemplates.AES256_GCM
-        KeyType.DAEAD -> DeterministicAeadKeyTemplates.AES256_SIV
-        KeyType.STREAMING_AEAD -> StreamingAeadKeyTemplates.AES128_GCM_HKDF_4KB
-        KeyType.MAC -> MacKeyTemplates.HMAC_SHA256_128BITTAG
-        KeyType.DIGITAL_SIGNATURE -> SignatureKeyTemplates.ECDSA_P256
-        KeyType.HYBRID_ENCRYPT, KeyType.HYBRID_ENCRYPT_DECRYPT ->
-          HybridKeyTemplates.ECIES_P256_HKDF_HMAC_SHA256_AES128_GCM
-        KeyType.PGP_DECRYPT, KeyType.PGP_ENCRYPT ->
-          throw UnsupportedOperationException("fake manager doesn't support pgp keys")
+      if (type == KeyType.PGP_ENCRYPT || type == KeyType.PGP_DECRYPT) {
+        throw UnsupportedOperationException("PGP keys are not supported yet as external keys")
       }
-      val handle = KeysetHandle.generateNew(template)
-
-      val baos = ByteArrayOutputStream()
-      val writer = JsonKeysetWriter.withOutputStream(baos)
-
-      CleartextKeysetHandle.write(handle, writer)
-      returnedKeysets[alias] =
-          Key(alias, type, MiskConfig.RealSecret(baos.toString(Charset.defaultCharset())))
+      val secret = keyTypeToSecret(type)
+      returnedKeysets[alias] = Key(alias, type, secret)
     }
   }
 
   // Mock local keys
   constructor(rawKeys: List<Key>) {
-    rawKeys.forEach { key -> returnedKeysets[key.key_name] = key }
+    rawKeys.forEach { key ->
+      val plaintextSecret = keyTypeToSecret(key.key_type)
+      val encryptedSecret = if ((key.key_type == KeyType.HYBRID_ENCRYPT && key.kms_uri == null) ||
+              key.key_type == KeyType.PGP_DECRYPT || key.key_type == KeyType.PGP_ENCRYPT) {
+        plaintextSecret
+      } else if (key.key_name == "obsolete") {
+        key.encrypted_key
+      } else {
+        TestKeysets.encryptSecret(plaintextSecret)
+      }
+      returnedKeysets[key.key_name] = key.copy(encrypted_key = encryptedSecret)
+    }
+  }
+
+  private fun keyTypeToSecret(type: KeyType): MiskConfig.RealSecret<String> {
+    return when (type) {
+      KeyType.AEAD -> TestKeysets.AEAD
+      KeyType.DAEAD -> TestKeysets.DAEAD
+      KeyType.STREAMING_AEAD -> TestKeysets.STREAMING_AEAD
+      KeyType.MAC -> TestKeysets.MAC
+      KeyType.DIGITAL_SIGNATURE -> TestKeysets.DIGITAL_SIGNATURE
+      KeyType.HYBRID_ENCRYPT, KeyType.HYBRID_ENCRYPT_DECRYPT ->
+        TestKeysets.HYBRID
+      KeyType.PGP_ENCRYPT -> TestKeysets.PGP_ENCRYPT
+      KeyType.PGP_DECRYPT -> TestKeysets.PGP_DECRYPT
+    }
   }
 
   override fun getKeyByAlias(alias: KeyAlias): Key? {
