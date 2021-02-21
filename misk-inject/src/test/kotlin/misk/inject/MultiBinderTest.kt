2fd5e9541c6badabc9885c7e926f60de69df2d32
Chris Ryan <chrisryan@squareup.com>
2021-02-22 09:24:27 +1000

    Fix test from merge issue

--- misk-inject/src/test/kotlin/misk/inject/MultiBinderTest.kt
+++ misk-inject/src/test/kotlin/misk/inject/MultiBinderTest.kt
@@ -1,40 +1,30 @@
 package misk.inject
 
-import com.google.inject.name.Names
-import javax.inject.Inject
-import javax.inject.Named
 import misk.testing.MiskTest
 import misk.testing.MiskTestModule
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
+import javax.inject.Inject
 
 @MiskTest
 class MultiBinderTest {
   @MiskTestModule
   val module = MultiBindingsModule()
 
-  @Inject @TestAnnotation private lateinit var intList: List<Int>
-  @Inject @TestAnnotation private lateinit var intSet: Set<Int>
-
   @Inject @TestAnnotation private lateinit var colorList: List<Color>
   @Inject @TestAnnotation private lateinit var colorSet: Set<Color>
-
-  @Inject @field:Named("hello") private lateinit var namedIntList: List<Int>
-  @Inject @field:Named("hello") private lateinit var namedIntSet: Set<Int>
+  @Inject @TestAnnotation private lateinit var intList: List<Int>
+  @Inject @TestAnnotation private lateinit var intSet: Set<Int>
 
   @Inject private lateinit var unqualifiedColorList: List<Color>
   @Inject private lateinit var unqualifiedColorSet: Set<Color>
 
   @Test
   fun testMultibinder() {
-    assertThat(intList).hasSize(1)
-    assertThat(intSet).hasSize(1)
-
     assertThat(colorList).hasSize(2)
     assertThat(colorSet).hasSize(2)
-
-    assertThat(namedIntList).hasSize(3)
-    assertThat(namedIntSet).hasSize(3)
+    assertThat(intList).hasSize(1)
+    assertThat(intSet).hasSize(1)
 
     assertThat(unqualifiedColorList).hasSize(1)
     assertThat(unqualifiedColorSet).hasSize(1)
@@ -53,12 +43,6 @@ class MultiBindingsModule : KAbstractModule() {
     newMultibinder<Color>(TestAnnotation::class).addBinding().toInstance(
       Red()
     )
-    multibind<Color>(TestAnnotation::class).toInstance(Red())
-
-    // Three named ints.
-    newMultibinder<Int>(Names.named("hello")).addBinding().toInstance(1)
-    multibind<Int>(Names.named("hello")).toInstance(2)
-    multibind<Int>(Names.named("hello")).toInstance(3)
 
     // One unannotated color.
     newMultibinder<Color>().addBinding().toInstance(Blue())
