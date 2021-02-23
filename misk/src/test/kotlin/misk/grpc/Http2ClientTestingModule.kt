55201bcce4ef96783cea92ed1c4acf0f826c3a8f
JP Podlech <jpodlech@squareup.com>
2021-02-22 16:46:15 -0800

    TEST: will squash later

--- misk/src/test/kotlin/misk/grpc/Http2ClientTestingModule.kt
+++ misk/src/test/kotlin/misk/grpc/Http2ClientTestingModule.kt
@@ -11,6 +11,7 @@ import misk.inject.KAbstractModule
 import misk.security.ssl.SslLoader
 import misk.security.ssl.TrustStoreConfig
 import misk.web.jetty.JettyService
+import java.time.Duration
 import javax.inject.Singleton
 
 /**
@@ -39,7 +40,8 @@ class Http2ClientTestingModule(val jetty: JettyService) : KAbstractModule() {
                 resource = "classpath:/ssl/server_cert.pem",
                 format = SslLoader.FORMAT_PEM
               )
-            )
+            ),
+            callTimeout = Duration.ofMillis(100)
           )
         )
       )
