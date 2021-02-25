7df413bf3ff3720fb0c8f36803b372e01cedf010
Michaël Ortali <mortali@squareup.com>
2021-02-25 13:01:47 -0500

    Adds access to the fakeClock from static context.
    For unit-tests as well as areas where bindings are not always available,
    we need to be able to access directly the fakeclock instance.

--- misk-testing/src/main/kotlin/misk/time/FakeClockModule.kt
+++ misk-testing/src/main/kotlin/misk/time/FakeClockModule.kt
@@ -6,6 +6,10 @@ import java.time.Clock
 class FakeClockModule : KAbstractModule() {
   override fun configure() {
     bind<Clock>().to<FakeClock>()
-    bind<FakeClock>().toInstance(FakeClock())
+    bind<FakeClock>().toInstance(fakeClock)
+  }
+
+  companion object {
+    val fakeClock by lazy { FakeClock() }
   }
 }
