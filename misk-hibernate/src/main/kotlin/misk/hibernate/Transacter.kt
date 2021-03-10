f6dbc11f36be044c8beba55b122b77bc8e401fde
Jesse Wilson <jwilson@squareup.com>
2021-03-10 18:00:45 -0500

    Don't track the previous session
    This was a source of complexity and potential memory leaks for very little value.

--- misk-hibernate/src/main/kotlin/misk/hibernate/Transacter.kt
+++ misk-hibernate/src/main/kotlin/misk/hibernate/Transacter.kt
@@ -78,11 +78,6 @@ interface Transacter {
 
   /** Returns KClasses for the bound DbEntities for the transacter */
   fun entities(): Set<KClass<out DbEntity<*>>>
-
-  /**
-   * Free resources, etc.
-   */
-  fun shutDown()
 }
 
 fun Transacter.shards() = transaction { it.shards() }
