a7b8e7bf59ac5efdf5cad649ebeea3e58bf69b02
Tristan Ohlson <tohlson@squareup.com>
2021-03-19 19:45:00 -0400

    Implements form request builder and sender

--- misk/web/tabs/web-actions/src/rewrite/WebActionCollapse.tsx
+++ misk/web/tabs/web-actions/src/rewrite/WebActionCollapse.tsx
@@ -5,21 +5,29 @@ interface Props {
   title: string
   subtitle?: string
   children?: React.ReactNode
+  doubleWidth?: boolean
 }
 
 export default function WebActionCollapse({
   title,
   subtitle,
-  children
+  children,
+  doubleWidth
 }: Props) {
   const [expanded, setExpanded] = useState(false)
 
   return (
-    <div style={{ maxWidth: "550px", wordBreak: "break-word" }}>
+    <div
+      style={{
+        maxWidth: doubleWidth ? "1100px" : "550px",
+        wordBreak: "break-word"
+      }}
+    >
       <MenuItem
         text={title}
         label={subtitle}
         icon={expanded ? "caret-down" : "caret-right"}
+        disabled={!title}
         onClick={() => setExpanded(expanded => !expanded)}
       />
       <Collapse isOpen={expanded} transitionDuration={0}>
