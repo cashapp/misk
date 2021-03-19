7935d6e2d27e0ad6d7d0405b16fefe65bd48edb2
Scotte Zinn <57361549+scotte-square@users.noreply.github.com>
2021-03-19 08:21:21 -0400

    Simple cron runner (#1799)
    * Simple cron runner
    
    * Remove need for priority queue of next executions

---
+++ misk/misk-cron/src/main/kotlin/misk/cron/CronService.kt
@@ -0,0 +1,38 @@
+package misk.cron
+
+import com.google.common.util.concurrent.AbstractIdleService
+import com.google.inject.Injector
+import misk.logging.getLogger
+import javax.inject.Inject
+import javax.inject.Singleton
+
+@Singleton
+internal class CronService @Inject constructor(
+  private val injector: Injector
+) : AbstractIdleService() {
+  @Inject private lateinit var cronManager: CronManager
+  @Inject private lateinit var cronRunnableEntries: List<CronRunnableEntry>
+
+  override fun startUp() {
+    logger.info { "CronService started" }
+
+    cronRunnableEntries.forEach { cronRunnable ->
+      val name = cronRunnable.runnableClass.qualifiedName!!
+      val annotations = cronRunnable.runnableClass.annotations
+      val cronPattern = annotations.find { it is CronPattern } as? CronPattern
+        ?: throw IllegalArgumentException("Expected $name to have @CronPattern specified")
+
+      val runnable = injector.getProvider(cronRunnable.runnableClass.java).get()
+      cronManager.addCron(name, cronPattern.pattern, runnable)
+    }
+  }
+
+  override fun shutDown() {
+    cronManager.removeAllCrons()
+    logger.info { "CronService stopped" }
+  }
+
+  companion object {
+    private val logger = getLogger<CronService>()
+  }
+}
