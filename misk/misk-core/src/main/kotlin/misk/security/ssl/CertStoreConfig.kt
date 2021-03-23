20dc3dceb38bfd43be175179065c71e58453d252
Ricardo Mariano <rmariano@squareup.com>
2021-03-23 16:43:45 +1100

    Move SSL support to wisp-ssl (#1812)
    Needed to decompose clients.

--- misk/misk-core/src/main/kotlin/misk/security/ssl/CertStoreConfig.kt
+++ misk/misk-core/src/main/kotlin/misk/security/ssl/CertStoreConfig.kt
@@ -1,9 +1,12 @@
 package misk.security.ssl
 
+import wisp.security.ssl.CertStoreConfig as WispCertStoreConfig
 import javax.inject.Inject
 
 data class CertStoreConfig @Inject constructor(
   val resource: String,
   val passphrase: String? = null,
   val format: String = SslLoader.FORMAT_JCEKS
-)
+) {
+  internal fun toWispConfig() = WispCertStoreConfig(resource, passphrase, format)
+}
