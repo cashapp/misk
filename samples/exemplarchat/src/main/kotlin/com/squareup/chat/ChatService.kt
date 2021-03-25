2e78da0c247ca11d8e13d58ab705ccb5575a07ba
Chris Ryan <74329397+chris-ryan-square@users.noreply.github.com>
2021-03-25 12:14:01 +1000

    wisp deployment (#1822)
    * Start on splitting Deployment to wisp
    
    * Make the deprecation a comment for now to avoid downstream confusion and angst

--- samples/exemplarchat/src/main/kotlin/com/squareup/chat/ChatService.kt
+++ samples/exemplarchat/src/main/kotlin/com/squareup/chat/ChatService.kt
@@ -4,7 +4,6 @@ import misk.MiskApplication
 import misk.MiskRealServiceModule
 import misk.config.ConfigModule
 import misk.config.MiskConfig
-import misk.environment.Deployment
 import misk.environment.DeploymentModule
 import misk.environment.Env
 import misk.environment.Environment
@@ -15,17 +14,17 @@ import misk.web.MiskWebModule
 fun main(args: Array<String>) {
   val environment = Environment.fromEnvironmentVariable()
   val env = Env(environment.name)
-  val deployment = Deployment(name = "exemplarchat", isLocalDevelopment = true)
+  val deployment = wisp.deployment.Deployment(name = "exemplarchat", isLocalDevelopment = true)
 
   val config = MiskConfig.load<ChatConfig>("chat", env)
 
   MiskApplication(
-      MiskRealServiceModule(),
-      MiskWebModule(config.web),
-      RealEventRouterModule(environment),
-      ChatModule(),
-      ConfigModule.create("chat", config),
-      DeploymentModule(deployment, env),
-      PrometheusMetricsServiceModule(config.prometheus)
+    MiskRealServiceModule(),
+    MiskWebModule(config.web),
+    RealEventRouterModule(environment),
+    ChatModule(),
+    ConfigModule.create("chat", config),
+    DeploymentModule(deployment, env),
+    PrometheusMetricsServiceModule(config.prometheus)
   ).run(args)
 }
