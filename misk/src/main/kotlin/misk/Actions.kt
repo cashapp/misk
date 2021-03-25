66fb5ae09f574e920defd76adab00b8611e1ca5c
Ricardo Mariano <rmariano@squareup.com>
2021-03-26 09:18:10 +1100

    Use all outer class names for nested actions
    The typical use case for this is when people write nested gRPC actions, e.g.
    
    ```
    class MyAction {
      class GrpcAction {
        // ...
      }
    }
    ```

--- misk/src/main/kotlin/misk/Actions.kt
+++ misk/src/main/kotlin/misk/Actions.kt
@@ -21,7 +21,9 @@ fun KFunction<*>.asAction(
   val actualParameters = parameters.drop(1)
   val actionName = instanceParameter.type.classifier?.let {
     when (it) {
-      is KClass<*> -> it.simpleName
+      // Use the class name including all outer classes, but excluding the package (assuming a
+      // packages start with a lowercase character and classes start with an uppercase one).
+      is KClass<*> -> it.qualifiedName?.replace(Regex("^[a-z.]*"), "")
       else -> name
     }
   } ?: name
@@ -47,9 +49,7 @@ fun KFunction<*>.asAction(
         }
         MediaTypes.APPLICATION_GRPC_MEDIA_TYPE
       }
-      else -> findAnnotation<ResponseContentType>()?.value?.let {
-        it.toMediaTypeOrNull()
-      }
+      else -> findAnnotation<ResponseContentType>()?.value?.toMediaTypeOrNull()
     }
 
   return Action(
