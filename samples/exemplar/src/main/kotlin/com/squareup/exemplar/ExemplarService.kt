ee46615363affe58760fdb2ce8add892582fd7f6
Chris Ryan <74329397+chris-ryan-square@users.noreply.github.com>
2021-03-25 12:14:01 +1000

    wisp deployment (#1822)
    * Start on splitting Deployment to wisp
    
    * Make the deprecation a comment for now to avoid downstream confusion and angst

--- samples/exemplar/src/main/kotlin/com/squareup/exemplar/ExemplarService.kt
+++ samples/exemplar/src/main/kotlin/com/squareup/exemplar/ExemplarService.kt
@@ -4,11 +4,15 @@ import misk.MiskApplication
 import misk.MiskRealServiceModule
 import misk.config.ConfigModule
 import misk.config.MiskConfig
-import misk.environment.*
+import misk.environment.DeploymentModule
+import misk.environment.Env
+import misk.environment.Environment
+import misk.environment.EnvironmentModule
 import misk.metrics.backends.prometheus.PrometheusMetricsServiceModule
 import misk.web.MiskWebModule
 import misk.web.dashboard.AdminDashboardModule
 import misk.web.dashboard.ConfigDashboardTabModule
+import wisp.deployment.Deployment
 
 fun main(args: Array<String>) {
   val environment = Environment.fromEnvironmentVariable()
@@ -16,15 +20,15 @@ fun main(args: Array<String>) {
   val deployment = Deployment(name = "exemplar", isLocalDevelopment = true)
   val config = MiskConfig.load<ExemplarConfig>("exemplar", env)
   MiskApplication(
-      MiskRealServiceModule(),
-      MiskWebModule(config.web),
-      ExemplarAccessModule(),
-      ExemplarWebActionsModule(),
-      ConfigModule.create("exemplar", config),
-      DeploymentModule(deployment, env),
-      PrometheusMetricsServiceModule(config.prometheus),
-      EnvironmentModule(environment = environment),
-      AdminDashboardModule(isDevelopment = true),
-      ConfigDashboardTabModule(isDevelopment = true)
+    MiskRealServiceModule(),
+    MiskWebModule(config.web),
+    ExemplarAccessModule(),
+    ExemplarWebActionsModule(),
+    ConfigModule.create("exemplar", config),
+    DeploymentModule(deployment, env),
+    PrometheusMetricsServiceModule(config.prometheus),
+    EnvironmentModule(environment = environment),
+    AdminDashboardModule(isDevelopment = true),
+    ConfigDashboardTabModule(isDevelopment = true)
   ).run(args)
 }
