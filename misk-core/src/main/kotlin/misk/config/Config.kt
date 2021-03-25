5f180bfc49132ac2f4ab880d9acdce45c68ac281
Chris Ryan <chrisryan@squareup.com>
2021-03-25 13:02:58 +1000

    Make the deprecation soft to avoid downstream angst

--- misk-core/src/main/kotlin/misk/config/Config.kt
+++ misk-core/src/main/kotlin/misk/config/Config.kt
@@ -1,10 +1,11 @@
 package misk.config
 
-@Deprecated(
-  message = "Use wisp.config.Config directly",
-  replaceWith = ReplaceWith(
-    "Config",
-    "wisp.config.Config"
-  )
-)
+// TODO(chrisryan): soft deprecating...
+// @Deprecated(
+//  message = "Use wisp.config.Config directly",
+//  replaceWith = ReplaceWith(
+//    "Config",
+//    "wisp.config.Config"
+//  )
+// )
 interface Config : wisp.config.Config
