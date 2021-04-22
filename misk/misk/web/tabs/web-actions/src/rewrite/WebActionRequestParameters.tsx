cb5f2f356179cc048318e4d99dbaba3340127d86
Ben Askins <baskins@squareup.com>
2021-04-23 08:46:54 +1000

    Show proto fields for protobuf based web actions (#1861)
    * If request type is a protobuf display the fields
    
    * Include proto fields for response types

---
+++ misk/misk/web/tabs/web-actions/src/rewrite/WebActionRequestParameters.tsx
@@ -0,0 +1,52 @@
+import React from "react"
+import { HTMLTable, UL } from "@blueprintjs/core"
+import { WebActionMetadata} from "./types"
+import WebActionParameter from "./WebActionParameter"
+import WebActionCollapse from "./WebActionCollapse"
+import WebActionProtoField from "./WebActionProtoField"
+
+interface Props {
+  webActionMetadata: WebActionMetadata
+}
+
+export default function WebActionRequestParameters({ webActionMetadata }: Props) {
+  const requestType = webActionMetadata.types[webActionMetadata.requestType]
+  const parameters = webActionMetadata.parameters
+
+  if (parameters.length == 0) {
+    return null
+  }
+
+  return (
+    <>
+      {requestType ? (
+        <WebActionCollapse
+          title={"Request Parameters"}
+          subtitle={webActionMetadata.requestType}
+          doubleWidth={true}>
+          <UL style={{ listStyle: "none" }}>
+            <li>
+              <HTMLTable style={{ marginBottom: "0px" }}>
+                {requestType.fields.map(field => (
+                  <WebActionProtoField field={field} />
+                ))}
+              </HTMLTable>
+            </li>
+          </UL>
+        </WebActionCollapse>
+      ) : (
+        <WebActionCollapse title={"Request Parameters"} doubleWidth={true}>
+          <UL style={{ listStyle: "none" }}>
+            <li>
+              <HTMLTable style={{ marginBottom: "0px" }}>
+                {parameters.map(parameter => (
+                  <WebActionParameter parameter={parameter} />
+                ))}
+              </HTMLTable>
+            </li>
+          </UL>
+        </WebActionCollapse>
+      )}
+    </>
+  )
+}
