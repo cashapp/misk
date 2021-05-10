d4081f2526283fb6e338a287b2179beb5f4069eb
Chris Ryan <chrisryan@squareup.com>
2021-05-10 15:17:46 +1000

    AWS environment support in wisp

--- misk-aws/src/main/kotlin/misk/cloud/aws/Types.kt
+++ misk-aws/src/main/kotlin/misk/cloud/aws/Types.kt
@@ -1,7 +1,15 @@
 package misk.cloud.aws
 
+/**
+ * Soft deprecation - use wisp.aws.environment
+ */
+
 /** [AwsRegion] is the region in which the service is running */
 data class AwsRegion(val name: String)
 
+fun wisp.aws.environment.AwsRegion.toMiskAwsRegion(): AwsRegion = AwsRegion(this.name)
+
 /** [AwsAccountId] is the id of the account in which the service is running */
 data class AwsAccountId(val value: String)
+
+fun wisp.aws.environment.AwsAccountId.toMiskAwsAccountId(): AwsAccountId = AwsAccountId(this.value)
