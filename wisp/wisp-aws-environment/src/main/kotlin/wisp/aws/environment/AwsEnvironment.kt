eae881c30ac7ce3b8d89a7463431b561d7e57e05
Chris Ryan <74329397+chris-ryan-square@users.noreply.github.com>
2021-05-12 09:11:00 +1000

    Provide defaults for Aws environment functions; add some tests :) (cashapp/misk#1902)

--- wisp/wisp-aws-environment/src/main/kotlin/wisp/aws/environment/AwsEnvironment.kt
+++ wisp/wisp-aws-environment/src/main/kotlin/wisp/aws/environment/AwsEnvironment.kt
@@ -4,14 +4,29 @@ import wisp.deployment.EnvironmentVariableLoader
 
 /** [AwsEnvironment] pulls region and account information from installed
  * environment variables
+ *
+ * Note: Because of different package names for AWS1 vs AWS2 sdks, we can't use AWS's Region class.
  */
-class AwsEnvironment {
+object AwsEnvironment {
 
-  fun awsRegion(environmentVariableLoader: EnvironmentVariableLoader): AwsRegion {
-    return AwsRegion(environmentVariableLoader.getEnvironmentVariable("REGION"))
+  fun awsRegion(
+    environmentVariableLoader: EnvironmentVariableLoader = EnvironmentVariableLoader.real,
+    environmentVariables: List<String> = listOf("REGION", "AWS_REGION"),
+    defaultAwsRegion: String = "us-west-2"
+  ): AwsRegion {
+    for (environmentVariable in environmentVariables) {
+      val region =
+        environmentVariableLoader.getEnvironmentVariableOrDefault(environmentVariable, "")
+      if (region.isNotEmpty()) {
+        return AwsRegion(region)
+      }
+    }
+    return AwsRegion(defaultAwsRegion)
   }
 
-  fun awsAccountId(environmentVariableLoader: EnvironmentVariableLoader): AwsAccountId {
-    return AwsAccountId(environmentVariableLoader.getEnvironmentVariable("ACCOUNT_ID"))
-  }
+  fun awsAccountId(
+    environmentVariableLoader: EnvironmentVariableLoader = EnvironmentVariableLoader.real,
+    environmentVariable: String = "ACCOUNT_ID",
+  ): AwsAccountId =
+    AwsAccountId(environmentVariableLoader.getEnvironmentVariable(environmentVariable))
 }
