ec1677660f6e0cf885faee7ad3016db8b4fba4df
Chris Ryan <74329397+chris-ryan-square@users.noreply.github.com>
2021-05-14 14:16:51 +1000

    Need a mutable map to change it... (cashapp/misk#1910)

--- wisp/wisp-aws-environment/src/test/kotlin/wisp/aws/environment/AwsEnvironmentTest.kt
+++ wisp/wisp-aws-environment/src/test/kotlin/wisp/aws/environment/AwsEnvironmentTest.kt
@@ -7,16 +7,14 @@ internal class AwsEnvironmentTest {
 
   @Test
   fun `AWS Region should be the default when no environment variables set`() {
-    val result = AwsEnvironment.awsRegion(
-      FakeEnvironmentVariableLoader(emptyMap())
-    )
+    val result = AwsEnvironment.awsRegion(FakeEnvironmentVariableLoader())
     kotlin.test.assertEquals(defaultAwsRegion, result)
   }
 
   @Test
   fun `AWS Region should be the supplied default when no environment variables set`() {
     val result = AwsEnvironment.awsRegion(
-      FakeEnvironmentVariableLoader(emptyMap()),
+      FakeEnvironmentVariableLoader(),
       defaultAwsRegion = "ap-east-1"
     )
     kotlin.test.assertEquals(AwsRegion("ap-east-1"), result)
@@ -26,7 +24,7 @@ internal class AwsEnvironmentTest {
   fun `AWS Region should be US_EAST_1 when REGION environment variable set to us-east-1`() {
     val result = AwsEnvironment.awsRegion(
       FakeEnvironmentVariableLoader(
-        mapOf(
+        mutableMapOf(
           "REGION" to "us-east-1"
         )
       )
@@ -38,7 +36,7 @@ internal class AwsEnvironmentTest {
   fun `AWS Region should be US_EAST_2 when AWS_REGION environment variable set to us-east-2`() {
     val result = AwsEnvironment.awsRegion(
       FakeEnvironmentVariableLoader(
-        mapOf(
+        mutableMapOf(
           "AWS_REGION" to "us-east-2"
         )
       )
@@ -51,7 +49,7 @@ internal class AwsEnvironmentTest {
     val myRegion = "MY_REGION"
     val result = AwsEnvironment.awsRegion(
       FakeEnvironmentVariableLoader(
-        mapOf(
+        mutableMapOf(
           myRegion to "us-east-2"
         )
       ),
