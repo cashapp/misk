ab40a3d1069fd0216d1a53e6d9e2ba335fbed3ab
Chris Ryan <74329397+chris-ryan-square@users.noreply.github.com>
2021-05-14 12:05:35 +1000

    Expose the environment variable map to allow for easier changes in tests (cashapp/misk#1909)

--- wisp/wisp-deployment-testing/src/main/kotlin/wisp/deployment/FakeEnvironmentVariableLoader.kt
+++ wisp/wisp-deployment-testing/src/main/kotlin/wisp/deployment/FakeEnvironmentVariableLoader.kt
@@ -3,8 +3,7 @@ package wisp.deployment
 /**
  * A Fake [EnvironmentVariableLoader] that loads from an in memory map
  */
-class FakeEnvironmentVariableLoader(private val vars: Map<String, String>) :
-  EnvironmentVariableLoader {
+class FakeEnvironmentVariableLoader(val vars: Map<String, String>) : EnvironmentVariableLoader {
   override fun getEnvironmentVariable(name: String): String {
     return vars[name] ?: throw IllegalStateException("$name environment variable not set")
   }
