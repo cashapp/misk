a1875e143c4d09f9929966e0512458532ce98f98
Jesse Wilson <jwilson@squareup.com>
2021-05-28 19:07:12 -0400

    FakeMetrics
    The Prometheus metrics API we're using isn't particularly test-friendly,
    but we can get at most of its insides. Most of the value in this class
    is convenience functions to extract the state of the Metrics object for
    making assertions.

---
+++ misk-metrics-testing/src/main/kotlin/misk/metrics/FakeMetrics.kt
@@ -0,0 +1,112 @@
+package misk.metrics
+
+import io.prometheus.client.Collector.MetricFamilySamples.Sample
+import io.prometheus.client.CollectorRegistry
+import io.prometheus.client.Counter
+import io.prometheus.client.Gauge
+import io.prometheus.client.Summary
+
+/**
+ * An in-memory metrics store with APIs to verify which metrics were collected.
+ *
+ * The only way to create an instance of this is with [FakeMetricsModule].
+ */
+class FakeMetrics internal constructor() : Metrics {
+  private val registry = CollectorRegistry(true)
+
+  override fun counter(name: String, help: String?, labelNames: List<String>): Counter =
+    Counter.build(name, help)
+      .labelNames(*labelNames.toTypedArray())
+      .register(registry)
+
+  override fun gauge(name: String, help: String, labelNames: List<String>): Gauge =
+    Gauge.build(name, help)
+      .labelNames(*labelNames.toTypedArray())
+      .register(registry)
+
+  override fun histogram(
+    name: String,
+    help: String,
+    labelNames: List<String>,
+    quantiles: Map<Double, Double>
+  ): Histogram {
+    val summary = Summary.build(name, help)
+      .labelNames(*labelNames.toTypedArray())
+      .apply {
+        quantiles.forEach { (key, value) ->
+          quantile(key, value)
+        }
+      }
+      .register(registry)
+
+    return object : Histogram {
+      override fun record(duration: Double, vararg labelValues: String) {
+        summary.labels(*labelValues).observe(duration)
+      }
+
+      override fun count(vararg labelValues: String): Int =
+        summary.labels(*labelValues).get().count.toInt()
+    }
+  }
+
+  /** Returns a measurement for a [counter] or [gauge]. */
+  fun get(name: String, vararg labels: Pair<String, String>): Double? =
+    getSample(name, labels)?.value
+
+  /** Returns the number of histogram samples taken. */
+  fun histogramCount(name: String, vararg labels: Pair<String, String>): Double? =
+    getSample(name, labels, sampleName = name + "_count")?.value
+
+  /** Returns the sum of all histogram samples taken. */
+  fun histogramSum(name: String, vararg labels: Pair<String, String>): Double? =
+    getSample(name, labels, sampleName = name + "_sum")?.value
+
+  /** Returns the average of all histogram samples taken. */
+  fun histogramMean(name: String, vararg labels: Pair<String, String>): Double? {
+    val sum = histogramSum(name, *labels) ?: return null
+    val count = histogramCount(name, *labels) ?: return null
+    return sum / count
+  }
+
+  /**
+   * Returns the median for a [histogram]. In small samples this is the element preceding
+   * the middle element.
+   */
+  fun histogramP50(name: String, vararg labels: Pair<String, String>): Double? =
+    histogramQuantile(name, "0.5", *labels)
+
+  /**
+   * Returns the 0.99th percentile for a [histogram]. In small samples this is the second largest
+   * element.
+   */
+  fun histogramP99(name: String, vararg labels: Pair<String, String>): Double? =
+    histogramQuantile(name, "0.99", *labels)
+
+  fun histogramQuantile(
+    name: String,
+    quantile: String,
+    vararg labels: Pair<String, String>
+  ): Double? {
+    val extraLabels = labels.toList() + ("quantile" to quantile)
+    return getSample(name, extraLabels.toTypedArray())?.value
+  }
+
+  fun getSample(
+    name: String,
+    labels: Array<out Pair<String, String>>,
+    sampleName: String = name
+  ): Sample? {
+    val metricFamilySamples = registry.metricFamilySamples()
+      .asSequence()
+      .firstOrNull { it.name == name }
+      ?: return null
+
+    val labelNames = labels.map { it.first }
+    val labelValues = labels.map { it.second }
+
+    return metricFamilySamples.samples
+      .firstOrNull {
+        it.name == sampleName && it.labelNames == labelNames && it.labelValues == labelValues
+      }
+  }
+}
--- misk/src/test/kotlin/misk/concurrent/NetflixMetricsAdapterTest.kt
+++ misk/src/test/kotlin/misk/concurrent/NetflixMetricsAdapterTest.kt
@@ -1,12 +1,11 @@
 package misk.concurrent
 
 import com.google.common.util.concurrent.AtomicDouble
-import io.prometheus.client.CollectorRegistry
 import javax.inject.Inject
 import misk.ServiceManagerModule
 import misk.inject.KAbstractModule
-import misk.metrics.backends.prometheus.PrometheusConfig
-import misk.metrics.backends.prometheus.PrometheusMetricsServiceModule
+import misk.metrics.FakeMetrics
+import misk.metrics.FakeMetricsModule
 import misk.testing.MiskTest
 import misk.testing.MiskTestModule
 import misk.time.ClockModule
@@ -18,11 +17,11 @@ internal class NetflixMetricsAdapterTest {
   @MiskTestModule val module = TestModule()
 
   @Inject lateinit var netflixMetricsAdapter: NetflixMetricsAdapter
-  @Inject lateinit var prometheusRegistry: CollectorRegistry
+  @Inject lateinit var metrics: FakeMetrics
 
   @Test
   internal fun counter() {
-    assertThat(countSample("test_gets", "status", "200")).isNull()
+    assertThat(metrics.get("test_gets", "status" to "200")).isNull()
 
     val get200s = netflixMetricsAdapter.create("test_")
       .counter("gets", "status", "200")
@@ -30,12 +29,12 @@ internal class NetflixMetricsAdapterTest {
     get200s.increment()
     get200s.increment()
 
-    assertThat(countSample("test_gets", "status", "200")).isEqualTo(3.0)
+    assertThat(metrics.get("test_gets", "status" to "200")).isEqualTo(3.0)
   }
 
   @Test
   internal fun histogram() {
-    assertThat(p50Sample("test_call_times", "status", "200")).isNull()
+    assertThat(metrics.histogramP50("test_call_times", "status" to "200")).isNull()
 
     val callTimes200s = netflixMetricsAdapter.create("test_")
       .distribution("call_times", "status", "200")
@@ -44,7 +43,8 @@ internal class NetflixMetricsAdapterTest {
     callTimes200s.addSample(99.0)
     callTimes200s.addSample(101.0)
 
-    assertThat(p50Sample("test_call_times", "status", "200")).isIn(99.0, 100.0, 101.0)
+    assertThat(metrics.histogramP50("test_call_times", "status" to "200")).isIn(99.0, 100.0, 101.0)
+    assertThat(metrics.histogramMean("test_call_times", "status" to "200")).isEqualTo(100.0)
   }
 
   @Test
@@ -60,15 +60,15 @@ internal class NetflixMetricsAdapterTest {
     get200s.increment()
     get200s.increment()
 
-    assertThat(countSample("test_gets", "status", "200")).isEqualTo(3.0)
-    assertThat(countSample("test_gets", "status", "503")).isEqualTo(1.0)
-    assertThat(countSample("test_posts", "status", "200")).isEqualTo(2.0)
+    assertThat(metrics.get("test_gets", "status" to "200")).isEqualTo(3.0)
+    assertThat(metrics.get("test_gets", "status" to "503")).isEqualTo(1.0)
+    assertThat(metrics.get("test_posts", "status" to "200")).isEqualTo(2.0)
   }
 
   @Test
   internal fun gauge() {
     val runningCount = AtomicDouble(0.0)
-    assertThat(countSample("thread_count", "state", "running")).isNull()
+    assertThat(metrics.get("thread_count", "state" to "running")).isNull()
     netflixMetricsAdapter.create("test_").gauge(
       "thread_count",
       { runningCount.get() },
@@ -78,11 +78,11 @@ internal class NetflixMetricsAdapterTest {
 
     runningCount.set(20.0)
     netflixMetricsAdapter.updateGauges()
-    assertThat(countSample("test_thread_count", "state", "running")).isEqualTo(20.0)
+    assertThat(metrics.get("test_thread_count", "state" to "running")).isEqualTo(20.0)
 
     runningCount.set(23.0)
     netflixMetricsAdapter.updateGauges()
-    assertThat(countSample("test_thread_count", "state", "running")).isIn(20.0, 23.0)
+    assertThat(metrics.get("test_thread_count", "state" to "running")).isIn(20.0, 23.0)
   }
 
   @Test
@@ -96,9 +96,9 @@ internal class NetflixMetricsAdapterTest {
     callTimes503s.addSample(30.0)
     callSizes200s.addSample(2048.0)
 
-    assertThat(p50Sample("test_call_times", "status", "200")).isEqualTo(100.0)
-    assertThat(p50Sample("test_call_times", "status", "503")).isEqualTo(30.0)
-    assertThat(p50Sample("test_call_sizes", "status", "200")).isEqualTo(2048.0)
+    assertThat(metrics.histogramP50("test_call_times", "status" to "200")).isEqualTo(100.0)
+    assertThat(metrics.histogramP50("test_call_times", "status" to "503")).isEqualTo(30.0)
+    assertThat(metrics.histogramP50("test_call_sizes", "status" to "200")).isEqualTo(2048.0)
   }
 
   @Test
@@ -132,30 +132,16 @@ internal class NetflixMetricsAdapterTest {
     sendingCount.set(456.0)
     netflixMetricsAdapter.updateGauges()
 
-    assertThat(countSample("test_thread_count", "state", "sleeping")).isEqualTo(10.0)
-    assertThat(countSample("test_thread_count", "state", "running")).isEqualTo(20.0)
-    assertThat(countSample("test_socket_count", "state", "sending")).isEqualTo(456.0)
+    assertThat(metrics.get("test_thread_count", "state" to "sleeping")).isEqualTo(10.0)
+    assertThat(metrics.get("test_thread_count", "state" to "running")).isEqualTo(20.0)
+    assertThat(metrics.get("test_socket_count", "state" to "sending")).isEqualTo(456.0)
   }
-
-  private fun countSample(name: String, labelName: String, labelValue: String): Double? =
-    prometheusRegistry.getSampleValue(
-      name,
-      arrayOf(labelName),
-      arrayOf(labelValue)
-    )
-
-  private fun p50Sample(name: String, labelName: String, labelValue: String): Double? =
-    prometheusRegistry.getSampleValue(
-      name,
-      arrayOf(labelName, "quantile"),
-      arrayOf(labelValue, "0.5")
-    )
 
   class TestModule : KAbstractModule() {
     override fun configure() {
       install(ClockModule())
       install(ExecutorsModule())
-      install(PrometheusMetricsServiceModule(PrometheusConfig(http_port = 0)))
+      install(FakeMetricsModule())
       install(NetflixMetricsAdapter.MODULE)
       install(ServiceManagerModule())
     }
