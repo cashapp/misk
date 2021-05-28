6b12e3f7a53839e26e2d8c1af851a8a340401916
Jesse Wilson <jwilson@squareup.com>
2021-05-28 09:25:25 -0400

    Add a health check for DynamoDB
    This restores reverted PR #1937, using describeTable() instead
    of listTables(). That endpoint requires a lot more configuration,
    including wiring the table names all the way through.
    
    This reverts commit c23012833a5d29ab515cd32c842d9e21064596f9.

--- misk-aws2-dynamodb/src/main/kotlin/misk/aws2/dynamodb/RealDynamoDbModule.kt
+++ misk-aws2-dynamodb/src/main/kotlin/misk/aws2/dynamodb/RealDynamoDbModule.kt
@@ -1,24 +1,27 @@
 package misk.aws2.dynamodb
 
 import com.google.inject.Provides
+import javax.inject.Singleton
 import misk.cloud.aws.AwsRegion
+import misk.healthchecks.HealthCheck
 import misk.inject.KAbstractModule
 import software.amazon.awssdk.auth.credentials.AwsCredentialsProvider
 import software.amazon.awssdk.core.client.config.ClientOverrideConfiguration
 import software.amazon.awssdk.regions.Region
 import software.amazon.awssdk.services.dynamodb.DynamoDbClient
-import javax.inject.Singleton
 
 /**
  * Install this module to have access to a DynamoDbClient.
  */
 class RealDynamoDbModule constructor(
   private val clientOverrideConfig: ClientOverrideConfiguration =
-    ClientOverrideConfiguration.builder().build()
+    ClientOverrideConfiguration.builder().build(),
+  private val requiredTables: List<RequiredDynamoDbTable> = listOf()
 ) : KAbstractModule() {
   override fun configure() {
     requireBinding<AwsCredentialsProvider>()
     requireBinding<AwsRegion>()
+    multibind<HealthCheck>().to<DynamoDbHealthCheck>()
   }
 
   @Provides @Singleton
@@ -32,4 +35,7 @@ class RealDynamoDbModule constructor(
       .overrideConfiguration(clientOverrideConfig)
       .build()
   }
+
+  @Provides @Singleton
+  fun provideRequiredTables(): List<RequiredDynamoDbTable> = requiredTables
 }
