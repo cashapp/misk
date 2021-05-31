0f48815ee67c26ad35a662d7fd1667658e32e250
Zhixuan Lai <zhxnlai@gmail.com>
2021-05-31 15:53:29 -0700

    Remove unused dependencies

--- misk-aws2-dynamodb-testing/build.gradle.kts
+++ misk-aws2-dynamodb-testing/build.gradle.kts
@@ -17,15 +17,6 @@ dependencies {
   implementation(project(":misk-testing"))
   api(project(":wisp-containers-testing"))
   api(project(":wisp-logging"))
-  implementation(Dependencies.docker)
-  // The docker-java we use in tests depends on an old version of junixsocket that depends on
-  // log4j. We force it up a minor version in packages that use it.
-  implementation("com.kohlschutter.junixsocket:junixsocket-native-common:2.3.3") {
-    isForce = true
-  }
-  implementation("com.kohlschutter.junixsocket:junixsocket-common:2.3.3") {
-    isForce = true
-  }
 
   testImplementation(Dependencies.assertj)
   testImplementation(Dependencies.awaitility)
