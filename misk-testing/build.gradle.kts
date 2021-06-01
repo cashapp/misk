ce63f18716a99f670f2a1871f16e118454687b3d
Ricardo Mariano <rmariano@squareup.com>
2021-06-01 10:02:55 +1000

    Revert "Use FakeMetrics in MiskTestingServiceModule (#1947)" (#1950)
    This reverts commit f34b8f4953e525062b28125eaf6eb5efce4b123e.

--- misk-testing/build.gradle.kts
+++ misk-testing/build.gradle.kts
@@ -36,7 +36,6 @@ dependencies {
   api(project(":misk-actions"))
   api(project(":misk-core"))
   api(project(":misk-inject"))
-  api(project(":misk-metrics-testing"))
   api(project(":misk-service"))
 }
 
