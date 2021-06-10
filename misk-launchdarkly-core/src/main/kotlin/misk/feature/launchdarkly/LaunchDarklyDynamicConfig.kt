7de97d00505ac1b348e40c0aaced0f39fe9dd316
Maksim Bezsaznyj <maksim@squareup.com>
2021-06-10 11:20:09 -0400

    Expose tracking API throught DynamicConfig/FeatureFlags (#1911)
    * Expose tracking API throught DynamicConfig/FeatureFlags
    
    * Make TrackerReference it's own interface and not Closeable
    
    * Add explicit executor into all track API methods
    
    Co-authored-by: Chris Ryan <74329397+chris-ryan-square@users.noreply.github.com>

--- misk-launchdarkly-core/src/main/kotlin/misk/feature/launchdarkly/LaunchDarklyDynamicConfig.kt
+++ misk-launchdarkly-core/src/main/kotlin/misk/feature/launchdarkly/LaunchDarklyDynamicConfig.kt
@@ -4,6 +4,7 @@ import misk.feature.Attributes
 import misk.feature.DynamicConfig
 import misk.feature.Feature
 import misk.feature.FeatureFlags
+import java.util.concurrent.Executor
 import javax.inject.Singleton
 
 @Singleton
@@ -13,23 +14,41 @@ class LaunchDarklyDynamicConfig(private val featureFlags: FeatureFlags) : Dynami
     val ATTRIBUTES = Attributes(anonymous = true)
   }
 
-  override fun getBoolean(feature: Feature): Boolean {
-    return featureFlags.getBoolean(feature, KEY, ATTRIBUTES)
-  }
+  override fun getBoolean(feature: Feature) =
+    featureFlags.getBoolean(feature, KEY, ATTRIBUTES)
 
-  override fun getInt(feature: Feature): Int {
-    return featureFlags.getInt(feature, KEY, ATTRIBUTES)
-  }
+  override fun getInt(feature: Feature) =
+    featureFlags.getInt(feature, KEY, ATTRIBUTES)
 
-  override fun getString(feature: Feature): String {
-    return featureFlags.getString(feature, KEY, ATTRIBUTES)
-  }
+  override fun getString(feature: Feature) =
+    featureFlags.getString(feature, KEY, ATTRIBUTES)
 
-  override fun <T : Enum<T>> getEnum(feature: Feature, clazz: Class<T>): T {
-    return featureFlags.getEnum(feature, KEY, clazz, ATTRIBUTES)
-  }
+  override fun <T : Enum<T>> getEnum(feature: Feature, clazz: Class<T>) =
+    featureFlags.getEnum(feature, KEY, clazz, ATTRIBUTES)
 
-  override fun <T> getJson(feature: Feature, clazz: Class<T>): T {
-    return featureFlags.getJson(feature, KEY, clazz, ATTRIBUTES)
-  }
+  override fun <T> getJson(feature: Feature, clazz: Class<T>) =
+    featureFlags.getJson(feature, KEY, clazz, ATTRIBUTES)
+
+  override fun trackBoolean(feature: Feature, executor: Executor, tracker: (Boolean) -> Unit) =
+    featureFlags.trackBoolean(feature, KEY, ATTRIBUTES, executor, tracker)
+
+  override fun trackInt(feature: Feature, executor: Executor, tracker: (Int) -> Unit) =
+    featureFlags.trackInt(feature, KEY, ATTRIBUTES, executor, tracker)
+
+  override fun trackString(feature: Feature, executor: Executor, tracker: (String) -> Unit) =
+    featureFlags.trackString(feature, KEY, ATTRIBUTES, executor, tracker)
+
+  override fun <T : Enum<T>> trackEnum(
+    feature: Feature,
+    clazz: Class<T>,
+    executor: Executor,
+    tracker: (T) -> Unit
+  ) = featureFlags.trackEnum(feature, KEY, clazz, ATTRIBUTES, executor, tracker)
+
+  override fun <T> trackJson(
+    feature: Feature,
+    clazz: Class<T>,
+    executor: Executor,
+    tracker: (T) -> Unit
+  ) = featureFlags.trackJson(feature, KEY, clazz, ATTRIBUTES, executor, tracker)
 }
