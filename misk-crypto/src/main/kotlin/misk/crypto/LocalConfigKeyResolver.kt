0d73305f826b1ccdb78faf9b8b3e6f57e7e6db71
Yan Ivnitskiy <yan@users.noreply.github.com>
2021-06-11 11:45:18 -0400

    Allow for passing KMS uri with a key (#1964)

--- misk-crypto/src/main/kotlin/misk/crypto/LocalConfigKeyResolver.kt
+++ misk-crypto/src/main/kotlin/misk/crypto/LocalConfigKeyResolver.kt
@@ -6,7 +6,7 @@ package misk.crypto
  */
 class LocalConfigKeyResolver(
   private val keys: List<Key>,
-  private val kmsUri: String
+  private val defaultKmsUri: String
 ) : KeyResolver {
 
   override val allKeyAliases: Map<KeyAlias, KeyType> =
@@ -15,7 +15,7 @@ class LocalConfigKeyResolver(
   override fun getKeyByAlias(alias: KeyAlias): Key? {
     return keys.find { key -> key.key_name == alias }?.let { key ->
       if (key.key_type != KeyType.HYBRID_ENCRYPT) {
-        key.copy(kms_uri = kmsUri)
+        key.copy(kms_uri = key.kms_uri ?: defaultKmsUri)
       } else {
         key
       }
