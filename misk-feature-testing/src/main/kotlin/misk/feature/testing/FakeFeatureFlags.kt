82179dadd7643ed570db0a9b25f07229556c80a6
Chris Ryan <chrisryan@squareup.com>
2021-06-23 14:09:08 +1000

    Move FakeFeatureFlags to wisp-feature-testing

--- misk-feature-testing/src/main/kotlin/misk/feature/testing/FakeFeatureFlags.kt
+++ misk-feature-testing/src/main/kotlin/misk/feature/testing/FakeFeatureFlags.kt
@@ -6,13 +6,8 @@ import misk.feature.FeatureService
 import wisp.feature.Attributes
 import wisp.feature.DynamicConfig
 import wisp.feature.Feature
-import wisp.feature.FeatureFlagValidation
 import wisp.feature.FeatureFlags
-import wisp.feature.TrackerReference
-import wisp.feature.fromSafeJson
 import wisp.feature.toSafeJson
-import java.util.PriorityQueue
-import java.util.concurrent.ConcurrentHashMap
 import java.util.concurrent.Executor
 import javax.inject.Inject
 import javax.inject.Provider
@@ -30,26 +25,24 @@ class FakeFeatureFlags @Inject constructor(
     val defaultAttributes = Attributes()
   }
 
+  private lateinit var delegate: wisp.feature.testing.FakeFeatureFlags
+
+  @Inject
+  fun postConstruct() {
+    delegate = wisp.feature.testing.FakeFeatureFlags(moshi.get())
+  }
+
   override fun startUp() {}
   override fun shutDown() {}
 
-  private val trackers = HashMap<MapKey, MutableList<TrackerMapValue<*>>>()
-  private val overrides = ConcurrentHashMap<MapKey, PriorityQueue<MapValue>>()
-
   override fun getBoolean(feature: Feature, key: String, attributes: Attributes): Boolean =
-    get(feature, key, attributes) as? Boolean ?: throw IllegalArgumentException(
-      "Boolean flag $feature must be overridden with override() before use; the default value of false has been DEPRECATED"
-    )
+    delegate.getBoolean(feature, key, attributes)
 
   override fun getInt(feature: Feature, key: String, attributes: Attributes): Int =
-    get(feature, key, attributes) as? Int ?: throw IllegalArgumentException(
-      "Int flag $feature must be overridden with override() before use"
-    )
+    delegate.getInt(feature, key, attributes)
 
   override fun getString(feature: Feature, key: String, attributes: Attributes): String =
-    get(feature, key, attributes) as? String ?: throw IllegalArgumentException(
-      "String flag $feature must be overridden with override() before use"
-    )
+    delegate.getString(feature, key, attributes)
 
   override fun <T : Enum<T>> getEnum(
     feature: Feature,
@@ -57,9 +50,7 @@ class FakeFeatureFlags @Inject constructor(
     clazz: Class<T>,
     attributes: Attributes
   ): T =
-    get(feature, key, attributes) as? T ?: throw IllegalArgumentException(
-      "Enum flag $feature must be overridden with override() before use; the default value of the first constant has been DEPRECATED"
-    )
+    delegate.getEnum(feature, key, clazz, attributes)
 
   override fun <T> getJson(
     feature: Feature,
@@ -67,74 +58,23 @@ class FakeFeatureFlags @Inject constructor(
     clazz: Class<T>,
     attributes: Attributes
   ): T {
-    val jsonFn = get(feature, key, attributes) as? Function0<*> ?: throw IllegalArgumentException(
-      "JSON flag $feature must be overridden with override() before use: ${get(feature, key)}"
-    )
-    // The JSON is lazily provided to handle the case where the override is provided by the
-    // FakeFeatureFlagModule and the Moshi instance cannot be accessed inside the module.
-    val json = jsonFn.invoke() as? String ?: throw IllegalArgumentException(
-      "JSON function did not provide a string"
-    )
-    return moshi.get().adapter(clazz).fromSafeJson(json)
-      ?: throw IllegalArgumentException("null value deserialized from $feature")
+    return delegate.getJson(feature, key, clazz, attributes)
   }
 
-  override fun getBoolean(feature: Feature) = getBoolean(feature, KEY)
-  override fun getInt(feature: Feature) = getInt(feature, KEY)
-  override fun getString(feature: Feature) = getString(feature, KEY)
-  override fun <T : Enum<T>> getEnum(feature: Feature, clazz: Class<T>): T = getEnum(
+  override fun getBoolean(feature: Feature) = delegate.getBoolean(feature, KEY)
+  override fun getInt(feature: Feature) = delegate.getInt(feature, KEY)
+  override fun getString(feature: Feature) = delegate.getString(feature, KEY)
+  override fun <T : Enum<T>> getEnum(feature: Feature, clazz: Class<T>): T = delegate.getEnum(
     feature,
     KEY,
     clazz
   )
 
-  override fun <T> getJson(feature: Feature, clazz: Class<T>): T = getJson(feature, KEY, clazz)
-
-  private fun get(
-    feature: Feature,
-    key: String,
-    attributes: Attributes = defaultAttributes
-  ): Any? {
-    FeatureFlagValidation.checkValidKey(feature, key)
-    // Override value lookup is structured in 3 levels: feature, key, and attributes level. The
-    // logic to get the override value is as follows:
-    //
-    // 1. Return feature level override if there is no override at the key level.
-    // 2. if there is an override at the key level, then:
-    //   2.1 Return the override value associated with the attribute override that contains
-    //       all the attributes of the given attributes.
-    //   2.2 If there is no match, return the override value defined at the key level.
-    val overrideMapValues = overrides[MapKey(feature, key)]
-      ?: return overrides[MapKey(feature)]?.first()?.value
-
-    val overrideMapValuesCopy = PriorityQueue(overrideMapValues)
-    var currentMapValue = overrideMapValuesCopy.poll()
-    while (overrideMapValuesCopy.isNotEmpty()) {
-      if (attributes.text.entries.containsAll(currentMapValue.attributes.text.entries)) {
-        break
-      }
-      currentMapValue = overrideMapValuesCopy.poll()
-    }
-    return currentMapValue.value
-  }
-
-  private fun <T> trackAny(
-    feature: Feature,
-    key: String,
-    attributes: Attributes,
-    executor: Executor,
-    tracker: (T) -> Unit
-  ): TrackerReference = synchronized(trackers) {
-    val bucket = trackers
-      .computeIfAbsent(MapKey(feature, key)) { mutableListOf() }
-    val value = TrackerMapValue(attributes, executor, tracker)
-    bucket.add(value)
-    return object : TrackerReference {
-      override fun unregister() {
-        bucket.remove(value)
-      }
-    }
-  }
+  override fun <T> getJson(feature: Feature, clazz: Class<T>): T = delegate.getJson(
+    feature,
+    KEY,
+    clazz
+  )
 
   override fun trackBoolean(
     feature: Feature,
@@ -142,7 +82,7 @@ class FakeFeatureFlags @Inject constructor(
     attributes: Attributes,
     executor: Executor,
     tracker: (Boolean) -> Unit
-  ) = trackAny(feature, key, attributes, executor, tracker)
+  ) = delegate.trackBoolean(feature, key, attributes, executor, tracker)
 
   override fun trackInt(
     feature: Feature,
@@ -150,7 +90,7 @@ class FakeFeatureFlags @Inject constructor(
     attributes: Attributes,
     executor: Executor,
     tracker: (Int) -> Unit
-  ) = trackAny(feature, key, attributes, executor, tracker)
+  ) = delegate.trackInt(feature, key, attributes, executor, tracker)
 
   override fun trackString(
     feature: Feature,
@@ -158,7 +98,7 @@ class FakeFeatureFlags @Inject constructor(
     attributes: Attributes,
     executor: Executor,
     tracker: (String) -> Unit
-  ) = trackAny(feature, key, attributes, executor, tracker)
+  ) = delegate.trackString(feature, key, attributes, executor, tracker)
 
   override fun <T : Enum<T>> trackEnum(
     feature: Feature,
@@ -167,7 +107,7 @@ class FakeFeatureFlags @Inject constructor(
     attributes: Attributes,
     executor: Executor,
     tracker: (T) -> Unit
-  ) = trackAny(feature, key, attributes, executor, tracker)
+  ) = delegate.trackEnum(feature, key, clazz, attributes, executor, tracker)
 
   override fun <T> trackJson(
     feature: Feature,
@@ -176,76 +116,75 @@ class FakeFeatureFlags @Inject constructor(
     attributes: Attributes,
     executor: Executor,
     tracker: (T) -> Unit
-  ) = trackAny(feature, key, attributes, executor, tracker)
+  ) = delegate.trackJson(feature, key, clazz, attributes, executor, tracker)
 
   override fun trackBoolean(
     feature: Feature,
     executor: Executor,
     tracker: (Boolean) -> Unit
-  ) = trackBoolean(feature, KEY, executor, tracker)
+  ) = delegate.trackBoolean(feature, KEY, executor, tracker)
 
   override fun trackInt(
     feature: Feature,
     executor: Executor,
     tracker: (Int) -> Unit
-  ) = trackInt(feature, KEY, executor, tracker)
+  ) = delegate.trackInt(feature, KEY, executor, tracker)
 
   override fun trackString(
     feature: Feature,
     executor: Executor,
     tracker: (String) -> Unit
-  ) = trackString(feature, KEY, executor, tracker)
+  ) = delegate.trackString(feature, KEY, executor, tracker)
 
   override fun <T : Enum<T>> trackEnum(
     feature: Feature,
     clazz: Class<T>,
     executor: Executor,
     tracker: (T) -> Unit
-  ) = trackEnum(feature, KEY, clazz, executor, tracker)
+  ) = delegate.trackEnum(feature, KEY, clazz, executor, tracker)
 
   override fun <T> trackJson(
     feature: Feature,
     clazz: Class<T>,
     executor: Executor,
     tracker: (T) -> Unit
-  ) = trackJson(feature, KEY, clazz, executor, tracker)
+  ) = delegate.trackJson(feature, KEY, clazz, executor, tracker)
 
   fun override(
     feature: Feature,
     value: Boolean
-  ) = override<Boolean>(feature, value)
+  ) = delegate.override<Boolean>(feature, value)
 
   fun override(
     feature: Feature,
     value: Int
-  ) = override<Int>(feature, value)
+  ) = delegate.override<Int>(feature, value)
 
   fun override(
     feature: Feature,
     value: String
-  ) = override<String>(feature, value)
+  ) = delegate.override<String>(feature, value)
 
   fun override(
     feature: Feature,
     value: Enum<*>
-  ) = override<Enum<*>>(feature, value)
+  ) = delegate.override<Enum<*>>(feature, value)
 
   fun <T> override(
     feature: Feature,
     value: T
-  ) = overrideKey(feature, KEY, value, defaultAttributes)
+  ) = delegate.overrideKey(feature, KEY, value, defaultAttributes)
 
   fun <T> override(
     feature: Feature,
     value: T,
     clazz: Class<T>
   ) {
-    val jsonValue = { moshi.get().adapter(clazz).toSafeJson(value) }
-    overrideKey(feature, KEY, jsonValue, defaultAttributes)
+    delegate.override(feature, value, clazz)
   }
 
   fun overrideJsonString(feature: Feature, json: String) {
-    overrideKey(feature, KEY, { json }, defaultAttributes)
+    delegate.overrideJsonString(feature, json)
   }
 
   inline fun <reified T> overrideJson(feature: Feature, value: T) {
@@ -258,7 +197,7 @@ class FakeFeatureFlags @Inject constructor(
     key: String,
     value: Boolean,
     attributes: Attributes = defaultAttributes
-  ) = overrideKey<Boolean>(feature, key, value, attributes)
+  ) = delegate.overrideKey<Boolean>(feature, key, value, attributes)
 
   @JvmOverloads
   fun overrideKey(
@@ -266,7 +205,7 @@ class FakeFeatureFlags @Inject constructor(
     key: String,
     value: Int,
     attributes: Attributes = defaultAttributes
-  ) = overrideKey<Int>(feature, key, value, attributes)
+  ) = delegate.overrideKey<Int>(feature, key, value, attributes)
 
   @JvmOverloads
   fun overrideKey(
@@ -274,7 +213,7 @@ class FakeFeatureFlags @Inject constructor(
     key: String,
     value: String,
     attributes: Attributes = defaultAttributes
-  ) = overrideKey<String>(feature, key, value, attributes)
+  ) = delegate.overrideKey<String>(feature, key, value, attributes)
 
   @JvmOverloads
   fun overrideKey(
@@ -282,7 +221,7 @@ class FakeFeatureFlags @Inject constructor(
     key: String,
     value: Enum<*>,
     attributes: Attributes = defaultAttributes
-  ) = overrideKey<Enum<*>>(feature, key, value, attributes)
+  ) = delegate.overrideKey<Enum<*>>(feature, key, value, attributes)
 
   fun <T> overrideKey(
     feature: Feature,
@@ -290,8 +229,7 @@ class FakeFeatureFlags @Inject constructor(
     value: T,
     clazz: Class<T>
   ) {
-    val jsonValue = { moshi.get().adapter(clazz).toSafeJson(value) }
-    overrideKey(feature, key, jsonValue, defaultAttributes)
+    delegate.overrideKey(feature, key, value, clazz)
   }
 
   @JvmOverloads
@@ -301,26 +239,7 @@ class FakeFeatureFlags @Inject constructor(
     value: T,
     attributes: Attributes = defaultAttributes
   ) {
-    val mapKey = MapKey(feature, key)
-    overrides
-      .computeIfAbsent(mapKey) { PriorityQueue() }
-      .add(
-        MapValue(
-          order = overrides[mapKey]!!.size + 1,
-          attributes = attributes,
-          value = value as Any
-        )
-      )
-
-    synchronized(trackers) {
-      trackers[mapKey]
-        ?.filter { r -> r.attributes.text.entries.containsAll(attributes.text.entries) }
-        ?.forEach { r ->
-          r.executor.execute {
-            (r as TrackerMapValue<T>).tracker(value)
-          }
-        }
-    }
+    delegate.overrideKey(feature, key, value, attributes)
   }
 
   @JvmOverloads
@@ -335,38 +254,7 @@ class FakeFeatureFlags @Inject constructor(
   }
 
   fun reset() {
-    overrides.clear()
+    delegate.reset()
   }
 
-  private data class MapKey(
-    val feature: Feature,
-    val key: String = KEY
-  )
-
-  /**
-   * Data class that holds the override value provided for the given [feature, key, attributes].
-   */
-  private data class MapValue(
-    val order: Int = 1, // Order is used to pick the latest provided map value.
-    val attributes: Attributes = defaultAttributes,
-    val value: Any
-  ) : Comparable<MapValue> {
-    override fun compareTo(other: MapValue): Int {
-      val attributeSizeCompare = -attributes.text.size.compareTo(other.attributes.text.size)
-      return if (attributeSizeCompare != 0) {
-        attributeSizeCompare
-      } else {
-        -order.compareTo(other.order)
-      }
-    }
-  }
-
-  /**
-   * Data class that holds a tracker for the given [feature, key, attributes].
-   */
-  private data class TrackerMapValue<T> (
-    val attributes: Attributes = defaultAttributes,
-    val executor: Executor,
-    val tracker: (T) -> Unit
-  )
 }
--- wisp-feature-testing/build.gradle.kts
+++ wisp-feature-testing/build.gradle.kts
@@ -5,21 +5,15 @@ sourceSets {
 }
 
 dependencies {
-  implementation(Dependencies.guice)
   implementation(Dependencies.kotlinStdLibJdk8)
   implementation(Dependencies.kotlinReflection)
   implementation(Dependencies.moshiCore)
   implementation(Dependencies.moshiKotlin)
   implementation(Dependencies.moshiAdapters)
-  api(project(":misk-feature"))
-  api(project(":misk-inject"))
-  api(project(":misk-service"))
   api(project(":wisp-feature"))
-  api(project(":wisp-feature-testing"))
 
   testImplementation(Dependencies.assertj)
   testImplementation(Dependencies.kotlinTest)
-  testImplementation(project(":misk-testing"))
 }
 
 afterEvaluate {
