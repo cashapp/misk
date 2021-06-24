e3b43a44039b6e08fb968d390e995c630a7f8bf2
Chris Ryan <74329397+chris-ryan-square@users.noreply.github.com>
2021-06-24 11:15:18 +1000

    misk.feature to wisp.feature (#1987)
    * misk.feature to wisp.feature
    
    * Moving directly to wisp.feature.* - two stage process is not worth it.
    
    * Move FakeFeatureFlags to wisp-feature-testing
    
    * Copy over FakeFeatureFlagsTest from misk.feature-testing
    
    * Another attempt to create delegate...
    
    * We're in tests, just inline the moshi creation for the FakeFeatureFlags
    
    * Can switch back to data class as we're removing misk.feature version

--- misk-launchdarkly-core/src/main/kotlin/misk/feature/launchdarkly/LaunchDarklyDynamicConfig.kt
+++ misk-launchdarkly-core/src/main/kotlin/misk/feature/launchdarkly/LaunchDarklyDynamicConfig.kt
@@ -1,9 +1,9 @@
 package misk.feature.launchdarkly
 
-import misk.feature.Attributes
-import misk.feature.DynamicConfig
-import misk.feature.Feature
-import misk.feature.FeatureFlags
+import wisp.feature.Attributes
+import wisp.feature.DynamicConfig
+import wisp.feature.Feature
+import wisp.feature.FeatureFlags
 import java.util.concurrent.Executor
 import javax.inject.Singleton
 
