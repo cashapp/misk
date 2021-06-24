c6423b9e77c7348d52e1ad4824a31f6dd5fea992
Chris Ryan <74329397+chris-ryan-square@users.noreply.github.com>
2021-06-24 11:15:18 +1000

    misk.feature to wisp.feature (#1987)
    * misk.feature to wisp.feature
    
    * Moving directly to wisp.feature.* - two stage process is not worth it.
    
    * Move FakeFeatureFlags to wisp-feature-testing
    
    * Copy over FakeFeatureFlagsTest from misk.feature-testing
    
    * Another attempt to create delegate...
    
    * We're in tests, just inline the moshi creation for the FakeFeatureFlags
    
    * Can switch back to data class as we're removing misk.feature version

--- wisp-feature/build.gradle.kts
+++ wisp-feature/build.gradle.kts
@@ -1,9 +1,7 @@
 dependencies {
   implementation(Dependencies.kotlinStdLibJdk8)
-  implementation(Dependencies.guava)
   implementation(Dependencies.moshiKotlin)
   implementation(Dependencies.loggingApi)
-  api(project(":wisp-feature"))
 }
 
 afterEvaluate {
