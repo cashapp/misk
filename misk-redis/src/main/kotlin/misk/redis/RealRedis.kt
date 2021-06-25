847536edbb9c45101c8d99adcf4f501b73ae140a
Danny Diekroeger <72826230+ddiekroeger@users.noreply.github.com>
2021-06-24 22:29:29 -0700

    Add hget, hset, and hgetAll to misk-redis (#2003)
    * Add hget, hset, and hgetAll to misk-redis
    
    * Use proper kotlin doc style for comments
    
    Co-authored-by: Chris Ryan <74329397+chris-ryan-square@users.noreply.github.com>

--- misk-redis/src/main/kotlin/misk/redis/RealRedis.kt
+++ misk-redis/src/main/kotlin/misk/redis/RealRedis.kt
@@ -8,21 +8,21 @@ import java.time.Duration
 
 /** For each command, a Jedis instance is retrieved from the pool and returned once the command has been issued. */
 class RealRedis(private val jedisPool: JedisPool) : Redis {
-  // Delete key
+  /** Delete key. */
   override fun del(key: String): Boolean {
     jedisPool.resource.use { jedis ->
       return (jedis.del(key) == 1L)
     }
   }
 
-  // Delete multiple keys
+  /** Delete multiple keys. */
   override fun del(vararg keys: String): Int {
     jedisPool.resource.use { jedis ->
       return jedis.del(*keys).toInt()
     }
   }
 
-  // Get multiple key values
+  /** Get multiple key values. */
   override fun mget(vararg keys: String): List<ByteString?> {
     val byteArrays = keys.map { it.toByteArray(charset) }.toTypedArray()
     jedisPool.resource.use { jedis ->
@@ -30,7 +30,7 @@ class RealRedis(private val jedisPool: JedisPool) : Redis {
     }
   }
 
-  // Set multiple key values
+  /** Set multiple key values. */
   override fun mset(vararg keyValues: ByteString) {
     require(keyValues.size % 2 == 0) { "Wrong number of arguments to mset" }
 
@@ -40,39 +40,64 @@ class RealRedis(private val jedisPool: JedisPool) : Redis {
     }
   }
 
-  // Get a ByteString value
+  /** Get a ByteString value. */
   override fun get(key: String): ByteString? {
     jedisPool.resource.use { jedis ->
       return jedis.get(key.toByteArray(charset))?.toByteString()
     }
   }
 
-  // Set a ByteArray value
+  /** Get a map of field -> value pairs for the given key. */
+  override fun hgetAll(key: String): Map<String, ByteString>? {
+    jedisPool.resource.use { jedis ->
+      return jedis.hgetAll(key.toByteArray(charset))?.mapKeys {
+        it.key.toString(charset)
+      }?.mapValues {
+        it.value.toByteString()
+      }
+    }
+  }
+
+  /** Get a ByteString value for the given key and field. */
+  override fun hget(key: String, field: String): ByteString? {
+    jedisPool.resource.use { jedis ->
+      return jedis.hget(key.toByteArray(charset), field.toByteArray(charset))?.toByteString()
+    }
+  }
+
+  /** Set a ByteArray value. */
   override fun set(key: String, value: ByteString) {
     jedisPool.resource.use { jedis ->
       jedis.set(key.toByteArray(charset), value.toByteArray())
     }
   }
 
-  // Set a ByteArray value with an expiration
+  /** Set a ByteArray value with an expiration. */
   override fun set(key: String, expiryDuration: Duration, value: ByteString) {
     jedisPool.resource.use { jedis ->
       jedis.setex(key.toByteArray(charset), expiryDuration.seconds.toInt(), value.toByteArray())
     }
   }
 
-  // Set a ByteArray value if it doesn't already exist
+  /** Set a ByteArray value if it doesn't already exist. */
   override fun setnx(key: String, value: ByteString) {
     jedisPool.resource.use { jedis ->
       jedis.setnx(key.toByteArray(charset), value.toByteArray())
     }
   }
 
-  // Set a ByteArray value if it doesn't already exist with an expiration
+  /** Set a ByteArray value if it doesn't already exist with an expiration. */
   override fun setnx(key: String, expiryDuration: Duration, value: ByteString) {
     val setParams = SetParams.setParams().ex(expiryDuration.seconds.toInt()).nx()
     jedisPool.resource.use { jedis ->
       jedis.set(key.toByteArray(charset), value.toByteArray(), setParams)
+    }
+  }
+
+  /** Set a ByteArray value for the given key and field. */
+  override fun hset(key: String, field: String, value: ByteString) {
+    jedisPool.resource.use { jedis ->
+      jedis.hset(key.toByteArray(charset), field.toByteArray(charset), value.toByteArray())
     }
   }
 
