6b680132bf456ccd638dc95b34ef9a1496dd3e17
Jesse Wilson <jwilson@squareup.com>
2021-07-14 22:48:06 -0400

    Update Okio to 3.0.0-alpha.8 (#2053)
    A bug in alpha.7 that was impacting GrpcReflectModule is now fixed.
    
    https://github.com/square/okio/blob/master/CHANGELOG.md#version-300-alpha8
    
    Co-authored-by: Chris Ryan <74329397+chris-ryan-square@users.noreply.github.com>

--- buildSrc/src/main/kotlin/Dependencies.kt
+++ buildSrc/src/main/kotlin/Dependencies.kt
@@ -88,7 +88,7 @@ object Dependencies {
   val nettyBom = "io.netty:netty-bom:4.1.65.Final"
   val okHttp = "com.squareup.okhttp3:okhttp:4.9.1"
   val okHttpMockWebServer = "com.squareup.okhttp3:mockwebserver:4.9.1"
-  val okio = "com.squareup.okio:okio:3.0.0-alpha.7"
+  val okio = "com.squareup.okio:okio:3.0.0-alpha.8"
   val openTracing = "io.opentracing:opentracing-api:0.33.0"
   val openTracingDatadog = "com.datadoghq:dd-trace-ot:0.80.0"
   val openTracingJdbc = "io.opentracing.contrib:opentracing-jdbc:0.2.15"
