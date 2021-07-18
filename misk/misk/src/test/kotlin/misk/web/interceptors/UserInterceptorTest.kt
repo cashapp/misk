c19ad7044ecc9c5f632544b95a4252ce3d786b07
Ricardo Mariano <rmariano@squareup.com>
2021-07-19 09:57:59 +1000

    Move action-related files to misk-actions  (#2064)
    also removes an unused import, and moves misk-actions to an API dependency.
    
    This should make moving misk admin out a bit easier.

--- misk/misk/src/test/kotlin/misk/web/interceptors/UserInterceptorTest.kt
+++ misk/misk/src/test/kotlin/misk/web/interceptors/UserInterceptorTest.kt
@@ -15,7 +15,6 @@ import misk.web.Response
 import misk.web.ResponseContentType
 import misk.web.WebActionModule
 import misk.web.WebServerTestingModule
-import misk.web.WebTestingModule
 import misk.web.actions.WebAction
 import misk.web.jetty.JettyService
 import misk.web.mediatype.MediaTypes
