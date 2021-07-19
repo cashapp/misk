ee079358caab5d4e4bc589f05c02b6720b2d7c95
Chris Ryan <chrisryan@squareup.com>
2021-07-19 14:58:14 +1000

    Upgrade to kotlin 1.5; fix misk-admin config; use --no-daemon --> still OOM/Metaspace :(

--- misk-admin/build.gradle.kts
+++ misk-admin/build.gradle.kts
@@ -27,11 +27,4 @@ dependencies {
   testImplementation(Dependencies.wireRuntime)
 }
 
-afterEvaluate {
-  project.tasks.dokka {
-    outputDirectory = "$rootDir/docs/0.x"
-    outputFormat = "gfm"
-  }
-}
-
 apply(from = "$rootDir/gradle-mvn-publish.gradle")
