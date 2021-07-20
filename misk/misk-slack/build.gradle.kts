e2107400774fe2f603a34544a1f0d3590601e1b1
Chris Ryan <74329397+chris-ryan-square@users.noreply.github.com>
2021-07-20 11:01:36 +1000

    WIP: Kotlin 1.5 with necessary dokka upgrade (#2055)
    * Upgrade mvn publish pluging; fix dokka setup for change
    
    * Attempt to get dokkaGfm to work properly - OOM/Metaspace problem, see: https://github.com/Kotlin/dokka/issues/1405
    
    * Upgrade to Kotlin 1.5
    
    * Upgrade to kotlin 1.5; fix misk-admin config; use --no-daemon --> still OOM/Metaspace :(
    
    * Do Dokka tasks in separate daemons and separate modules; reduce memeory required
    
    * Remove non-existent dir call
    
    * Only do the publish docs when building master

--- misk/misk-slack/build.gradle.kts
+++ misk/misk-slack/build.gradle.kts
@@ -23,11 +23,4 @@ dependencies {
   }
 }
 
-afterEvaluate {
-  project.tasks.dokka {
-    outputDirectory = "$rootDir/docs/0.x"
-    outputFormat = "gfm"
-  }
-}
-
 apply(from = "$rootDir/gradle-mvn-publish.gradle")
