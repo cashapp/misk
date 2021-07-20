2f4aae2f584a6e90aed73b96857191997a0f40c2
Ricardo Mariano <rmariano@squareup.com>
2021-07-20 12:08:27 +1000

    Generate Wire sources using the Gradle plugin (#2072)
    Adds extra config to make the sources test-only. Also moves Watches to kotlin sources.

--- misk/build.gradle.kts
+++ misk/build.gradle.kts
@@ -1,16 +1,5 @@
-sourceSets {
-  val main by getting {
-    resources.srcDir(listOf(
-      "web/tabs/admin-dashboard/lib",
-      "web/tabs/config/lib",
-      "web/tabs/database/lib",
-      "web/tabs/web-actions/lib"
-    ))
-    resources.exclude("**/node_modules")
-  }
-  val test by getting {
-    java.srcDir("src/test/kotlin/")
-  }
+plugins {
+  id("com.squareup.wire")
 }
 
 dependencies {
@@ -81,6 +70,40 @@ dependencies {
   }
   testImplementation(Dependencies.openTracingMock)
   testImplementation(Dependencies.guavaTestLib)
+}
+
+val generatedSourceDir = "$buildDir/generated/source/wire-test"
+
+wire {
+  sourcePath {
+    srcDir("src/test/proto/")
+  }
+  java {
+    out = generatedSourceDir
+  }
+}
+
+// Make sure the Wire-generated sources are test-only.
+afterEvaluate {
+  val generatedSourceGlob = "$generatedSourceDir/**"
+
+  sourceSets {
+    val main by getting {
+      java.setSrcDirs(java.srcDirs.filter { !it.path.contains(generatedSourceDir) })
+    }
+    val test by getting {
+      java.srcDir(generatedSourceDir)
+    }
+  }
+
+  tasks {
+    compileJava {
+      exclude(generatedSourceGlob)
+    }
+    compileTestJava {
+      include(generatedSourceGlob)
+    }
+  }
 }
 
 apply(from = "$rootDir/gradle-mvn-publish.gradle")
