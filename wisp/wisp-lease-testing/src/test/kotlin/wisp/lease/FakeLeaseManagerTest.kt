4cb9fcf3867cf80d275ccc0b0b8bc7e9b01a3c4f
Chris Ryan <74329397+chris-ryan-square@users.noreply.github.com>
2021-07-27 15:44:08 +1000

    Add a wisp LeaseManager interface and a Fake for it (cashapp/misk#2085)
    * Add a wisp LeaseManager interface and a Fake for it
    
    * Fix titles
    
    * Update the README docs

---
+++ wisp/wisp-lease-testing/src/test/kotlin/wisp/lease/FakeLeaseManagerTest.kt
@@ -0,0 +1,26 @@
+package wisp.lease
+
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+
+internal class FakeLeaseManagerTest {
+
+  private var leaseManager = FakeLeaseManager()
+
+  @Test fun leaseRespectsAuthority() {
+    val lease = leaseManager.requestLease("my-lease")
+    val otherLease = leaseManager.requestLease("my-other-lease")
+
+    // leases are held by the current process by default
+    assertThat(lease.checkHeld()).isTrue()
+    assertThat(otherLease.checkHeld()).isTrue()
+
+    leaseManager.markLeaseHeldElsewhere("my-lease")
+    assertThat(lease.checkHeld()).isFalse()
+    assertThat(otherLease.checkHeld()).isTrue()
+
+    leaseManager.markLeaseHeld("my-lease")
+    assertThat(lease.checkHeld()).isTrue()
+    assertThat(otherLease.checkHeld()).isTrue()
+  }
+}
