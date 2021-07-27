33736fdf05ef0fa66b057301476717f7f571a2c3
Chris Ryan <74329397+chris-ryan-square@users.noreply.github.com>
2021-07-27 15:44:08 +1000

    Add a wisp LeaseManager interface and a Fake for it (#2085)
    * Add a wisp LeaseManager interface and a Fake for it
    
    * Fix titles
    
    * Update the README docs

---
+++ wisp-lease-testing/build.gradle.kts
@@ -0,0 +1,17 @@
+dependencies {
+  api(project(":wisp-lease"))
+  implementation(Dependencies.kotlinStdLibJdk8)
+  implementation(Dependencies.kotlinReflection)
+
+  testImplementation(Dependencies.assertj)
+  testImplementation(Dependencies.kotlinTest)
+}
+
+afterEvaluate {
+  project.tasks.dokka {
+    outputDirectory = "$rootDir/docs/0.x"
+    outputFormat = "gfm"
+  }
+}
+
+apply(from = "$rootDir/gradle-mvn-publish.gradle")
