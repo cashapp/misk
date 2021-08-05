da715c33f503a0a92307917e31902965a39777df
Ricardo Mariano <rmariano@squareup.com>
2021-08-05 16:27:33 +1000

    Bump misk-admin to misk-web 0.3.3

--- misk-admin/web/tabs/web-actions/src/rewrite/WebActionRequestParameters.tsx
+++ misk-admin/web/tabs/web-actions/src/rewrite/WebActionRequestParameters.tsx
@@ -1,6 +1,6 @@
 import React from "react"
 import { HTMLTable, UL } from "@blueprintjs/core"
-import { WebActionMetadata} from "./types"
+import { WebActionMetadata } from "./types"
 import WebActionParameter from "./WebActionParameter"
 import WebActionCollapse from "./WebActionCollapse"
 import WebActionProtoField from "./WebActionProtoField"
@@ -9,7 +9,9 @@ interface Props {
   webActionMetadata: WebActionMetadata
 }
 
-export default function WebActionRequestParameters({ webActionMetadata }: Props) {
+export default function WebActionRequestParameters({
+  webActionMetadata
+}: Props) {
   const requestType = webActionMetadata.types[webActionMetadata.requestType]
   const parameters = webActionMetadata.parameters
 
@@ -23,7 +25,8 @@ export default function WebActionRequestParameters({ webActionMetadata }: Props)
         <WebActionCollapse
           title={"Request Parameters"}
           subtitle={webActionMetadata.requestType}
-          doubleWidth={true}>
+          doubleWidth={true}
+        >
           <UL style={{ listStyle: "none" }}>
             <li>
               <HTMLTable style={{ marginBottom: "0px" }}>
