4ea77866fb2adb78f86c8ce44ceb74e47c382d18
Chris Ryan <74329397+chris-ryan-square@users.noreply.github.com>
2021-08-13 14:30:27 +1000

    Deployment location identification.  For use in identifying host or kubernetes pod, etc. (#2111)

---
+++ wisp-deployment/src/main/kotlin/wisp/deployment/DeploymentLocation.kt
@@ -0,0 +1,12 @@
+package wisp.deployment
+
+/**
+ * For information about the deployment location.
+ */
+interface DeploymentLocation {
+
+  /**
+   * Deployment identification, e.g. Kubernetes pod name or host name, etc.
+   */
+  val id: String
+}
