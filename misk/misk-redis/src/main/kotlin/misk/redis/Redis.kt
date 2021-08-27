abf1e397602f6eda9b42b9d168a7525ecbf83a40
Felix Jancso-Szabo <jancsoszabo@squareup.com>
2021-08-27 09:56:58 -0400

    Add incr and incrBy operations to misk-redis (#2119)
    * Add incr and incrBy operations to misk-redis
    
    * Add api dump

--- misk/misk-redis/src/main/kotlin/misk/redis/Redis.kt
+++ misk/misk-redis/src/main/kotlin/misk/redis/Redis.kt
@@ -107,4 +107,27 @@ interface Redis {
    * @param value the value to set
    */
   fun hset(key: String, field: String, value: ByteString)
+
+  /**
+   * Increments the number stored at key by one. If the key does not exist, it is set to 0 before
+   * performing the operation. An error is returned if the key contains a value of the wrong type or
+   * contains a string that can not be represented as integer.
+   *
+   * Note: this is a string operation because Redis does not have a dedicated integer type. The
+   * string stored at the key is interpreted as a base-10 64 bit signed integer to execute the
+   * operation.
+   *
+   * Redis stores integers in their integer representation, so for string values that actually hold
+   * an integer, there is no overhead for storing the string representation of the integer.
+   */
+  fun incr(key: String): Long
+
+  /**
+   * Increments the number stored at key by increment. If the key does not exist, it is set to 0
+   * before performing the operation. An error is returned if the key contains a value of the wrong
+   * type or contains a string that can not be represented as integer.
+   *
+   * See [incr] for extra information.
+   */
+  fun incrBy(key: String, increment: Long): Long
 }
