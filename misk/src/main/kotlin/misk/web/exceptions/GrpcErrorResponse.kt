fc3ccc221ec97db59b7def1b6f9b62148eb5c37a
Sever Banesiu <sever@squareup.com>
2021-09-01 11:35:28 -0700

    Make GrpcErrorResponse backward compatible

--- misk/src/main/kotlin/misk/web/exceptions/GrpcErrorResponse.kt
+++ misk/src/main/kotlin/misk/web/exceptions/GrpcErrorResponse.kt
@@ -8,7 +8,14 @@ data class GrpcErrorResponse(
   val message: String?,
   val details: List<AnyMessage> = listOf(),
 ) {
+  // backward compatibility
+  constructor(status: GrpcStatus, message: String?) : this(status, message, listOf())
+
   companion object {
     val INTERNAL_SERVER_ERROR = GrpcErrorResponse(GrpcStatus.UNKNOWN, "internal server error")
   }
+
+  // backward compatibility
+  fun copy(status: GrpcStatus = this.status, message: String? = this.message) =
+    GrpcErrorResponse(status, message)
 }
