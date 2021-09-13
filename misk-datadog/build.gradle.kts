d99f4cdae9faa098b7dc3667712bd8c0d7bc850c
Jesse Wilson <jwilson@squareup.com>
2021-09-13 16:10:03 -0400

    Restore "Update the Maven publish plugin (#2125)" (#2139)
    This reverts commit bed52bc1851c262bcbbb47c79df7004f120e95e2.

--- misk-datadog/build.gradle.kts
+++ misk-datadog/build.gradle.kts
@@ -5,11 +5,4 @@ dependencies {
   api(project(":wisp-logging"))
 }
 
-afterEvaluate {
-  project.tasks.dokka {
-    outputDirectory = "$rootDir/docs/0.x"
-    outputFormat = "gfm"
-  }
-}
-
 apply(from = "$rootDir/gradle-mvn-publish.gradle")
