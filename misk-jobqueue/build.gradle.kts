ce697725a43ec18630b5a38a1b4c5c14c3aff616
Chris Ryan <74329397+chris-ryan-square@users.noreply.github.com>
2021-09-17 09:31:57 +1000

    Remove the deps that aren't required (#2147)

--- misk-jobqueue/build.gradle.kts
+++ misk-jobqueue/build.gradle.kts
@@ -1,9 +1,4 @@
 dependencies {
-  implementation(Dependencies.guava)
-  implementation(Dependencies.guice)
-  implementation(project(":misk-core"))
-
-  testImplementation(project(":misk-testing"))
 }
 
 apply(from = "$rootDir/gradle-mvn-publish.gradle")
