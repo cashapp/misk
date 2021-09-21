507155ea36750c8bfe0c63868e73af32f01d2305
Matthew Dolan <MatthewDolan@users.noreply.github.com>
2021-09-21 17:13:40 -0500

    Remove unused imports (#2149)

--- misk-metrics-testing/src/main/kotlin/misk/metrics/FakeMetricsModule.kt
+++ misk-metrics-testing/src/main/kotlin/misk/metrics/FakeMetricsModule.kt
@@ -1,9 +1,6 @@
 package misk.metrics
 
-import io.prometheus.client.CollectorRegistry
 import misk.inject.KAbstractModule
-import javax.inject.Inject
-import javax.inject.Provider
 import misk.metrics.v2.FakeMetricsModule as FakeMetricsModuleV2
 
 class FakeMetricsModule : KAbstractModule() {
