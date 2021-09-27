4f4a66bc6c9805671d25a48684fc3179519dd58f
Chris Ryan <74329397+chris-ryan-square@users.noreply.github.com>
2021-09-27 17:44:21 +1000

    Add wisp-task (cashapp/misk#2154)
    * Add wisp-task (TODO: add metrics)
    
    * Remove the name from the TaskConfig and pass the name as a separate parameter to the task
    
    * Fix apiDump

---
+++ wisp/wisp-task/src/main/kotlin/wisp/task/exception/TaskAlreadyExistsException.kt
@@ -0,0 +1,7 @@
+package wisp.task.exception
+
+/**
+ * Thrown if an attempt to create a [RepeatedTask] that already exists with the name supplied.
+ */
+class TaskAlreadyExistsException(taskName: String) :
+  Exception("Repeated Task with name: $taskName already exists!")
