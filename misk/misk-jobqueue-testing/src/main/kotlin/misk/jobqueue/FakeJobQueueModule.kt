739d8569b11316537f2a574080842ed5fb0ad613
tso <tohlson@squareup.com>
2021-10-20 17:20:11 -0400

    Re-add mistakingly deleted binding for TransactionalJobQueue (#2183)

--- misk/misk-jobqueue-testing/src/main/kotlin/misk/jobqueue/FakeJobQueueModule.kt
+++ misk/misk-jobqueue-testing/src/main/kotlin/misk/jobqueue/FakeJobQueueModule.kt
@@ -5,5 +5,6 @@ import misk.inject.KAbstractModule
 class FakeJobQueueModule : KAbstractModule() {
   override fun configure() {
     bind<JobQueue>().to<FakeJobQueue>()
+    bind<TransactionalJobQueue>().to<FakeJobQueue>()
   }
 }
