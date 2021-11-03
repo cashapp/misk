31dcd967b561d2a310c3ea39d779e2d78bf89d23
Dean Papastrat <dean.g.papastrat@gmail.com>
2021-11-02 21:29:05 -0700

    Add GoogleSpannerModule to misk-gcp (#2188)
    This commit adds support for loading, configuring, using, and testing a Google Spanner client in Misk services. It makes Spanner feel more like a standard datastore in Misk, like DynamoDB.
    
    It contains code to download an emulator from Docker and run it for tests in both local development and CI for convenience, as well as utilities for clearing the DB in between tests.
    
    Lastly, there is now documentation written on how to use the GoogleSpannerModule. It's not perfect, but it's a start and should be useful to engineers on my team that are using it.

---
+++ misk-gcp/src/test/kotlin/misk/cloud/gcp/spanner/GoogleSpannerModuleTest.kt
@@ -0,0 +1,43 @@
+package misk.cloud.gcp.spanner
+
+import com.google.cloud.spanner.Spanner
+import com.google.inject.util.Modules
+import misk.MiskTestingServiceModule
+import misk.environment.DeploymentModule
+import misk.testing.MiskTest
+import misk.testing.MiskTestModule
+import org.junit.jupiter.api.Assertions.assertTrue
+import org.junit.jupiter.api.Test
+import wisp.deployment.TESTING
+import javax.inject.Inject
+
+@MiskTest(startService = true)
+class GoogleSpannerModuleTest {
+  val spannerConfig = SpannerConfig(
+    project_id = "test-project",
+    instance_id = "test-instance",
+    database = "test-database",
+    emulator = SpannerEmulatorConfig(
+      enabled = true
+    )
+  )
+
+  @MiskTestModule
+  val module = Modules.combine(
+    MiskTestingServiceModule(),
+    DeploymentModule(TESTING),
+    GoogleSpannerEmulatorModule(spannerConfig),
+    GoogleSpannerModule(spannerConfig),
+  )
+
+  @Inject lateinit var spanner: Spanner
+
+  @Test
+  fun `it creates a Spanner client and emulator to develop with`() {
+    assertTrue(spanner.instanceAdminClient
+      .getInstance(spannerConfig.instance_id)
+      .getDatabase(spannerConfig.database)
+      .exists()
+    )
+  }
+}
