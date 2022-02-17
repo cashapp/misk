529dc6b97169d8004b86faebbaf397113a0bd6a8
Chris Goddard <cgoddard@squareup.com>
2022-02-17 10:01:03 -0800

    Allow services to configure max_age_in_seconds for Prometheus (take 2) (#2292)
    * Revert "Revert "Allow services to configure max_age_in_seconds for Prometheus Summary (#2290)" (#2291)"
    
    This reverts commit 85c5698c08c21442d151e82d16cafade22e2f7c9.
    
    * Sidestep Guice binding errors with default values for all config parameters.
    
    * revert out binding the config in a second place
    
    * create explicit @Inject-ed no-arg constructor for config
    
    * trigger rerun

--- misk/misk-prometheus/src/main/kotlin/misk/metrics/backends/prometheus/PrometheusConfig.kt
+++ misk/misk-prometheus/src/main/kotlin/misk/metrics/backends/prometheus/PrometheusConfig.kt
@@ -1,10 +1,20 @@
 package misk.metrics.backends.prometheus
 
 import wisp.config.Config
+import javax.inject.Inject
 
 /** Configuration for exporting metrics to prometheus */
 data class PrometheusConfig(
   // The hostname on which metrics are exposed; if null uses any addr bound to this host
   val hostname: String? = null,
-  val http_port: Int // The port on metrics are exposed
-) : Config
+  // The port on metrics are exposed
+  val http_port: Int = 9102,
+  // How long observations are kept before they are discarded. Only used for Summary.
+  val max_age_in_seconds: Long? = null,
+) : Config {
+  @Inject constructor() : this(
+    hostname = null,
+    http_port = 9102,
+    max_age_in_seconds = null,
+  )
+}
