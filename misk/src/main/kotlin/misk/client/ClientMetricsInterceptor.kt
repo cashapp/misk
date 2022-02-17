c8eb9b4a1a489f4593b1452187ed643e93aeb58d
Chris Goddard <cgoddard@squareup.com>
2022-02-17 10:01:03 -0800

    Allow services to configure max_age_in_seconds for Prometheus (take 2) (#2292)
    * Revert "Revert "Allow services to configure max_age_in_seconds for Prometheus Summary (#2290)" (#2291)"
    
    This reverts commit b2f11f73b30ea4c0cdd3e63fb2badde2fe3e4131.
    
    * Sidestep Guice binding errors with default values for all config parameters.
    
    * revert out binding the config in a second place
    
    * create explicit @Inject-ed no-arg constructor for config
    
    * trigger rerun

--- misk/src/main/kotlin/misk/client/ClientMetricsInterceptor.kt
+++ misk/src/main/kotlin/misk/client/ClientMetricsInterceptor.kt
@@ -5,6 +5,7 @@ import com.google.common.base.Ticker
 import com.squareup.wire.GrpcMethod
 import io.prometheus.client.Histogram
 import io.prometheus.client.Summary
+import misk.metrics.backends.prometheus.PrometheusConfig
 import misk.metrics.v2.Metrics
 import okhttp3.Interceptor
 import okhttp3.Response
@@ -57,11 +58,15 @@ class ClientMetricsInterceptor private constructor(
   }
 
   @Singleton
-  class Factory @Inject internal constructor(m: Metrics) {
+  class Factory @Inject internal constructor(
+    m: Metrics,
+    config: PrometheusConfig,
+  ) {
     internal val requestDuration = m.summary(
       name = "client_http_request_latency_ms",
       help = "count and duration in ms of outgoing client requests",
-      labelNames = listOf("action", "code")
+      labelNames = listOf("action", "code"),
+      maxAgeSeconds = config.max_age_in_seconds,
     )
     internal val requestDurationHistogram = m.histogram(
       name = "histo_client_http_request_latency_ms",
