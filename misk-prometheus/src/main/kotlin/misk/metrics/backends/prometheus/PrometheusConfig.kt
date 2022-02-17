662259e8d93325ea16403ca96fe2b526069acd9f
Chris Goddard <cgoddard@squareup.com>
2022-02-17 08:31:42 -0800

    Sidestep Guice binding errors with default values for all config parameters.

--- misk-prometheus/src/main/kotlin/misk/metrics/backends/prometheus/PrometheusConfig.kt
+++ misk-prometheus/src/main/kotlin/misk/metrics/backends/prometheus/PrometheusConfig.kt
@@ -7,7 +7,7 @@ data class PrometheusConfig(
   // The hostname on which metrics are exposed; if null uses any addr bound to this host
   val hostname: String? = null,
   // The port on metrics are exposed
-  val http_port: Int,
+  val http_port: Int = 9102,
   // How long observations are kept before they are discarded. Only used for Summary.
   val max_age_in_seconds: Long? = null,
 ) : Config
