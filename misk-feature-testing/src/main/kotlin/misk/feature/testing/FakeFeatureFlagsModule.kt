014457616fdb046f0ba1021b12afa24595ac3f12
Mario Acosta <maacosta@squareup.com>
2022-03-04 13:08:05 -0500

    Make FakeFeatureFlag overrides composable with FakeFeatureFlagsOverrideModule (#2306)
    * Make FakeFeatureFlag overrides composable with FakeFeatureFlagsOverrideModule
    
    * Bind overrides to annotated feature flags

--- misk-feature-testing/src/main/kotlin/misk/feature/testing/FakeFeatureFlagsModule.kt
+++ misk-feature-testing/src/main/kotlin/misk/feature/testing/FakeFeatureFlagsModule.kt
@@ -1,34 +1,52 @@
 package misk.feature.testing
 
+import com.google.inject.Provider
+import com.google.inject.TypeLiteral
 import com.squareup.moshi.Moshi
 import misk.ServiceModule
 import misk.feature.DynamicConfig
 import misk.feature.FeatureFlags
 import misk.feature.FeatureService
+import misk.feature.testing.FakeFeatureFlagsOverrideModule.FakeFeatureFlagsOverride
 import misk.inject.KAbstractModule
+import misk.inject.asSingleton
+import misk.inject.parameterizedType
 import misk.inject.toKey
+import misk.inject.typeLiteral
+import javax.inject.Inject
 import kotlin.reflect.KClass
 
 /**
  * Binds a [FakeFeatureFlags] that allows tests to override values.
+ *
+ * To define overrides (especially test defaults) use [FakeFeatureFlagsOverrideModule].
+ * In a given misk service's test setup, there is one [FakeFeatureFlagsModule] installed and many
+ * [FakeFeatureFlagsOverrideModule] installed.
  */
 class FakeFeatureFlagsModule(
   private val qualifier: KClass<out Annotation>? = null
 ) : KAbstractModule() {
-  private val overrides = mutableListOf<FakeFeatureFlags.() -> Unit>()
+  private val overrides = mutableListOf<FakeFeatureFlagsOverrideModule>()
 
   override fun configure() {
     requireBinding<Moshi>()
-    val testFeatureFlags = FakeFeatureFlags(getProvider(Moshi::class.java))
+    newMultibinder<FakeFeatureFlagsOverride>()
+    overrides.forEach { install(it) }
     val key = FakeFeatureFlags::class.toKey(qualifier)
-    bind(key).toInstance(testFeatureFlags)
+    val overridesType =
+      parameterizedType<Set<*>>(FakeFeatureFlagsOverride::class.java).typeLiteral() as TypeLiteral<Set<FakeFeatureFlagsOverride>>
+    val overrides = getProvider(overridesType.toKey(qualifier))
+    bind(key).toProvider(object : Provider<FakeFeatureFlags> {
+      @Inject private lateinit var moshi: Provider<Moshi>
+      override fun get() = FakeFeatureFlags(moshi).apply {
+        overrides.get().forEach { it.overrideLambda(this) }
+      }
+    }).asSingleton()
+
     bind(FeatureFlags::class.toKey(qualifier)).to(key)
     bind(FeatureService::class.toKey(qualifier)).to(key)
     bind(DynamicConfig::class.toKey(qualifier)).to(key)
     install(ServiceModule(FeatureService::class.toKey(qualifier)))
-    overrides.forEach {
-      it.invoke(testFeatureFlags)
-    }
   }
 
   /**
@@ -41,9 +59,12 @@ class FakeFeatureFlagsModule(
    *   override(Feature("foo"), true)
    * })
    * ```
+   *
+   * For overriding in many modules see [FakeFeatureFlagsOverrideModule]
    */
   fun withOverrides(lambda: FakeFeatureFlags.() -> Unit): FakeFeatureFlagsModule {
-    overrides.add(lambda)
+    overrides.add(FakeFeatureFlagsOverrideModule(qualifier, lambda))
     return this
   }
 }
+
--- misk-feature-testing/src/test/kotlin/misk/feature/testing/FakeFeatureFlagsModuleTest.kt
+++ misk-feature-testing/src/test/kotlin/misk/feature/testing/FakeFeatureFlagsModuleTest.kt
@@ -1,16 +1,94 @@
 package misk.feature.testing
 
 import com.google.inject.Guice
-import misk.feature.testing.FakeFeatureFlagsTest.JsonFeature
-import org.junit.jupiter.api.Test
+import com.google.inject.Key
 import misk.feature.Feature
 import misk.feature.FeatureFlags
 import misk.feature.getJson
+import misk.feature.testing.FakeFeatureFlagsTest.JsonFeature
+import misk.inject.KAbstractModule
+import misk.inject.keyOf
+import org.junit.jupiter.api.Test
+import javax.inject.Qualifier
 import kotlin.test.assertEquals
 
 class FakeFeatureFlagsModuleTest {
   @Test
-  fun testModule() {
+  fun `uses overrides from FakeFeatureFlagsOverrideModule`() {
+    val injector = Guice.createInjector(
+      FakeFeatureFlagsModule(),
+      MoshiTestingModule(),
+      object : KAbstractModule() {
+        override fun configure() {
+          install(FakeFeatureFlagsOverrideModule { override(Feature("foo"), 24) })
+          install(FakeFeatureFlagsOverrideModule {
+            overrideJson(
+              Feature("jsonFeature"),
+              JsonFeature("testValue")
+            )
+          })
+        }
+      }
+    )
+
+    val flags = injector.getInstance(FeatureFlags::class.java)
+    assertEquals(24, flags.getInt(Feature("foo"), "bar"))
+    assertEquals(
+      "testValue",
+      flags.getJson<JsonFeature>(
+        Feature("jsonFeature"),
+        "key"
+      ).value
+    )
+  }
+
+  @Test
+  fun `uses overrides from FakeFeatureFlagsOverrideModule with annotation`() {
+    val injector = Guice.createInjector(
+      FakeFeatureFlagsModule(AnotherFeatureFlag::class),
+      FakeFeatureFlagsModule(),
+      MoshiTestingModule(),
+      object : KAbstractModule() {
+        override fun configure() {
+          install(FakeFeatureFlagsOverrideModule { override(Feature("foo"), 24) })
+          install(FakeFeatureFlagsOverrideModule(AnotherFeatureFlag::class) { override(Feature("foo"), 10101) })
+        }
+      }
+    )
+
+    val flags = injector.getInstance(FeatureFlags::class.java)
+    val anotherFeatureFlag = injector.getInstance(Key.get(FeatureFlags::class.java, AnotherFeatureFlag::class.java))
+    assertEquals(24, flags.getInt(Feature("foo"), "bar"))
+    assertEquals(10101, anotherFeatureFlag.getInt(Feature("foo"), "bar"))
+  }
+
+  @Test
+  fun `withOverrides method and FakeFeatureFlagsOverrideModule both work`() {
+    val injector = Guice.createInjector(
+      FakeFeatureFlagsModule().withOverrides {
+        overrideJson(Feature("jsonFeature"), JsonFeature("testValue"))
+      },
+      MoshiTestingModule(),
+      object : KAbstractModule() {
+        override fun configure() {
+          install(FakeFeatureFlagsOverrideModule { override(Feature("foo"), 24) })
+        }
+      }
+    )
+
+    val flags = injector.getInstance(FeatureFlags::class.java)
+    assertEquals(24, flags.getInt(Feature("foo"), "bar"))
+    assertEquals(
+      "testValue",
+      flags.getJson<JsonFeature>(
+        Feature("jsonFeature"),
+        "key"
+      ).value
+    )
+  }
+
+  @Test
+  fun `deprecated withOverrides method`() {
     val injector = Guice.createInjector(
       FakeFeatureFlagsModule().withOverrides {
         override(Feature("foo"), 24)
@@ -30,3 +108,7 @@ class FakeFeatureFlagsModuleTest {
     )
   }
 }
+
+@Qualifier
+@Target(AnnotationTarget.FUNCTION, AnnotationTarget.FIELD)
+annotation class AnotherFeatureFlag
