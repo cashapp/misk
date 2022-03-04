cee4bfd74fc115cd92bb8e89df353db4e09727a9
Chris Ryan <chrisryan@squareup.com>
2022-03-04 11:15:51 +1000

    Upgrade the docker deps to use apache httpclient5 instead of netty (to avoid issues with M1 macs)

--- wisp-containers-testing/build.gradle.kts
+++ wisp-containers-testing/build.gradle.kts
@@ -4,15 +4,8 @@ plugins {
 }
 
 dependencies {
-  implementation(Dependencies.docker)
-  // The docker-java we use in tests depends on an old version of junixsocket that depends on
-  // log4j. We force it up a minor version in packages that use it.
-  implementation("com.kohlschutter.junixsocket:junixsocket-native-common:2.4.0") {
-    isForce = true
-  }
-  implementation("com.kohlschutter.junixsocket:junixsocket-common:2.4.0") {
-    isForce = true
-  }
+  implementation(Dependencies.dockerCore)
+  implementation(Dependencies.dockerTransport)
   api(Dependencies.loggingApi)
   api(Dependencies.logbackClassic)
   api(project(":wisp-logging"))
