cf40b8e957c2dc10d4f487378c65f9a0f48d47a5
Jesse Wilson <jwilson@squareup.com>
2022-04-05 15:39:53 -0400

    Fill out the Misk BOM (#2353)
    It was only specifying the SLF4J version. Now it specifies
    the version of all Misk modules.

--- misk-bom/build.gradle.kts
+++ misk-bom/build.gradle.kts
@@ -8,6 +8,11 @@ dependencies {
     // Jetty 10+ uses SLF4J 2.0-alpha, which is unstable. It works well with 1.7 substituted in.
     // See: https://github.com/eclipse/jetty.project/issues/5943
     api(Dependencies.slf4jApi)
+    project.rootProject.subprojects.forEach { subproject ->
+      if (subproject.name != "misk-bom") {
+        api(subproject)
+      }
+    }
   }
 }
 
