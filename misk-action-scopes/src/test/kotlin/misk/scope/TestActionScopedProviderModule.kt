4722f29d8f949f3dd9daaf143d4c19a07a2f4b43
Jeff Gulbronson <JGulbronson@users.noreply.github.com>
2022-04-26 15:20:21 -0400

    Allow seeding ActionScope with a parameterized type (#2378)

--- misk-action-scopes/src/test/kotlin/misk/scope/TestActionScopedProviderModule.kt
+++ misk-action-scopes/src/test/kotlin/misk/scope/TestActionScopedProviderModule.kt
@@ -3,14 +3,17 @@ package misk.scope
 import com.google.inject.TypeLiteral
 import com.google.inject.name.Named
 import com.google.inject.name.Names
+import java.util.Optional
 import javax.inject.Inject
 
 internal class TestActionScopedProviderModule : ActionScopedProviderModule() {
   override fun configureProviders() {
     bindSeedData(String::class, Names.named("from-seed"))
+    bindSeedData(object : TypeLiteral<Optional<String>>() {})
     bindProvider(String::class, FooProvider::class, Names.named("foo"))
     bindProvider(String::class, BarProvider::class, Names.named("bar"))
     bindProvider(String::class, ZedProvider::class, Names.named("zed"))
+    bindProvider(String::class, OptionalProvider::class, Names.named("optional"))
     bindProvider(nullableStringTypeLiteral, NullableFooProvider::class, Names.named("nullable-foo"))
     bindProvider(
       nullableStringTypeLiteral, NullableBasedOnFooProvider::class,
@@ -22,6 +25,18 @@ internal class TestActionScopedProviderModule : ActionScopedProviderModule() {
     @Named("from-seed") private val seedData: ActionScoped<String>
   ) : ActionScopedProvider<String> {
     override fun get(): String = "${seedData.get()} and bar"
+  }
+
+  class OptionalProvider @Inject internal constructor(
+    private val seedData: ActionScoped<Optional<String>>
+  ) : ActionScopedProvider<String> {
+    override fun get(): String {
+      return if (seedData.get().isEmpty) {
+        "empty"
+      } else {
+        seedData.get().get()
+      }
+    }
   }
 
   class FooProvider @Inject internal constructor(
