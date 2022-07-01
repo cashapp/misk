a3296a6b4d88332813756e1eca1d4ac1f94bd329
Francisco Rojas <frojas@squareup.com>
2022-07-01 13:08:19 -0700

    Rollback deprecation of backoff library. (#2429)
    There's no kotlin standard library and the `kotlin-retry` library (most
    popular) requires use of coroutines that messes up how we handle trace
    information.

--- misk/misk-core/src/main/kotlin/misk/backoff/FlatBackoff.kt
+++ misk/misk-core/src/main/kotlin/misk/backoff/FlatBackoff.kt
@@ -2,7 +2,6 @@ package misk.backoff
 
 import java.time.Duration
 
-@Deprecated("Use kotlinRetry instead")
 class FlatBackoff(val duration: Duration = Duration.ofMillis(0)) : Backoff {
   override fun reset() {}
   override fun nextRetry() = duration
