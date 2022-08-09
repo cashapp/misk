5dfe274c7ef64eba2f7e797ada82323c301b7426
Greg Kerr <kerrnel@squareup.com>
2022-08-09 10:25:22 -0700

    Follow-up: Add default constructor for binary compat.

--- misk-policy-testing/src/main/kotlin/misk/policy/opa/OpaDevelopmentModule.kt
+++ misk-policy-testing/src/main/kotlin/misk/policy/opa/OpaDevelopmentModule.kt
@@ -10,6 +10,11 @@ class OpaDevelopmentModule(
   private val withLogging: Boolean = false,
   private val preferredImageVersion: String = "latest-debug"
 ) : KAbstractModule() {
+  constructor(
+    policyDirectory: String = DEFAULT_POLICY_DIRECTORY,
+    withLogging: Boolean = false
+  ) : this(policyDirectory, withLogging, "latest-debug")
+
   override fun configure() {
     install(ServiceModule<LocalOpaService>())
     bind(keyOf<LocalOpaService>()).toInstance(LocalOpaService(policyDirectory, withLogging, preferredImageVersion))
