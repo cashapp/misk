01d4430bc60fe2755c552c34ce4987e6634db5ad
Chris Ryan <74329397+chris-ryan-square@users.noreply.github.com>
2022-08-09 13:52:42 +1000

    Upgrade wisp dependencies (#2465)

--- misk-core/src/test/kotlin/misk/logging/SampledLoggerTest.kt
+++ misk-core/src/test/kotlin/misk/logging/SampledLoggerTest.kt
@@ -42,30 +42,30 @@ class SampledLoggerTest {
 
     val events = logCollector.takeEvents(LoggingTest::class)
     assertThat(events).hasSize(3)
-    assertThat(events[0]).satisfies {
+    assertThat(events[0]).satisfies({
       assertThat(it.level).isEqualTo(Level.INFO)
       assertThat(it.message).isEqualTo("test 1")
       assertThat(it.throwableProxy).isNull()
       assertThat(it.mdcPropertyMap).containsExactly(
         "user-id" to "blah1"
       )
-    }
-    assertThat(events[1]).satisfies {
+    })
+    assertThat(events[1]).satisfies({
       assertThat(it.level).isEqualTo(Level.ERROR)
       assertThat(it.message).isEqualTo("test 2")
       assertThat(it.throwableProxy.className).isEqualTo(IllegalStateException::class.qualifiedName)
       assertThat(it.mdcPropertyMap).containsExactly(
         "user-id" to "blah2"
       )
-    }
-    assertThat(events[2]).satisfies {
+    })
+    assertThat(events[2]).satisfies({
       assertThat(it.level).isEqualTo(Level.WARN)
       assertThat(it.message).isEqualTo("test 3")
       assertThat(it.throwableProxy).isNull()
       assertThat(it.mdcPropertyMap).containsExactly(
         "user-id" to "blah3"
       )
-    }
+    })
 
     // Sampled logger is rate-limited
     sampledLogger.info("user-id" to "blerb1") { "sampled test 1" }
@@ -78,15 +78,15 @@ class SampledLoggerTest {
 
     val sampledEvents = logCollector.takeEvents(LoggingTest::class)
     assertThat(sampledEvents).hasSize(2)
-    assertThat(sampledEvents[0]).satisfies {
+    assertThat(sampledEvents[0]).satisfies({
       assertThat(it.level).isEqualTo(Level.INFO)
       assertThat(it.message).isEqualTo("sampled test 1")
       assertThat(it.throwableProxy).isNull()
       assertThat(it.mdcPropertyMap).containsExactly(
         "user-id" to "blerb1"
       )
-    }
-    assertThat(sampledEvents[1]).satisfies {
+    })
+    assertThat(sampledEvents[1]).satisfies({
       assertThat(it.level).isEqualTo(Level.ERROR)
       assertThat(it.message).isEqualTo("sampled test 2")
       assertThat(it.throwableProxy.className).isEqualTo(NullPointerException::class.qualifiedName)
@@ -94,7 +94,7 @@ class SampledLoggerTest {
         "user-id" to "blerb2",
         "context-id" to "111111"
       )
-    }
+    })
 
     // Wait 1 second
     fakeTicker.sleepMs(1000L)
@@ -102,13 +102,13 @@ class SampledLoggerTest {
     sampledLogger.warn("user-id" to "blerb4") { "sampled test 4" }
     val sampledEvents2 = logCollector.takeEvents(LoggingTest::class)
     assertThat(sampledEvents2).hasSize(1)
-    assertThat(sampledEvents2[0]).satisfies {
+    assertThat(sampledEvents2[0]).satisfies({
       assertThat(it.level).isEqualTo(Level.WARN)
       assertThat(it.message).isEqualTo("sampled test 4")
       assertThat(it.throwableProxy).isNull()
       assertThat(it.mdcPropertyMap).containsExactly(
         "user-id" to "blerb4"
       )
-    }
+    })
   }
 }
