4017c520261dc60c27eb62d3b84e6615c2102fb6
Andrew (Paradi) Alexander <adrw@squareup.com>
2022-11-11 14:39:45 -0500

    Update package-lock.json files, rename web-actions-old to web-actions-v1 (#2580)

--- misk-admin/src/main/kotlin/misk/web/dashboard/WebActionsDashboardTabModule.kt
+++ misk-admin/src/main/kotlin/misk/web/dashboard/WebActionsDashboardTabModule.kt
@@ -1,18 +1,18 @@
 package misk.web.dashboard
 
 import misk.inject.KAbstractModule
-import misk.web.NetworkInterceptor
 import misk.web.WebActionModule
-import misk.web.interceptors.WideOpenDevelopmentInterceptorFactory
 import misk.web.metadata.webaction.WebActionMetadataAction
 
 /**
  * Installs WebActions dashboard tab which allows introspection
  * and exercising actions from a UI form.
+ *
+ * This installs both versions of the WebActions tab, v1 and v2.
  */
 class WebActionsDashboardTabModule(private val isDevelopment: Boolean): KAbstractModule() {
   override fun configure() {
-    // Web Actions
+    // Web Actions v2
     install(WebActionModule.create<WebActionMetadataAction>())
     multibind<DashboardTab>().toProvider(
       DashboardTabProvider<AdminDashboard, AdminDashboardAccess>(
@@ -22,6 +22,18 @@ class WebActionsDashboardTabModule(private val isDevelopment: Boolean): KAbstrac
         category = "Container Admin"
       )
     )
+
+    // Web Actions v1
+    multibind<DashboardTab>().toProvider(
+      DashboardTabProvider<AdminDashboard, AdminDashboardAccess>(
+        slug = "web-actions",
+        url_path_prefix = "/_admin/web-actions-v1/",
+        name = "Web Actions V1",
+        category = "Container Admin"
+      )
+    )
+
+    // Both v1 and v2 live in the same tab code so only one resource module is installed
     install(
       WebTabResourceModule(
         isDevelopment = isDevelopment,
--- misk-admin/web/tabs/web-actions/src/routes/index.tsx
+++ misk-admin/web/tabs/web-actions/src/routes/index.tsx
@@ -1,13 +1,13 @@
 import * as React from "react"
 import { Route, Switch } from "react-router"
-import { TabContainer as OldTabContainer } from "../containers"
+import { TabContainer as TabContainerV1 } from "../containers"
 import TabContainer from "../rewrite/TabContainer"
 
 const routes = (
   <div>
     <Switch>
       <Route path="/_admin/web-actions/" component={TabContainer} />
-      <Route path="/_admin/web-actions-old/" component={OldTabContainer} />
+      <Route path="/_admin/web-actions-v1/" component={TabContainerV1} />
       {/* Do not include a Route without a path or it will display during on all tabs */}
     </Switch>
   </div>
