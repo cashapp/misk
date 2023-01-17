19701857fd9da9c9fd50c744ac2494cc0b4df5a8
Chris Ryan <chrisryan@squareup.com>
2023-01-17 14:36:41 +1000

    Clean up wisp-bom

--- wisp/wisp-bom/build.gradle.kts
+++ wisp/wisp-bom/build.gradle.kts
@@ -1,37 +1,28 @@
 plugins {
-    `java-platform`
-    alias(libs.plugins.mavenPublishGradlePlugin)
+  `java-platform`
+  alias(libs.plugins.mavenPublishGradlePlugin)
 }
 
 dependencies {
-    constraints {
-        // TODO - check constraints...
-        project.rootProject.subprojects.forEach { subproject ->
-            if (subproject.name != "wisp-bom") {
-                api(subproject)
-            }
-        }
+  constraints {
+    // TODO - check constraints...
+    project.rootProject.subprojects.forEach { subproject ->
+      if (subproject.name != "wisp-bom") {
+        api(subproject)
+      }
     }
+  }
 }
 
 
 plugins.withId("com.vanniktech.maven.publish.base") {
-    val publishingExtension = extensions.getByType(PublishingExtension::class.java)
-    configure<com.vanniktech.maven.publish.MavenPublishBaseExtension> {
-        pomFromGradleProperties()
-        publishToMavenCentral(com.vanniktech.maven.publish.SonatypeHost.DEFAULT, false)
-        signAllPublications()
-    }
-    if (path.startsWith(":wisp-bom")) {
-        publishingExtension.publications.create<MavenPublication>("maven") {
-            from(components["javaPlatform"])
-        }
-        // JavaLibrary(
-        //   javadocJar = JavadocJar.Javadoc(),
-        //   sourcesJar = true,
-        // )
-    } else {
-        // JavaPlatform()
-    }
-
+  val publishingExtension = extensions.getByType(PublishingExtension::class.java)
+  configure<com.vanniktech.maven.publish.MavenPublishBaseExtension> {
+    pomFromGradleProperties()
+    publishToMavenCentral(com.vanniktech.maven.publish.SonatypeHost.DEFAULT, false)
+    signAllPublications()
+  }
+  publishingExtension.publications.create<MavenPublication>("maven") {
+    from(components["javaPlatform"])
+  }
 }
