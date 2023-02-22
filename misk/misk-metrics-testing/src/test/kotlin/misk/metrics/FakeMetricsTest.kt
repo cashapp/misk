f588c83362b7715d301f674a83d035fc5c28addb
Chris Ryan <74329397+chris-ryan-square@users.noreply.github.com>
2023-02-22 10:44:53 +1000

    Upgrade the prometheus client + wisp (#2695)

--- misk/misk-metrics-testing/src/test/kotlin/misk/metrics/FakeMetricsTest.kt
+++ misk/misk-metrics-testing/src/test/kotlin/misk/metrics/FakeMetricsTest.kt
@@ -101,30 +101,30 @@ class FakeMetricsTest {
 
     histogram.record(450.0)
     assertThat(metrics.histogramP50("call_times")).isEqualTo(400.0)
-    assertThat(metrics.histogramP99("call_times")).isEqualTo(400.0)
-
-    histogram.record(500.0)
-    assertThat(metrics.histogramP50("call_times")).isEqualTo(400.0)
     assertThat(metrics.histogramP99("call_times")).isEqualTo(450.0)
 
-    histogram.record(550.0)
+    histogram.record(500.0)
     assertThat(metrics.histogramP50("call_times")).isEqualTo(450.0)
     assertThat(metrics.histogramP99("call_times")).isEqualTo(500.0)
 
-    histogram.record(600.0)
+    histogram.record(550.0)
     assertThat(metrics.histogramP50("call_times")).isEqualTo(450.0)
     assertThat(metrics.histogramP99("call_times")).isEqualTo(550.0)
+
+    histogram.record(600.0)
+    assertThat(metrics.histogramP50("call_times")).isEqualTo(500.0)
+    assertThat(metrics.histogramP99("call_times")).isEqualTo(600.0)
   }
 
   @Test
   internal fun `get all samples`() {
-    metrics.counter("counter", "-", listOf("foo")).labels("bar").inc()
+    metrics.counter("counter_total", "-", listOf("foo")).labels("bar").inc()
     metrics.gauge("gauge", "-", listOf("foo")).labels("bar").inc()
     val quantiles = mapOf(0.5 to 0.5, 0.99 to 0.99)
     metrics.histogram("histogram", "-", listOf("foo"), quantiles).record(1.0, "bar")
 
-    assertThat(metrics.getAllSamples().toList()).containsExactlyInAnyOrder(
-      Sample("counter", listOf("foo"), listOf("bar"), 1.0),
+    assertThat(metrics.getAllSamples().toList()).contains(
+      Sample("counter_total", listOf("foo"), listOf("bar"), 1.0),
       Sample("gauge", listOf("foo"), listOf("bar"), 1.0),
       Sample("histogram", listOf("foo", "quantile"), listOf("bar", "0.5"), 1.0),
       Sample("histogram", listOf("foo", "quantile"), listOf("bar", "0.99"), 1.0),
