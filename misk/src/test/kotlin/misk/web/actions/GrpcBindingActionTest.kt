a755eb6a1dc289894794963f43ff5279aec9d007
Francisco Rojas <frojas@squareup.com>
2023-03-08 14:34:58 -0800

    Issuing binding gRPC action with empty request

---
+++ misk/src/test/kotlin/misk/web/actions/GrpcBindingActionTest.kt
@@ -0,0 +1,69 @@
+package misk.web.actions
+
+import com.google.inject.Guice
+import com.google.inject.Provides
+import com.squareup.protos.test.grpc.GreeterClient
+import com.squareup.protos.test.grpc.GrpcGreeterClient
+import misk.MiskTestingServiceModule
+import misk.client.GrpcClientModule
+import misk.client.HttpClientEndpointConfig
+import misk.client.HttpClientFactory
+import misk.client.HttpClientsConfig
+import misk.inject.KAbstractModule
+import misk.inject.getInstance
+import misk.testing.MiskTest
+import misk.testing.MiskTestModule
+import misk.web.jetty.JettyService
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+import javax.inject.Inject
+import javax.inject.Singleton
+
+@MiskTest(startService = true)
+class GrpcBindingActionTest {
+
+  @MiskTestModule
+  val module = TestWebActionModule()
+
+  @Inject private lateinit var jetty: JettyService
+  @Inject private lateinit var httpClientFactory: HttpClientFactory
+
+  private lateinit var grpcClient: GreeterClient
+
+  @BeforeEach
+  fun beforeEach() {
+    val clientInjector = Guice.createInjector(
+      ClientModule(
+        jetty
+      )
+    )
+    grpcClient = clientInjector.getInstance()
+  }
+
+  class ClientModule(val jetty: JettyService) : KAbstractModule() {
+    override fun configure() {
+      install(MiskTestingServiceModule())
+      install(GrpcClientModule.create<GreeterClient, GrpcGreeterClient>("greeter"))
+    }
+
+    @Provides
+    @Singleton
+    fun provideHttpClientConfig(): HttpClientsConfig {
+      return HttpClientsConfig(
+        endpoints = mapOf(
+          "greeter" to HttpClientEndpointConfig(jetty.httpServerUrl.toString())
+        )
+      )
+    }
+  }
+
+  @Test
+  fun boom() {
+    val response = grpcClient.Greet().executeBlocking(Unit)
+    assertThat(response.message).isEqualTo("asdsadas")
+  }
+
+
+
+}
