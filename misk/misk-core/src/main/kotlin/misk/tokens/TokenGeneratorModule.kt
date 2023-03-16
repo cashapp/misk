1757e41799505505c0a3ce826e4b7b3f6ea7eae5
tso <tohlson@squareup.com>
2023-03-16 14:23:33 -0400

    Re-add token generator so that we can advance forward (#2724)

--- misk/misk-core/src/main/kotlin/misk/tokens/TokenGeneratorModule.kt
+++ misk/misk-core/src/main/kotlin/misk/tokens/TokenGeneratorModule.kt
@@ -5,5 +5,7 @@ import misk.inject.KAbstractModule
 class TokenGeneratorModule : KAbstractModule() {
   override fun configure() {
     bind<TokenGenerator>().to<RealTokenGenerator>()
+    // TODO: Remove. Only here to allow breaking cycles.
+    bind<wisp.token.TokenGenerator>().toInstance(wisp.token.RealTokenGenerator())
   }
 }
