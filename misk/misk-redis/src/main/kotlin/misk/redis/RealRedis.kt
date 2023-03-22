dc2a2a425aabd1cb9cc430037111ff25bf6ecd9c
Keefer Rourke <keefer@squareup.com>
2023-03-22 15:04:25 -0400

    Add client metrics to misk-redis (#2729)

--- misk/misk-redis/src/main/kotlin/misk/redis/RealRedis.kt
+++ misk/misk-redis/src/main/kotlin/misk/redis/RealRedis.kt
@@ -6,88 +6,84 @@ import redis.clients.jedis.JedisPool
 import redis.clients.jedis.Pipeline
 import redis.clients.jedis.Transaction
 import redis.clients.jedis.args.ListDirection
+import redis.clients.jedis.commands.JedisBinaryCommands
 import redis.clients.jedis.params.SetParams
+import java.lang.reflect.InvocationHandler
+import java.lang.reflect.Method
+import java.lang.reflect.Proxy
 import java.time.Duration
+import kotlin.reflect.cast
 
 /**
  * For each command, a Jedis instance is retrieved from the pool and returned once the command has
  * been issued.
  */
-class RealRedis(private val jedisPool: JedisPool) : Redis {
+class RealRedis(
+  private val jedisPool: JedisPool,
+  private val clientMetrics: RedisClientMetrics,
+) : Redis {
   override fun del(key: String): Boolean {
-    jedisPool.resource.use { jedis ->
-      return (jedis.del(key) == 1L)
-    }
+    val keyBytes = key.toByteArray(charset)
+    return jedis { del(keyBytes) == 1L }
   }
 
   override fun del(vararg keys: String): Int {
-    jedisPool.resource.use { jedis ->
-      return jedis.del(*keys).toInt()
-    }
+    val keysAsBytes = keys.map { it.toByteArray(charset) }.toTypedArray()
+    return jedis { del(*keysAsBytes) }.toInt()
   }
 
   override fun mget(vararg keys: String): List<ByteString?> {
-    val byteArrays = keys.map { it.toByteArray(charset) }.toTypedArray()
-    jedisPool.resource.use { jedis ->
-      return jedis.mget(*byteArrays).map { it?.toByteString() }
-    }
+    val keysAsBytes = keys.map { it.toByteArray(charset) }.toTypedArray()
+    return jedis { mget(*keysAsBytes) }
+      .map { it?.toByteString() }
   }
 
   override fun mset(vararg keyValues: ByteString) {
     require(keyValues.size % 2 == 0) { "Wrong number of arguments to mset" }
-
     val byteArrays = keyValues.map { it.toByteArray() }.toTypedArray()
-    jedisPool.resource.use { jedis ->
-      jedis.mset(*byteArrays)
-    }
+    jedis { mset(*byteArrays) }
   }
 
   override fun get(key: String): ByteString? {
-    jedisPool.resource.use { jedis ->
-      return jedis.get(key.toByteArray(charset))?.toByteString()
-    }
+    val keyBytes = key.toByteArray(charset)
+    return jedis { get(keyBytes) }?.toByteString()
   }
 
   override fun hdel(key: String, vararg fields: String): Long {
-    jedisPool.resource.use { jedis ->
-      val fieldsAsByteArrays = fields.map { it.toByteArray(charset) }.toTypedArray()
-      return jedis.hdel(key.toByteArray(charset), *fieldsAsByteArrays)
-    }
+    val fieldsAsByteArrays = fields.map { it.toByteArray(charset) }.toTypedArray()
+    val keyBytes = key.toByteArray(charset)
+    return jedis { hdel(keyBytes, *fieldsAsByteArrays) }
   }
 
   override fun hgetAll(key: String): Map<String, ByteString>? {
-    jedisPool.resource.use { jedis ->
-      return jedis.hgetAll(key.toByteArray(charset))?.mapKeys {
-        it.key.toString(charset)
-      }?.mapValues {
-        it.value.toByteString()
-      }
-    }
+    val keyBytes = key.toByteArray(charset)
+    return jedis { hgetAll(keyBytes) }
+      ?.mapKeys { it.key.toString(charset) }
+      ?.mapValues { it.value.toByteString() }
   }
 
   override fun hlen(key: String): Long {
-    jedisPool.resource.use { jedis ->
-      return jedis.hlen(key.toByteArray(charset))
-    }
+    val keyBytes = key.toByteArray(charset)
+    return jedis { hlen(keyBytes) }
   }
 
   override fun hmget(key: String, vararg fields: String): List<ByteString?> {
-    jedisPool.resource.use { jedis ->
-      val fieldsAsByteArrays = fields.map { it.toByteArray(charset) }.toTypedArray()
-      return jedis.hmget(key.toByteArray(charset), *fieldsAsByteArrays).map { it?.toByteString() }
-    }
+    val fieldsAsByteArrays = fields.map { it.toByteArray(charset) }.toTypedArray()
+    val keyBytes = key.toByteArray(charset)
+    return jedis { hmget(keyBytes, *fieldsAsByteArrays) ?: emptyList() }
+      .map { it?.toByteString() }
   }
 
   override fun hget(key: String, field: String): ByteString? {
-    jedisPool.resource.use { jedis ->
-      return jedis.hget(key.toByteArray(charset), field.toByteArray(charset))?.toByteString()
-    }
+    val keyBytes = key.toByteArray(charset)
+    val fieldBytes = field.toByteArray(charset)
+    return jedis { hget(keyBytes, fieldBytes) }?.toByteString()
   }
 
   override fun hincrBy(key: String, field: String, increment: Long): Long {
-    jedisPool.resource.use { jedis ->
-      return jedis.hincrBy(key, field, increment)
-    }
+    val keyBytes = key.toByteArray(charset)
+    val fieldBytes = field.toByteArray(charset)
+    return jedis { hincrBy(keyBytes, fieldBytes, increment) }
   }
 
   /**
@@ -97,11 +93,10 @@ class RealRedis(private val jedisPool: JedisPool) : Redis {
    */
   override fun hrandFieldWithValues(key: String, count: Long): Map<String, ByteString>? {
     checkHrandFieldCount(count)
-    return jedisPool.resource.use { jedis ->
-      jedis.hrandfieldWithValues(key.toByteArray(charset), count)
-        ?.mapKeys { (key, _) -> key.toString(charset) }
-        ?.mapValues { (_, value) -> value.toByteString() }
-    }
+    val keyBytes = key.toByteArray(charset)
+    return jedis { hrandfieldWithValues(keyBytes, count) }
+      ?.mapKeys { (key, _) -> key.toString(charset) }
+      ?.mapValues { (_, value) -> value.toByteString() }
   }
 
   /**
@@ -111,63 +106,57 @@ class RealRedis(private val jedisPool: JedisPool) : Redis {
    */
   override fun hrandField(key: String, count: Long): List<String> {
     checkHrandFieldCount(count)
-    return jedisPool.resource.use { jedis ->
-      jedis.hrandfield(key.toByteArray(charset), count)
-        .map { it.toString(charset) }
-    }
+    val keyBytes = key.toByteArray(charset)
+    return jedis { hrandfield(keyBytes, count) }
+      .map { it.toString(charset) }
   }
 
   override fun set(key: String, value: ByteString) {
-    jedisPool.resource.use { jedis ->
-      jedis.set(key.toByteArray(charset), value.toByteArray())
-    }
+    val keyBytes = key.toByteArray(charset)
+    val valueBytes = value.toByteArray()
+    jedis { set(keyBytes, valueBytes) }
   }
 
   override fun set(key: String, expiryDuration: Duration, value: ByteString) {
-    jedisPool.resource.use { jedis ->
-      jedis.setex(key.toByteArray(charset), expiryDuration.seconds, value.toByteArray())
-    }
+    val keyBytes = key.toByteArray(charset)
+    val valueBytes = value.toByteArray()
+    jedis { setex(keyBytes, expiryDuration.seconds, valueBytes) }
   }
 
   override fun setnx(key: String, value: ByteString): Boolean {
-    return jedisPool.resource.use { jedis ->
-      jedis.setnx(key.toByteArray(charset), value.toByteArray()) == 1L
-    }
+    val keyBytes = key.toByteArray(charset)
+    val valueBytes = value.toByteArray()
+    return jedis { setnx(keyBytes, valueBytes) == 1L }
   }
 
   override fun setnx(key: String, expiryDuration: Duration, value: ByteString): Boolean {
-    return jedisPool.resource.use { jedis ->
-      jedis.set(
-        key.toByteArray(charset),
-        value.toByteArray(),
-        SetParams().nx().px(expiryDuration.toMillis())
-      ) == "OK"
-    }
+    val keyBytes = key.toByteArray(charset)
+    val valueBytes = value.toByteArray()
+    val params = SetParams().nx().px(expiryDuration.toMillis())
+    return jedis { set(keyBytes, valueBytes, params) == "OK" }
   }
 
   override fun hset(key: String, field: String, value: ByteString): Long {
-    return jedisPool.resource.use { jedis ->
-      jedis.hset(key.toByteArray(charset), field.toByteArray(charset), value.toByteArray())
-    }
+    val keyBytes = key.toByteArray(charset)
+    val fieldBytes = field.toByteArray(charset)
+    val valueBytes = value.toByteArray()
+    return jedis { hset(keyBytes, fieldBytes, valueBytes) }
   }
 
   override fun hset(key: String, hash: Map<String, ByteString>): Long {
     val hashBytes = hash.entries.associate { it.key.toByteArray(charset) to it.value.toByteArray() }
-    return jedisPool.resource.use { jedis ->
-      jedis.hset(key.toByteArray(charset), hashBytes)
-    }
+    val keyBytes = key.toByteArray(charset)
+    return jedis { hset(keyBytes, hashBytes) }
   }
 
   override fun incr(key: String): Long {
-    return jedisPool.resource.use { jedis ->
-      jedis.incr(key.toByteArray(charset))
-    }
+    val keyBytes = key.toByteArray(charset)
+    return jedis { incr(keyBytes) }
   }
 
   override fun incrBy(key: String, increment: Long): Long {
-    return jedisPool.resource.use { jedis ->
-      jedis.incrBy(key.toByteArray(charset), increment)
-    }
+    val keyBytes = key.toByteArray(charset)
+    return jedis { incrBy(keyBytes, increment) }
   }
 
   override fun blmove(
@@ -177,15 +166,9 @@ class RealRedis(private val jedisPool: JedisPool) : Redis {
     to: ListDirection,
     timeoutSeconds: Double
   ): ByteString? {
-    return jedisPool.resource.use { jedis ->
-      jedis.blmove(
-        sourceKey.toByteArray(charset),
-        destinationKey.toByteArray(charset),
-        from,
-        to,
-        timeoutSeconds
-      )?.toByteString()
-    }
+    val sourceKeyBytes = sourceKey.toByteArray(charset)
+    val destKeyBytes = destinationKey.toByteArray(charset)
+    return jedis { blmove(sourceKeyBytes, destKeyBytes, from, to, timeoutSeconds) }?.toByteString()
   }
 
   override fun brpoplpush(
@@ -193,13 +176,9 @@ class RealRedis(private val jedisPool: JedisPool) : Redis {
     destinationKey: String,
     timeoutSeconds: Int
   ): ByteString? {
-    return jedisPool.resource.use { jedis ->
-      jedis.brpoplpush(
-        sourceKey.toByteArray(charset),
-        destinationKey.toByteArray(charset),
-        timeoutSeconds,
-      )?.toByteString()
-    }
+    val sourceKeyBytes = sourceKey.toByteArray(charset)
+    val destinationKeyBytes = destinationKey.toByteArray(charset)
+    return jedis { brpoplpush(sourceKeyBytes, destinationKeyBytes, timeoutSeconds) }?.toByteString()
   }
 
   override fun lmove(
@@ -208,122 +187,135 @@ class RealRedis(private val jedisPool: JedisPool) : Redis {
     from: ListDirection,
     to: ListDirection
   ): ByteString? {
-    return jedisPool.resource.use { jedis ->
-      jedis.lmove(
-        sourceKey.toByteArray(charset),
-        destinationKey.toByteArray(charset),
-        from,
-        to
-      )?.toByteString()
-    }
+    val sourceKeyBytes = sourceKey.toByteArray(charset)
+    val destKeyBytes = destinationKey.toByteArray(charset)
+    return jedis { lmove(sourceKeyBytes, destKeyBytes, from, to) }?.toByteString()
   }
 
   override fun lpush(key: String, vararg elements: ByteString): Long {
-    return jedisPool.resource.use { jedis ->
-      jedis.lpush(key.toByteArray(charset), *elements.map { it.toByteArray() }.toTypedArray())
-    }
+    val keyBytes = key.toByteArray(charset)
+    val byteArrays = elements.map { it.toByteArray() }.toTypedArray()
+    return jedis { lpush(keyBytes, *byteArrays) }
   }
 
   override fun rpush(key: String, vararg elements: ByteString): Long {
-    return jedisPool.resource.use { jedis ->
-      jedis.rpush(key.toByteArray(charset), *elements.map { it.toByteArray() }.toTypedArray())
-    }
+    val keyBytes = key.toByteArray(charset)
+    val byteArrays = elements.map { it.toByteArray() }.toTypedArray()
+    return jedis { rpush(keyBytes, *byteArrays) }
   }
 
   override fun lpop(key: String, count: Int): List<ByteString?> {
-    return jedisPool.resource.use { jedis ->
-      jedis.lpop(key.toByteArray(charset), count)?.map { it?.toByteString() } ?: emptyList()
-    }
+    val keyBytes = key.toByteArray(charset)
+    return jedis { lpop(keyBytes, count) ?: emptyList() }
+      .map { it?.toByteString() }
   }
 
   override fun lpop(key: String): ByteString? {
-    return jedisPool.resource.use { jedis ->
-      jedis.lpop(key.toByteArray(charset))?.toByteString()
-    }
+    val keyBytes = key.toByteArray(charset)
+    return jedis { lpop(keyBytes) }?.toByteString()
   }
 
   override fun rpop(key: String, count: Int): List<ByteString?> {
-    return jedisPool.resource.use { jedis ->
-      jedis.rpop(key.toByteArray(charset), count)?.map { it?.toByteString() } ?: emptyList()
-    }
+    val keyBytes = key.toByteArray(charset)
+    return jedis { rpop(keyBytes, count) ?: emptyList() }
+      .map { it?.toByteString() }
   }
 
   override fun rpop(key: String): ByteString? {
-    return jedisPool.resource.use { jedis ->
-      jedis.rpop(key.toByteArray(charset))?.toByteString()
-    }
+    val keyBytes = key.toByteArray(charset)
+    return jedis { rpop(keyBytes) }?.toByteString()
   }
 
   override fun lrange(key: String, start: Long, stop: Long): List<ByteString?> {
-    return jedisPool.resource.use { jedis ->
-      jedis.lrange(key.toByteArray(charset), start, stop).map { it?.toByteString() }
-    }
+    val keyBytes = key.toByteArray(charset)
+    return jedis { lrange(keyBytes, start, stop) ?: emptyList() }
+      .map { it?.toByteString() }
   }
 
   override fun lrem(key: String, count: Long, element: ByteString): Long {
-    return jedisPool.resource.use { jedis ->
-      jedis.lrem(key.toByteArray(charset), count, element.toByteArray())
-    }
+    val keyBytes = key.toByteArray(charset)
+    val elementBytes = element.toByteArray()
+    return jedis { lrem(keyBytes, count, elementBytes) }
   }
 
   override fun rpoplpush(sourceKey: String, destinationKey: String): ByteString? {
-    return jedisPool.resource.use { jedis ->
-      jedis.rpoplpush(sourceKey.toByteArray(charset), destinationKey.toByteArray(charset))
-        ?.toByteString()
-    }
+    val sourceKeyBytes = sourceKey.toByteArray(charset)
+    val destinationKeyBytes = destinationKey.toByteArray(charset)
+    return jedis { rpoplpush(sourceKeyBytes, destinationKeyBytes) }?.toByteString()
   }
 
   override fun expire(key: String, seconds: Long): Boolean {
-    return jedisPool.resource.use { jedis ->
-      jedis.expire(key, seconds) == 1L
-    }
+    val keyBytes = key.toByteArray(charset)
+    return jedis { expire(keyBytes, seconds) == 1L }
   }
 
   override fun expireAt(key: String, timestampSeconds: Long): Boolean {
-    return jedisPool.resource.use { jedis ->
-      jedis.expireAt(key, timestampSeconds) == 1L
-    }
+    val keyBytes = key.toByteArray(charset)
+    return jedis { expireAt(keyBytes, timestampSeconds) == 1L }
   }
 
   override fun pExpire(key: String, milliseconds: Long): Boolean {
-    return jedisPool.resource.use { jedis ->
-      jedis.pexpire(key, milliseconds) == 1L
-    }
+    val keyBytes = key.toByteArray(charset)
+    return jedis { pexpire(keyBytes, milliseconds) == 1L }
   }
 
   override fun pExpireAt(key: String, timestampMilliseconds: Long): Boolean {
-    return jedisPool.resource.use { jedis ->
-      jedis.pexpireAt(key, timestampMilliseconds) == 1L
-    }
+    val keyBytes = key.toByteArray(charset)
+    return jedis { pexpireAt(keyBytes, timestampMilliseconds) == 1L }
   }
 
   override fun watch(vararg keys: String) {
-    jedisPool.resource.use { jedis ->
-      jedis.watch(*keys)
-    }
+    val keysAsBytes = keys.map { it.toByteArray() }.toTypedArray()
+    jedisPool.resource.use { jedis -> jedis.watch(*keysAsBytes) }
   }
 
   override fun unwatch(vararg keys: String) {
-    jedisPool.resource.use { jedis ->
-      jedis.unwatch()
-    }
+    jedisPool.resource.use { jedis -> jedis.unwatch() }
   }
 
+  // Transactions do not get client histogram metrics right now.
+  // multi() returns the jedis to the pool, despite returning a Transaction that holds a reference.
+  // This is a bug, and will be fixed in a follow-up.
   override fun multi(): Transaction {
-    jedisPool.resource.use { jedis ->
-      return jedis.multi()
-    }
+    return jedisPool.resource.use { jedis -> jedis.multi() }
   }
 
+  // Pipelined requests do not get client histogram metrics right now.
+  // pipelined() returns the jedis to the pool, despite returning a Pipeline that holds a reference
+  // to the borrowed jedis connection.
+  // This is a bug, and will be fixed in a follow-up.
   override fun pipelined(): Pipeline {
-    jedisPool.resource.use { jedis ->
-      return jedis.pipelined()
-    }
+    return jedisPool.resource.use { jedis -> jedis.pipelined() }
   }
 
   /** Closes the connection to Redis. */
   override fun close() {
     return jedisPool.close()
+  }
+
+  // Gets a Jedis instance from the pool, and times the requested method invocations.
+  private fun <T> jedis(op: JedisBinaryCommands.() -> T): T {
+    return jedisPool.resource.use { jedisResource ->
+      val invocationHandler = JedisTimedInvocationHandler(jedisResource, clientMetrics)
+      val timedProxy = JedisBinaryCommands::class.cast(
+        Proxy.newProxyInstance(
+          ClassLoader.getSystemClassLoader(),
+          arrayOf(JedisBinaryCommands::class.java),
+          invocationHandler
+        )
+      )
+      timedProxy.op()
+    }
+  }
+
+  private class JedisTimedInvocationHandler(
+    private val jedis: JedisBinaryCommands,
+    private val clientMetrics: RedisClientMetrics,
+  ) : InvocationHandler {
+    override fun invoke(proxy: Any, method: Method, args: Array<out Any?>?): Any? =
+      clientMetrics.timed(method.name) {
+        method.invoke(jedis, *(args ?: arrayOf()))
+      }
   }
 
   companion object {
--- misk/misk-redis/src/main/kotlin/misk/redis/RedisModule.kt
+++ misk/misk-redis/src/main/kotlin/misk/redis/RedisModule.kt
@@ -4,12 +4,31 @@ import com.google.inject.Provides
 import com.google.inject.Singleton
 import misk.ServiceModule
 import misk.inject.KAbstractModule
+import misk.metrics.v2.Metrics
 import redis.clients.jedis.JedisPool
 import redis.clients.jedis.JedisPoolConfig
 import wisp.deployment.Deployment
 
 /**
- * Configures a JedisPool to connect to a Redis instance. The use of a JedisPool ensures thread safety.
+ * Configures a [Redis] client with metrics, this also installs a [ServiceModule] for [RedisService].
+ * If other services require a working client connection to Redis before they can be used, specify a
+ * dependency like:
+ *
+ * ```
+ * install(ServiceModule<MyService>()
+ *     .dependsOn(keyOf<RedisService>())
+ * )
+ * ```
+ *
+ * You must pass in configuration for your Redis client.
+ *
+ * [redisConfig]: Only one replication group config is supported; this module will use the first
+ * configuration it finds. An empty [RedisReplicationGroupConfig.redis_auth_password] is only
+ * permitted in fake environments. See [Deployment].
+ *
+ * [jedisPoolConfig]: Misk-redis is backed by a [JedisPool], you may not want to use the
+ * [JedisPoolConfig] defaults! Be sure to understand them!
+ *
  * See: https://github.com/xetorthio/jedis/wiki/Getting-started#using-jedis-in-a-multithreaded-environment
  */
 class RedisModule(
@@ -20,27 +39,29 @@ class RedisModule(
   override fun configure() {
     bind<RedisConfig>().toInstance(redisConfig)
     install(ServiceModule<RedisService>())
+    requireBinding<Metrics>()
   }
 
   @Provides @Singleton
-  internal fun provideRedisClient(config: RedisConfig, deployment: Deployment): Redis {
+  internal fun provideRedisClient(
+    config: RedisConfig,
+    deployment: Deployment,
+    metrics: Metrics
+  ): Redis {
     // Get the first replication group, we only support 1 replication group per service.
     val replicationGroup = config[config.keys.first()]
       ?: throw RuntimeException("At least 1 replication group must be specified")
 
-    // Create our jedis pool
-    val jedisPool = JedisPool(
-      jedisPoolConfig,
-      replicationGroup.writer_endpoint.hostname,
-      replicationGroup.writer_endpoint.port,
-      replicationGroup.timeout_ms,
-      replicationGroup.redis_auth_password.ifEmpty {
-        check(deployment.isFake) { "Redis auth password cannot be empty in a real environment!" }
-        null
-      },
-      useSsl,
+    // Create our jedis pool with client-side metrics.
+    val clientMetrics = RedisClientMetrics(metrics)
+    val jedisPoolWithMetrics = JedisPoolWithMetrics(
+      metrics = clientMetrics,
+      poolConfig = jedisPoolConfig,
+      replicationGroupConfig = replicationGroup,
+      ssl = useSsl,
+      requiresPassword = deployment.isReal
     )
 
-    return RealRedis(jedisPool)
+    return RealRedis(jedisPoolWithMetrics, clientMetrics)
   }
 }
