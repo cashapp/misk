245a96c10eacdd161a49c84cfaeb953ec58fba7c
tso <tohlson@squareup.com>
2023-05-11 15:42:34 -0400

    Delete the zookeeper packges (#2769)

--- misk/misk-zookeeper/src/main/kotlin/misk/clustering/zookeeper/ForZkLease.kt
+++
@@ -1,7 +0,0 @@
-package misk.clustering.zookeeper
-
-import javax.inject.Qualifier
-
-@Qualifier
-@Target(AnnotationTarget.FIELD, AnnotationTarget.FUNCTION, AnnotationTarget.VALUE_PARAMETER)
-annotation class ForZkLease
