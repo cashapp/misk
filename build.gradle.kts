88c74606db59bba605c644fb7c3d0401b1887e5d
Tristan Ohlson <tohlson@squareup.com>
2023-05-16 15:32:55 -0400

    Add dependency analysis to the checks

--- build.gradle.kts
+++ build.gradle.kts
@@ -34,8 +34,8 @@ dependencyAnalysis {
       onAny {
         severity("fail")
         // Due to kotlin 1.8.20 see https://github.com/autonomousapps/dependency-analysis-android-gradle-plugin/issues/884
-//        exclude("() -> java.io.File?")
-//        exclude("org.jetbrains.kotlin:kotlin-test:1.8.21")
+        exclude("() -> java.io.File?")
+        exclude("org.jetbrains.kotlin:kotlin-test:1.8.21")
       }
     }
     // False positives.
@@ -59,11 +59,11 @@ dependencyAnalysis {
         exclude("javax.annotation:javax.annotation-api:1.3.2")
       }
     }
-//    project(":misk-jooq") {
-//      onIncorrectConfiguration {
-//        exclude("org.jooq:jooq:3.18.2")
-//      }
-//    }
+    project(":misk-jooq") {
+      onIncorrectConfiguration {
+        exclude("org.jooq:jooq:3.18.2")
+      }
+    }
   }
 }
 
