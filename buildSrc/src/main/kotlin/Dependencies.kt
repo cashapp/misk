64f197eb2160e849923b14961f7f36522add8d6e
tso <tohlson@squareup.com>
2023-05-18 16:40:09 -0400

    Update wisp to 1.4.1 (#2786)

--- buildSrc/src/main/kotlin/Dependencies.kt
+++ buildSrc/src/main/kotlin/Dependencies.kt
@@ -158,7 +158,7 @@ object Dependencies {
   val wireRuntime = "com.squareup.wire:wire-runtime:4.6.2"
   val wireSchema = "com.squareup.wire:wire-schema:4.6.2"
   val wispAwsEnvironment = "app.cash.wisp:wisp-aws-environment"
-  val wispBom = "app.cash.wisp:wisp-bom:1.4.0"
+  val wispBom = "app.cash.wisp:wisp-bom:1.4.1"
   val wispClient = "app.cash.wisp:wisp-client"
   val wispConfig = "app.cash.wisp:wisp-config"
   val wispContainersTesting = "app.cash.wisp:wisp-containers-testing"
