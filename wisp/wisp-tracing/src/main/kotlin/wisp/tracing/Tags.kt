302862462ee5321442dad1a2c00c496a99a764f0
Keefer Rourke <keefer@squareup.com>
2023-05-29 17:41:56 -0400

    Simplify bulk-add Span#setTags

--- wisp/wisp-tracing/src/main/kotlin/wisp/tracing/Tags.kt
+++ wisp/wisp-tracing/src/main/kotlin/wisp/tracing/Tags.kt
@@ -1,15 +1,18 @@
 package wisp.tracing
 
 import io.opentracing.Span
-import io.opentracing.tag.AbstractTag
-import io.opentracing.tag.BooleanTag
-import io.opentracing.tag.StringTag
 
 /**
  * A Tag is a name-value pair which will be added to a [Span].
  * Only primitive types are supported like [Boolean]s, [Number]s, and [String]s.
  */
-data class Tag<T>(val name: String, val value: T)
+data class Tag<T>(val name: String, val value: T) {
+    init {
+        check(value is Boolean || value is Number || value is String) {
+            "Only primitive trace tag value types are allowed."
+        }
+    }
+}
 
 /**
  * Conveniently set tags all at once.
@@ -18,19 +21,8 @@ fun Span.setTags(tags: Collection<Tag<*>>) = tags.forEach { setTag(it) }
 
 fun Span.setTag(tag: Tag<*>) {
     when (tag.value) {
-        is Boolean -> BooleanTag(tag.name).set(this, tag.value)
-        is Number -> NumberTag(tag.name).set(this, tag.value)
-        is String -> StringTag(tag.name).set(this, tag.value)
-        else -> error("Unsupported tag type ($tag). Only primitives are allowed.")
-    }
-}
-
-// NB: Open tracing provides IntTag, but spans accept tags with Number type values
-// using the Span.setTag(key, value) API.
-// This seems like a short-sight, so we provide NumberTag.
-
-private class NumberTag(key: String) : AbstractTag<Number>(key) {
-    override fun set(span: Span, tagValue: Number) {
-        span.setTag(key, tagValue)
+        is Boolean -> setTag(tag.name, tag.value)
+        is Number -> setTag(tag.name, tag.value)
+        is String -> setTag(tag.name, tag.value)
     }
 }
