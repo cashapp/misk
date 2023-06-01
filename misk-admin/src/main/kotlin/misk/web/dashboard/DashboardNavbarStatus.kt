32be5fa8b162b9aa6f8fa5ca56be0d72f7148fc1
Andrew Alexander <adrw@squareup.com>
2023-06-01 13:27:27 -0400

    Deprecate DashboardNavbarStatus and PR feedback for MiskWebTabIndexAction

--- misk-admin/src/main/kotlin/misk/web/dashboard/DashboardNavbarStatus.kt
+++ misk-admin/src/main/kotlin/misk/web/dashboard/DashboardNavbarStatus.kt
@@ -6,11 +6,13 @@ import misk.web.dashboard.ValidWebEntry.Companion.slugify
  * Banner text to show below the dashboard navbar
  * 0 or 1 [DashboardNavbarStatus] should be bound per dashboard
  */
+@Deprecated("DashboardNavbarStatus will not be supported in Misk Dashboards v2 and will be deleted shortly. Use custom UI to surface status information.")
 data class DashboardNavbarStatus(
   val dashboard_slug: String,
   val status: String
 ) : ValidWebEntry(valid_slug = dashboard_slug)
 
+@Deprecated("DashboardNavbarStatus will not be supported in Misk Dashboards v2 and will be deleted shortly. Use custom UI to surface status information.")
 inline fun <reified DA : Annotation> DashboardNavbarStatus(
   status: String
 ) = DashboardNavbarStatus(
