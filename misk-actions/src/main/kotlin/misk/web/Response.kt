a6c618f704db5d92f0d27c8a69b4de4e6240c1de
Francisco Rojas <frojas@squareup.com>
2023-06-27 07:22:10 -0700

    Add missing secondary constructor in Misk Response (#2837)
    * Add missing constructors in Misk Response
    
    This change is done to improve backward compatibility in binary

--- misk-actions/src/main/kotlin/misk/web/Response.kt
+++ misk-actions/src/main/kotlin/misk/web/Response.kt
@@ -5,7 +5,7 @@ import okhttp3.Headers.Companion.headersOf
 import okio.BufferedSink
 
 /** An HTTP response body, headers, and status code. */
-data class Response<out T>(
+data class Response<out T> @JvmOverloads constructor(
   val body: T,
   val headers: Headers = headersOf(),
   val statusCode: Int = 200,
