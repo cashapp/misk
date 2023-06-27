3c04b01b312c5e270d433cfa57270fe1d764cdca
Francisco Rojas <frojas@squareup.com>
2023-06-27 10:07:32 -0400

    Use JvmOverloads

--- misk-actions/src/main/kotlin/misk/web/Response.kt
+++ misk-actions/src/main/kotlin/misk/web/Response.kt
@@ -5,19 +5,12 @@ import okhttp3.Headers.Companion.headersOf
 import okio.BufferedSink
 
 /** An HTTP response body, headers, and status code. */
-data class Response<out T>(
+data class Response<out T> @JvmOverloads constructor(
   val body: T,
   val headers: Headers = headersOf(),
   val statusCode: Int = 200,
   val trailers: () -> Headers? = { null }
 ) {
-
-  constructor(body: T, headers: Headers = headersOf(), statusCode: Int = 200) : this(
-    body,
-    headers,
-    statusCode,
-    { null }
-  )
   override fun toString(): String = "body=$body, statusCode=$statusCode"
 }
 
