fac97c0371f95f415dc43829b9f97c2d9af4e95e
Josh Matthews <jmatthews@squareup.com>
2023-07-05 13:18:42 -0400

    Remove deprecation warning. (#2848)

--- misk-core/src/main/kotlin/misk/config/Config.kt
+++ misk-core/src/main/kotlin/misk/config/Config.kt
@@ -1,12 +1,3 @@
 package misk.config
 
-// TODO(chrisryan): soft deprecating...
-// @Deprecated(
-//  message = "Use wisp.config.Config directly",
-//  replaceWith = ReplaceWith(
-//    "Config",
-//    "wisp.config.Config"
-//  )
-// )
-@Deprecated("Use from wisp-config instead")
 interface Config : wisp.config.Config
