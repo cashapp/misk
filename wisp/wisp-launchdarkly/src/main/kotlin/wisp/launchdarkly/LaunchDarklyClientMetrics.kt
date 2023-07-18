6229f1788a0a1f977600eecd75f6eb37ef2cdb56
Adrian Liu <adrianliu@squareup.com>
2023-07-17 22:42:46 -0700

    change the implementation of ld metrics (#182)
    * add logs for launchdarkly client startup
    
    * change the implementation of ld metrics

--- wisp/wisp-launchdarkly/src/main/kotlin/wisp/launchdarkly/LaunchDarklyClientMetrics.kt
+++ wisp/wisp-launchdarkly/src/main/kotlin/wisp/launchdarkly/LaunchDarklyClientMetrics.kt
@@ -1,31 +1,31 @@
 package wisp.launchdarkly
 
 import io.micrometer.core.instrument.Counter
-import io.micrometer.core.instrument.DistributionSummary
 import io.micrometer.core.instrument.MeterRegistry
 
-class LaunchDarklyClientMetrics(meterRegistry: MeterRegistry) {
+class LaunchDarklyClientMetrics(
+  private val meterRegistry: MeterRegistry) {
 
-  internal fun initializationDuration(
-    meterRegistry: MeterRegistry
-  ): DistributionSummary =
-    DistributionSummary.builder(HISTO_DURATION_NAME)
-      .description("count and duration in ms of ld client initialization")
-      .percentilePrecision(4)
-      .publishPercentiles(0.5, 0.75, 0.95, 0.99, 0.999)
-      .publishPercentileHistogram()
-      .register(meterRegistry)
+  private var initSuccess: Counter = meterRegistry
+    .counter(SUCCESS_COUNTER_NAME)
 
-  internal val successCount: Counter = Counter.builder(SUCCESS_COUNTER_NAME)
-    .description("count of successful ld client initializations")
-    .register(meterRegistry)
+  private var initFailure: Counter = meterRegistry
+    .counter(FAILED_COUNTER_NAME)
 
-  internal val failedCount: Counter = Counter.builder(FAILED_COUNTER_NAME)
-    .description("count of ld client initialization failures")
-    .register(meterRegistry)
+  fun onInitSuccess(duration: Long) {
+    initSuccess.increment()
+    meterRegistry.gauge(
+      INITIALIZATION_DURATION_NAME,
+      duration,
+    )
+  }
+
+  fun onInitFailure() {
+    initFailure.increment()
+  }
 
   companion object {
-    const val HISTO_DURATION_NAME = "histo_ld_initialization_duration_ms"
+    const val INITIALIZATION_DURATION_NAME = "ld_initialization_duration_ms"
     const val SUCCESS_COUNTER_NAME = "ld_initialization_success_total"
     const val FAILED_COUNTER_NAME = "ld_initialization_failed_total"
   }
