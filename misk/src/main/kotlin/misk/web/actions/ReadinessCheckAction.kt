82ffa48b238faeef84a1c7716b2891d49ed6680c
Tim Kye <tkye@squareup.com>
2023-07-21 13:14:22 -0700

    feat: add memory cache to readiness check

--- misk/src/main/kotlin/misk/web/actions/ReadinessCheckAction.kt
+++ misk/src/main/kotlin/misk/web/actions/ReadinessCheckAction.kt
@@ -7,25 +7,35 @@ import misk.web.AvailableWhenDegraded
 import misk.web.Get
 import misk.web.Response
 import misk.web.ResponseContentType
+import misk.web.WebConfig
 import misk.web.mediatype.MediaTypes
 import wisp.logging.getLogger
 import javax.inject.Inject
 import javax.inject.Provider
 import javax.inject.Singleton
+import kotlinx.coroutines.*
+import misk.healthchecks.HealthStatus
+import java.time.Clock
+import java.time.Instant
 
 private val logger = getLogger<ReadinessCheckAction>()
 
 @Singleton
 class ReadinessCheckAction @Inject internal constructor(
   private val serviceManagerProvider: Provider<ServiceManager>,
-  @JvmSuppressWildcards private val healthChecks: List<HealthCheck>
+  @JvmSuppressWildcards private val healthChecks: List<HealthCheck>,
+  val config: WebConfig,
+  val clock: Clock
 ) : WebAction {
+
+  var lastUpdate: Instant? = null
+  var statuses: List<HealthStatus> = listOf()
 
   @Get("/_readiness")
   @ResponseContentType(MediaTypes.APPLICATION_JSON)
   @Unauthenticated
   @AvailableWhenDegraded
-  fun readinessCheck(): Response<String> {
+  fun readinessCheck(): Response<String> = runBlocking {
     val servicesNotRunning = serviceManagerProvider.get().servicesByState().values().asList()
       .filterNot { it.isRunning }
 
@@ -37,20 +47,42 @@ class ReadinessCheckAction @Inject internal constructor(
       // Don't do healthchecks if services haven't all started. The app isn't in a good state yet,
       // and a health check could end up triggering random errors that we don't want to flood the
       // logs with.
-      return Response("", statusCode = 503)
+      return@runBlocking Response("", statusCode = 503)
     }
 
-    val failedHealthChecks = healthChecks
-      .map { it.status() }
-      .filter { !it.isHealthy }
+    // First run needs to block and get statuses
+    if (lastUpdate == null) {
+      refreshStatuses()
+    } else {
+      // This needs to be independent of the max age check so that services can recover
+      // If refreshStatuses() is never run then a failed/stale check can never recover
+      if (clock.instant().isAfter(lastUpdate!!.plusMillis(config.readiness_refresh_interval_ms.toLong()))) {
+        // Launch in the background so that we can still return the cached state
+        launch(Job()) {
+          refreshStatuses()
+        }
+      }
+
+      if (clock.instant().isAfter(lastUpdate!!.plusMillis(config.readiness_max_age_ms.toLong()))) {
+        logger.info("Failed health check: last status check is older than max age")
+        return@runBlocking Response("", statusCode = 503)
+      }
+    }
+
+    val failedHealthChecks = statuses.filter { !it.isHealthy }
 
     if (failedHealthChecks.isEmpty()) {
-      return Response("", statusCode = 200)
+      return@runBlocking Response("", statusCode = 200)
     }
 
     for (healthCheck in failedHealthChecks) {
       logger.info("Failed health check: ${healthCheck.messages}")
     }
-    return Response("", statusCode = 503)
+    return@runBlocking Response("", statusCode = 503)
+  }
+
+  private fun refreshStatuses() {
+    statuses = healthChecks.map { it.status() }
+    lastUpdate = clock.instant()
   }
 }
--- misk/src/test/kotlin/misk/web/actions/ReadinessCheckActionTest.kt
+++ misk/src/test/kotlin/misk/web/actions/ReadinessCheckActionTest.kt
@@ -5,13 +5,20 @@ import com.google.inject.util.Modules
 import misk.MiskTestingServiceModule
 import misk.healthchecks.FakeHealthCheck
 import misk.healthchecks.FakeHealthCheckModule
+import misk.healthchecks.HealthCheck
+import misk.healthchecks.HealthStatus
+import misk.inject.KAbstractModule
 import misk.services.FakeServiceModule
 import misk.testing.MiskTest
 import misk.testing.MiskTestModule
+import misk.time.FakeClock
 import misk.web.WebActionModule
+import misk.web.WebConfig
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
+import java.util.concurrent.TimeUnit
 import javax.inject.Inject
+import javax.inject.Singleton
 
 @MiskTest
 class ReadinessCheckActionTest {
@@ -20,25 +27,85 @@ class ReadinessCheckActionTest {
     MiskTestingServiceModule(),
     FakeServiceModule(),
     FakeHealthCheckModule(),
-    WebActionModule.create<ReadinessCheckAction>()
+    WebActionModule.create<ReadinessCheckAction>(),
+    object : KAbstractModule() {
+      override fun configure() {
+        multibind<HealthCheck>().to<CountingHealthCheck>()
+        bind<WebConfig>().toInstance(WebConfig(8000))
+      }
+    }
   )
 
+  @Inject lateinit var clock: FakeClock
   @Inject lateinit var readinessCheckAction: ReadinessCheckAction
   @Inject lateinit var serviceManager: ServiceManager
-  @Inject lateinit var healthCheck: FakeHealthCheck
+  @Inject lateinit var fakeHealthCheck: FakeHealthCheck
+  @Inject lateinit var countingHealthCheck: CountingHealthCheck
 
   @Test fun readinessDependsOnServiceStateAndHealthChecksPassing() {
     serviceManager.startAsync()
     serviceManager.awaitHealthy()
     assertThat(readinessCheckAction.readinessCheck().statusCode).isEqualTo(200)
 
-    healthCheck.setUnhealthy()
+    fakeHealthCheck.setUnhealthy()
+    forceRefresh()
     assertThat(readinessCheckAction.readinessCheck().statusCode).isEqualTo(503)
-    healthCheck.setHealthy()
+
+    fakeHealthCheck.setHealthy()
+    forceRefresh()
     assertThat(readinessCheckAction.readinessCheck().statusCode).isEqualTo(200)
 
     serviceManager.stopAsync()
     serviceManager.awaitStopped()
     assertThat(readinessCheckAction.readinessCheck().statusCode).isEqualTo(503)
+  }
+
+  @Test fun readinessReusesStatusUntilElapsedTtl() {
+    serviceManager.startAsync()
+    serviceManager.awaitHealthy()
+    assertThat(readinessCheckAction.readinessCheck().statusCode).isEqualTo(200)
+    assertThat(countingHealthCheck.checks).isEqualTo(1)
+
+    // still the same
+    assertThat(readinessCheckAction.readinessCheck().statusCode).isEqualTo(200)
+    assertThat(countingHealthCheck.checks).isEqualTo(1)
+
+    // move up and verify count increases just once
+    forceRefresh()
+    assertThat(readinessCheckAction.readinessCheck().statusCode).isEqualTo(200)
+    assertThat(readinessCheckAction.readinessCheck().statusCode).isEqualTo(200)
+    assertThat(countingHealthCheck.checks).isEqualTo(2)
+  }
+
+  @Test fun readinessReturnsErrorPastMaxAgeThenRefreshes() {
+    serviceManager.startAsync()
+    serviceManager.awaitHealthy()
+    assertThat(readinessCheckAction.readinessCheck().statusCode).isEqualTo(200)
+
+    // move past max age
+    clock.add((readinessCheckAction.config.readiness_max_age_ms + 1).toLong(), TimeUnit.MILLISECONDS)
+    assertThat(readinessCheckAction.readinessCheck().statusCode).isEqualTo(503)
+
+    // Next run should still see the updated state
+    assertThat(readinessCheckAction.readinessCheck().statusCode).isEqualTo(200)
+  }
+
+  private fun forceRefresh() {
+    // advance time so that a refresh is triggered
+    clock.add((readinessCheckAction.config.readiness_refresh_interval_ms + 1).toLong(), TimeUnit.MILLISECONDS)
+    // first hit will still return cache and queue update in background
+    // result can be ignored since it will be the same as previous
+    readinessCheckAction.readinessCheck()
+  }
+
+  @Singleton
+  class CountingHealthCheck @Inject constructor() : HealthCheck {
+    var checks = 0
+    var status = HealthStatus.healthy()
+
+    override fun status(): HealthStatus{
+      checks++
+      return status
+    }
   }
 }
