99fe5e41f94d6fde6342b4e52b8d82d0638755dc
Andrew Alexander <adrw@squareup.com>
2023-08-03 15:11:34 -0400

    Set server as s01

--- build.gradle.kts
+++ build.gradle.kts
@@ -263,13 +263,12 @@ subprojects {
 allprojects {
   plugins.withId("com.vanniktech.maven.publish.base") {
     configure<MavenPublishBaseExtension> {
-      var sonatypeHost = SonatypeHost.S01
+      val sonatypeHost = SonatypeHost.S01
       var repo_url = "https://github.com/cashapp/misk/"
       var scm_connection = "scm:git:git://github.com/cashapp/misk.git"
       var scm_dev_connection = "scm:git:ssh://git@github.com/cashapp/misk.git"
       //Use old wisp settings for now until we get access to Sonatype
       if (project.path.startsWith(":wisp")) {
-        sonatypeHost = SonatypeHost.DEFAULT
         repo_url = "https://github.com/cashapp/wisp/"
         scm_connection = "scm:git:git://github.com/cashapp/wisp.git"
         scm_dev_connection = "scm:git:ssh://git@github.com/cashapp/wisp.git"
