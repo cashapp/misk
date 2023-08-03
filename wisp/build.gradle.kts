f0fc72d1736c69926fa60182370fac68a1da8c99
Raine Cabal <raine@squareup.com>
2023-08-03 09:33:54 -0700

    Use wisp existing params for publishing

--- wisp/build.gradle.kts
+++ wisp/build.gradle.kts
@@ -4,7 +4,6 @@ subprojects {
     apply(plugin = "java")
     apply(plugin = "kotlin")
     apply(plugin = "com.google.protobuf")
-    //disable wisp publishing until sonatype issue is resolved
-    //apply(plugin = "com.vanniktech.maven.publish")
+    apply(plugin = "com.vanniktech.maven.publish")
   }
 }
