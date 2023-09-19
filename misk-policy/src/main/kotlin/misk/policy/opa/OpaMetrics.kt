d2de27fd4d1ce01e5fe2f5f516c1aaddaac7f3f0
Jason Stiefel <jstiefel@squareup.com>
2023-09-19 14:09:41 -0400

    Deprecates `MiskOpaMetrics.MetricType` (#2953)
    - Adds a test to make darn sure `RealOpaPolicyEngine` is a `@Singleton`

--- misk-policy/src/main/kotlin/misk/policy/opa/OpaMetrics.kt
+++ misk-policy/src/main/kotlin/misk/policy/opa/OpaMetrics.kt
@@ -2,6 +2,15 @@ package misk.policy.opa
 
 interface OpaMetrics {
 
+  enum class Names {
+    opa_server_query_cache_hit,
+    opa_rego_external_resolve,
+    opa_rego_input_parse,
+    opa_rego_query_eval,
+    opa_server_handler,
+    opa_rego_evaluated
+  }
+
   /**
    * Increments a counter to indicate policy evaluation, whether metrics are enabled on the
    * request or not.
