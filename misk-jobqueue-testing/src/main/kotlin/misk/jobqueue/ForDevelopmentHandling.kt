686ebcbaa0cc04f8573ba4b3b0e23bbc02b4e3c6
tso <tohlson@squareup.com>
2023-09-21 14:48:58 -0400

    Remove more deprecated *-test modules (#2957)
    * Remove more deprecated *-test modules

--- misk-jobqueue-testing/src/main/kotlin/misk/jobqueue/ForDevelopmentHandling.kt
+++
@@ -1,8 +0,0 @@
-package misk.jobqueue
-
-import jakarta.inject.Qualifier
-
-@Deprecated("Replace the dependency on misk-jobqueue-testing with testFixtures(misk-jobqueue)")
-@Qualifier
-@Target(AnnotationTarget.FIELD, AnnotationTarget.FUNCTION, AnnotationTarget.VALUE_PARAMETER)
-internal annotation class ForDevelopmentHandling
