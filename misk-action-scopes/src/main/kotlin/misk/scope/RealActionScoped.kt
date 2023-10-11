bcf5cb013258b8e101e8bd8c8f16a7a024e993e7
Mehdi Mollaverdi <mmollaverdi@squareup.com>
2023-10-11 15:32:46 +1100

    Changes to internal APIs are ignored

--- misk-action-scopes/src/main/kotlin/misk/scope/RealActionScoped.kt
+++ misk-action-scopes/src/main/kotlin/misk/scope/RealActionScoped.kt
@@ -3,9 +3,10 @@ package misk.scope
 import com.google.inject.Key
 import jakarta.inject.Inject
 
-internal class RealActionScoped<T> @Inject internal constructor(
+internal class RealActionScoped<T> @Inject internal @JvmOverloads constructor(
   val key: Key<T>,
-  val scope: ActionScope
+  val scope: ActionScope,
+  val newArg: String? = null
 ) : ActionScoped<T> {
   override fun get(): T = scope.get(key)
 }
