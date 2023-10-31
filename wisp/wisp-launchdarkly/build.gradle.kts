fce08baa5b225f8aaaa63a7a760826b8f0587839
Adrian Liu <adrianliu@squareup.com>
2023-10-31 15:32:01 -0700

    add ff value into tracing span tags

--- wisp/wisp-launchdarkly/build.gradle.kts
+++ wisp/wisp-launchdarkly/build.gradle.kts
@@ -7,14 +7,17 @@ dependencies {
     api(Dependencies.kotlinLogging)
     api(Dependencies.micrometerCore)
     api(Dependencies.moshi)
+    api(Dependencies.openTracingApi)
     api(project(":wisp:wisp-client"))
     api(project(":wisp:wisp-config"))
     api(project(":wisp:wisp-feature"))
     api(project(":wisp:wisp-resource-loader"))
     api(project(":wisp:wisp-ssl"))
+    implementation(Dependencies.openTracingDatadog)
 
     testImplementation(Dependencies.assertj)
     testImplementation(Dependencies.junitApi)
     testImplementation(Dependencies.mockitoCore)
     testImplementation(project(":wisp:wisp-moshi"))
+    testImplementation(project(":wisp:wisp-tracing"))
 }
