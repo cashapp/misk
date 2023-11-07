a8f42e740094ea4d207f27ac3de713e0d5a1ed08
Tyler Gregory <tgregory@squareup.com>
2023-11-07 12:39:31 -0600

    Implement modules for business-logic rate limiting (#3001)
    * Stand up RateLimiter interface
    
    * Wire up redis rate limiter module
    
    * Wire up dynamodb rate limiter module
    
    * Use dedicated JedisPool
    
    * API dump
    
    * Switch to prometheus meter registry so we can test metrics
    
    * Implement sample rate limited action
    
    * api dump
    
    * JvmOverloads fix
    
    * Add MySQL bucket4j module
    
    * Use SimpleMeterRegistry
    
    * api dump
    
    * Fix dependency analysis linter
    
    * comments

---
+++ samples/exemplar/src/test/kotlin/com/squareup/exemplar/AbstractRateLimitedActionTests.kt
@@ -0,0 +1,99 @@
+package com.squareup.exemplar
+
+import com.squareup.exemplar.actions.ExampleRateLimitConfiguration
+import com.squareup.exemplar.actions.RateLimitedAction
+import io.micrometer.core.instrument.MeterRegistry
+import jakarta.inject.Inject
+import misk.exceptions.TooManyRequestsException
+import misk.redis.testing.DockerRedis
+import misk.testing.MiskExternalDependency
+import misk.time.FakeClock
+import org.assertj.core.api.Assertions
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertDoesNotThrow
+import org.junit.jupiter.api.assertThrows
+import wisp.ratelimiting.RateLimiterMetrics
+
+abstract class AbstractRateLimitedActionTests {
+  @Inject private lateinit var fakeClock: FakeClock
+
+  @Inject private lateinit var metricRegistry: MeterRegistry
+
+  @Inject private lateinit var rateLimitedAction: RateLimitedAction
+
+  private val metrics by lazy {
+    RateLimiterMetrics(metricRegistry)
+  }
+
+  private val consumedMetrics by lazy {
+    metrics.consumptionAttempts(
+      ExampleRateLimitConfiguration,
+      RateLimiterMetrics.ConsumptionResult.SUCCESS
+    )
+  }
+
+  private val rejectedMetrics by lazy {
+    metrics.consumptionAttempts(
+      ExampleRateLimitConfiguration,
+      RateLimiterMetrics.ConsumptionResult.REJECTED
+    )
+  }
+
+  private val exceptionMetrics by lazy {
+    metrics.consumptionAttempts(
+      ExampleRateLimitConfiguration,
+      RateLimiterMetrics.ConsumptionResult.EXCEPTION
+    )
+  }
+
+  @Suppress("unused")
+  @MiskExternalDependency
+  private val dockerRedis = DockerRedis
+
+  abstract fun setException()
+
+  @Test
+  fun `should throw when we reach limit`() {
+    repeat(ExampleRateLimitConfiguration.capacity.toInt()) {
+      val response = assertDoesNotThrow {
+        rateLimitedAction.rateLimitedExample()
+      }
+      Assertions.assertThat(response.number).isNotNull()
+      Assertions.assertThat(consumedMetrics.count()).isEqualTo(it.toDouble() + 1.0)
+      Assertions.assertThat(rejectedMetrics.count()).isZero()
+      Assertions.assertThat(exceptionMetrics.count()).isZero()
+    }
+    assertThrows<TooManyRequestsException> {
+      rateLimitedAction.rateLimitedExample()
+    }
+    Assertions.assertThat(consumedMetrics.count()).isEqualTo(ExampleRateLimitConfiguration.capacity.toDouble())
+    Assertions.assertThat(rejectedMetrics.count()).isOne()
+    Assertions.assertThat(exceptionMetrics.count()).isZero()
+
+    fakeClock.add(ExampleRateLimitConfiguration.refillPeriod)
+
+    val response = assertDoesNotThrow {
+      rateLimitedAction.rateLimitedExample()
+    }
+    Assertions.assertThat(consumedMetrics.count()).isEqualTo(
+      ExampleRateLimitConfiguration.capacity.toDouble() + 1.0
+    )
+    Assertions.assertThat(rejectedMetrics.count()).isOne()
+    Assertions.assertThat(exceptionMetrics.count()).isZero()
+
+    Assertions.assertThat(response.number).isNotNull()
+  }
+
+  @Test
+  fun `exception metrics are tracked`() {
+    rateLimitedAction.rateLimitedExample()
+    // Force exception
+    setException()
+    assertThrows<Throwable> {
+      rateLimitedAction.rateLimitedExample()
+    }
+    Assertions.assertThat(consumedMetrics.count()).isOne()
+    Assertions.assertThat(rejectedMetrics.count()).isZero()
+    Assertions.assertThat(exceptionMetrics.count()).isOne()
+  }
+}
---
+++ samples/exemplar/src/test/kotlin/com/squareup/exemplar/DynamoDBV1RateLimitedActionTests.kt
@@ -0,0 +1,36 @@
+package com.squareup.exemplar
+
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDB
+import com.google.inject.Module
+import io.micrometer.core.instrument.MeterRegistry
+import io.micrometer.core.instrument.simple.SimpleMeterRegistry
+import jakarta.inject.Inject
+import misk.aws.dynamodb.testing.DockerDynamoDbModule
+import misk.aws.dynamodb.testing.DynamoDbTable
+import misk.inject.KAbstractModule
+import misk.ratelimiting.bucket4j.dynamodb.v1.DynamoDbV1Bucket4jRateLimiterModule
+import misk.testing.MiskTest
+import misk.testing.MiskTestModule
+
+@MiskTest(startService = true)
+class DynamoDBV1RateLimitedActionTests : AbstractRateLimitedActionTests() {
+  @Suppress("unused")
+  @MiskTestModule val module: Module = object : KAbstractModule() {
+    override fun configure() {
+      install(ExemplarTestModule())
+      install(
+        DockerDynamoDbModule(
+          DynamoDbTable(DyRateLimitBucket::class)
+        )
+      )
+      install(DynamoDbV1Bucket4jRateLimiterModule("rate_limit_buckets"))
+      bind<MeterRegistry>().toInstance(SimpleMeterRegistry())
+    }
+  }
+
+  @Inject private lateinit var dynamoDb: AmazonDynamoDB
+
+  override fun setException() {
+    dynamoDb.shutdown()
+  }
+}
