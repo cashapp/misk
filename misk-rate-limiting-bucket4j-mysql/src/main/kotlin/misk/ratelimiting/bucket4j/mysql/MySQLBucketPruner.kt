de9f911afbcaec9b1056da39b2a7c3edc8448c5d
Tyler Gregory <tgregory@squareup.com>
2023-11-29 15:33:32 -0600

    Add a MySQL bucket4j pruner (#3024)
    * Fix metric name
    
    * Implement MySQL pruner
    
    * Add metrics
    
    * Skip locked is MySQL 8 only
    
    * api dump
    
    * Accept breaking change
    
    * Add missing explicit dep
    
    * Remove unneeded val

---
+++ misk-rate-limiting-bucket4j-mysql/src/main/kotlin/misk/ratelimiting/bucket4j/mysql/MySQLBucketPruner.kt
@@ -0,0 +1,148 @@
+package misk.ratelimiting.bucket4j.mysql
+
+import io.github.bucket4j.distributed.remote.RemoteBucketState
+import io.github.bucket4j.distributed.serialization.DataOutputSerializationAdapter
+import io.micrometer.core.instrument.MeterRegistry
+import wisp.logging.getLogger
+import wisp.ratelimiting.RateLimitPruner
+import wisp.ratelimiting.RateLimitPrunerMetrics
+import wisp.ratelimiting.bucket4j.ClockTimeMeter
+import java.io.ByteArrayInputStream
+import java.io.DataInputStream
+import java.sql.Connection
+import java.sql.ResultSet
+import java.sql.SQLException
+import java.time.Clock
+import javax.sql.DataSource
+import kotlin.system.measureTimeMillis
+
+class MySQLBucketPruner @JvmOverloads constructor(
+  clock: Clock,
+  private val dataSource: DataSource,
+  private val idColumn: String,
+  meterRegistry: MeterRegistry,
+  private val stateColumn: String,
+  tableName: String,
+  isMySQL8: Boolean = false,
+  pageSize: Long = 1000
+) : RateLimitPruner {
+  private val clockTimeMeter = ClockTimeMeter(clock)
+  private val deleteStatement = """
+    DELETE FROM $tableName
+    WHERE $idColumn = ?
+  """.trimIndent()
+
+  private val lockingClause = if (isMySQL8) {
+    "FOR UPDATE SKIP LOCKED"
+  } else {
+    "FOR UPDATE"
+  }
+  /**
+   * Notes about this query:
+   * * We use `SKIP LOCKED` because that indicates bucket4j is currently using the row
+   * * We use cursor based pagination because it's faster and because OFFSET based pagination
+   * requires extra bookkeeping when deleting rows
+   */
+  private val pageStatement = """
+    SELECT $idColumn, $stateColumn
+    FROM $tableName
+    WHERE $idColumn > ?
+    ORDER BY $idColumn
+    LIMIT $pageSize
+    $lockingClause
+  """.trimIndent()
+
+  private val prunerMetrics = RateLimitPrunerMetrics(meterRegistry)
+
+  /**
+   * Prunes the rate limit table, returning the number of rows pruned.
+   */
+  override fun prune() {
+    val connection = dataSource.connection
+    try {
+      connection.autoCommit = false
+      val millisTaken = measureTimeMillis {
+        pruneLoop(connection)
+      }
+      prunerMetrics.pruningDuration.record(millisTaken.toDouble())
+    } catch (e: Exception) {
+      connection.rollback()
+      logger.warn(e) { "Caught exception, rolling back current pruning transaction" }
+    } finally {
+      connection.close()
+    }
+  }
+
+  private fun pruneLoop(connection: Connection) {
+    var cursor = ""
+    while (true) {
+      val fetchPageStatement = connection.prepareStatement(pageStatement)
+      fetchPageStatement.setString(1, cursor)
+      val resultSet = fetchPageStatement.executeQuery()
+
+      // Advance result set cursor, breaking if we're out of rows
+      if (isResultSetEmpty(resultSet)) {
+        break
+      }
+
+      var deletedBuckets = 0
+      while (true) {
+        val key = resultSet.getString(idColumn)
+        val stateBytes = resultSet.getBytes(stateColumn)
+        val state = try {
+          deserializeState(stateBytes)
+        } catch (e: Exception) {
+          logger.warn(e) { "Failed to deserialize state column for key $key" }
+          continue
+        }
+
+        if (isBucketStale(state)) {
+          val deleteBucketStatement = connection.prepareStatement(deleteStatement)
+          deleteBucketStatement.setString(1, key)
+          deleteBucketStatement.execute()
+          deletedBuckets++
+        }
+
+        // Advance result set cursor, breaking if we're out of rows
+        if (isResultSetEmpty(resultSet)) {
+          // Set cursor to the last read key
+          cursor = key
+          break
+        }
+      }
+
+      connection.commit()
+      prunerMetrics.bucketsPruned.increment(deletedBuckets.toDouble())
+    }
+  }
+
+  /**
+   * Advances the result set, returning `true` if there were no more rows
+   */
+  private fun isResultSetEmpty(resultSet: ResultSet): Boolean {
+    return try {
+      !resultSet.next()
+    } catch (_: SQLException) {
+      // The docs for ResultSet.next() say that it can throw SQLException
+      // if the result set is empty
+      true
+    }
+  }
+
+  private fun isBucketStale(state: RemoteBucketState): Boolean {
+    val refillTimeNanos = state.calculateFullRefillingTime(clockTimeMeter.currentTimeNanos())
+    return refillTimeNanos <= 0L
+  }
+
+  private fun deserializeState(bytes: ByteArray): RemoteBucketState {
+    val inputStream = DataInputStream(ByteArrayInputStream(bytes))
+    return RemoteBucketState.SERIALIZATION_HANDLE.deserialize(
+      DataOutputSerializationAdapter.INSTANCE,
+      inputStream
+    )
+  }
+
+  companion object {
+    private val logger = getLogger<MySQLBucketPruner>()
+  }
+}
---
+++ misk-rate-limiting-bucket4j-mysql/src/test/kotlin/misk/ratelimiting/bucket4j/mysql/MySQLBucketPrunerTests.kt
@@ -0,0 +1,150 @@
+package misk.ratelimiting.bucket4j.mysql
+
+import com.google.inject.Injector
+import io.micrometer.core.instrument.MeterRegistry
+import io.micrometer.core.instrument.simple.SimpleMeterRegistry
+import jakarta.inject.Inject
+import misk.MiskTestingServiceModule
+import misk.config.MiskConfig
+import misk.environment.DeploymentModule
+import misk.inject.KAbstractModule
+import misk.inject.keyOf
+import misk.jdbc.DataSourceService
+import misk.jdbc.JdbcModule
+import misk.jdbc.JdbcTestingModule
+import misk.testing.MiskTest
+import misk.testing.MiskTestModule
+import misk.time.FakeClock
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+import wisp.deployment.TESTING
+import wisp.ratelimiting.RateLimitConfiguration
+import wisp.ratelimiting.RateLimitPruner
+import wisp.ratelimiting.RateLimitPrunerMetrics
+import wisp.ratelimiting.RateLimiter
+import wisp.ratelimiting.testing.TestRateLimitConfig
+import java.time.Duration
+
+@MiskTest(startService = true)
+class MySQLBucketPrunerTests {
+  @Suppress("unused")
+  @MiskTestModule val module = object : KAbstractModule() {
+    override fun configure() {
+      install(MiskTestingServiceModule())
+      install(DeploymentModule(TESTING))
+      val config = MiskConfig.load<MySQLBucket4jRateLimiterTests.RootConfig>(
+        "mysql_rate_limiter_test", TESTING
+      )
+      install(JdbcTestingModule(RateLimits::class))
+      install(JdbcModule(RateLimits::class, config.mysql_data_source))
+
+      install(
+        MySQLBucket4jRateLimiterModule(
+          qualifier = RateLimits::class,
+          tableName = MySQLBucket4jRateLimiterTests.TABLE_NAME,
+          idColumn = MySQLBucket4jRateLimiterTests.ID_COLUMN,
+          stateColumn = MySQLBucket4jRateLimiterTests.STATE_COLUMN,
+          prunerPageSize = 2L
+        )
+      )
+      bind<MeterRegistry>().toInstance(SimpleMeterRegistry())
+    }
+  }
+
+  @Inject private lateinit var fakeClock: FakeClock
+
+  @Inject private lateinit var injector: Injector
+
+  @Inject private lateinit var meterRegistry: MeterRegistry
+
+  @Inject private lateinit var pruner: RateLimitPruner
+
+  @Inject private lateinit var rateLimiter: RateLimiter
+
+  private val getAllKeysQuery = """
+      SELECT ${MySQLBucket4jRateLimiterTests.ID_COLUMN} 
+      FROM ${MySQLBucket4jRateLimiterTests.TABLE_NAME}
+    """.trimIndent()
+
+  private val prunerMetrics by lazy {
+    RateLimitPrunerMetrics(meterRegistry)
+  }
+
+  @Test
+  fun `pruning deletes only expired rows`() {
+    // Create group of expired keys
+    val expiredBucketKeys = buildList {
+      repeat(10) {
+        add("expired-$it")
+      }
+    }
+    expiredBucketKeys.forEach { key ->
+      rateLimiter.consumeToken(key, TestRateLimitConfig)
+    }
+
+    val mixedBucketKeys = buildList {
+      repeat(10) {
+        add("maybeexpired-$it")
+      }
+    }
+
+    // Create group of interleaved short and long buckets
+    mixedBucketKeys.forEachIndexed { idx, key ->
+      if (idx % 2 == 0) {
+        rateLimiter.consumeToken(key, LongTestRateLimitConfig)
+      } else {
+        rateLimiter.consumeToken(key, TestRateLimitConfig)
+      }
+    }
+
+    fakeClock.add(TestRateLimitConfig.refillPeriod)
+
+    // Create group of non-expired keys
+    val nonExpiredShortBucketKeys = buildList {
+      repeat(10) {
+        add("nonexpired-short-$it")
+      }
+    }
+    nonExpiredShortBucketKeys.forEach { key ->
+      rateLimiter.consumeToken(key, TestRateLimitConfig)
+    }
+
+    pruner.prune()
+
+    assertThat(prunerMetrics.bucketsPruned.count()).isEqualTo(15.0)
+    // Use count to verify interaction instead of checking the duration
+    //  sometimes in tests the time can be so fast it registers as 0, resulting in a flaky test
+    assertThat(prunerMetrics.pruningDuration.count()).isGreaterThan(0L)
+
+    val partitionedBucketKeys = mixedBucketKeys.withIndex().partition {
+      it.index % 2 == 0
+    }
+    val nonExpiredLongKeys = partitionedBucketKeys.first.map { it.value }
+
+    val dataSource = injector.getInstance(keyOf<DataSourceService>(RateLimits::class)).dataSource
+    val connection = dataSource.connection
+    val statement = connection.createStatement()
+    val resultSet = statement.executeQuery(getAllKeysQuery)
+    val extantKeys = buildList {
+      while (resultSet.next()) {
+        add(resultSet.getString(MySQLBucket4jRateLimiterTests.ID_COLUMN))
+      }
+    }
+
+    // We should have deleted only the short duration keys that were created before advancing time,
+    // leaving the long duration keys and the short duration keys created after advancing time
+    assertThat(extantKeys).containsExactlyElementsOf(
+      nonExpiredLongKeys + nonExpiredShortBucketKeys
+    )
+  }
+
+  private object LongTestRateLimitConfig : RateLimitConfiguration {
+    private const val BUCKET_CAPACITY = 5L
+    private val REFILL_DURATION: Duration = Duration.ofHours(1L)
+
+    override val capacity = BUCKET_CAPACITY
+    override val name = "long_test_configuration"
+    override val refillAmount = BUCKET_CAPACITY
+    override val refillPeriod: Duration = REFILL_DURATION
+  }
+}
