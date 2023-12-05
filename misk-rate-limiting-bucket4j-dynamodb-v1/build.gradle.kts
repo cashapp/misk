22ed9bd6d1cbf77182bd24b64313c55ca0333df2
Tyler Gregory <tgregory@squareup.com>
2023-12-05 09:36:15 -0600

    linter

--- misk-rate-limiting-bucket4j-dynamodb-v1/build.gradle.kts
+++ misk-rate-limiting-bucket4j-dynamodb-v1/build.gradle.kts
@@ -12,13 +12,13 @@ plugins {
 dependencies {
   api(project(":misk-inject"))
   api(project(":wisp:wisp-rate-limiting"))
+  api(project(":wisp:wisp-rate-limiting:bucket4j"))
   api(Dependencies.awsDynamodb)
   api(Dependencies.guice)
   api(Dependencies.jakartaInject)
   api(Dependencies.micrometerCore)
 
   implementation(project(":wisp:wisp-logging"))
-  implementation(project(":wisp:wisp-rate-limiting:bucket4j"))
   implementation(Dependencies.bucket4jCore)
   implementation(Dependencies.bucket4jDynamoDbV1)
   implementation(Dependencies.kotlinLogging)
