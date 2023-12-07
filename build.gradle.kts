b2abd37434c29cbac3322bf657e2656079b7e0e0
Tyler Gregory <tgregory@squareup.com>
2023-12-07 09:19:27 -0600

    Use dockerhub instead of public ECR

--- build.gradle.kts
+++ build.gradle.kts
@@ -322,7 +322,7 @@ tasks.register("startRedis") {
     val redisVersion = "6.2"
     val redisPort = System.getenv("REDIS_PORT") ?: "6379"
     val redisContainerName = "miskTestRedis-$redisPort"
-    val redisImage = "public.ecr.aws/docker/library/redis:$redisVersion"
+    val redisImage = "redis:$redisVersion"
 
     val portIsOccupied = try {
       Socket("localhost", redisPort.toInt()).close()
