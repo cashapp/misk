c8d0cef3a23b0fa8851b3e1550caec9e9dcebcc1
Tyler Gregory <tgregory@squareup.com>
2024-01-19 09:40:58 -0600

    Project health

--- misk-rate-limiting-bucket4j-redis/build.gradle.kts
+++ misk-rate-limiting-bucket4j-redis/build.gradle.kts
@@ -11,27 +11,23 @@ plugins {
 
 dependencies {
   api(project(":misk-inject"))
-  api(project(":misk-metrics"))
-  api(project(":misk-redis"))
-  api(project(":wisp:wisp-deployment"))
   api(project(":wisp:wisp-rate-limiting"))
-  api(libs.guava)
   api(libs.guice)
   api(libs.jakartaInject)
   api(libs.jedis)
   api(libs.micrometerCore)
 
-  implementation(project(":misk-service"))
   implementation(project(":wisp:wisp-rate-limiting:bucket4j"))
   implementation(libs.bucket4jCore)
   implementation(libs.bucket4jRedis)
 
-  testFixturesApi(project(":misk-redis"))
   testFixturesApi(project(":misk-testing"))
 
   testImplementation(project(":misk"))
   testImplementation(project(":misk-rate-limiting-bucket4j-redis"))
+  testImplementation(project(":misk-redis"))
   testImplementation(project(":misk-testing"))
+  testImplementation(project(":wisp:wisp-deployment"))
   testImplementation(testFixtures(project(":misk-redis")))
   testImplementation(testFixtures(project(":wisp:wisp-rate-limiting")))
   testImplementation(libs.assertj)
