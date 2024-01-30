de78b2ebe1da035523a41f5579e621b1ed6e0849
Isaac Wismer <10574297+iwismer@users.noreply.github.com>
2024-01-30 16:06:27 -0500

    Add a way to disable non error request logging (#3121)
    * Add a way to disable non error request logging
    
    This makes changes to the request logging interceptor in order to allow disabling
    logging of non-error requests completely.
    
    Currently, the logging interceptor does not allow you to set the rate limit to 0
    as 0 is used to specify no limit. I didn't want to break backwards compatility to
    change it from 0 to -1 for no limit and 0 for no logging. Instead I've added a
    new config enum that can be used to set it to not log for non-error responses.
    
    The reason that I've added this is that a rate limit of 1 is too high for high
    volume APIs when there are a large number of pods handling requests. The rate
    limiting is done on a per pod basis, so a rate limit of 1/second with 100 pods
    will result in about 8.5million logs per day. I'd like a way to just turn these
    off!
    
    * add a test
    
    * use an enum instead of a bool
    
    * update API
    
    * add revapi changes
    
    * more revapi changes
    
    * rename enum

--- misk/src/main/kotlin/misk/web/interceptors/RequestLoggingInterceptor.kt
+++ misk/src/main/kotlin/misk/web/interceptors/RequestLoggingInterceptor.kt
@@ -36,6 +36,7 @@ class RequestLoggingInterceptor internal constructor(
   private val errorBodySampling: Double,
   private val bodyCapture: RequestResponseCapture,
   private val requestLoggingTransformers: List<RequestLoggingTransformer>,
+  private val requestLoggingMode: RequestLoggingMode,
 ) : NetworkInterceptor {
   @Singleton
   class Factory @Inject internal constructor(
@@ -81,6 +82,7 @@ class RequestLoggingInterceptor internal constructor(
         config.errorBodySampling,
         bodyCapture,
         requestLoggingTransformers,
+        config.requestLoggingMode,
       )
     }
   }
@@ -124,6 +126,10 @@ class RequestLoggingInterceptor internal constructor(
     }
 
     val isError = statusCode > 299 || error != null
+
+    if (!isError && requestLoggingMode == RequestLoggingMode.ERROR_ONLY) {
+      return
+    }
 
     val rateLimit = if (isError) errorRatePerSecond else ratePerSecond
     val loggingBucketId = LogBucketId(actionClass = action.name, isError = isError)
@@ -180,6 +186,7 @@ data class ActionLoggingConfig @JvmOverloads constructor(
   val bodySampling: Double = 0.0,
   val errorBodySampling: Double = 0.0,
   val excludedEnvironments: List<String> = listOf(),
+  val requestLoggingMode: RequestLoggingMode = RequestLoggingMode.ALL,
 ) {
   companion object {
     fun fromAnnotation(logRequestResponse: LogRequestResponse): ActionLoggingConfig = ActionLoggingConfig(
@@ -188,6 +195,7 @@ data class ActionLoggingConfig @JvmOverloads constructor(
       bodySampling = logRequestResponse.bodySampling,
       errorBodySampling = logRequestResponse.errorBodySampling,
       excludedEnvironments = logRequestResponse.excludedEnvironments.toList(),
+      requestLoggingMode = logRequestResponse.requestLoggingMode,
     )
 
     fun fromConfigMapOrAnnotation(
