16371686eed25f851f502bb4885564ecf8517271
Mehdi Mollaverdi <mmollaverdi@squareup.com>
2024-02-06 18:10:13 +1100

    Extract out misk client interfaces and classes into the api module for cross-framework support

--- misk/src/main/kotlin/misk/client/ClientInvocationHandler.kt
+++ misk/src/main/kotlin/misk/client/ClientInvocationHandler.kt
@@ -126,15 +126,6 @@ internal class ClientInvocationHandler(
   }
 }
 
-internal class NetworkInterceptorWrapper(
-  val action: ClientAction,
-  val interceptor: ClientNetworkInterceptor
-) : Interceptor {
-  override fun intercept(chain: Interceptor.Chain): okhttp3.Response {
-    return interceptor.intercept(RealClientNetworkChain(chain, action))
-  }
-}
-
 private val KFunction<*>.isRetrofitMethod: Boolean
   get() {
     return annotations.any {
