241c058537a291059611fbb5cd6f44a8969ea28c
Mehdi Mollaverdi <mmollaverdi@squareup.com>
2024-02-16 23:44:34 +1100

    Make MiskMdc a public object to use in places where MDC is statically used for backwards-compat

--- misk/src/main/kotlin/misk/logging/MdcModule.kt
+++ misk/src/main/kotlin/misk/logging/MdcModule.kt
@@ -1,11 +1,10 @@
 package misk.logging
 
 import misk.inject.KAbstractModule
-import misk.inject.asSingleton
 
 internal class MdcModule : KAbstractModule() {
 
   override fun configure() {
-    bind<Mdc>().to<MiskMdc>().asSingleton()
+    bind<Mdc>().toInstance(MiskMdc)
   }
 }
