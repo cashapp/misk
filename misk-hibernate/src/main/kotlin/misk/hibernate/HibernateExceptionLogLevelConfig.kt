46c52c19280115de412a94f006bee125d26c99f0
Qiyu Zhao <qiyu.zhao@afterpay.com>
2024-02-29 13:42:50 -0800

    Update dependency

--- misk-hibernate/src/main/kotlin/misk/hibernate/HibernateExceptionLogLevelConfig.kt
+++ misk-hibernate/src/main/kotlin/misk/hibernate/HibernateExceptionLogLevelConfig.kt
@@ -2,12 +2,15 @@ package misk.hibernate
 
 import org.slf4j.event.Level
 import wisp.config.Config
+import javax.inject.Singleton
 
 /**
  * Configures the log [Level] for a HibernateException.
  *
  * @property log_level
  */
+
+@Singleton
 data class HibernateExceptionLogLevelConfig @JvmOverloads constructor(
   val log_level: Level = Level.WARN
 ) : Config
