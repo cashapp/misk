692a8ad25ae05e21ec2f2b4b59872f19fbed2444
Qiyu Zhao <56849426+qiyu-zhao@users.noreply.github.com>
2024-03-04 13:29:29 -0800

    Make the HibernateExceptionMapper log level configurable through HibernateModule (#3166)
    * Make the Hibernate exception mapper log level configurable through HibernateModule
    
    * Address comments
    
    * add api
    
    * Accept revapi
    
    * Update dependency
    
    * Remove @Singleton, it's probably redundent

---
+++ misk-hibernate/src/main/kotlin/misk/hibernate/HibernateExceptionLogLevelConfig.kt
@@ -0,0 +1,14 @@
+package misk.hibernate
+
+import org.slf4j.event.Level
+import wisp.config.Config
+
+/**
+ * Configures the log [Level] for a HibernateException.
+ *
+ * @property log_level
+ */
+
+data class HibernateExceptionLogLevelConfig @JvmOverloads constructor(
+  val log_level: Level = Level.WARN
+) : Config
