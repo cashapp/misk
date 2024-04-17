60d085d7009fdf12225ac65a3b02dbf7a4dd4f2e
Andrew Alexander <adrw@squareup.com>
2024-04-17 17:01:20 -0400

    Fix broken API

--- misk-config/src/main/kotlin/misk/web/metadata/MetadataProvider.kt
+++ misk-config/src/main/kotlin/misk/web/metadata/MetadataProvider.kt
@@ -1,6 +1,6 @@
 package misk.web.metadata
 
-import javax.inject.Provider
+import com.google.inject.Provider
 
 interface MetadataProvider<T: Metadata> : Provider<T> {
   /** Unique identifier for the type of metadata. Ie. "web-actions" or "config". */
