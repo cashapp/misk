c381875e8419cc07ea4acce43c3df82bbd2682ee
Ronnie Ghose <1313566+RONNCC@users.noreply.github.com>
2024-04-30 14:53:58 -0700

    Re-add DontRetryException default constructor to fix backwards compat (#3248)

--- misk-core/src/main/kotlin/misk/backoff/Retries.kt
+++ misk-core/src/main/kotlin/misk/backoff/Retries.kt
@@ -39,8 +39,7 @@ fun <A> retry(
 }
 
 class DontRetryException : Exception {
-  constructor(message: String?) : super(message)
+  constructor(message: String? = null) : super(message)
   constructor(cause: Exception?) : super(cause)
   constructor(message: String?, cause: Exception?) : super(message, cause)
 }
-
