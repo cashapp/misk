e3871f0755037d986bf4ebee366d2d5f8c6ec5b5
Mehdi Mollaverdi <mmollaverdi@squareup.com>
2024-06-04 10:51:29 +1000

    Remove the ApiStatus.Experimental annotation from HttpRequest (#3284)

--- misk-api/src/main/kotlin/misk/api/HttpRequest.kt
+++ misk-api/src/main/kotlin/misk/api/HttpRequest.kt
@@ -3,13 +3,11 @@ package misk.api
 import misk.web.DispatchMechanism
 import okhttp3.Headers
 import okhttp3.HttpUrl
-import org.jetbrains.annotations.ApiStatus
 
 /**
  * Represents the current incoming request. Misk makes an instance of this interface available in the action scope
  * with the `HttpRequest` key, for use in interceptors, other action scope providers, etc..
  */
-@ApiStatus.Experimental
 interface HttpRequest {
   val url: HttpUrl
   val dispatchMechanism: DispatchMechanism
