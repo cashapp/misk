490758f726cdcf8a50355b0545a96ab3c2a17b5f
Mehdi Mollaverdi <mmollaverdi@squareup.com>
2024-06-27 23:18:48 +1000

    More detekt failures

--- misk-service/src/main/kotlin/misk/ServiceManagerConfig.kt
+++ misk-service/src/main/kotlin/misk/ServiceManagerConfig.kt
@@ -3,7 +3,7 @@ package misk
 import wisp.config.Config
 
 @Suppress("PropertyName")
-data class ServiceManagerConfig(
+data class ServiceManagerConfig @JvmOverloads constructor(
   /**
    * If true, writes the full graph of [ServiceModule] services and their dependencies to info-level
    * logs.
