f18c1e4b004ab3281ef30def564958d5b55e3851
Jesse Jackson <85544300+jessejacksonafterpay@users.noreply.github.com>
2024-06-28 11:28:55 +1000

    Fixed failing ResourceLoaderTest test (#3343)
    * Fixed failing test
    
    * Additional tests to avoid regression of resource loading regex

--- misk-config/src/test/kotlin/misk/resources/ResourceLoaderTest.kt
+++ misk-config/src/test/kotlin/misk/resources/ResourceLoaderTest.kt
@@ -14,6 +14,11 @@ import okio.sink
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.ValueSource
+import wisp.resources.ClasspathResourceLoaderBackend
+import wisp.resources.FilesystemLoaderBackend
+import wisp.resources.MemoryResourceLoaderBackend
 
 @MiskTest
 class ResourceLoaderTest {
@@ -163,11 +168,16 @@ class ResourceLoaderTest {
     assertFailsWith<IllegalArgumentException> {
       resourceLoader.open(":/")
     }
+  }
+
+  @ParameterizedTest
+  @ValueSource(strings = [ClasspathResourceLoaderBackend.SCHEME, FilesystemLoaderBackend.SCHEME, MemoryResourceLoaderBackend.SCHEME])
+  fun pathBasedAddressValidation(resource: String) {
     assertFailsWith<IllegalArgumentException> {
-      resourceLoader.open("a:/")
+      resourceLoader.open("$resource:/")
     }
     assertFailsWith<IllegalArgumentException> {
-      resourceLoader.open("a://")
+      resourceLoader.open("$resource://")
     }
   }
 
