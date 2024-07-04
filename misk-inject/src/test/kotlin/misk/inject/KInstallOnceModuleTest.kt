bd4f368e1913208a2d2ada2c69655baeee0d97aa
Andrew Alexander <adrw@squareup.com>
2024-07-04 11:54:19 -0400

    Add failure exception assert

--- misk-inject/src/test/kotlin/misk/inject/KInstallOnceModuleTest.kt
+++ misk-inject/src/test/kotlin/misk/inject/KInstallOnceModuleTest.kt
@@ -1,10 +1,14 @@
 package misk.inject
 
+import com.google.inject.CreationException
+import com.google.inject.Guice
 import jakarta.inject.Inject
 import misk.testing.MiskTest
 import misk.testing.MiskTestModule
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
+import kotlin.test.assertEquals
+import kotlin.test.assertFailsWith
 
 @MiskTest
 class KInstallOnceModuleTest {
@@ -18,6 +22,43 @@ class KInstallOnceModuleTest {
     assertThat(map.size).isEqualTo(1)
   }
 
+  @Test fun failure() {
+    val exception = assertFailsWith<CreationException> {
+      Guice.createInjector(TestFailureModule())
+    }
+    assertEquals("""
+      |com.google.inject.CreationException: Unable to create injector, see the following errors:
+      |
+      |1) [Guice/DuplicateMapKey]: Duplicate key "key" found in Map<String, KInstallOnceModuleTest${"$"}TestValue>.
+      |
+      |Duplicates:
+      |  Key: "key"
+      |  Bound at:
+      |    1 : KInstallOnceModuleTest${"$"}TestDuplicateFailureModule.configure(KInstallOnceModuleTest.kt:86)
+      |      \_ installed by: KInstallOnceModuleTest${"$"}TestFailureModule -> KInstallOnceModuleTest${"$"}TestDuplicateFailureModule
+      |    2 : KInstallOnceModuleTest${"$"}TestDuplicateFailureModule.configure(KInstallOnceModuleTest.kt:86)
+      |      \_ installed by: KInstallOnceModuleTest${"$"}TestFailureModule -> KInstallOnceModuleTest${"$"}TestDuplicateFailureModule
+      |
+      |MapBinder declared at:
+      |  KInstallOnceModuleTest${"$"}TestDuplicateFailureModule.configure(KInstallOnceModuleTest.kt:96)
+      |      \_ installed by: KInstallOnceModuleTest${"$"}TestFailureModule -> KInstallOnceModuleTest${"$"}TestDuplicateFailureModule -> RealMapBinder
+      |
+      |1 error
+      |
+      |======================
+      |Full classname legend:
+      |======================
+      |KInstallOnceModuleTest${"$"}TestDuplicateFailureModule: "misk.inject.KInstallOnceModuleTest${"$"}TestDuplicateFailureModule"
+      |KInstallOnceModuleTest${"$"}TestFailureModule:          "misk.inject.KInstallOnceModuleTest${"$"}TestFailureModule"
+      |KInstallOnceModuleTest${"$"}TestValue:                  "misk.inject.KInstallOnceModuleTest${"$"}TestValue"
+      |RealMapBinder:                                     "com.google.inject.internal.RealMapBinder"
+      |========================
+      |End of classname legend:
+      |========================
+      |
+      """.trimMargin(), exception.toString())
+  }
+
   class TestModule : KAbstractModule() {
     override fun configure() {
       install(TestInstallOnceModule("abc"))
@@ -26,6 +67,20 @@ class KInstallOnceModuleTest {
   }
 
   class TestInstallOnceModule(val token: String) : KInstallOnceModule() {
+    override fun configure() {
+      val binder = newMapBinder<String, TestValue>()
+      binder.addBinding("key").toInstance(TestValue(token))
+    }
+  }
+
+  class TestFailureModule : KAbstractModule() {
+    override fun configure() {
+      install(TestDuplicateFailureModule("abc"))
+      install(TestDuplicateFailureModule("123"))
+    }
+  }
+
+  class TestDuplicateFailureModule(val token: String) : KAbstractModule() {
     override fun configure() {
       val binder = newMapBinder<String, TestValue>()
       binder.addBinding("key").toInstance(TestValue(token))
