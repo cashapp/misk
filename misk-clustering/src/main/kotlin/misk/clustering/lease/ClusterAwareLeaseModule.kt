c7e54aaa4603c6c4708d6b10f7154537020633b2
Shivani Katukota <54708104+katukota@users.noreply.github.com>
2024-07-15 17:21:59 -0400

    CCPOKR-13331 | [SQS <> DDB] Implement new "bypass" module
    Currently, SQS requires a LeaseManager setup for Leases, even if you do
    not want to restrict job processing to specific pods. The default Lease
    implementation uses DynamoDb. Adding a new bypass module that has leases
    that always return true for acquire() and checkHeld() if the app is
    running in the active region. This can be used with SQS to ignore the
    required lease injection.
    
    GitOrigin-RevId: 2a40ed1147d882cc892561688225f686038dfd66

---
+++ misk-clustering/src/main/kotlin/misk/clustering/lease/ClusterAwareLeaseModule.kt
@@ -0,0 +1,16 @@
+package misk.clustering.lease
+
+import misk.inject.KAbstractModule
+import wisp.lease.LeaseManager
+
+/**
+ * Configures a LeaseManager that has leases that always return true for acquire() and
+ * checkHeld() if the app is running in the active region.
+ *
+ * This can be used to ignore the lease injection required for Misk SQS Jobs.
+ */
+class ClusterAwareLeaseModule : KAbstractModule() {
+  override fun configure() {
+    bind<LeaseManager>().to<ClusterAwareLeaseManager>()
+  }
+}
---
+++ misk-clustering/src/test/kotlin/misk/clustering/lease/ClusterAwareLeaseTest.kt
@@ -0,0 +1,57 @@
+package misk.clustering.lease
+
+import com.google.inject.Module
+import com.google.inject.util.Modules
+import jakarta.inject.Inject
+import misk.MiskTestingServiceModule
+import misk.clustering.weights.FakeClusterWeight
+import misk.clustering.weights.FakeClusterWeightModule
+import misk.testing.MiskTest
+import misk.testing.MiskTestModule
+import org.junit.jupiter.api.Assertions.assertFalse
+import org.junit.jupiter.api.Assertions.assertNotEquals
+import org.junit.jupiter.api.Assertions.assertNotNull
+import org.junit.jupiter.api.Assertions.assertTrue
+import org.junit.jupiter.api.Test
+import wisp.lease.LeaseManager
+
+@MiskTest
+internal class ClusterAwareLeaseTest {
+  @Suppress("unused")
+  @MiskTestModule val module: Module = Modules.combine(
+    MiskTestingServiceModule(),
+    FakeClusterWeightModule(),
+    ClusterAwareLeaseModule()
+  )
+
+  @Inject private lateinit var leaseManager: LeaseManager
+
+  @Inject private lateinit var clusterWeight: FakeClusterWeight
+
+  @Test fun testActiveCluster() {
+    val lease1 = leaseManager.requestLease("lease1")
+    val lease2 = leaseManager.requestLease("lease2")
+
+    assertNotEquals(lease1, lease2)
+
+    // This should work for other lease managers as well.
+    assertTrue(lease1.acquire())
+    assertTrue(lease1.checkHeld())
+
+    // This will be false for any lease managers other than the ClusterAwareLeaseManager
+    // because lease hasn't been acquired yet.
+    assertTrue(lease2.checkHeld())
+    assertTrue(lease2.acquire())
+  }
+
+  @Test fun testPassiveClusterAwareIgnoreLeaseManager() {
+    clusterWeight.setClusterWeight(0)
+    val lease = leaseManager.requestLease("lease3")
+
+    assertNotNull(lease)
+
+    // Both should be false for ClusterAwareLeaseManager
+    assertFalse(lease.acquire())
+    assertFalse(lease.checkHeld())
+  }
+}
