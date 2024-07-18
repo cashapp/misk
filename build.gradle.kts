f3aa257fa41773248695d83ed24e7a9c626acd33
Kartikaya Gupta (kats) <staktrace@users.noreply.github.com>
2024-07-18 10:09:29 -0400

    Clean up the shard-splitting code
    Instead of having hacky "hibernate1" and "hibernate2" groups inside
    misk, unify them back into a single group. We can use gradle task
    exclusion to run certain tasks in separate shards as needed.
    
    GitOrigin-RevId: 8ad2c94fcf2c6529ff3f87f5daa8fdf153c96a73

--- build.gradle.kts
+++ build.gradle.kts
@@ -131,30 +131,15 @@ val testShardRedis = tasks.register("testShardRedis") {
   description = "These tests use redis and thus can't run in parallel"
 }
 
-val testShardHibernate1 = tasks.register("testShardHibernate1") {
+val testShardHibernate = tasks.register("testShardHibernate") {
   group = "Continuous integration"
   description = "These tests use a DB and thus can't run in parallel"
 }
 
-val testShardHibernate2 = tasks.register("testShardHibernate2") {
-  group = "Continuous integration"
-  description = "These tests use a DB and thus can't run in parallel"
-}
-
-// I've split the hibernate-requiring projects into two groups
-// that should take roughly the same amount of time, based on
-// a recent build scan:
-// https://scans.gradle.com/s/lchid7o4myy7m/tests/overview?toggled=WyI6bWlzay1qZGJjOnRlc3QtMCIsIjptaXNrLWhpYmVybmF0ZTp0ZXN0LTEiLCI6bWlzay1hd3M6dGVzdC0yIiwiOm1pc2stc3FsZGVsaWdodDp0ZXN0LTMiLCI6bWlzay1ldmVudHM6dGVzdC00IiwiOm1pc2stam9icXVldWU6dGVzdC01IiwiOm1pc2stcmF0ZS1saW1pdGluZy1idWNrZXQ0ai1teXNxbDp0ZXN0LTYiXQ
-// If we ever need to fiddle with this again we should do this in
-// a more dynamic way.
-
-val hibernateProjects1 = listOf(
+val hibernateProjects = listOf(
   "misk-aws",
-  "misk-hibernate",
-)
-
-val hibernateProjects2 = listOf(
   "misk-events",
+  "misk-hibernate",
   "misk-jobqueue",
   "misk-jobqueue-testing",
   "misk-jdbc",
@@ -275,10 +260,8 @@ subprojects {
 
   plugins.withType<BasePlugin> {
     val subproj = project
-    if (hibernateProjects1.contains(project.name)) {
-      testShardHibernate1.configure { dependsOn("${subproj.path}:check") }
-    } else if (hibernateProjects2.contains(project.name)) {
-      testShardHibernate2.configure { dependsOn("${subproj.path}:check") }
+    if (hibernateProjects.contains(project.name)) {
+      testShardHibernate.configure { dependsOn("${subproj.path}:check") }
     } else if (redisProjects.contains(project.name)) {
       testShardRedis.configure { dependsOn("${subproj.path}:check") }
     } else {
