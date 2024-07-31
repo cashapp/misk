f4c9b337f7ae640965a59867f47d10d45a9ae353
Shivani Katukota <54708104+katukota@users.noreply.github.com>
2024-07-31 18:06:22 -0400

    Fix flaky test traceHopsFromClientToServerToServer
    GitOrigin-RevId: 6921856babfe76debca75cc20d2c5c6ca3cdf582

--- misk-testing/src/main/kotlin/misk/testing/ConcurrentMockTracer.kt
+++ misk-testing/src/main/kotlin/misk/testing/ConcurrentMockTracer.kt
@@ -6,7 +6,6 @@ import java.util.concurrent.LinkedBlockingDeque
 import java.util.concurrent.TimeUnit
 import jakarta.inject.Inject
 import jakarta.inject.Singleton
-import wisp.logging.getLogger
 
 /**
  * Extends [MockTracer] for use in concurrent environments, such as a web server and test client.
@@ -20,7 +19,7 @@ class ConcurrentMockTracer @Inject constructor() : MockTracer() {
 
   /** Awaits a span, removes it, and returns it. */
   fun take(): MockSpan {
-    return queue.poll(1000, TimeUnit.MILLISECONDS) ?: throw IllegalArgumentException("no spans!")
+    return queue.poll(500, TimeUnit.MILLISECONDS) ?: throw IllegalArgumentException("no spans!")
   }
 
   /**
@@ -31,16 +30,11 @@ class ConcurrentMockTracer @Inject constructor() : MockTracer() {
     while (true) {
       val span = take()
       if (span.operationName() == operationName) return span
-      else log.info("Span dropped: ${span.operationName()} \n" )
     }
   }
 
   override fun onSpanFinished(mockSpan: MockSpan) {
     super.onSpanFinished(mockSpan)
     queue.put(mockSpan)
-  }
-
-  companion object {
-    private val log = getLogger<ConcurrentMockTracer>()
   }
 }
