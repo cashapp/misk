7c32ffd509921d932bfa762955fa435e8ffd5c5b
Mehdi Mollaverdi <mmollaverdi@squareup.com>
2024-08-06 09:31:43 +1000

    Implement TestFixture for log collectors
    GitOrigin-RevId: 453df97428102c5f1959afd94c83d52623e8387c

--- wisp/wisp-logging-testing/build.gradle.kts
+++ wisp/wisp-logging-testing/build.gradle.kts
@@ -5,12 +5,13 @@ plugins {
 }
 
 dependencies {
-    api(libs.logbackClassic)
-    api(libs.assertj)
-    implementation(libs.logbackCore)
-    implementation(libs.slf4jApi)
+  api(project(":misk-testing-api"))
+  api(libs.logbackClassic)
+  api(libs.assertj)
+  implementation(libs.logbackCore)
+  implementation(libs.slf4jApi)
 
-    testImplementation(libs.junitApi)
-    testImplementation(libs.kotlinTest)
-    testImplementation(project(":wisp:wisp-logging"))
+  testImplementation(libs.junitApi)
+  testImplementation(libs.kotlinTest)
+  testImplementation(project(":wisp:wisp-logging"))
 }
