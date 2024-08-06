1eab6621223c128344429b3d07cf7dd13e73717f
David Amar <100435712+damar-block@users.noreply.github.com>
2024-08-06 13:16:09 -0700

    Support more source formats and Guice explorer
    GitOrigin-RevId: b6a255f455c40ef5e21059ab5582a939be572b35

--- misk-admin/src/test/kotlin/misk/web/metadata/guice/GitHubSourceUrlProviderTest.kt
+++ misk-admin/src/test/kotlin/misk/web/metadata/guice/GitHubSourceUrlProviderTest.kt
@@ -7,8 +7,8 @@ class GitHubSourceUrlProviderTest {
   @Test
   fun testUrlForSource() {
     verifyUrl(
-      "com.squareup.skim.dashboard.SkimDashboardModule.configure(SkimDashboardModule.kt:23)",
-      "https://github.com/search?q=%22package+com.squareup.skim.dashboard%22+SkimDashboardModule+configure&type=code"
+      "com.squareup.misk.dashboard.MiskDashboardModule.configure(MiskDashboardModule.kt:23)",
+      "https://github.com/search?q=%22package+com.squareup.misk.dashboard%22+MiskDashboardModule+configure&type=code"
     )
   }
 
@@ -25,6 +25,46 @@ class GitHubSourceUrlProviderTest {
     verifyUrl(
       "package.Class\$InnerClass.function(Class.kt:23)",
       "https://github.com/search?q=%22package+package%22+Class+InnerClass+function&type=code"
+    )
+  }
+
+  @Test
+  fun testUrlForFunction() {
+    verifyUrl(
+      "public final com.squareup.wire.reflector.SchemaReflector misk.grpc.reflect.GrpcReflectModule.provideServiceReflector(com.squareup.wire.schema.Schema, com.squareup.wire.schema.Schema)",
+      "https://github.com/search?q=%22package+misk.grpc.reflect%22+GrpcReflectModule+provideServiceReflector&type=code"
+    )
+  }
+
+  @Test
+  fun testUrlForFunctionWithDollar() {
+    verifyUrl(
+      "public final com.squareup.wire.reflector.SchemaReflector misk.grpc.reflect.GrpcReflectModule.provideServiceReflector\$service(com.squareup.wire.schema.Schema, com.squareup.wire.schema.Schema)",
+      "https://github.com/search?q=%22package+misk.grpc.reflect%22+GrpcReflectModule+provideServiceReflector&type=code"
+    )
+  }
+
+  @Test
+  fun testUrlForFunctionWithNoArguments() {
+    verifyUrl(
+      "public final com.squareup.wire.reflector.SchemaReflector misk.grpc.reflect.GrpcReflectModule.provideServiceReflector()",
+      "https://github.com/search?q=%22package+misk.grpc.reflect%22+GrpcReflectModule+provideServiceReflector&type=code"
+    )
+  }
+
+  @Test
+  fun testUrlForClass() {
+    verifyUrl(
+      "class com.squareup.skim.logging.SkimLoggingStartupCheck",
+      "https://github.com/search?q=%22package+com.squareup.skim.logging%22+SkimLoggingStartupCheck&type=code"
+    )
+  }
+
+  @Test
+  fun testUrlForInnerClass() {
+    verifyUrl(
+      "class misk.metrics.MetricsModule\$V2MetricsProvider",
+      "https://github.com/search?q=%22package+misk.metrics%22+MetricsModule+V2MetricsProvider&type=code"
     )
   }
 
