30a98f93f2db47340f1e0041fd750e9a146a10b8
Terry Yiu <963907+tyiu@users.noreply.github.com>
2024-08-07 18:58:04 -0400

    Fix InProcessDynamoDbTests for Apple Silicon
    GitOrigin-RevId: 294c578de959770de4a3b1de8d99c5b122b85984

--- misk-clustering-dynamodb/build.gradle.kts
+++ misk-clustering-dynamodb/build.gradle.kts
@@ -27,8 +27,9 @@ dependencies {
   testImplementation(testFixtures(project(":misk-aws2-dynamodb")))
 
   if (org.apache.tools.ant.taskdefs.condition.Os.isArch("aarch64")) {
-    // Without this, we can't compile on Apple Silicon currently. This is likely not necessary to
-    // have longterm, so we should remove it when platform fixes things across Square.
+    // Without this, we can't compile on Apple Silicon currently.
+    // This is likely not necessary to have long term,
+    // so we should remove it when things get fixed upstream.
     testImplementation("io.github.ganadist.sqlite4java:libsqlite4java-osx-aarch64:1.0.392")
   }
 
