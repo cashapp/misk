c10e0fec1cc1e05e4c11256003aafa4316a30eb4
Mehdi Mollaverdi <mmollaverdi@squareup.com>
2024-08-14 10:25:17 +1000

    Injector reuse - support test modules with constructor arguments
    GitOrigin-RevId: 05141a51cfaa6cb608383b5aa7d163b72347b114

---
+++ misk-inject/misk-inject-guice7-test/src/test/kotlin/misk/inject/ReusableTestModuleTest.kt
@@ -0,0 +1,42 @@
+package misk.inject
+
+import misk.annotation.ExperimentalMiskApi
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Assertions.assertNotEquals
+import org.junit.jupiter.api.Test
+
+@OptIn(ExperimentalMiskApi::class)
+class ReusableTestModuleTest {
+
+  class NoArgTestModuleOne: ReusableTestModule()
+  class NoArgTestModuleTwo: ReusableTestModule()
+  class OneArgTestModuleOne(private val installX: Boolean): ReusableTestModule()
+  class OneArgTestModuleTwo(private val someValue: Int): ReusableTestModule()
+  class TwoArgsTestModuleOne(private val installX: Boolean, installY: Boolean): ReusableTestModule()
+
+  @Test
+  fun testEquality() {
+    assertEquals(NoArgTestModuleOne(), NoArgTestModuleOne())
+    assertNotEquals(NoArgTestModuleOne(), NoArgTestModuleTwo())
+
+    assertEquals(OneArgTestModuleOne(true), OneArgTestModuleOne(true))
+    assertNotEquals(OneArgTestModuleOne(true), OneArgTestModuleOne(false))
+    assertNotEquals(OneArgTestModuleTwo(1), OneArgTestModuleTwo(2))
+
+    assertEquals(TwoArgsTestModuleOne(true, false), TwoArgsTestModuleOne(true, false))
+    assertNotEquals(TwoArgsTestModuleOne(true, false), TwoArgsTestModuleOne(false, false))
+  }
+
+  @Test
+  fun testHashcode() {
+    assertEquals(NoArgTestModuleOne().hashCode(), NoArgTestModuleOne().hashCode())
+    assertNotEquals(NoArgTestModuleOne().hashCode(), NoArgTestModuleTwo().hashCode())
+
+    assertEquals(OneArgTestModuleOne(true).hashCode(), OneArgTestModuleOne(true).hashCode())
+    assertNotEquals(OneArgTestModuleOne(true).hashCode(), OneArgTestModuleOne(false).hashCode())
+    assertNotEquals(OneArgTestModuleTwo(1).hashCode(), OneArgTestModuleTwo(2).hashCode())
+
+    assertEquals(TwoArgsTestModuleOne(true, false).hashCode(), TwoArgsTestModuleOne(true, false).hashCode())
+    assertNotEquals(TwoArgsTestModuleOne(true, false).hashCode(), TwoArgsTestModuleOne(false, false).hashCode())
+  }
+}
