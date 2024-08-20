2fd946011cfe6ddc42bd0ece97d9e0ef76a95b16
Meghan Stewart <132411442+meghans-cash@users.noreply.github.com>
2024-08-20 09:31:28 -0700

    Add flush_interval to the LaunchDarklyConfig, default to the default flush interval to allow configuring how often events are sent
    GitOrigin-RevId: 5e36390bc9fe80da834a7b8baf036591e85536fa

--- wisp/wisp-launchdarkly/src/main/kotlin/wisp/launchdarkly/LaunchDarklyConfig.kt
+++ wisp/wisp-launchdarkly/src/main/kotlin/wisp/launchdarkly/LaunchDarklyConfig.kt
@@ -1,13 +1,16 @@
 package wisp.launchdarkly
 
 import com.launchdarkly.sdk.server.integrations.EventProcessorBuilder.DEFAULT_CAPACITY
+import com.launchdarkly.sdk.server.integrations.EventProcessorBuilder.DEFAULT_FLUSH_INTERVAL
 import wisp.client.HttpClientSSLConfig
 import wisp.config.Config
+import java.time.Duration
 
 data class LaunchDarklyConfig @JvmOverloads constructor(
     val sdk_key: String,
     val base_uri: String,
     val ssl: HttpClientSSLConfig? = null,
     val offline: Boolean = false,
-    val event_capacity: Int = DEFAULT_CAPACITY
+    val event_capacity: Int = DEFAULT_CAPACITY,
+    val flush_interval: Duration = DEFAULT_FLUSH_INTERVAL
 ) : Config
