e1384413acd0f5fd0642f07cfb1a1f81b5c7e4b4
Riz Versfeld <riz@squareup.com>
2024-09-03 10:47:39 -0700

    BatchJobHandler API in misk-jobqueue & SQS implementation
    COPYBARA_INTEGRATE_REVIEW=https://github.com/cashapp/misk/pull/3349 from
    GitOrigin-RevId: bbc24843483f4ffd5f2c74a1ab0fc8c590c2a83f

--- misk-aws/src/main/kotlin/misk/jobqueue/sqs/AwsSqsJobQueueModule.kt
+++ misk-aws/src/main/kotlin/misk/jobqueue/sqs/AwsSqsJobQueueModule.kt
@@ -61,7 +61,7 @@ open class AwsSqsJobQueueModule(
     install(
       ExecutorServiceModule.withUnboundThreadPool(
         ForSqsReceiving::class,
-        "sqs-receiver"
+        "sqs-receiver-%d"
       )
     )
 
