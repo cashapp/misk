c92b75d67418abfa21580c9b93f7ac58d2452f9c
Adam Erb <aerb@squareup.com>
2024-10-16 14:25:21 -0230

    Add misk admin v4 as dependency of misk-admin build
    GitOrigin-RevId: be8657b06a6d52d68b00e117a1ff4db67aad622a

---
+++ misk-admin/web/tabs/web-actions-v4/webpack.config.js
@@ -0,0 +1,55 @@
+const path = require('path');
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+const TsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin');
+
+module.exports = {
+  mode: 'development',
+  entry: path.resolve(__dirname, 'src/index.tsx'),
+  module: {
+    rules: [
+      {
+        test: /\.(ts|tsx)$/,
+        use: "babel-loader",
+        exclude: /node_modules/
+      },{
+        test: /\.html$/,
+        use: 'html-loader'
+      }
+    ],
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js',],
+    alias: {
+      '@misk-console': path.resolve(__dirname, 'src/misk-console/')
+    },
+    plugins: [new TsconfigPathsPlugin({ configFile: "./tsconfig.json" })],
+  },
+  performance: {
+    maxAssetSize: 2000000,
+    maxEntrypointSize: 2000000,
+  },
+  plugins: [
+    new HtmlWebPackPlugin({
+      template: path.resolve(__dirname, 'src/index.html'),
+      filename: "./index.html"
+    })
+  ],
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'lib/web/_tab/web-actions-v4'),
+  },
+  devServer: {
+    static: {
+      directory: path.join(__dirname, 'lib/web/_tab/web-actions-v4'),
+    },
+    port: 9000,
+    open: true,
+    hot: true,
+    proxy: [
+      {
+        context: ['/api', '/squareup'],
+        target: 'http://127.0.0.1:8080',
+      },
+    ],
+  },
+};
