89eb54358329f566bf97d1c04fd7c1da2ecca62f
Raine <135167440+rainecp@users.noreply.github.com>
2025-01-06 10:32:20 -0800

    Implement FakeFixture for FakeTicker and FakeSleeper
    GitOrigin-RevId: a8328958a75444a42231a5a108c3abcced52688c

--- misk-testing/src/main/kotlin/misk/concurrent/FakeSleeperModule.kt
+++ misk-testing/src/main/kotlin/misk/concurrent/FakeSleeperModule.kt
@@ -1,9 +1,11 @@
 package misk.concurrent
 
 import misk.inject.KAbstractModule
+import misk.testing.TestFixture
 
 class FakeSleeperModule : KAbstractModule() {
   override fun configure() {
     bind<Sleeper>().to<FakeSleeper>()
+    multibind<TestFixture>().to<FakeSleeper>()
   }
 }
