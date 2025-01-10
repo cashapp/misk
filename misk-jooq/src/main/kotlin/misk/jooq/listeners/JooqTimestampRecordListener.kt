bac16769d5c10ae39ff2fea2c21064965963128f
Sethuraman <sethuraman@squareup.com>
2025-01-10 15:15:26 +1100

    Do not set timestamps if already set
    GitOrigin-RevId: 07c5fc448c43ae922af1f4c32c1f479fa5b59ba7

--- misk-jooq/src/main/kotlin/misk/jooq/listeners/JooqTimestampRecordListener.kt
+++ misk-jooq/src/main/kotlin/misk/jooq/listeners/JooqTimestampRecordListener.kt
@@ -2,9 +2,12 @@ package misk.jooq.listeners
 
 import misk.jooq.toLocalDateTime
 import org.jooq.RecordContext
+import org.jooq.RecordListener
 import org.jooq.impl.DSL
-import org.jooq.impl.DefaultRecordListener
 import java.time.Clock
+import java.time.Instant
+import java.time.LocalDateTime
+import java.time.temporal.ChronoUnit
 
 /**
  * A Record Listener that will automatically set the current timestamp for the [createdAtColumnName]
@@ -14,21 +17,41 @@ class JooqTimestampRecordListener(
   private val clock: Clock,
   private val createdAtColumnName: String,
   private val updatedAtColumnName: String,
-) : DefaultRecordListener() {
+) : RecordListener {
 
   override fun updateStart(ctx: RecordContext?) {
-    if (ctx?.record()?.field(updatedAtColumnName) != null) {
-      ctx.record().set(DSL.field(updatedAtColumnName), clock.instant().toLocalDateTime())
-    }
+    setTime(ctx, updatedAtColumnName)
   }
 
   override fun insertStart(ctx: RecordContext?) {
-    if (ctx?.record()?.field(createdAtColumnName) != null) {
-      ctx.record().set(DSL.field(createdAtColumnName), clock.instant().toLocalDateTime())
+    setTime(ctx, createdAtColumnName)
+    setTime(ctx, updatedAtColumnName)
+  }
+
+  private fun setTime(ctx: RecordContext?, columnName: String) {
+    if (
+      ctx?.record()?.field(columnName) != null &&
+      (ctx.record().get(DSL.field(columnName)) == null || !ctx.record().changed(DSL.field(columnName)))
+    ) {
+      val precision = ctx.recordType().field(DSL.field(columnName))!!.dataType.precision()
+      ctx.record().set(DSL.field(columnName), clock.instant().truncateBasedOnPrecision(precision))
     }
-    if (ctx?.record()?.field(updatedAtColumnName) != null) {
-      ctx.record().set(DSL.field(updatedAtColumnName), clock.instant().toLocalDateTime())
-    }
+  }
+
+  /**
+   * MySQL's precision for a timestamp is millis. But in the Kube Pod, where the code runs the JVM timestamp is in
+   * nanos. So when we store the data, the signature is computed with nanos, but when we load the data from the DB, the
+   * nanos are lost. It is best we keep the same precision when we store and load the data. This method truncates the
+   * instant based on the precision. The check with precision is required to be able to test this on a MAC. Mac JVM's
+   * precision is millis. So in order to test truncation we need to create a mysql timestamp with a precision of 0. This
+   * also allows this signature to work for any column created in prod where the precision is 0 (in the sense,
+   * restricted to store seconds alone).
+   */
+  private fun Instant.truncateBasedOnPrecision(precision: Int): LocalDateTime {
+    return when {
+      precision < 3 -> truncatedTo(ChronoUnit.SECONDS)
+      else -> truncatedTo(ChronoUnit.MILLIS)
+    }.toLocalDateTime()
   }
 }
 
