5eb2c8564df795b1ebe14c82d4b301baf3ea6c55
Jeff Clyne <jclyne@squareup.com>
2025-03-07 09:20:47 -0600

    Adds generic return value for scoped MDC logging calls
    GitOrigin-RevId: 5132d20842a1c8991d654f5e6381257075e042d4

--- misk/src/main/kotlin/misk/logging/coroutines/ScopedMdc.kt
+++ misk/src/main/kotlin/misk/logging/coroutines/ScopedMdc.kt
@@ -4,25 +4,24 @@ import kotlinx.coroutines.slf4j.MDCContext
 import kotlinx.coroutines.withContext
 import misk.logging.Mdc
 import mu.KotlinLogging
-import wisp.logging.getLogger
 import kotlin.coroutines.coroutineContext
 
 /**
  * Adds the given key, value pair to the MDC for the duration of the block.
  * This is coroutine safe, so the additions will be added to the coroutine context
  */
-suspend inline fun Mdc.withMdc(key: String, value: String, crossinline block: suspend () -> Unit) =
+suspend inline fun <R> Mdc.withMdc(key: String, value: String, crossinline block: suspend () -> R) =
   withMdc(key to value, block = block)
 
 /**
  * Adds the given tags to the MDC for the duration of the block.
  * This is coroutine safe, so the additions will be added to the coroutine context
  */
-suspend inline fun Mdc.withMdc(
+suspend inline fun <R> Mdc.withMdc(
   vararg tags: Pair<String, String>,
-  crossinline block: suspend () -> Unit
-) {
-  if(coroutineContext[MDCContext] == null) {
+  crossinline block: suspend () -> R
+): R {
+  if (coroutineContext[MDCContext] == null) {
     KotlinLogging.logger("misk.logging.coroutines.ScopedMdc").warn {
       "MDCContext is not present in the coroutine context, this is required to restore the previous MDC state"
     }
