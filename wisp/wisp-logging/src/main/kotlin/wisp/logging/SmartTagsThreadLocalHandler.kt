a9621e9193f40a4aad279a9d81b17fe0a36e9343
afkelsall <56988739+afkelsall@users.noreply.github.com>
2025-03-17 14:18:40 +1100

    Enhance `smartTags` to have the ability to peek the tags without removing them from `ThreadLocal` context. Use case is to catch and log an exception within nested `withSmartTags` blocks and include the tags that were present when exception was thrown.
    GitOrigin-RevId: 71b1bb410839c8594a9e989242ff7d2b5a827c3c

--- wisp/wisp-logging/src/main/kotlin/wisp/logging/SmartTagsThreadLocalHandler.kt
+++ wisp/wisp-logging/src/main/kotlin/wisp/logging/SmartTagsThreadLocalHandler.kt
@@ -13,15 +13,25 @@ object SmartTagsThreadLocalHandler {
    * Note: the thread local storage is only populated when an exception is thrown within a
    * `withSmartTags()` block.
    */
-  fun popThreadLocalSmartTags() = threadLocalMdcContext
+  fun popThreadLocalSmartTags() = peekThreadLocalSmartTags()
+    .also { clearSmartTags() }
+
+  /**
+   * Retrieves all the logging MDC tags that were added to the logger via `withSmartTags()` but
+   * leaves them in place. Useful for catching and logging exceptions within a nested withSmartTags
+   * block.
+   *
+   * Note: the thread local storage is only populated when an exception is thrown within a
+   * `withSmartTags()` block.
+   */
+  fun peekThreadLocalSmartTags() = threadLocalMdcContext
     .get()
     ?.tags
-    ?.also { threadLocalMdcContext.remove() }
     ?: emptySet()
 
-  internal fun clear() = threadLocalMdcContext.remove()
+  private fun clearSmartTags() = threadLocalMdcContext.remove()
 
-  private fun setOrAppendTags(exception: Exception, tags: Set<Tag>) {
+  private fun setOrAppendSmartTags(exception: Exception, tags: Set<Tag>) {
     val existingContext = threadLocalMdcContext.get()
 
     if (existingContext == null || !existingContext.wasTriggeredBy(exception)) {
@@ -37,13 +47,13 @@ object SmartTagsThreadLocalHandler {
         // Exiting this block gracefully: Lets do some cleanup to keep the ThreadLocal clear.
         // The scenario here is that when nested `withSmartTags` threw an exception and it was
         // caught and handled by this `withSmartTags`, it should clean up the unused and unneeded context.
-        SmartTagsThreadLocalHandler.clear()
+        clearSmartTags()
       }
     } catch (e: Exception) {
       // Calls to `withSmartTags` can be nested - only set if there is not already a context set
       // This will be cleared upon logging of the exception within misk or if the thrown exception
       // is handled by a higher level `withSmartTags`
-      SmartTagsThreadLocalHandler.setOrAppendTags(e, tags.toSet())
+      setOrAppendSmartTags(e, tags.toSet())
 
       throw e
     }
