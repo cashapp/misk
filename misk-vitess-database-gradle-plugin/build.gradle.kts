544d4a17bfba1f56cc004bb6d69c1c3957be46f9
Armand Parajon <83522714+aparajon@users.noreply.github.com>
2025-03-21 16:37:09 -0400

    Add a new Misk module which includes a Gradle plugin for starting a Vitess test database
    GitOrigin-RevId: 3b4699e9df7c4d83ba6fdf340de4e0e9503a77cc

---
+++ misk-vitess-database-gradle-plugin/build.gradle.kts
@@ -0,0 +1,32 @@
+import com.vanniktech.maven.publish.GradlePlugin
+import com.vanniktech.maven.publish.JavadocJar
+
+plugins {
+  id("org.jetbrains.kotlin.jvm")
+  id("com.vanniktech.maven.publish.base")
+  id("java-gradle-plugin")
+}
+
+gradlePlugin {
+  plugins {
+    create("VitessDatabasePlugin") {
+      id = "com.squareup.misk.vitess.vitess-database"
+      implementationClass = "misk.vitess.gradle.VitessDatabasePlugin"
+    }
+  }
+}
+
+dependencies {
+  implementation(testFixtures(project(":misk-vitess")))
+  testImplementation(gradleTestKit())
+  testImplementation(libs.assertj)
+  testImplementation(libs.junitApi)
+  testImplementation(libs.junitParams)
+  testImplementation(libs.mysqlConnector)
+}
+
+mavenPublishing {
+  configure(
+    GradlePlugin(javadocJar = JavadocJar.Dokka("dokkaGfm"))
+  )
+}
---
+++ misk-vitess-database-gradle-plugin/src/main/kotlin/misk/vitess/gradle/StartVitessDatabaseTask.kt
@@ -0,0 +1,77 @@
+package misk.vitess.gradle
+
+import misk.vitess.testing.VitessTestDb
+import org.gradle.api.DefaultTask
+import org.gradle.api.provider.Property
+import org.gradle.api.tasks.Input
+import org.gradle.api.tasks.TaskAction
+import org.gradle.api.tasks.UntrackedTask
+import java.time.Duration
+
+@UntrackedTask(because = "Every task run re-initializes the database if needed")
+abstract class StartVitessDatabaseTask: DefaultTask() {
+
+  @get:Input
+  abstract val autoApplySchemaChanges: Property<Boolean>
+
+  @get:Input
+  abstract val containerName: Property<String>
+
+  @get:Input
+  abstract val debugStartup: Property<Boolean>
+
+  @get:Input
+  abstract val enableDeclarativeSchemaChanges: Property<Boolean>
+
+  @get:Input
+  abstract val enableScatters: Property<Boolean>
+
+  @get:Input
+  abstract val keepAlive: Property<Boolean>
+
+  @get:Input
+  abstract val lintSchema: Property<Boolean>
+
+  @get:Input
+  abstract val mysqlVersion: Property<String>
+
+  @get:Input
+  abstract val port: Property<Int>
+
+  @get:Input
+  abstract val schemaDir: Property<String>
+
+  @get:Input
+  abstract val sqlMode: Property<String>
+
+  @get:Input
+  abstract val transactionTimeoutSeconds: Property<Duration>
+
+  @get:Input
+  abstract val vitessImage: Property<String>
+
+  @get:Input
+  abstract val vitessVersion: Property<Int>
+
+  @TaskAction
+  fun start() {
+    val vitessTestDb = VitessTestDb(
+      autoApplySchemaChanges = autoApplySchemaChanges.get(),
+      containerName = containerName.get(),
+      debugStartup = debugStartup.get(),
+      enableDeclarativeSchemaChanges = enableDeclarativeSchemaChanges.get(),
+      enableScatters = enableScatters.get(),
+      keepAlive = keepAlive.get(),
+      lintSchema = lintSchema.get(),
+      mysqlVersion = mysqlVersion.get(),
+      port = port.get(),
+      schemaDir = schemaDir.get(),
+      sqlMode = sqlMode.get(),
+      transactionTimeoutSeconds = transactionTimeoutSeconds.get(),
+      vitessImage = vitessImage.get(),
+      vitessVersion = vitessVersion.get()
+    )
+
+    vitessTestDb.run()
+  }
+}
