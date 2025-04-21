20afe189907f2753783c55bffd9fcea6b12fa32b
Armand Parajon <83522714+aparajon@users.noreply.github.com>
2025-04-21 12:23:41 -0400

    Harden Vitess Destination parse logic
    GitOrigin-RevId: 3cdb4bf5fcae691a1faad6ad317d60a00e922072

--- misk-jdbc/src/main/kotlin/misk/vitess/TabletType.kt
+++ misk-jdbc/src/main/kotlin/misk/vitess/TabletType.kt
@@ -1,3 +1,21 @@
 package misk.vitess
 
-enum class TabletType(val value: Int) { PRIMARY(1), REPLICA(2) }
+enum class TabletType(val value: Int) {
+  PRIMARY(1),
+  REPLICA(2);
+
+  fun toDestinationQualifier(): String = "@${name.lowercase()}"
+
+  companion object {
+    private val destinationQualifierToTabletType = mapOf(
+      "primary" to PRIMARY,
+      "master" to PRIMARY, // `@master` is deprecated, but we support it for backwards compatibility
+      "replica" to REPLICA
+    )
+
+    fun fromDestinationQualifier(destinationQualifier: String): TabletType {
+      return destinationQualifierToTabletType[destinationQualifier.lowercase()]
+        ?: throw IllegalArgumentException("Invalid destination qualifier: `$destinationQualifier`, must be one of: [`@primary`, `@replica`]")
+    }
+  }
+}
--- misk-jdbc/src/test/kotlin/misk/vitess/DestinationTest.kt
+++ misk-jdbc/src/test/kotlin/misk/vitess/DestinationTest.kt
@@ -1,27 +1,100 @@
 package misk.vitess
 
 import org.assertj.core.api.Assertions
+import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 
 class DestinationTest {
   @Test
-  fun parse() {
-    Assertions.assertThat(Destination.parse("").isBlank()).isTrue()
-    Assertions.assertThat(Destination.parse("@primary").tabletType)
-      .isEqualTo(TabletType.PRIMARY)
-    Assertions.assertThat(Destination.parse("@master").tabletType)
-      .isEqualTo(TabletType.PRIMARY)
-    Assertions.assertThat(Destination.parse("ks/-80").shard)
-        .isEqualTo(Shard(Keyspace("ks"), "-80"))
-    Assertions.assertThat(Destination.parse("ks/-80@replica").tabletType)
-        .isEqualTo(TabletType.REPLICA)
+  fun parsePrimaryDestination() {
+    val primaryDestination = Destination.parse("@primary")
+    assertThat(primaryDestination.tabletType).isEqualTo(TabletType.PRIMARY)
+    assertThat(primaryDestination.keyspace).isNull()
+    assertThat(primaryDestination.shard).isNull()
+
+    val backwardsCompatibleDestination = Destination.parse("@master")
+    assertThat(backwardsCompatibleDestination.tabletType).isEqualTo(TabletType.PRIMARY)
+    assertThat(backwardsCompatibleDestination.keyspace).isNull()
+    assertThat(backwardsCompatibleDestination.shard).isNull()
+  }
+
+  @Test
+  fun parseReplicaDestination() {
+    val destination = Destination.parse("@replica")
+    assertThat(destination.tabletType).isEqualTo(TabletType.REPLICA)
+    assertThat(destination.keyspace).isNull()
+    assertThat(destination.shard).isNull()
+  }
+
+  @Test
+  fun parseKeyspaceWithShardDestination() {
+    val destination = Destination.parse("ks/-80")
+    assertThat(destination.tabletType).isNull()
+    assertThat(destination.keyspace).isEqualTo(Keyspace("ks"))
+    assertThat(destination.shard).isEqualTo(Shard(Keyspace("ks"), "-80"))
+  }
+
+  @Test
+  fun parseKeyspaceWithShardDestinationAndQualifier() {
+    val destination = Destination.parse("ks/-80@replica")
+    assertThat(destination.tabletType).isEqualTo(TabletType.REPLICA)
+    assertThat(destination.keyspace).isEqualTo(Keyspace("ks"))
+    assertThat(destination.shard).isEqualTo(Shard(Keyspace("ks"), "-80"))
+  }
+
+  @Test
+  fun parseOnlyKeyspace() {
+    val destination = Destination.parse("ks")
+    assertThat(destination.tabletType).isNull()
+    assertThat(destination.keyspace).isEqualTo(Keyspace("ks"))
+    assertThat(destination.shard).isNull()
+  }
+
+  @Test
+  fun parseBlankCatalogString() {
+    val blankDestination = Destination.parse("")
+    assertThat(blankDestination.isBlank()).isTrue()
+    assertThat(blankDestination.shard).isNull()
+    assertThat(blankDestination.tabletType).isNull()
+    assertThat(blankDestination.keyspace).isNull()
+  }
+
+  @Test
+  fun parseInvalidDestinationQualifier() {
+    val exception = Assertions.catchThrowable {
+      Destination.parse("@invalid")
+    }
+    assertThat(exception)
+      .isInstanceOf(IllegalArgumentException::class.java)
+      .hasMessageContaining("Invalid destination qualifier")
+  }
+
+  @Test
+  fun primary() {
+    val primaryDestination = Destination.primary()
+    assertThat(primaryDestination.tabletType).isEqualTo(TabletType.PRIMARY)
+    assertThat(primaryDestination.keyspace).isNull()
+    assertThat(primaryDestination.shard).isNull()
+    assertThat("$primaryDestination").isEqualTo("@primary")
+  }
+
+  @Test
+  fun replica() {
+    val replicaDestination = Destination.replica()
+    assertThat(replicaDestination.tabletType).isEqualTo(TabletType.REPLICA)
+    assertThat(replicaDestination.keyspace).isNull()
+    assertThat(replicaDestination.shard).isNull()
+    assertThat("$replicaDestination").isEqualTo("@replica")
   }
 
   @Test
   fun testToString() {
-    Assertions.assertThat(Destination(null, null, null).toString()).isEqualTo("")
-    Assertions.assertThat(Destination(Shard(Keyspace("ks"), "-80")).toString()).isEqualTo("ks/-80")
-    Assertions.assertThat(Destination(Shard(Keyspace("ks"), "-80"), TabletType.REPLICA).toString()).isEqualTo("ks/-80@replica")
-    Assertions.assertThat(Destination(Keyspace("ks"), null, TabletType.REPLICA).toString()).isEqualTo("ks@replica")
+    assertThat("${Destination(null, null, null)}").isEqualTo("")
+    assertThat("${Destination(null, null, TabletType.PRIMARY)}").isEqualTo("@primary")
+    assertThat("${Destination(null, null, TabletType.REPLICA)}").isEqualTo("@replica")
+    assertThat("${Destination(Shard(Keyspace("ks"), "-80"))}").isEqualTo("ks/-80")
+    assertThat("${Destination(Shard(Keyspace("ks"), "-80"), TabletType.REPLICA)}").isEqualTo("ks/-80@replica")
+    assertThat("${Destination(Keyspace("ks"), null, TabletType.REPLICA)}").isEqualTo("ks@replica")
+    assertThat("${Destination(Keyspace("ks"), null, null)}").isEqualTo("ks")
   }
 }
