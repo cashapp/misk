a2dbad44fd2d4ad9d8691fe219c8735942f9629c
github-cash-renovate-jvm-2 <127262513+github-cash-renovate-jvm-2@users.noreply.github.com>
2025-04-22 23:58:46 +1000

    chore(deps): update internal-deps
    GitOrigin-RevId: b5b33a6b5289659c92b7b8c08055962906a22ee0

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -8,6 +8,9 @@ pluginManagement {
 }
 
 plugins {
+  // When updating the cash plugin versions, update .buildkite/scripts/copy.bara.sky too
+  id("com.squareup.cash.develocity") version "1.227.0"
+  id("com.squareup.cash.remotecache") version "1.227.0"
   id("com.gradle.develocity") version "3.18.2"
 }
 
