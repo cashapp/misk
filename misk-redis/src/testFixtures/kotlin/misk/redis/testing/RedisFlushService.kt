961160f8da2ba25657d170fcfec5e2b4f6566df7
Mehdi Mollaverdi <mmollaverdi@squareup.com>
2025-05-23 10:21:10 +1000

    Flush redis in a TestFixture for tests using real redis
    GitOrigin-RevId: b7eaf37d85c0f950c37b3063a7782e983fa3d94d

--- misk-redis/src/testFixtures/kotlin/misk/redis/testing/RedisFlushService.kt
+++ misk-redis/src/testFixtures/kotlin/misk/redis/testing/RedisFlushService.kt
@@ -4,6 +4,7 @@ import com.google.common.util.concurrent.AbstractIdleService
 import com.google.inject.Inject
 import jakarta.inject.Singleton
 import misk.redis.Redis
+import misk.testing.TestFixture
 import wisp.logging.getLogger
 
 /**
@@ -13,15 +14,23 @@ import wisp.logging.getLogger
  * you will need instead to manually flush redis via `@BeforeEach`. See examples in `RealRedisTest`
  */
 @Singleton
-class RedisFlushService @Inject constructor() : AbstractIdleService() {
+class RedisFlushService @Inject constructor() : AbstractIdleService(), TestFixture {
   @Inject(optional = true) private lateinit var redis: Redis
   override fun startUp() {
+    flushAll()
+  }
+
+  override fun shutDown() {}
+
+  override fun reset() {
+    flushAll()
+  }
+
+  private fun flushAll() {
     logger.info("Flushing Redis")
     redis.flushAll()
     logger.info("Flushed Redis")
   }
-
-  override fun shutDown() {}
 
   companion object {
     private val logger = getLogger<RedisFlushService>()
