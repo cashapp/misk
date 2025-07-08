736c206a077fec32d013473095d92c6493220cf6
Kartikaya Gupta (kats) <staktrace@users.noreply.github.com>
2025-07-08 10:50:42 -0400

    Enable misk-admin web build in all environments and improve caching robustness
    GitOrigin-RevId: ff877e1f0b8723cad7fc197f82242cc76f09c1e9

--- misk-admin/build.gradle.kts
+++ misk-admin/build.gradle.kts
@@ -43,159 +43,48 @@ dependencies {
   testImplementation(project(":misk-testing"))
 }
 
-private class ExecOperationsHelper(
-  private val ops: ExecOperations
-) {
-  internal data class ResultWithOutput(
-    val stdOut: String,
-    val stdErr: String,
-    val workingDir: String,
-    val exitCode: Int
-  ) {
-    internal fun assertNormalExitValue() {
-      check(exitCode == 0) {
-        "Command failed with exit code $exitCode: $stdErr"
-      }
-    }
-  }
+val buildMiskWeb = tasks.register("buildMiskWeb", Exec::class.java) {
+  setWorkingDir(layout.projectDirectory)
+  commandLine("web/build.sh")
+  inputs.files(project.fileTree(layout.projectDirectory.dir("web")) {
+    exclude("tabs/database/node_modules/**")
+    exclude("tabs/database/lib/**")
+    exclude("tabs/database/tsconfig.json")
+    exclude("tabs/database/tslint.json")
+    exclude("tabs/database/webpack.config.js")
+  }).withPropertyName("webfiles").withPathSensitivity(PathSensitivity.RELATIVE)
+  outputs.dir(layout.buildDirectory.dir("web"))
 
-  internal fun exec(vararg command: String, action: Action<ExecSpec>? = null): ResultWithOutput {
-    val stdOut = ByteArrayOutputStream()
-    val stdErr = ByteArrayOutputStream()
-    lateinit var workingDir: String
-    val result = ops.exec {
-      commandLine(*command)
-      standardOutput = stdOut
-      errorOutput = stdErr
-      action?.execute(this)
-      workingDir = this.workingDir.absolutePath
-    }
-    return ResultWithOutput(
-      stdOut = String(stdOut.toByteArray(), Charset.defaultCharset()),
-      stdErr = String(stdErr.toByteArray(), Charset.defaultCharset()),
-      workingDir = workingDir,
-      exitCode = result.exitValue
-    )
-  }
+  outputs.cacheIf("all inputs/outputs are declared") { true }
 }
 
-@CacheableTask
-abstract class MiskWebBuildTask @Inject constructor(
-  @Internal val execOps: ExecOperations
-): DefaultTask() {
-  @get:Internal
-  abstract val rootDir: RegularFileProperty
+val buildWebActions = tasks.register("buildWebActions", Exec::class.java) {
+  setWorkingDir(layout.projectDirectory)
+  commandLine("web-actions/build.sh")
 
-  @get:Internal
-  abstract val projectDir: RegularFileProperty
+  inputs.files(project.fileTree(layout.projectDirectory.dir("web-actions")) {
+    include("src/**")
+    include("babel.config.json")
+    include("jest.config.js")
+    include("package.json")
+    include("tsconfig.json")
+    include("webpack.config.js")
+    include("build.sh")
+  }).withPropertyName("webfiles").withPathSensitivity(PathSensitivity.RELATIVE)
+  outputs.dir(layout.buildDirectory.dir("web-actions"))
 
-  @get:PathSensitive(PathSensitivity.RELATIVE)
-  @get:InputFiles
-  abstract val inputFiles: ConfigurableFileCollection
-
-  @get:OutputDirectories
-  abstract val outputFiles: ConfigurableFileCollection
-
-  @TaskAction
-  fun buildTabs() {
-    val rootFile = rootDir.asFile.get()
-    val projectDir = projectDir.asFile.get()
-
-    logger.lifecycle("Running miskweb build...")
-
-    val npmPath = rootFile.resolve("bin/npm").absolutePath
-    val npmInstallResult = ExecOperationsHelper(execOps).exec(
-      npmPath, "install", "@misk/cli"
-    ) {
-      // This needs to run from the project root dir, so that the node_modules directory is
-      // just inside HERMIT_ENV. This is because the hermit node package explicitly adds
-      // "$HERMIT_ENV/node_modules/.bin" to the PATH, and we need that for miskweb to be found.
-      // Except sometimes we have a different hermit env active, so we deal with that below.
-      workingDir = rootFile
-      isIgnoreExitValue = true
-    }
-    logger.quiet("Ran $npmPath from ${npmInstallResult.workingDir}")
-    logger.quiet(npmInstallResult.stdOut)
-    npmInstallResult.assertNormalExitValue()
-
-    // Per the Node docs (https://docs.npmjs.com/cli/v10/configuring-npm/folders#executables),
-    // executables are installed in 'node_modules/.bin'.
-    val nodeModulesBin = rootFile.resolve("node_modules/.bin").absolutePath
-    val hermitBin = rootFile.resolve("bin").absolutePath
-    val miskWebPath = rootFile.resolve("node_modules/.bin/miskweb").absolutePath
-    val miskWebBuildResult = ExecOperationsHelper(execOps).exec(
-      miskWebPath, "ci-build", "-e",
-    ) {
-      // this should run in one of the parent directories of the one containing the miskTab.json.
-      // What ExecOps does varies by gradle version, it seems, so use the project directory for
-      // the current project explicitly.
-      workingDir = projectDir
-      isIgnoreExitValue = true // we will assert ourselves after dumping stdout
-      // We need miskweb to be on the PATH. In some environments we have multiple hermit environments
-      // and the misk hermit env is not the one that is active, so the misk/node_modules/.bin folder
-      // isn't automatically on the PATH. Let's add it explicitly to make sure it's there.
-      environment(mapOf("PATH" to "$nodeModulesBin:$hermitBin:${environment["PATH"]}"))
-    }
-    logger.quiet("Ran $miskWebPath from ${miskWebBuildResult.workingDir}")
-    logger.quiet(miskWebBuildResult.stdOut)
-    miskWebBuildResult.assertNormalExitValue()
-
-    logger.lifecycle("Miskweb build complete!")
-  }
+  outputs.cacheIf("all inputs/outputs are declared") { true }
 }
 
-val tabDirs = listOf(
-  "web/tabs/database",
-)
-
-val buildMiskWeb = tasks.register("buildMiskWeb", MiskWebBuildTask::class.java) {
-  // For each folder containing a miskTab.json, take the entire folder as an input,
-  // minus the listed excludes. Those files are generated by the miskweb invocation.
-  // The lib subdirectory is the one we actually care about as an output; the
-  // other ones are not bundled into the jar and can be ignored for caching
-  // purposes.
-  val inputs = tabDirs.map { tabDir ->
-    project.fileTree(tabDir) {
-      exclude("lib/")
-      exclude("node_modules/")
-      exclude("tsconfig.json")
-      exclude("tslint.json")
-      exclude("webpack.config.js")
-    }
-  }
-  val outputs = tabDirs.map { tabDir ->
-    File(tabDir, "lib")
-  }
-  rootDir.set(project.rootDir)
-  projectDir.set(project.projectDir)
-  inputFiles.setFrom(inputs)
-  outputFiles.setFrom(outputs)
-}
-
-// buildMiskWeb is expensive and generally not needed locally. Only build it on CI, or if
-// specifically requested.
-val isCi = System.getenv("CI") == "true" || System.getenv("GITHUB_ACTIONS") != null
-if (isCi || System.getProperty("misk.admin.buildMiskWeb") == "true") {
-  tasks.named { it == "explodeCodeSourceMain" || it == "processResources" || it == "sourcesJar" }.configureEach {
-    dependsOn(buildMiskWeb)
-    dependsOn("buildAndCopyWebActions")
-  }
-}
-
-tasks.register<Copy>("buildAndCopyWebActions") {
-  from({
-    project(":misk-admin-web-actions").tasks.named("buildWebActionsTab").get().outputs.files
-  })
-  into(project.layout.buildDirectory.dir("resources/main/web/_tab/web-actions"))
-  dependsOn(":misk-admin-web-actions:buildWebActionsTab")
+tasks.named { it == "explodeCodeSourceMain" || it == "processResources" || it == "sourcesJar" }.configureEach {
+  dependsOn(buildMiskWeb)
+  dependsOn(buildWebActions)
 }
 
 sourceSets {
   main {
-    resources.srcDir(tabDirs.map { tabDir ->
-      "$tabDir/lib"
-    })
-    resources.exclude("**/node_modules")
+    resources.srcDir(buildMiskWeb)
+    resources.srcDir(buildWebActions)
   }
 }
 
---
+++ misk-admin/web/build.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+set -ueo pipefail
+
+# Ensure the proper hermit environment is activated
+pushd ..
+. bin/activate-hermit
+popd
+
+# Log all the commands and output, as this build is notoriously difficult to debug.
+set -x
+
+# This needs to run from the project root dir, so that the node_modules directory is
+# just inside HERMIT_ENV. This is because the hermit node package explicitly adds
+# "$HERMIT_ENV/node_modules/.bin" to the PATH, and we need that for miskweb to be found.
+# Except sometimes we have a different hermit env active, so we deal with that below.
+pushd ..
+bin/npm install @misk/cli
+popd
+
+../node_modules/.bin/miskweb ci-build -e
+
+rm -rf build/web
+mv web/tabs/database/lib build/web
