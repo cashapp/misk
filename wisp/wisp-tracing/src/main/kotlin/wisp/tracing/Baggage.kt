a3a0e6dd405d94d2e7a7570c6fd3b42ef7a5a823
Eugene <105677590+eugene-deng@users.noreply.github.com>
2025-07-14 15:18:49 -0400

    ## Summary - deprecate `setBaggageItems` and `setItems` from wisp.tracing module - replace the usage of `setBaggageItems` with stream methods
    ## Test
    - all tests
    
    GitOrigin-RevId: 208d9c03e6c8e1c7df8810098aef907c926729ae

--- wisp/wisp-tracing/src/main/kotlin/wisp/tracing/Baggage.kt
+++ wisp/wisp-tracing/src/main/kotlin/wisp/tracing/Baggage.kt
@@ -5,6 +5,7 @@ import io.opentracing.Span
 /**
  * Conveniently sets baggage items all at once. Baggage values come from [Any.toString].
  */
+@Deprecated("This module is deprecated as wisp.tracing is deprecated.")
 fun Span.setBaggageItems(baggage: Map<String, Any>) {
     for ((key, value) in baggage) {
         setBaggageItem(key, value.toString())
