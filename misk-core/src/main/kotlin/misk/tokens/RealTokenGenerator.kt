37b4d55ce06f9a0321fd7b220787dc403fb24c9b
Sam W <87729887+sw-square@users.noreply.github.com>
2025-07-16 12:14:48 +0100

    This PR marks the wisp tokens package as depreacted, and moves their functionality over to their equivalent `misk.tokens.{Real,Fake,}TokenGenerator2` versions. (The `2` is because a migration was already in place; once the wisp -> misk migration is done, it should be able to be completed more smoothly)
    GitOrigin-RevId: 27d160f0016e8d1cee625d5803cd7dae78dfa87a

--- misk-core/src/main/kotlin/misk/tokens/RealTokenGenerator.kt
+++ misk-core/src/main/kotlin/misk/tokens/RealTokenGenerator.kt
@@ -2,15 +2,33 @@ package misk.tokens
 
 import jakarta.inject.Inject
 import jakarta.inject.Singleton
+import misk.tokens.TokenGenerator2.Companion.CANONICALIZE_LENGTH_MAX
+import misk.tokens.TokenGenerator2.Companion.CANONICALIZE_LENGTH_MIN
+import misk.tokens.TokenGenerator2.Companion.indexToChar
+import java.security.SecureRandom
+import kotlin.experimental.and
 
 @Singleton
 class RealTokenGenerator @Inject constructor() : TokenGenerator by wisp.token.RealTokenGenerator()
 
+private const val REAL_TOKEN_GENERATOR_BIT_MASK = 31.toByte()
 @Singleton
 class RealTokenGenerator2 @Inject constructor() : TokenGenerator2 {
-  private val tokenGenerator = wisp.token.RealTokenGenerator()
+  private val random = SecureRandom()
 
   override fun generate(label: String?, length: Int): String {
-    return tokenGenerator.generate(label, length)
+    require(length in CANONICALIZE_LENGTH_MIN..CANONICALIZE_LENGTH_MAX) {
+      "unexpected length: $length"
+    }
+
+    val byteArray = ByteArray(length)
+    random.nextBytes(byteArray)
+
+    val result = CharArray(length)
+    for (i in 0 until length) {
+      result[i] = indexToChar[(byteArray[i] and REAL_TOKEN_GENERATOR_BIT_MASK).toInt()]
+    }
+
+    return String(result)
   }
 }
---
+++ misk-core/src/test/kotlin/misk/tokens/RealTokenGeneratorTest.kt
@@ -0,0 +1,66 @@
+package misk.tokens
+
+import io.kotest.assertions.throwables.shouldThrow
+import io.kotest.core.spec.style.FreeSpec
+import io.kotest.matchers.shouldBe
+import io.kotest.matchers.shouldNotBe
+import io.kotest.matchers.string.shouldMatch
+
+class RealTokenGeneratorTest : FreeSpec({
+  val tokenGenerator: TokenGenerator2 = RealTokenGenerator2()
+
+  "Happy Path" {
+    val token0 = tokenGenerator.generate()
+    val token1 = tokenGenerator.generate()
+
+    token0 shouldMatch "[${TokenGenerator2.alphabet}]{25}"
+    token1 shouldMatch "[${TokenGenerator2.alphabet}]{25}"
+    token0 shouldNotBe token1
+  }
+
+  "Labels Are Ignored" {
+    tokenGenerator.generate("payment").contains("payment") shouldNotBe true
+  }
+
+  "Custom Length" {
+    tokenGenerator.generate(length = 4) shouldMatch "[${TokenGenerator2.alphabet}]{4}"
+    tokenGenerator.generate(length = 12) shouldMatch "[${TokenGenerator2.alphabet}]{12}"
+    tokenGenerator.generate(length = 25) shouldMatch "[${TokenGenerator2.alphabet}]{25}"
+  }
+
+  "Length Out Of Bounds" {
+    shouldThrow<IllegalArgumentException> {
+      tokenGenerator.generate(length = 3)
+    }
+    shouldThrow<IllegalArgumentException> {
+      tokenGenerator.generate(length = 26)
+    }
+  }
+
+  "Canonicalize" {
+    TokenGenerator2.canonicalize("iIlLoO") shouldBe "111100"
+    TokenGenerator2.canonicalize("Pterodactyl") shouldBe "pter0dacty1"
+    TokenGenerator2.canonicalize("Veloci Raptor") shouldBe "ve10c1rapt0r"
+  }
+
+  "Canonicalize Unexpected Characters" {
+    shouldThrow<IllegalArgumentException> {
+      TokenGenerator2.canonicalize("Dinosaur") // u.
+    }
+    shouldThrow<IllegalArgumentException> {
+      TokenGenerator2.canonicalize("Veloci_Raptor") // _.
+    }
+    shouldThrow<IllegalArgumentException> {
+      TokenGenerator2.canonicalize("Velociräptor") // ä.
+    }
+  }
+
+  "Canonicalize Unexpected Length" {
+    shouldThrow<IllegalArgumentException> {
+      TokenGenerator2.canonicalize("a b c") // 3 characters after stripping spaces.
+    }
+    shouldThrow<IllegalArgumentException> {
+      TokenGenerator2.canonicalize("12345678901234567890123456") // 26 characters.
+    }
+  }
+})
