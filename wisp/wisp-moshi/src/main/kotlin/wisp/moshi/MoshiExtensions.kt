a630196274685cb562755ad4de7fc42c79d1ceea
Sam W <87729887+sw-square@users.noreply.github.com>
2025-07-22 16:09:43 +0100

    This PR deprecates wisp's `MoshiExtensions.kt`. The misk one is identical, and didnt need to be updated.
    GitOrigin-RevId: 11e3a67701eb9be05a355f51e40a219431a28f55

--- wisp/wisp-moshi/src/main/kotlin/wisp/moshi/MoshiExtensions.kt
+++ wisp/wisp-moshi/src/main/kotlin/wisp/moshi/MoshiExtensions.kt
@@ -2,4 +2,11 @@ package wisp.moshi
 
 import com.squareup.moshi.Moshi
 
+@Deprecated(
+  message = "Duplicate implementations in Wisp are being migrated to the unified type in Misk.",
+  replaceWith = ReplaceWith(
+    expression = "adapter()",
+    imports = ["misk.moshi.adapter"]
+  )
+)
 inline fun <reified T> Moshi.adapter() = this.adapter(T::class.java)!!
