25a41893361960a1505861e4cb48f12c98a4c731
Sam W <87729887+sw-square@users.noreply.github.com>
2025-07-23 21:43:57 +0100

    [wisk.sampling,misk.sampling] Deprecate wisp.sampling and move unique behaviour to misk.sampling
    This PR migrates `wisp.sampling` over to `misk.sampling`. A few things
    to note:
    - `misk.sampling.Sampler` didn't have documentation or a companion
    object, so both of those have been copied over from
    `wisp.sampling.Sampler`.
    - The `misk.sampling.PercentSampler` is named differently from the wisp
    `wisp.sampling.PercentageSampler`. The migration from wisp to misk'll
    have to rename these
    - The `misk.sampling.Ratelimiter` is constructed differently than the
    wisp one, via `Ratelimiter.Factory(ticker, sleeper).create(rate)`. The
    wisp one has default values in it, but I opted not to add those to the
    misk one (as it's injected)
    - The wisp `RateLimiter` has java annotations, eg `@JvmOverloads`. I've
    not migrated those over either, as I suspect it's not needed anymore.
    (If it turns out to be, we can always add the annotations later)
    - misk's `SamplerTest.kt` was expanded to include tests from the wisp
    one
    
    GitOrigin-RevId: f7009dfbd54d57bd385e97842f76e4ca54f32283

--- misk-core/src/main/kotlin/misk/sampling/Sampler.kt
+++ misk-core/src/main/kotlin/misk/sampling/Sampler.kt
@@ -7,6 +7,22 @@ import java.util.concurrent.TimeUnit
 import jakarta.inject.Inject
 import jakarta.inject.Singleton
 
+/**
+ * A [Sampler] is used to provide "sampled" probabilistic execution of a function.
+ *
+ * A common usage of the sampler would look like:
+ *
+ * ```kt
+ * if (sampler.sample()) {
+ *     performAction()
+ * }
+ * ```
+ *
+ * The frequency at which `sample` returns `true` or `false` is based on the implementation's policy. For example,
+ * [Sampler.always] creates a [Sampler] that only returns `true`, while [Sampler.percentage] will only return `true` for a
+ * given percentage of samples. For a more complex example, [Sampler.rateLimiting] will limit the number of `true`
+ * samples to a given rate per second.
+ */
 interface Sampler {
   /** If an action should be taken based on the implementation's policy, returns true */
   fun sample(): Boolean
@@ -16,6 +32,38 @@ interface Sampler {
     if (sample()) {
       f()
     }
+  }
+
+  companion object {
+    /**
+     * Creates a [Sampler] that limits positive results to a percentage chance.
+     *
+     * @param samplePercentage percentage chance of positive results
+     *
+     * @return percentage [Sampler] instance
+     */
+    fun percentage(samplePercentage: Int): Sampler = PercentSampler(samplePercentage) {
+      ThreadLocalRandom.current().nextInt(0, 100)
+    }
+
+    /**
+     * Creates a [Sampler] that limits positive results to a limited rate per second.
+     *
+     * @param ratePerSecond the number of positive results per second
+     *
+     * @return rate limiting [Sampler] instance
+     */
+    fun rateLimiting(ratePerSecond: Long): Sampler = RateLimitingSampler(RateLimiter.Factory(
+      Ticker.systemTicker(),
+      Sleeper.DEFAULT,
+    ).create(ratePerSecond))
+
+    /**
+     * Creates a [Sampler] that always returns positive results.
+     *
+     * @return always [Sampler] instance
+     */
+    fun always(): Sampler = AlwaysSampler()
   }
 }
 
--- wisp/wisp-sampling/src/main/kotlin/wisp/sampling/Ticker.kt
+++ wisp/wisp-sampling/src/main/kotlin/wisp/sampling/Ticker.kt
@@ -1,5 +1,12 @@
 package wisp.sampling
 
+@Deprecated(
+  message = "Duplicate implementations in Wisp are being migrated to the unified type in Misk.",
+  replaceWith = ReplaceWith(
+    expression = "Ticker",
+    imports = ["com.google.common.base.Ticker"]
+  )
+)
 /**
  * Abstraction for `System.nanoTime()` that allows for testing.
  */
