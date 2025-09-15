9794cc06a032e368e59b1747c72347dfcda75100
Mehdi Mollaverdi <mmollaverdi@squareup.com>
2025-09-15 11:38:20 +1000

    Make it easier to reuse injector in misk tests using Modules.override
    Provides an API for overriding modules in tests in a way which supports
    Misk test injector reuse. The API is kept similar to the original
    Modules.override API to make the migration easier.
    
    GitOrigin-RevId: 429f976784df6077b1f7a7b2077b0a0bb0ac5e07

---
+++ misk-inject/src/main/kotlin/misk/inject/ReusableOverridesTestModule.kt
@@ -0,0 +1,39 @@
+package misk.inject
+
+import com.google.inject.util.Modules
+import java.util.SortedSet
+
+/**
+  * Allows overriding reusable test modules when reusing injectors in Misk tests.
+  *
+  * Usage:
+  * ```
+  * @MiskTestModule
+  * val module = overrideReusable(OriginalReusableTestModule()).with(OverrideTestModuleOne(), OverrideTestModuleOne(), ...)
+  * ```
+  */
+fun overrideReusable(vararg modules: ReusableTestModule) =
+  ReusableOverriddenModuleBuilder(modules = modules.toSortedSet(ReusableTestModuleComparator))
+
+class ReusableOverridesTestModule(
+  private val originalTestModules: SortedSet<ReusableTestModule>,
+  private val overrides: SortedSet<ReusableTestModule>,
+) : ReusableTestModule() {
+  override fun configure() {
+    install(Modules.override(originalTestModules).with(overrides))
+  }
+}
+
+private object ReusableTestModuleComparator : Comparator<ReusableTestModule> {
+  override fun compare(o1: ReusableTestModule, o2: ReusableTestModule): Int {
+    return o1.javaClass.name.compareTo(o2.javaClass.name)
+  }
+}
+
+class ReusableOverriddenModuleBuilder(private val modules: SortedSet<ReusableTestModule>) {
+
+  fun with(vararg overrides: ReusableTestModule) = ReusableOverridesTestModule(
+    originalTestModules = modules,
+    overrides = overrides.toSortedSet(ReusableTestModuleComparator)
+  )
+}
