4f064f3637a016500f792ba172170c53ba687305
Michael Peyper <mpeyper@squareup.com>
2025-09-23 16:40:34 +1000

    add ability to disable SQS consumers with DISABLE_ASYNC_TASKS env var
    This change introduces a check on a `DISABLE_ASYNC_TASKS` env var before
    adding subscribers to topics. When set to `true` (or `TRUE` or `1`), the
    subscriber is not added, otherwise it is added as before.
    
    This check has been added to both v1 and v2 libraries for SQS
    
    GitOrigin-RevId: 2f30b12ecd093a15a18daa7752c0ee82570602a7

--- misk-aws/src/main/kotlin/misk/jobqueue/sqs/AwsSqsJobHandlerSubscriptionService.kt
+++ misk-aws/src/main/kotlin/misk/jobqueue/sqs/AwsSqsJobHandlerSubscriptionService.kt
@@ -3,9 +3,11 @@ package misk.jobqueue.sqs
 import com.google.common.util.concurrent.AbstractIdleService
 import jakarta.inject.Inject
 import jakarta.inject.Singleton
+import misk.environment.EnvVarLoader
 import misk.jobqueue.BatchJobHandler
 import misk.jobqueue.JobHandler
 import misk.jobqueue.QueueName
+import misk.logging.getLogger
 
 @Singleton
 internal class AwsSqsJobHandlerSubscriptionService @Inject constructor(
@@ -14,7 +16,8 @@ internal class AwsSqsJobHandlerSubscriptionService @Inject constructor(
   private val individualConsumerMapping: Map<QueueName, JobHandler>,
   private val batchConsumerMapping: Map<QueueName, BatchJobHandler>,
   private val externalQueues: Map<QueueName, AwsSqsQueueConfig>,
-  private val config: AwsSqsJobQueueConfig
+  private val config: AwsSqsJobQueueConfig,
+  private val envVarLoader: EnvVarLoader
 ) : AbstractIdleService() {
   override fun startUp() {
     check(individualConsumerMapping.isNotEmpty() || batchConsumerMapping.isNotEmpty()) {
@@ -24,8 +27,14 @@ internal class AwsSqsJobHandlerSubscriptionService @Inject constructor(
     val duplicateQueues = individualConsumerMapping.keys.intersect(batchConsumerMapping.keys)
     check(duplicateQueues.isEmpty()) { "Queues $duplicateQueues have multiple handlers" }
 
-    individualConsumerMapping.forEach { consumer.subscribe(it.key, it.value) }
-    batchConsumerMapping.forEach { consumer.subscribe(it.key, it.value) }
+    if (!asyncTasksDisabled()) {
+      individualConsumerMapping.forEach { consumer.subscribe(it.key, it.value) }
+      batchConsumerMapping.forEach { consumer.subscribe(it.key, it.value) }
+    } else {
+      val allQueues = (individualConsumerMapping.keys + batchConsumerMapping.keys).joinToString(", ")
+      logger.info { "Async tasks are disabled with DISABLE_ASYNC_TASKS, not subscribing to queues ($allQueues)" }
+    }
+
     externalQueues.forEach { attributeImporter.import(it.key) }
   }
 
@@ -35,5 +44,14 @@ internal class AwsSqsJobHandlerSubscriptionService @Inject constructor(
       attributeImporter.shutDown()
       consumer.shutDown()
     }
+  }
+
+  private fun asyncTasksDisabled(): Boolean {
+    val envVarValue = envVarLoader.getEnvironmentVariableOrDefault("DISABLE_ASYNC_TASKS", "").trim()
+    return envVarValue.toBoolean() || envVarValue == "1"
+  }
+
+  companion object {
+    private val logger = getLogger<AwsSqsJobHandlerSubscriptionService>()
   }
 }
