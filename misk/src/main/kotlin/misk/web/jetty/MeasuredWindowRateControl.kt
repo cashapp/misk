c45d5c3ae198ba3fd798a04413647293691f1ff6
Kevin Kim <kevink@squareup.com>
2025-09-29 12:56:51 -0700

    address comments

--- misk/src/main/kotlin/misk/web/jetty/MeasuredWindowRateControl.kt
+++ misk/src/main/kotlin/misk/web/jetty/MeasuredWindowRateControl.kt
@@ -1,3 +1,15 @@
+//
+// ========================================================================
+// Copyright (c) 1995 Mort Bay Consulting Pty Ltd and others.
+//
+// This program and the accompanying materials are made available under the
+// terms of the Eclipse Public License v. 2.0 which is available at
+// https://www.eclipse.org/legal/epl-2.0, or the Apache License, Version 2.0
+// which is available at https://www.apache.org/licenses/LICENSE-2.0.
+//
+// SPDX-License-Identifier: EPL-2.0 OR Apache-2.0
+// ========================================================================
+//
 package misk.web.jetty
 
 import misk.metrics.v2.Metrics
@@ -17,11 +29,11 @@ internal class MeasuredWindowRateControl internal constructor(
   private val maxEvents: Int,
 ) : RateControl {
 
-  private val gauge = metrics.peakGauge(
+  private val rateEventsPeakGauge = metrics.peakGauge(
     "jetty_http2_rate_control_events_peak",
     "Peak gauge of observed events per second"
   )
-  private val counter = metrics.counter(
+  private val rateLimitedEventCounter = metrics.counter(
     "jetty_http2_rate_control_events_limited",
     "Count of rate limited events"
   )
@@ -42,12 +54,12 @@ internal class MeasuredWindowRateControl internal constructor(
     events.add(now + window)
 
     val count = size.incrementAndGet()
-    gauge.record(count.toDouble())
+    rateEventsPeakGauge.record(count.toDouble())
 
     if (maxEvents == -1) return true
 
     val allowed = count <= maxEvents
-    if (!allowed) counter.inc()
+    if (!allowed) rateLimitedEventCounter.inc()
     return allowed
   }
 
