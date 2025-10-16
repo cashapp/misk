4eea4ccd906e25a44d943d1f651c5d5667ba7bf6
David Grochowski <dgrochowski@squareup.com>
2025-10-15 23:15:26 -0400

    feat: remove unused pooled leases implementation

--- wisp/wisp-lease/build.gradle.kts
+++ wisp/wisp-lease/build.gradle.kts
@@ -2,11 +2,3 @@ plugins {
   id("org.jetbrains.kotlin.jvm")
   id("com.vanniktech.maven.publish")
 }
-
-dependencies {
-    api(project(":wisp:wisp-deployment"))
-
-    testImplementation(libs.junitApi)
-    testImplementation(libs.kotlinTest)
-    testImplementation(libs.mockitoCore)
-}
