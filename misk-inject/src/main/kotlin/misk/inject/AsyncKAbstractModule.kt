5294049044cf61a1bed7b911cdcda67b2acf9344
Andrew (Paradi) Alexander <adrw@squareup.com>
2025-10-21 12:22:06 -0400

    Move to interface for AsyncModule instead (#3477)

--- misk-inject/src/main/kotlin/misk/inject/AsyncKAbstractModule.kt
+++ misk-inject/src/main/kotlin/misk/inject/AsyncKAbstractModule.kt
@@ -9,6 +9,9 @@ import misk.annotation.ExperimentalMiskApi
  * At service build time, these modules can be optionally filtered out before the Guice injector is created,
  * in cases where async processing is not desired, such as in separated main and jobs deployments.
  */
+@Deprecated(
+  message = "Extend both AsyncModule interface and KAbstractModule() or KInstallOnceModule() directly.",
+)
 open class AsyncKAbstractModule : KAbstractModule() {
   /**
    * Returns a module that would be installed when async tasks are disabled.
@@ -17,4 +20,16 @@ open class AsyncKAbstractModule : KAbstractModule() {
    */
   @ExperimentalMiskApi
   open fun moduleWhenAsyncDisabled(): KAbstractModule? = null
+}
+
+/**
+ * This class should be extended by modules that want to contribute tasks to which involve async processing.
+ * For example, background jobs, job queues, eventing, message pub/sub.
+ *
+ * At service build time, these modules can be optionally filtered out before the Guice injector is created,
+ * in cases where async processing is not desired, such as in separated main and jobs deployments.
+ */
+interface AsyncModule {
+  @ExperimentalMiskApi
+  fun moduleWhenAsyncDisabled(): KAbstractModule? = null
 }
