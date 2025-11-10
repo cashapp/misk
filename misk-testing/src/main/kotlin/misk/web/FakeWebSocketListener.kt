97e7b28612a8d28cf5b7754d4bc12a4acfdbff7e
benny <benny.reese1@gmail.com>
2025-11-10 09:32:25 -0500

    feat: websocket outgoing binary message support
    Adds support for sending binary messages over websockets.
    Also corrects some typos in comments.

--- misk-testing/src/main/kotlin/misk/web/FakeWebSocketListener.kt
+++ misk-testing/src/main/kotlin/misk/web/FakeWebSocketListener.kt
@@ -1,14 +1,22 @@
 package misk.web
 
+import okhttp3.WebSocket
+import okio.ByteString
 import java.util.concurrent.LinkedBlockingDeque
 import java.util.concurrent.TimeUnit
 
 class FakeWebSocketListener : okhttp3.WebSocketListener() {
   val messages = LinkedBlockingDeque<String>()
+  val binaryMessages = LinkedBlockingDeque<ByteString>()
 
   override fun onMessage(webSocket: okhttp3.WebSocket, text: String) {
     messages.add(text)
   }
 
+  override fun onMessage(webSocket: WebSocket, bytes: ByteString) {
+    binaryMessages.add(bytes)
+  }
+
   fun takeMessage() = messages.pollFirst(2, TimeUnit.SECONDS)
+  fun takeBinaryMessage() = binaryMessages.pollFirst(2, TimeUnit.SECONDS)
 }
