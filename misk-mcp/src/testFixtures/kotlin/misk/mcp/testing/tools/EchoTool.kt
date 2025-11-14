658b984e57b2f02d0227b2016ae77d687b23f937
Jeff Clyne <jclyne@squareup.com>
2025-11-14 12:12:00 -0600

    fixes backward incompatible change for newMapBinding (#3540)

--- misk-mcp/src/testFixtures/kotlin/misk/mcp/testing/tools/EchoTool.kt
+++ misk-mcp/src/testFixtures/kotlin/misk/mcp/testing/tools/EchoTool.kt
@@ -25,16 +25,15 @@ abstract class AbstractEchoTool : StructuredMcpTool<EchoToolInput, EchoToolOutpu
     return ToolResult(result = EchoToolOutput(buildGreeting(input.name)))
   }
 
+  override val name = "EchoTool"
   open fun buildGreeting(name: String) = "Hello, $name!"
 }
 
 class EchoTool @Inject constructor() : AbstractEchoTool() {
-  override val name = "EchoTool"
   override val description = "This tool will return Hello, <name>!"
 }
 
 class EchoToolV2 @Inject constructor() : AbstractEchoTool() {
-  override val name = "EchoTool"
   override val description = "This tool will return Hello again, <name>!"
 
   override fun buildGreeting(name: String) = "Hello again, $name!"
