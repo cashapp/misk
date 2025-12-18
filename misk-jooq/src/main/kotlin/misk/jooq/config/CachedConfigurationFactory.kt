7b13e5601109a0bf2ea7319457af2caacdb9336a
Tyler Gregory <tgregory@squareup.com>
2025-12-18 11:35:16 -0600

    Use atomic computeIfAbsent method on concurrent hash map

--- misk-jooq/src/main/kotlin/misk/jooq/config/CachedConfigurationFactory.kt
+++ misk-jooq/src/main/kotlin/misk/jooq/config/CachedConfigurationFactory.kt
@@ -1,21 +1,21 @@
 package misk.jooq.config
 
 import com.google.common.annotations.VisibleForTesting
+import java.time.Clock
+import java.util.concurrent.ConcurrentHashMap
 import misk.jdbc.DataSourceConfig
 import misk.jdbc.DataSourceService
 import misk.jooq.JooqTransacter
 import misk.jooq.TransactionIsolationLevel
 import misk.jooq.listeners.JooqTimestampRecordListenerOptions
 import org.jooq.Configuration
-import java.time.Clock
-import java.util.concurrent.ConcurrentHashMap
 
 /**
  * This [ConfigurationFactory] generates configurations and caches them by their associated [TransactionIsolationLevel].
  *
  * This is doable because the only user-modifiable parameter that can change the [Configuration] after the Guice
- * injector is built (i.e. after JooqModule is installed), is the transaction isolation level. It is important to
- * cache the [Configuration] and share it among transactions because [it contains the reflection cache](
+ * injector is built (i.e. after JooqModule is installed), is the transaction isolation level. It is important to cache
+ * the [Configuration] and share it among transactions because [it contains the reflection cache](
  * https://www.jooq.org/doc/latest/manual/sql-building/dsl-context/thread-safety/).
  */
 internal class CachedConfigurationFactory(
@@ -33,15 +33,16 @@ internal class CachedConfigurationFactory(
     get() = concurrentHashMap.entries.toSet()
 
   override fun getConfiguration(options: JooqTransacter.TransacterOptions): Configuration {
-    return concurrentHashMap.getOrPut(options.isolationLevel) {
+    return concurrentHashMap.computeIfAbsent(options.isolationLevel) {
       buildConfiguration(
-        clock = clock,
-        dataSourceConfig = dataSourceConfig,
-        dataSourceService = dataSourceService,
-        jooqCodeGenSchemaName = jooqCodeGenSchemaName,
-        jooqTimestampRecordListenerOptions = jooqTimestampRecordListenerOptions,
-        options = options,
-      ).apply(jooqConfigExtension)
+          clock = clock,
+          dataSourceConfig = dataSourceConfig,
+          dataSourceService = dataSourceService,
+          jooqCodeGenSchemaName = jooqCodeGenSchemaName,
+          jooqTimestampRecordListenerOptions = jooqTimestampRecordListenerOptions,
+          options = options,
+        )
+        .apply(jooqConfigExtension)
     }
   }
 }
