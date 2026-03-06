156e647d811fc6bd8bcfe4932194e0dcb2d0ff2d
Tyler Gregory <tgregory@squareup.com>
2026-03-06 15:58:53 -0600

    Hand roll implementation of dynamo v1 proxy manager

---
+++ misk-rate-limiting-bucket4j-dynamodb-v1/src/main/kotlin/misk/ratelimiting/bucket4j/dynamodb/v1/proxymanager/LongDynamoDBProxyManager.kt
@@ -0,0 +1,33 @@
+package misk.ratelimiting.bucket4j.dynamodb.v1.proxymanager
+
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDB
+import com.amazonaws.services.dynamodbv2.model.AttributeValue
+import com.amazonaws.services.dynamodbv2.model.DeleteItemRequest
+import io.github.bucket4j.distributed.proxy.ClientSideConfig
+import io.github.bucket4j.distributed.proxy.generic.compare_and_swap.AsyncCompareAndSwapOperation
+import io.github.bucket4j.distributed.proxy.generic.compare_and_swap.CompareAndSwapOperation
+import java.util.concurrent.CompletableFuture
+import misk.ratelimiting.bucket4j.dynamodb.v1.transaction.BaseDynamoDBTransaction.Companion.DEFAULT_KEY_NAME
+import misk.ratelimiting.bucket4j.dynamodb.v1.transaction.LongDynamoDBTransaction
+
+internal class LongDynamoDBProxyManager(dynamoDb: AmazonDynamoDB, table: String, config: ClientSideConfig) :
+  BaseDynamoDBProxyManager<Long>(dynamoDb, table, config) {
+  override fun beginCompareAndSwapOperation(key: Long): CompareAndSwapOperation =
+    LongDynamoDBTransaction(key, dynamoDb, table)
+
+  override fun beginAsyncCompareAndSwapOperation(key: Long?): AsyncCompareAndSwapOperation {
+    throw UnsupportedOperationException()
+  }
+
+  override fun removeProxy(key: Long?) {
+    val attributes = mapOf(DEFAULT_KEY_NAME to AttributeValue().withN(key.toString()))
+    val deleteItemRequest = DeleteItemRequest(table, attributes)
+    dynamoDb.deleteItem(deleteItemRequest)
+  }
+
+  override fun isAsyncModeSupported() = false
+
+  override fun removeAsync(key: Long?): CompletableFuture<Void> {
+    throw UnsupportedOperationException()
+  }
+}
