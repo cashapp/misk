1b294720fb043de17ef61182ac3f9a429a2055f9
Kartikaya Gupta <kartikaya@squareup.com>
2026-06-10 10:36:55 -0400

    Address review comment to use error instead of TODO

--- misk-api/src/main/kotlin/misk/client/OkHttpInterceptorChain.kt
+++ misk-api/src/main/kotlin/misk/client/OkHttpInterceptorChain.kt
@@ -40,136 +40,136 @@ open class OkHttpInterceptorChain(private val chain: ClientNetworkChain?) : Inte
   }
 
   override fun withWriteTimeout(timeout: Int, unit: TimeUnit): Interceptor.Chain {
-    TODO("not implemented")
+    error("not implemented")
   }
 
   override fun connectTimeoutMillis(): Int {
-    TODO("not implemented")
+    error("not implemented")
   }
 
   override fun withConnectTimeout(timeout: Int, unit: TimeUnit): Interceptor.Chain {
-    TODO("not implemented")
+    error("not implemented")
   }
 
   override fun withReadTimeout(timeout: Int, unit: TimeUnit): Interceptor.Chain {
-    TODO("not implemented")
+    error("not implemented")
   }
 
   override fun writeTimeoutMillis(): Int {
-    TODO("not implemented")
+    error("not implemented")
   }
 
   override fun call(): Call {
-    TODO("not implemented")
+    error("not implemented")
   }
 
   override fun readTimeoutMillis(): Int {
-    TODO("not implemented")
+    error("not implemented")
   }
 
   override val followSslRedirects: Boolean
-    get() = TODO("not implemented")
+    get() = error("not implemented")
 
   override val followRedirects: Boolean
-    get() = TODO("not implemented")
+    get() = error("not implemented")
 
   override val dns: Dns
-    get() = TODO("not implemented")
+    get() = error("not implemented")
 
   override val socketFactory: SocketFactory
-    get() = TODO("not implemented")
+    get() = error("not implemented")
 
   override val retryOnConnectionFailure: Boolean
-    get() = TODO("not implemented")
+    get() = error("not implemented")
 
   override val authenticator: Authenticator
-    get() = TODO("not implemented")
+    get() = error("not implemented")
 
   override val cookieJar: CookieJar
-    get() = TODO("not implemented")
+    get() = error("not implemented")
 
   override val cache: Cache?
-    get() = TODO("not implemented")
+    get() = error("not implemented")
 
   override val proxy: Proxy?
-    get() = TODO("not implemented")
+    get() = error("not implemented")
 
   override val proxySelector: ProxySelector
-    get() = TODO("not implemented")
+    get() = error("not implemented")
 
   override val proxyAuthenticator: Authenticator
-    get() = TODO("not implemented")
+    get() = error("not implemented")
 
   override val sslSocketFactoryOrNull: SSLSocketFactory?
-    get() = TODO("not implemented")
+    get() = error("not implemented")
 
   override val x509TrustManagerOrNull: X509TrustManager?
-    get() = TODO("not implemented")
+    get() = error("not implemented")
 
   override val hostnameVerifier: HostnameVerifier
-    get() = TODO("not implemented")
+    get() = error("not implemented")
 
   override val certificatePinner: CertificatePinner
-    get() = TODO("not implemented")
+    get() = error("not implemented")
 
   override val connectionPool: ConnectionPool
-    get() = TODO("not implemented")
+    get() = error("not implemented")
 
   override val eventListener: EventListener
-    get() = TODO("not implemented")
+    get() = error("not implemented")
 
   override fun withDns(dns: Dns): Interceptor.Chain {
-    TODO("not implemented")
+    error("not implemented")
   }
 
   override fun withSocketFactory(socketFactory: SocketFactory): Interceptor.Chain {
-    TODO("not implemented")
+    error("not implemented")
   }
 
   override fun withRetryOnConnectionFailure(retryOnConnectionFailure: Boolean): Interceptor.Chain {
-    TODO("not implemented")
+    error("not implemented")
   }
 
   override fun withAuthenticator(authenticator: Authenticator): Interceptor.Chain {
-    TODO("not implemented")
+    error("not implemented")
   }
 
   override fun withCookieJar(cookieJar: CookieJar): Interceptor.Chain {
-    TODO("not implemented")
+    error("not implemented")
   }
 
   override fun withCache(cache: Cache?): Interceptor.Chain {
-    TODO("not implemented")
+    error("not implemented")
   }
 
   override fun withProxy(proxy: Proxy?): Interceptor.Chain {
-    TODO("not implemented")
+    error("not implemented")
   }
 
   override fun withProxySelector(proxySelector: ProxySelector): Interceptor.Chain {
-    TODO("not implemented")
+    error("not implemented")
   }
 
   override fun withProxyAuthenticator(proxyAuthenticator: Authenticator): Interceptor.Chain {
-    TODO("not implemented")
+    error("not implemented")
   }
 
   override fun withSslSocketFactory(
     sslSocketFactory: SSLSocketFactory?,
     x509TrustManager: X509TrustManager?,
   ): Interceptor.Chain {
-    TODO("not implemented")
+    error("not implemented")
   }
 
   override fun withHostnameVerifier(hostnameVerifier: HostnameVerifier): Interceptor.Chain {
-    TODO("not implemented")
+    error("not implemented")
   }
 
   override fun withCertificatePinner(certificatePinner: CertificatePinner): Interceptor.Chain {
-    TODO("not implemented")
+    error("not implemented")
   }
 
   override fun withConnectionPool(connectionPool: ConnectionPool): Interceptor.Chain {
-    TODO("not implemented")
+    error("not implemented")
   }
 }
